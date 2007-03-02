74e12bae7b59645063fe24829381843d6e767338
npwoods <npwoods@9849079f-6229-0410-a07f-84406ce305ba>
2007-03-02 01:50:21 +0000

    [COUPE] Proper fix to 0.112u4 regression
    git-svn-id: svn://messdev.no-ip.org/mess@54 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/sound/saa1099.c
+++ src/emu/sound/saa1099.c
@@ -69,8 +69,6 @@
 #include "saa1099.h"
 #include <math.h>
 
-/* default to 4x oversampling */
-#define DEFAULT_SAMPLE_RATE (48000 * 4)
 
 #define LEFT	0x00
 #define RIGHT	0x01
@@ -320,7 +318,7 @@ static void *saa1099_start(int sndindex, int clock, const void *config)
 	memset(saa, 0, sizeof(*saa));
 
 	/* copy global parameters */
-	saa->sample_rate = clock ? (clock / 256) : DEFAULT_SAMPLE_RATE;
+	saa->sample_rate = clock / 256;
 
 	/* for each chip allocate one stream */
 	saa->stream = stream_create(0, 2, saa->sample_rate, saa, saa1099_update);
