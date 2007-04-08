b52576f2240055b7c054453339828cdfc50bc54c
npwoods <npwoods@9849079f-6229-0410-a07f-84406ce305ba>
2007-04-08 13:58:56 +0000

    [Windows Imgtool] Fixed Win32 Imgtool Association Dialog
    git-svn-id: svn://messdev.no-ip.org/mess@253 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/tools/imgtool/windows/assocdlg.c
+++ src/mess/tools/imgtool/windows/assocdlg.c
@@ -1,17 +1,49 @@
-#include <stdio.h>
+//============================================================
+//
+//	wimgtool.c - Win32 Imgtool File Association Dialog
+//
+//============================================================
+
+// standard windows headers
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <tchar.h>
+
+// standard C headers
+#include <stdio.h>
+
+// MESS headers
 #include "assoc.h"
 #include "wimgres.h"
 #include "strconv.h"
 #include "../imgtool.h"
 
+
+//============================================================
+//  PARAMETERS
+//============================================================
+
 #define CONTROL_START 10000
 
-struct assocdlg_info
+
+
+//============================================================
+//  TYPE DEFINITIONS
+//============================================================
+
+typedef struct _assocdlg_info assocdlg_info;
+struct _assocdlg_info
 {
 	int extension_count;
 	const char *extensions[128];
+	char buffer[1024];
 };
+
+
+
+//============================================================
+//  LOCAL VARIABLES
+//============================================================
 
 static const struct win_association_info assoc_info =
 {
@@ -21,11 +53,16 @@ static const struct win_association_info assoc_info =
 };
 
 
+
+//============================================================
+//  win_association_dialog_proc
+//============================================================
+
 static INT_PTR CALLBACK win_association_dialog_proc(HWND dialog, UINT message,
 	WPARAM wparam, LPARAM lparam)
 {
 	INT_PTR rc = 0;
-	struct assocdlg_info *dlginfo;
+	assocdlg_info *dlginfo;
 	HWND ok_button, cancel_button, control;
 	LRESULT font;
 	LONG_PTR l;
@@ -42,7 +79,7 @@ static INT_PTR CALLBACK win_association_dialog_proc(HWND dialog, UINT message,
 	switch(message)
 	{
 		case WM_INITDIALOG:
-			dlginfo = (struct assocdlg_info *) lparam;
+			dlginfo = (assocdlg_info *) lparam;
 			ok_button = GetDlgItem(dialog, IDOK);
 			cancel_button = GetDlgItem(dialog, IDCANCEL);
 			font = SendMessage(ok_button, WM_GETFONT, 0, 0);
@@ -92,7 +129,7 @@ static INT_PTR CALLBACK win_association_dialog_proc(HWND dialog, UINT message,
 			if (HIWORD(wparam) == BN_CLICKED)
 			{
 				l = GetWindowLongPtr(dialog, GWLP_USERDATA);
-				dlginfo = (struct assocdlg_info *) l;
+				dlginfo = (assocdlg_info *) l;
 				id = LOWORD(wparam);
 
 				if ((id == IDOK) || (id == IDCANCEL))
@@ -125,6 +162,10 @@ static INT_PTR CALLBACK win_association_dialog_proc(HWND dialog, UINT message,
 
 
 
+//============================================================
+//  extension_compare
+//============================================================
+
 static int CLIB_DECL extension_compare(const void *p1, const void *p2)
 {
 	const char *e1 = *((const char **) p1);
@@ -134,49 +175,63 @@ static int CLIB_DECL extension_compare(const void *p1, const void *p2)
 
 
 
-static void setup_extensions(struct assocdlg_info *dlginfo)
+//============================================================
+//  setup_extensions
+//============================================================
+
+static void setup_extensions(assocdlg_info *dlginfo)
 {
 	const imgtool_module *module = NULL;
-	const char *ext;
-	int i;
+	char *s;
+	int buffer_pos;
 
-	dlginfo->extension_count = 0;
-
+	// merge all file extensions onto one comma-delimited list
 	for (module = imgtool_find_module(NULL); module; module = module->next)
+		specify_extension(dlginfo->buffer, ARRAY_LENGTH(dlginfo->buffer) - 1, module->extensions);
+
+	// split the comma delimited list, and convert it to a string array
+	dlginfo->extension_count = 0;
+	buffer_pos = 0;
+	while(dlginfo->buffer[buffer_pos] != '\0')
 	{
-		ext = module->extensions;
-		while(ext[0])
-		{
-			for (i = 0; i < dlginfo->extension_count; i++)
-			{
-				if (!strcmp(dlginfo->extensions[i], ext))
-					break;
-			}
-			if (i >= dlginfo->extension_count)
-			{
-				assert(dlginfo->extension_count < sizeof(dlginfo->extensions) / sizeof(dlginfo->extensions[0]));
-				if (dlginfo->extension_count < sizeof(dlginfo->extensions) / sizeof(dlginfo->extensions[0]))
-				{
-					dlginfo->extensions[dlginfo->extension_count++] = ext;
-				}
-			}
-			ext = ext + strlen(ext) + 1;
-		}
+		// an assertion to check to see if we are out of extensions
+		assert(dlginfo->extension_count < ARRAY_LENGTH(dlginfo->extensions));
+
+		// search for a comma, and if found null it out
+		s = strchr(&dlginfo->buffer[buffer_pos], ',');
+		if (s)
+			*s = '\0';
+
+		// append this to the extensions list
+		dlginfo->extensions[dlginfo->extension_count++] = &dlginfo->buffer[buffer_pos];
+
+		// move to next extension
+		if (s)
+			buffer_pos += s - &dlginfo->buffer[buffer_pos] + 1;
+		else
+			buffer_pos += strlen(&dlginfo->buffer[buffer_pos]);
 	}
 
+	// sort the extensions list
 	qsort((void *) dlginfo->extensions, dlginfo->extension_count,
 		sizeof(dlginfo->extensions[0]), extension_compare);
 }
 
 
 
+//============================================================
+//  win_association_dialog
+//============================================================
+
 void win_association_dialog(HWND parent)
 {
-	struct assocdlg_info dlginfo;
+	assocdlg_info dlginfo;
 
+	// initialize data structure
 	memset(&dlginfo, 0, sizeof(dlginfo));
 	setup_extensions(&dlginfo);
 
+	// display dialog
 	DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ASSOCIATIONS), parent,
 		win_association_dialog_proc, (LPARAM) &dlginfo);
 }
