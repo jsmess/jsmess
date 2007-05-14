dad63c38e66a08fd159ca797c799edbb0d780d8a
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2007-05-14 12:27:46 +0000

    Add Sega SG-1000 Mark II driver
    git-svn-id: svn://messdev.no-ip.org/mess@397 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/messdriv.c
+++ src/mess/messdriv.c
@@ -125,6 +125,7 @@ const game_driver *test_drivers[] =
 
 	/* SEGA */
 	DRIVER( sg1000 )	/* Sega SG-1000 (Japan)								*/
+	DRIVER( sg1000m2 )	/* Sega SG-1000 Mark II (Japan)						*/
 	DRIVER( sc3000 )	/* Sega SC-3000 (Japan)								*/
 	DRIVER( sf7000 )	/* Sega SC-3000 w/ SF-7000 (Japan)					*/
 
