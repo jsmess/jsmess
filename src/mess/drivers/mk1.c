40cb2cacdf0fbab6b5c035474634b36b7fb5823b
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2007-08-17 09:53:24 +0000

    [MK1] Marked the MK1 driver as working.
    git-svn-id: svn://messdev.no-ip.org/mess@837 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mk1.c
+++ src/mess/drivers/mk1.c
@@ -199,5 +199,5 @@ static DRIVER_INIT( mk1 )
 
 // seams to be developed by mostek (MK)
 /*     YEAR   NAME  PARENT  COMPAT	MACHINE INPUT   INIT	CONFIG	COMPANY                 FULLNAME */
-CONS( 1979,  mk1,  0, 		0,		mk1,	mk1,	mk1,	NULL,	"Computer Electronic",  "Chess Champion MK I", GAME_NOT_WORKING)
+CONS( 1979,  mk1,  0, 		0,		mk1,	mk1,	mk1,	NULL,	"Computer Electronic",  "Chess Champion MK I", 0 )
 
