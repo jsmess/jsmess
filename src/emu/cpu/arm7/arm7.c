61c157d3aedeba5b2eba04adacf0826621678199
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2007-09-15 21:04:21 +0000

    [ARM7] Missed one file.
    git-svn-id: svn://messdev.no-ip.org/mess@973 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/cpu/arm7/arm7.c
+++ src/emu/cpu/arm7/arm7.c
@@ -257,7 +257,7 @@ void arm7_get_info(UINT32 state, cpuinfo *info)
 		case CPUINFO_INT_DEFAULT_IRQ_VECTOR:			info->i = 0;							break;
 		case CPUINFO_INT_ENDIANNESS:					info->i = CPU_IS_LE;					break;
 		case CPUINFO_INT_CLOCK_DIVIDER:					info->i = 1;							break;
-		case CPUINFO_INT_MIN_INSTRUCTION_BYTES:			info->i = 4;							break;
+		case CPUINFO_INT_MIN_INSTRUCTION_BYTES:			info->i = 2;							break;
 		case CPUINFO_INT_MAX_INSTRUCTION_BYTES:			info->i = 4;							break;
 		case CPUINFO_INT_MIN_CYCLES:					info->i = 3;							break;
 		case CPUINFO_INT_MAX_CYCLES:					info->i = 4;							break;
