aa3175f8bbdc9f041bbc1079f7010c3e1c953b92
npwoods <npwoods@9849079f-6229-0410-a07f-84406ce305ba>
2007-12-20 02:48:02 +0000

    [Windows] Fixed bug #1446; must have been a regression introduced in the move to using MAME's makefile
    git-svn-id: svn://messdev.no-ip.org/mess@1424 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/uimess.c
+++ src/mess/uimess.c
@@ -192,7 +192,7 @@ UINT32 ui_menu_image_info(UINT32 state)
 
 int mess_use_new_ui(void)
 {
-#if defined(WIN32) && !defined(SDLMAME_WIN32)
+#if (defined(WIN32) || defined(_MSC_VER)) && !defined(SDLMAME_WIN32)
 	if (options_get_bool(mame_options(), "newui"))
 		return TRUE;
 #endif
