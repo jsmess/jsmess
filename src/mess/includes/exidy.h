f585fdbca94141051496407793ee7c5a0e269404
duke <duke@9849079f-6229-0410-a07f-84406ce305ba>
2008-01-12 09:41:43 +0000

    [EXIDY] Changed to use the common black and white palette
    git-svn-id: svn://messdev.no-ip.org/mess@1502 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/includes/exidy.h
+++ src/mess/includes/exidy.h
@@ -8,8 +8,6 @@
 #define EXIDY_H_
 
 
-#define EXIDY_NUM_COLOURS 2
-
 /* 64 chars wide, 30 chars tall */
 #define EXIDY_SCREEN_WIDTH        (64*8)
 #define EXIDY_SCREEN_HEIGHT       (30*8)
@@ -17,9 +15,7 @@
 
 /*----------- defined in video/exidy.c -----------*/
 
-VIDEO_START( exidy );
 VIDEO_UPDATE( exidy );
-PALETTE_INIT( exidy );
 
 
 #endif /* EXIDY_H_ */
--- src/mess/video/exidy.c
+++ src/mess/video/exidy.c
@@ -7,43 +7,10 @@
 ***************************************************************************/
 
 #include "driver.h"
-#include "../includes/exidy.h"
+#include "includes/exidy.h"
 #include "mslegacy.h"
 
-/***************************************************************************
-  Start the video hardware emulation.
-***************************************************************************/
 
-VIDEO_START( exidy )
-{
-}
-
-/* two colours */
-static const unsigned short exidy_colour_table[EXIDY_NUM_COLOURS] =
-{
-	0, 1
-};
-
-/* black/white */
-static const unsigned char exidy_palette[EXIDY_NUM_COLOURS * 3] =
-{
-    0x000, 0x000, 0x000,
-    0x0ff, 0x0ff, 0x0ff
-};
-
-
-/* Initialise the palette */
-PALETTE_INIT( exidy )
-{
-	palette_set_colors_rgb(machine, 0, exidy_palette, sizeof(exidy_palette) / 3);
-	memcpy(colortable, exidy_colour_table, sizeof (exidy_colour_table));
-}
-
-/***************************************************************************
-  Draw the game screen in the given mame_bitmap.
-  Do NOT call osd_update_display() from this function,
-  it will be called by the main emulation engine.
-***************************************************************************/
 VIDEO_UPDATE( exidy )
 {
 	int x,y;
@@ -93,4 +60,3 @@ VIDEO_UPDATE( exidy )
 	}
 	return 0;
 }
-
