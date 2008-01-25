3ade1ac3e0a43e5ce890ecc7ddac911ab43e0503
duke <duke@9849079f-6229-0410-a07f-84406ce305ba>
2008-01-25 20:11:24 +0000

    [SYM1] Fixed ROM handler
    git-svn-id: svn://messdev.no-ip.org/mess@1605 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/sym1.c
+++ src/mess/machine/sym1.c
@@ -305,6 +305,6 @@ MACHINE_RESET( sym1 )
 	/* make 0xf800 to 0xffff point to the last half of the monitor ROM
 	   so that the CPU can find its reset vectors */
 	memory_install_readwrite8_handler(0, ADDRESS_SPACE_PROGRAM,
-			0xf800, 0xffff, 0, 0, MRA8_BANK1, MWA8_ROM);
+			0xf800, 0xffff, 0, 0, MRA8_BANK1, MWA8_NOP);
 	memory_set_bankptr(1, sym1_monitor + 0x800);
 }
