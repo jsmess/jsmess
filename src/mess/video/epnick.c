a28edb57213e6eb4fd48fb13318159d432ce50f5
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-02-21 17:01:32 +0000

    [EP128] Fixed graphics display. (bug #1478)
    git-svn-id: svn://messdev.no-ip.org/mess@1703 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/epnick.c
+++ src/mess/video/epnick.c
@@ -18,7 +18,6 @@
 
 /* TODO: Remove dependency on this */
 #include "deprecat.h"
-#include "mslegacy.h"
 
 
 /*************************************************************/
@@ -48,50 +47,6 @@ static void nick_write_pixel(NICK_STATE *nick, int ci)
 /* Enterprise has 256 colours, all may be on the screen at once!
 the NICK_GET_RED8, NICK_GET_GREEN8, NICK_GET_BLUE8 macros
 return a 8-bit colour value for the index specified.  */
-static unsigned char nick_colour_palette[256*3];
-
-/* the mapping doesn't change - EP uses 0..256 index's for
-colour specification - don't know if I require this for the
-driver or not. */
-static const unsigned short nick_colour_table[256]=
-{
-	0,1,2,3,4,5,6,7,8,
-	9,10,11,12,13,14,15,16,
-	17,18,19,20,21,22,23,24,
-	25,26,27,28,29,30,31,32,
-	33,34,35,36,37,38,39,40,
-	41,42,43,44,45,46,47,48,
-	49,50,51,52,53,54,55,56,
-	57,58,59,60,61,62,63,64,
-	65,66,67,68,69,70,71,72,
-	73,74,75,76,77,78,79,80,
-	81,82,83,84,85,86,87,88,
-	89,90,91,92,93,94,95,96,
-	97,98,99,100,101,102,103,104,
-	105,106,107,108,109,110,111,
-	112,113,114,115,116,117,118,
-	119,120,121,122,123,124,125,
-	126,127,128,129,130,131,132,
-	133,134,135,136,137,138,139,
-	140,141,142,143,144,145,146,
-	147,148,149,150,151,152,153,
-	154,155,156,157,158,159,160,
-	161,162,163,164,165,166,167,
-	168,169,170,171,172,173,174,
-	175,176,177,178,179,180,181,
-	182,183,184,185,186,187,188,
-	189,190,191,192,193,194,195,
-	196,197,198,199,200,201,202,
-	203,204,205,206,207,208,209,
-	210,211,212,213,214,215,216,
-	217,218,219,220,221,222,223,
-	224,225,226,227,228,229,230,
-	231,232,233,234,235,236,237,
-	238,239,240,241,242,243,244,
-	245,246,247,248,249,250,251,
-	252,253,254,255
-};
-
 
 /* initial the palette */
 PALETTE_INIT( nick )
@@ -100,14 +55,8 @@ PALETTE_INIT( nick )
 
 	for (i=0; i<256; i++)
 	{
-		nick_colour_palette[(i*3)] = NICK_GET_RED8(i);
-		nick_colour_palette[(i*3)+1] = NICK_GET_GREEN8(i);
-		nick_colour_palette[(i*3)+2] = NICK_GET_BLUE8(i);
+		palette_set_color_rgb( machine, i, NICK_GET_RED8(i), NICK_GET_GREEN8(i), NICK_GET_BLUE8(i) );
 	}
-
-	palette_set_colors_rgb(machine, 0, nick_colour_palette, sizeof(nick_colour_palette) / 3);
-	memcpy(colortable, nick_colour_table, sizeof(nick_colour_table));
-	//color_prom=malloc(0*sizeof(char));
 }
 
 /* first clock visible on left hand side */
@@ -958,6 +907,8 @@ void    Nick_DoScreen(mame_bitmap *bm)
 
     /* set write address for line */
     Nick.dest = BITMAP_ADDR16(bm, line, 0);
+	Nick.dest_pos = 0;
+	Nick.dest_max_pos = bm->width;
 
     /* write line */
     Nick_DoLine();
