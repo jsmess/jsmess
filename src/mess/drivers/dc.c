88282c6a480003e29136c79c410fd3a2cbe20562
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2008-02-23 22:48:05 +0000

    [DC] Hooked up AICA & sound RAM, hooked up PDTRA state machine so BIOS test passes.
    git-svn-id: svn://messdev.no-ip.org/mess@1715 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/dc.c
+++ src/mess/drivers/dc.c
@@ -13,10 +13,60 @@
 #include "driver.h"
 #include "cpu/arm7/arm7core.h"
 #include "dc.h"
+#include "sound/aica.h"
+#include "deprecat.h"
 
-static UINT64 *dc_sound_ram;
+#define DC_VIDEO_TYPE	(3<<8)		// composite
+
+static UINT32 *dc_sound_ram;
 
 UINT32 dma_offset;	/* silly MAME global variable */
+
+static UINT64 portA = 0x3, PDTRA, latched_PDTRA = 0, w;
+static int state = 0;
+
+static READ64_HANDLER( dc_pdtra_r )
+{
+	w = portA;
+	if (!state)
+	{
+		if ((latched_PDTRA == 1) || (latched_PDTRA == 2))
+		{
+			w &= ~3;
+		}
+	}
+
+	return w;
+}
+
+static WRITE64_HANDLER( dc_pdtra_w )
+{
+	if ((data) && (!PDTRA))
+	{
+		latched_PDTRA = data;
+		state = 1;
+	}
+	else if ((!data) && (!PDTRA))
+	{
+		latched_PDTRA = 0;
+	}
+	else
+	{
+		state = 0;
+	}
+
+	COMBINE_DATA(&PDTRA);
+}
+
+static READ64_HANDLER( dc_arm_r )
+{
+	return *((UINT64 *)dc_sound_ram+offset);
+}
+
+static WRITE64_HANDLER( dc_arm_w )
+{
+	COMBINE_DATA((UINT64 *)dc_sound_ram + offset);
+}
 
 static ADDRESS_MAP_START( dc_map, ADDRESS_SPACE_PROGRAM, 64 )
 	AM_RANGE(0x00000000, 0x001fffff) AM_ROM						// BIOS
@@ -31,7 +81,7 @@ static ADDRESS_MAP_START( dc_map, ADDRESS_SPACE_PROGRAM, 64 )
 	AM_RANGE(0x00600000, 0x006007ff) AM_READWRITE( dc_modem_r, dc_modem_w )
 	AM_RANGE(0x00700000, 0x00707fff) AM_READWRITE( dc_aica_reg_r, dc_aica_reg_w )
 	AM_RANGE(0x00710000, 0x0071000f) AM_READWRITE( dc_rtc_r, dc_rtc_w )
-	AM_RANGE(0x00800000, 0x009fffff) AM_RAM	AM_BASE( &dc_sound_ram )		// sound RAM
+	AM_RANGE(0x00800000, 0x009fffff) AM_READWRITE( dc_arm_r, dc_arm_w )
 	AM_RANGE(0x04000000, 0x04ffffff) AM_RAM	AM_SHARE(2)	// texture memory
 	AM_RANGE(0x05000000, 0x05ffffff) AM_RAM AM_SHARE(2)	// mirror of texture RAM
 	AM_RANGE(0x0c000000, 0x0cffffff) AM_RAM AM_SHARE(1)
@@ -42,22 +92,46 @@ static ADDRESS_MAP_START( dc_map, ADDRESS_SPACE_PROGRAM, 64 )
 	AM_RANGE(0xa0000000, 0xa01fffff) AM_ROM AM_REGION(REGION_CPU1, 0)
 ADDRESS_MAP_END
 
+static ADDRESS_MAP_START( dc_port, ADDRESS_SPACE_IO, 64 )
+	AM_RANGE(0x00000000, 0x00000007) AM_READWRITE( dc_pdtra_r, dc_pdtra_w )
+ADDRESS_MAP_END
+
 static ADDRESS_MAP_START( dc_audio_map, ADDRESS_SPACE_PROGRAM, 32 )
-	AM_RANGE(0x00000000, 0x001fffff) AM_RAM		/* shared with SH-4 */
+	AM_RANGE(0x00000000, 0x001fffff) AM_RAM AM_BASE(&dc_sound_ram)		/* shared with SH-4 */
+	AM_RANGE(0x00800000, 0x00807fff) AM_READWRITE(dc_arm_aica_r, dc_arm_aica_w)
 ADDRESS_MAP_END
 
 static INPUT_PORTS_START( dc )
 INPUT_PORTS_END
 
+static MACHINE_RESET( dc_console )
+{
+	MACHINE_RESET_CALL(dc);
+	AICA_set_ram_base(0, dc_sound_ram, 2*1024*1024);
+}
+
+static void aica_irq(int irq)
+{
+	cpunum_set_input_line(Machine, 1, ARM7_FIRQ_LINE, irq ? ASSERT_LINE : CLEAR_LINE);
+}
+
+static const struct AICAinterface aica_interface =
+{
+	REGION_CPU1,		// temporary
+	0,
+	aica_irq
+};
+
 static MACHINE_DRIVER_START( dc )
 	/* basic machine hardware */
 	MDRV_CPU_ADD_TAG("main", SH4, 200000000)
 	MDRV_CPU_PROGRAM_MAP(dc_map,0)
+	MDRV_CPU_IO_MAP(dc_port,0)
 
 	MDRV_CPU_ADD_TAG("sound", ARM7, 45000000)
 	MDRV_CPU_PROGRAM_MAP(dc_audio_map, 0)
 
-	MDRV_MACHINE_RESET( dc )
+	MDRV_MACHINE_RESET( dc_console )
 
 	/* video hardware */
 	MDRV_SCREEN_ADD("main", RASTER)
@@ -70,6 +144,12 @@ static MACHINE_DRIVER_START( dc )
 
 	MDRV_VIDEO_START(dc)
 	MDRV_VIDEO_UPDATE(dc)
+
+	MDRV_SPEAKER_STANDARD_STEREO("left", "right")
+	MDRV_SOUND_ADD(AICA, 0)
+	MDRV_SOUND_CONFIG(aica_interface)
+	MDRV_SOUND_ROUTE(0, "left", 2.0)
+	MDRV_SOUND_ROUTE(0, "right", 2.0)
 MACHINE_DRIVER_END
 
 ROM_START(dc)
