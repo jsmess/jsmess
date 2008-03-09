d3229a54cf508156cf17b530a78b7538dc604df9
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-09 18:31:46 +0000

    [HP48] Updated video code a bit.
    git-svn-id: svn://messdev.no-ip.org/mess@1877 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/hp48.c
+++ src/mess/video/hp48.c
@@ -1,6 +1,4 @@
 #include "driver.h"
-#include "mslegacy.h"
-
 #include "includes/hp48.h"
 
 static const UINT8 hp48_palette[] =
@@ -14,7 +12,11 @@ static const UINT8 hp48_palette[] =
 
 PALETTE_INIT( hp48 )
 {
-	palette_set_colors_rgb(machine, 0, hp48_palette, sizeof(hp48_palette) / 3);
+	int i;
+
+	for ( i = 0; i < 4; i++ ) {
+		palette_set_color_rgb( machine, i, hp48_palette[i*3], hp48_palette[i*3+1], hp48_palette[i*3+2] );
+	}
 }
 
 
@@ -103,39 +105,72 @@ static const HP48_FIGURE hp48_orange={
 
 #define DOWN 98
 #define RIGHT 40
-#define LCD_ENABLE hp48_hardware.data[0]&8
-#define LCD_VERTICAL_OFFSET hp48_hardware.data[0]&7
-#define LCD_BASE_ADDRESS (hp48_hardware.data[0x20]|(hp48_hardware.data[0x21]<<4)\
+#define LCD_ENABLE (hp48_hardware.data[0]&8)
+#define LCD_VERTICAL_OFFSET (hp48_hardware.data[0]&7)
+#define LCD_MAIN_BASE_ADDRESS (hp48_hardware.data[0x20]|(hp48_hardware.data[0x21]<<4)\
 		|(hp48_hardware.data[0x22]<<8)|(hp48_hardware.data[0x23]<<12)\
 		|(hp48_hardware.data[0x24]<<16))
+#define LCD_MENU_BASE_ADDRESS (hp48_hardware.data[0x30]|(hp48_hardware.data[0x31]<<4)\
+		|(hp48_hardware.data[0x32]<<8)|(hp48_hardware.data[0x33]<<12)\
+		|(hp48_hardware.data[0x34]<<16))
+#define LCD_MAIN_SIZE (hp48_hardware.data[0x28]|(hp48_hardware.data[0x29]<<4))
 
 #define LCD_LINE_OFFSET (hp48_hardware.data[0x25]|(hp48_hardware.data[0x26]<<4)\
 		|(hp48_hardware.data[0x27]<<8))
 
 VIDEO_UPDATE( hp48 )
 {
-	int x, y, i;
+	int x, y, i, p, data, max_y;
 	int color[2];
-	int contrast=(hp48_hardware.data[1]|((hp48_hardware.data[2]&1)<<4));
+//	int contrast=(hp48_hardware.data[1]|((hp48_hardware.data[2]&1)<<4));
 
 	/* HJB: we cannot initialize array with values from other arrays, thus... */
     color[0] = machine->pens[0];
 //    color[0] = machine->pens[1];
 	color[1] = machine->pens[1];
 
+	i = LCD_MAIN_BASE_ADDRESS;
+	max_y = MIN(64, LCD_MAIN_SIZE + 1 );
+	if ( max_y < 3 ) {
+		max_y = 64;
+	}
 
-	for (y=0,i=LCD_BASE_ADDRESS; y<64; y+=8, i+=LCD_LINE_OFFSET) {
+	i = LCD_MAIN_BASE_ADDRESS;
+	for (y=0; y< max_y; y++) {
+		int j = ( LCD_VERTICAL_OFFSET & 4 ? 1 : 0 );
+		p = LCD_VERTICAL_OFFSET & 0x03;
+		data = program_read_byte( i + j );
 		for (x=0; x<131; x++) {
-			drawgfx(bitmap, machine->gfx[0],
-					program_read_byte(i+x),
-					contrast,0,0,
-					x*2+RIGHT,y*2+DOWN,
-					0, TRANSPARENCY_NONE,0);
+			*BITMAP_ADDR16(bitmap, DOWN + y, RIGHT + x) = ( ( data << p ) & 0x08 ) ? 3 : 2;
+			p++;
+			if ( p > 3 ) {
+				j++;
+				data = program_read_byte( i + j );
+				p = 0;
+			}
 		}
+		i += 34 + LCD_LINE_OFFSET;
+	}
+
+	i = LCD_MENU_BASE_ADDRESS;
+	for ( ; y < 64; y++ ) {
+		int j = 0;
+		p = 0;
+		data = program_read_byte( i + j );
+		for ( x = 0; x < 131; x++ ) {
+			*BITMAP_ADDR16(bitmap, DOWN + y, RIGHT + x) = ( ( data << p ) & 0x08 ) ? 3 : 2;
+			p++;
+			if ( p > 3 ) {
+				j++;
+				data = program_read_byte( i + j );
+				p = 0;
+			}
+		}
+		i += 34;
 	}
 
 	hp48_draw_special(bitmap,RIGHT+12,DOWN-13,hp48_orange,
-					  hp48_hardware.data[0xb]&1?color[1]:color[0]);
+					  hp48_hardware.data[0xb]&1?color[1]:color[1]);
 	hp48_draw_special(bitmap,RIGHT+57,DOWN-13,hp48_blue,
 					  hp48_hardware.data[0xb]&2?color[1]:color[0]);
 	hp48_draw_special(bitmap,RIGHT+102,DOWN-13,hp48_alpha,
