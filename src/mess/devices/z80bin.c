afef51335916f0745753d8c1f2c6ba91580c0249
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-10 01:34:26 +0000

    [Z80BIN] Fixed some bugs.
    git-svn-id: svn://messdev.no-ip.org/mess@1880 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/devices/z80bin.c
+++ src/mess/devices/z80bin.c
@@ -5,42 +5,88 @@
 	A binary quickload format used by the Microbee, the Exidy Sorcerer
 	VZ200/300 and the Super 80
 
+	NOTE: INIT_FAIL == 1, INIT_PASS == 0, EOF == 0xffffffff
+
+	The return_code consists of start_addr | end_addr | exec_addr | status.
+	Status: 0 = good data, 1 = corrupted quickload file.
+
 *********************************************************************/
 
 #include "driver.h"
+#include "ui.h"
 #include "z80bin.h"
 #include "snapquik.h"
 
-
-static QUICKLOAD_LOAD( z80bin )
+UINT64 z80bin_load_file( mess_image *image, const char *file_type )
 {
 	int ch;
 	UINT16 args[3];
-	UINT16 start_addr, end_addr, exec_addr, i;
+	UINT16 i=0, j, size;
+	UINT64 start_addr, exec_addr, end_addr;	/* need to be this size for return at end */
 	UINT8 data;
+	char * pgmname;
+	pgmname = (char *) malloc(256);
 
 	image_fseek(image, 7, SEEK_SET);
 
 	while((ch = image_fgetc(image)) != 0x1A)
 	{
 		if (ch == EOF)
+		{
+			ui_popup_time(10,"QUICKLOAD: Unexpected EOF while getting file name");	/* 10 seconds */
+			free(pgmname);
 			return INIT_FAIL;
+		}
+
+		if (ch)
+		{
+			pgmname[i] = ch;	/* build program name */
+			i++;
+		}
 	}
 
+	pgmname[i]=0;	/* terminate string with a null */
+
 	if (image_fread(image, args, sizeof(args)) != sizeof(args))
+	{
+		ui_popup_time(10,"QUICKLOAD: %s\nUnexpected EOF while getting file size",pgmname);
+		free(pgmname);
 		return INIT_FAIL;
+	}
 
 	exec_addr	= LITTLE_ENDIANIZE_INT16(args[0]);
 	start_addr	= LITTLE_ENDIANIZE_INT16(args[1]);
 	end_addr	= LITTLE_ENDIANIZE_INT16(args[2]);
 
-	for (i = start_addr; i <= end_addr; i++)
+	size = (end_addr - start_addr + 1) & 0xffff;
+
+	popmessage("%s\nsize=%04X : start=%04X : end=%04X : exec=%04X",pgmname,size,(int)start_addr,(int)end_addr,(int)exec_addr);
+
+	for (i = 0; i < size; i++)
 	{
+		j = (start_addr + i) & 0xffff;
 		if (image_fread(image, &data, 1) != 1)
+		{
+			ui_popup_time(10,"QUICKLOAD: %s\nUnexpected EOF while writing byte to %04X",pgmname,j);
+			free(pgmname);
 			return INIT_FAIL;
-		program_write_byte(i, data);
+		}
+		program_write_byte(j, data);
 	}
-	cpunum_set_reg(0, REG_PC, exec_addr);
+
+	free(pgmname);
+
+	return (start_addr << 48) | (end_addr << 32) | (exec_addr << 16);
+}
+
+static QUICKLOAD_LOAD( z80bin )
+{
+	UINT16 exec_addr;
+	UINT64 return_info = z80bin_load_file( image, file_type );	/* load file */
+	if (return_info == INIT_FAIL) return INIT_FAIL;			/* failure */
+	exec_addr = (return_info & 0xffff0000) >> 16;			/* get program run address */
+	if (exec_addr == 0xffff) return INIT_PASS;			/* non-executable data file */
+	cpunum_set_reg(0, REG_PC, exec_addr);				/* start program */
 	return INIT_PASS;
 }
 
@@ -58,7 +104,7 @@ void z80bin_quickload_getinfo(const mess_device_class *devclass, UINT32 state, u
 		/* --- the following bits of info are returned as pointers to data or functions --- */
 		case MESS_DEVINFO_PTR_QUICKLOAD_LOAD:				info->f = (genf *) quickload_load_z80bin; break;
 
-		default:										quickload_device_getinfo(devclass, state, info); break;
+		default:							quickload_device_getinfo(devclass, state, info); break;
 	}
 }
 
--- src/mess/devices/z80bin.h
+++ src/mess/devices/z80bin.h
@@ -16,5 +16,7 @@
 
 void z80bin_quickload_getinfo(const mess_device_class *devclass, UINT32 state, union devinfo *info);
 
+UINT64 z80bin_load_file( mess_image *image, const char *file_type );
+
 
 #endif /* Z80BIN_H */
