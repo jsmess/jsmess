ba87f844783013016c019dc0ec768eaf0bf40ec4
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-14 10:12:59 +0000

    [MIKRO80][UT88] PPI 8255 used as on real machines
    git-svn-id: svn://messdev.no-ip.org/mess@1990 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/mikro80.c
+++ src/mess/machine/mikro80.c
@@ -10,47 +10,67 @@
 #include "driver.h"
 #include "cpu/i8085/i8085.h"
 #include "devices/cassette.h"
-#include <stdio.h>
-
+#include "machine/8255ppi.h"
 
 static int mikro80_keyboard_line;
-
 
 /* Driver initialization */
 DRIVER_INIT(mikro80)
 {
 }
 
+READ8_HANDLER (mikro80_8255_portb_r )
+{	
+	return readinputport(mikro80_keyboard_line);
+}
+
+READ8_HANDLER (mikro80_8255_portc_r )
+{
+	return readinputport(8) & 0x07;	
+}
+
+WRITE8_HANDLER (mikro80_8255_porta_w )
+{	
+	switch (data ^ 0xff) {
+	  	case 0x01 : mikro80_keyboard_line = 0;break;
+	  	case 0x02 : mikro80_keyboard_line = 1;break;
+	  	case 0x04 : mikro80_keyboard_line = 2;break;
+	  	case 0x08 : mikro80_keyboard_line = 3;break;
+	  	case 0x10 : mikro80_keyboard_line = 4;break;
+	  	case 0x20 : mikro80_keyboard_line = 5;break;
+	  	case 0x40 : mikro80_keyboard_line = 6;break;
+	  	case 0x80 : mikro80_keyboard_line = 7;break;
+	}	
+}
+
+static const ppi8255_interface mikro80_ppi8255_interface =
+{
+	1,
+	{NULL},
+	{mikro80_8255_portb_r},
+	{mikro80_8255_portc_r},
+	{mikro80_8255_porta_w},
+	{NULL},
+	{NULL},
+};
+
 MACHINE_RESET( mikro80 )
 {
+	ppi8255_init(&mikro80_ppi8255_interface);
 	mikro80_keyboard_line = 0;
 }
 
 
 READ8_HANDLER( mikro80_keyboard_r )
-{	
-	if (offset==2) {		
-  		switch (mikro80_keyboard_line) {
-	  		case 0x01 : return readinputport(0);break;
-	  		case 0x02 : return readinputport(1);break;
-	  		case 0x04 : return readinputport(2);break;
-	  		case 0x08 : return readinputport(3);break;
-	  		case 0x10 : return readinputport(4);break;
-	  		case 0x20 : return readinputport(5);break;
-	  		case 0x40 : return readinputport(6);break;
-	  		case 0x80 : return readinputport(7);break;
-		}
-	}
-	if (offset==1) {
-		return readinputport(8) & 0x07;
-	}
-	return 0xff;
+{
+	return ppi8255_0_r(machine, offset^0x03);	
 }
 
 WRITE8_HANDLER( mikro80_keyboard_w )
 {
-  	mikro80_keyboard_line = data ^ 0xff;
+	ppi8255_0_w(machine, offset^0x03, data);
 }
+
 
 WRITE8_HANDLER( mikro80_tape_w )
 {
--- src/mess/machine/ut88.c
+++ src/mess/machine/ut88.c
@@ -11,7 +11,7 @@
 #include "cpu/i8085/i8085.h"
 #include "sound/dac.h"
 #include "devices/cassette.h"
-#include <stdio.h>
+#include "machine/8255ppi.h"
 
 
 static int ut88_keyboard_line;
@@ -20,37 +20,60 @@ static int ut88_keyboard_line;
 /* Driver initialization */
 DRIVER_INIT(ut88)
 {
+
 }
+
+
+READ8_HANDLER (ut88_8255_portb_r )
+{
+	return readinputport(ut88_keyboard_line) & 0x7f;
+}
+
+READ8_HANDLER (ut88_8255_portc_r )
+{
+	return readinputport(8) & 0x07;	
+}
+
+WRITE8_HANDLER (ut88_8255_porta_w )
+{	
+	switch (data ^ 0xff) {
+	  	case 0x01 : ut88_keyboard_line = 0;break;
+	  	case 0x02 : ut88_keyboard_line = 1;break;
+	  	case 0x04 : ut88_keyboard_line = 2;break;
+	  	case 0x08 : ut88_keyboard_line = 3;break;
+	  	case 0x10 : ut88_keyboard_line = 4;break;
+	  	case 0x20 : ut88_keyboard_line = 5;break;
+	  	case 0x40 : ut88_keyboard_line = 6;break;
+	  	case 0x80 : ut88_keyboard_line = 7;break;
+	}	
+}
+
+static const ppi8255_interface ut88_ppi8255_interface =
+{
+	1,
+	{NULL},
+	{ut88_8255_portb_r},
+	{ut88_8255_portc_r},
+	{ut88_8255_porta_w},
+	{NULL},
+	{NULL},
+};
 
 MACHINE_RESET( ut88 )
 {
+	ppi8255_init(&ut88_ppi8255_interface);
 	ut88_keyboard_line = 0;
 }
 
 
 READ8_HANDLER( ut88_keyboard_r )
 {
-	if (offset==2) {
-		return ut88_keyboard_line;
-	}
-	if (offset==1) {
-		return readinputport(8) & 0x07;
-	}
-	 return 0xff;
+	return ppi8255_0_r(machine, offset^0x03);	
 }
 
 WRITE8_HANDLER( ut88_keyboard_w )
 {
-  switch (data ^ 0xff) {
-	  	case 0x01 : ut88_keyboard_line = readinputport(0) & 0x7f;break;
-	  	case 0x02 : ut88_keyboard_line = readinputport(1) & 0x7f;break;
-	  	case 0x04 : ut88_keyboard_line = readinputport(2) & 0x7f;break;
-	  	case 0x08 : ut88_keyboard_line = readinputport(3) & 0x7f;break;
-	  	case 0x10 : ut88_keyboard_line = readinputport(4) & 0x7f;break;
-	  	case 0x20 : ut88_keyboard_line = readinputport(5) & 0x7f;break;
-	  	case 0x40 : ut88_keyboard_line = readinputport(6) & 0x7f;break;
-	  	case 0x80 : ut88_keyboard_line = readinputport(7) & 0x7f;break;
-	}
+	ppi8255_0_w(machine, offset^0x03, data);
 }
 
 WRITE8_HANDLER( ut88_sound_w )
