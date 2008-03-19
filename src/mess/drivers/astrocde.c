f64f9ea1c7f7c7e756e5cf80383902dcd33b74e5
duke <duke@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-19 11:37:00 +0000

    [ASTROCDE] Updated for 0.123u6
    git-svn-id: svn://messdev.no-ip.org/mess@2020 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/astrocde.c
+++ src/mess/drivers/astrocde.c
@@ -27,8 +27,7 @@ ADDRESS_MAP_END
 
 
 static ADDRESS_MAP_START( astrocade_io, ADDRESS_SPACE_IO, 8 )
-	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x00, 0x1f) AM_READWRITE(astrocade_data_chip_register_r, astrocade_data_chip_register_w)
+	AM_RANGE(0x00, 0x1f) AM_MIRROR(0xff00) AM_MASK(0xffff) AM_READWRITE(astrocade_data_chip_register_r, astrocade_data_chip_register_w)
 ADDRESS_MAP_END
 
 
