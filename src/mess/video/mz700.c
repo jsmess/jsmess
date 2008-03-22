713f6e1e05e710c7f32218706b5b5e72229cacf7
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-22 01:17:31 +0000

    [MZ700] Fixed palette (bug 1537).
    git-svn-id: svn://messdev.no-ip.org/mess@2051 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/mz700.c
+++ src/mess/video/mz700.c
@@ -25,31 +25,33 @@ PALETTE_INIT( mz700 )
 {
 	int i;
 
-    for (i = 0; i < 8; i++)
+	machine->colortable = colortable_alloc(machine, 8);
+
+	for (i = 0; i < 8; i++)
+		colortable_palette_set_color(machine->colortable, i, MAKE_RGB((i & 2) ? 0xff : 0x00, (i & 4) ? 0xff : 0x00, (i & 1) ? 0xff : 0x00));
+	
+	for (i = 0; i < 256; i++)
 	{
-		palette_set_color_rgb(machine, i, (i & 2) ? 0xff : 0x00, (i & 4) ? 0xff : 0x00, (i & 1) ? 0xff : 0x00);
+		colortable_entry_set_value(machine->colortable, i*2, i & 7);
+        	colortable_entry_set_value(machine->colortable, i*2+1, (i >> 4) & 7);
 	}
 }
 
 
 VIDEO_UPDATE( mz700 )
 {
-    int offs;
+	int offs;
 
-    fillbitmap(bitmap, get_black_pen(screen->machine), cliprect);
+	fillbitmap(bitmap, get_black_pen(screen->machine), cliprect);
 
 	for(offs = 0; offs < 40*25; offs++)
 	{
 		int sx, sy, code, color;
-
-        sy = (offs / 40) * 8;
+	        sy = (offs / 40) * 8;
 		sx = (offs % 40) * 8;
-		code = videoram[offs];
 		color = colorram[offs];
-		code |= (color & 0x80) << 1;
-
-        drawgfx(bitmap, screen->machine->gfx[0], code, color, 0, 0, sx, sy,
-			cliprect, TRANSPARENCY_NONE, 0);
+		code = videoram[offs] | (color & 0x80) << 1;
+		drawgfx(bitmap, screen->machine->gfx[0], code, color, 0, 0, sx, sy, cliprect, TRANSPARENCY_NONE, 0);
 	}
 
 	return 0;
