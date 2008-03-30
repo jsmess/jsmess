fdeb812854bb302d164a2c47bdcb251d304748d1
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-03-30 12:45:44 +0000

    [PC_CGA] Moved some defines to the header file. Added a method to set the character rom base address.
    git-svn-id: svn://messdev.no-ip.org/mess@2158 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/pc_cga.c
+++ src/mess/video/pc_cga.c
@@ -48,7 +48,7 @@
 ***************************************************************************/
 
 #include "driver.h"
-#include "pc_cga.h"
+#include "video/pc_cga.h"
 #include "video/mc6845.h"
 #include "video/pc_video.h"
 #include "memconv.h"
@@ -128,28 +128,6 @@ MACHINE_DRIVER_END
 	Local variables and declarations
 
 ***************************************************************************/
-
-/* CGA dipswitch settings. These have to be kept consistent over all systems
- * that use CGA. */
-
-#define CGA_FONT_0				0
-#define CGA_FONT_1				1
-#define CGA_FONT_2				2
-#define CGA_FONT_3				3
-
-#define CGA_MONITOR		(readinputport(20)&0x1C)
-#define CGA_MONITOR_RGB			0x00	/* Colour RGB */
-#define CGA_MONITOR_MONO		0x04	/* Greyscale RGB */
-#define CGA_MONITOR_COMPOSITE	0x08	/* Colour composite */
-#define CGA_MONITOR_TELEVISION	0x0C	/* Television */
-#define CGA_MONITOR_LCD			0x10	/* LCD, eg PPC512 */
-
-#define CGA_CHIPSET		(readinputport(20)&0xE0)
-#define CGA_CHIPSET_IBM			0x00	/* Original IBM CGA */
-#define CGA_CHIPSET_PC1512		0x20	/* PC1512 CGA subset */
-#define CGA_CHIPSET_PC200		0x40	/* PC200 in CGA mode */
-#define CGA_CHIPSET_ATI			0x60	/* ATI (supports Plantronics) */
-#define CGA_CHIPSET_PARADISE	0x80	/* Paradise (used in PC1640) */
 
 static struct
 {
@@ -345,6 +323,11 @@ static PALETTE_INIT( pc_cga )
 			}
 		}
 	}
+}
+
+
+void pcvideo_cga_set_character_base( UINT8 *character_base ) {
+	cga.chr_gen = character_base;
 }
 
 
--- src/mess/video/pc_cga.h
+++ src/mess/video/pc_cga.h
@@ -12,6 +12,9 @@ extern const unsigned char cga_palette[CGA_PALETTE_SETS * 16][3];
 
 MACHINE_DRIVER_EXTERN( pcvideo_cga );
 
+/* Set the base address for the character generation */
+void pcvideo_cga_set_character_base( UINT8 *character_base );
+
 /* has a special 640x200 in 16 color mode, 4 banks at 0xb8000 */
 MACHINE_DRIVER_EXTERN( pcvideo_pc1512 );
 
@@ -25,4 +28,19 @@ WRITE16_HANDLER( pc1512_videoram16le_w );
 /* Dipswitch for font selection */
 #define CGA_FONT		(readinputport(20)&3)
 
+/* Dipswitch for monitor selection */
+#define CGA_MONITOR     (readinputport(20)&0x1C)
+#define CGA_MONITOR_RGB         0x00    /* Colour RGB */
+#define CGA_MONITOR_MONO        0x04    /* Greyscale RGB */
+#define CGA_MONITOR_COMPOSITE   0x08    /* Colour composite */
+#define CGA_MONITOR_TELEVISION  0x0C    /* Television */
+#define CGA_MONITOR_LCD         0x10    /* LCD, eg PPC512 */
+
+/* Dipswitch for chipset selection */
+#define CGA_CHIPSET     (readinputport(20)&0xE0)
+#define CGA_CHIPSET_IBM         0x00    /* Original IBM CGA */
+#define CGA_CHIPSET_PC1512      0x20    /* PC1512 CGA subset */
+#define CGA_CHIPSET_PC200       0x40    /* PC200 in CGA mode */
+#define CGA_CHIPSET_ATI         0x60    /* ATI (supports Plantronics) */
+#define CGA_CHIPSET_PARADISE    0x80    /* Paradise (used in PC1640) */
 
