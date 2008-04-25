a604214723c9ffc490fd880ab17032a14062eb98
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-04-25 11:39:24 +0000

    [PC_VIDEO] Fixed wrong parameter order in 16le_w function.
    git-svn-id: svn://messdev.no-ip.org/mess@2467 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/pc_video.c
+++ src/mess/video/pc_video.c
@@ -131,7 +131,7 @@ WRITE8_HANDLER ( pc_video_videoram_w )
 }
 
 
-WRITE16_HANDLER( pc_video_videoram16le_w ) { write16le_with_write8_handler(pc_video_videoram_w, machine, offset, mem_mask, data); }
+WRITE16_HANDLER( pc_video_videoram16le_w ) { write16le_with_write8_handler(pc_video_videoram_w, machine, offset, data, mem_mask); }
 
 WRITE32_HANDLER( pc_video_videoram32_w )
 {
