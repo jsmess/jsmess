b7e7898f22e68fe3e70c9a6cd9fb08fc07d1a8ed
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2008-05-11 03:43:37 +0000

    [SDLMESS] Improved clipboard paste for OS X [Tim Lindner]
    git-svn-id: svn://messdev.no-ip.org/mess@2601 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/osd/sdl/sdlmess.c
+++ src/mess/osd/sdl/sdlmess.c
@@ -202,7 +202,7 @@ file_error osd_rmdir(const char *dir)
 
 char *osd_get_clipboard_text(void)
 {
-	char *result = NULL; /* core expects a malloced buffer of uft8 data */
+	char *result = NULL; /* core expects a malloced C string of uft8 data */
 
 	PasteboardRef pasteboard_ref;
 	OSStatus err;
@@ -213,7 +213,7 @@ char *osd_get_clipboard_text(void)
 	CFIndex flavor_index;
 	ItemCount item_count;
 	UInt32 item_index;
-	Boolean	success = 0;
+	Boolean	success = false;
 	
 	err = PasteboardCreate(kPasteboardClipboard, &pasteboard_ref);
 
@@ -239,29 +239,45 @@ char *osd_get_clipboard_text(void)
 					{
 						CFStringRef flavor_type;
 						CFDataRef flavor_data;
+						CFStringEncoding encoding;
+						CFStringRef string_ref;
+						CFDataRef data_ref;
+						CFIndex length;
+						CFRange range;
 						
 						flavor_type = (CFStringRef)CFArrayGetValueAtIndex(flavor_type_array, flavor_index);
 						
-						if (UTTypeConformsTo(flavor_type, CFSTR("public.utf16-plain-text")))
+						if (UTTypeConformsTo (flavor_type, kUTTypeUTF16PlainText))
+							encoding = kCFStringEncodingUTF16;
+						else if (UTTypeConformsTo (flavor_type, kUTTypeUTF8PlainText))
+							encoding = kCFStringEncodingUTF8;
+						else if (UTTypeConformsTo (flavor_type, kUTTypePlainText))
+							encoding = kCFStringEncodingMacRoman;
+						else
+							continue;
+
+						err = PasteboardCopyItemFlavorData(pasteboard_ref, item_id, flavor_type, &flavor_data);
+						
+						if( !err )
 						{
-							CFStringRef string_ref;
-							CFIndex length;
+							string_ref = CFStringCreateFromExternalRepresentation (kCFAllocatorDefault, flavor_data, encoding);
+							data_ref = CFStringCreateExternalRepresentation (kCFAllocatorDefault, string_ref, kCFStringEncodingUTF8, '?');
 							
-							err = PasteboardCopyItemFlavorData(pasteboard_ref, item_id, flavor_type, &flavor_data);
-							string_ref = CFStringCreateWithBytes(NULL, CFDataGetBytePtr(flavor_data), CFDataGetLength(flavor_data), kCFStringEncodingUTF16, false );
-							length = CFStringGetLength(string_ref) * 2;
-							result = malloc(length);
-							success = CFStringGetCString(string_ref, result, length, kCFStringEncodingUTF8);
+							length = CFDataGetLength (data_ref);
+							range = CFRangeMake (0,length);
 							
-							if (!success)
+							result = malloc (length+1);
+							if (result != NULL)
 							{
-								free(result);
-								result = NULL;
+								CFDataGetBytes (data_ref, range, (unsigned char *)result);
+								result[length] = 0;
+								success = true;
+								break;
 							}
-				
+
+							CFRelease(data_ref);
 							CFRelease(string_ref);
 							CFRelease(flavor_data);
-							break;
 						}
 					}
 				
