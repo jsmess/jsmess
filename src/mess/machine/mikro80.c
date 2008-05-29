e032687a28e70334deec44f7e5fadf27f3ca85a7
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2008-05-29 13:10:59 +0000

    Some input port fixes
    git-svn-id: svn://messdev.no-ip.org/mess@2764 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/mikro80.c
+++ src/mess/machine/mikro80.c
@@ -1,9 +1,9 @@
 /***************************************************************************
 
-		Mikro-80 machine driver by Miodrag Milanovic
+        Mikro-80 machine driver by Miodrag Milanovic
 
-		10/03/2008 Preliminary driver.
-		     
+        10/03/2008 Preliminary driver.
+
 ****************************************************************************/
 
 
@@ -12,40 +12,40 @@
 #include "devices/cassette.h"
 #include "machine/8255ppi.h"
 
-static int mikro80_keyboard_line;
+static int mikro80_keyboard_mask;
 
 /* Driver initialization */
 DRIVER_INIT(mikro80)
 {
 	/* set initialy ROM to be visible on first bank */
-	UINT8 *RAM = memory_region(REGION_CPU1);	
+	UINT8 *RAM = memory_region(REGION_CPU1);
 	memset(RAM,0x0000,0x0800); // make frist page empty by default
-  memory_configure_bank(1, 1, 2, RAM, 0x0000);
-	memory_configure_bank(1, 0, 2, RAM, 0xf800);	
+  	memory_configure_bank(1, 1, 2, RAM, 0x0000);
+	memory_configure_bank(1, 0, 2, RAM, 0xf800);
 }
 
 READ8_HANDLER (mikro80_8255_portb_r )
-{	
-	return input_port_read_indexed(machine, mikro80_keyboard_line);
+{
+	UINT8 key = 0xff;
+	if ((mikro80_keyboard_mask & 0x01)!=0) { key &= input_port_read_indexed(machine,0); }
+	if ((mikro80_keyboard_mask & 0x02)!=0) { key &= input_port_read_indexed(machine,1); }
+	if ((mikro80_keyboard_mask & 0x04)!=0) { key &= input_port_read_indexed(machine,2); }
+	if ((mikro80_keyboard_mask & 0x08)!=0) { key &= input_port_read_indexed(machine,3); }
+	if ((mikro80_keyboard_mask & 0x10)!=0) { key &= input_port_read_indexed(machine,4); }
+	if ((mikro80_keyboard_mask & 0x20)!=0) { key &= input_port_read_indexed(machine,5); }
+	if ((mikro80_keyboard_mask & 0x40)!=0) { key &= input_port_read_indexed(machine,6); }
+	if ((mikro80_keyboard_mask & 0x80)!=0) { key &= input_port_read_indexed(machine,7); }
+	return key;
 }
 
 READ8_HANDLER (mikro80_8255_portc_r )
 {
-	return input_port_read_indexed(machine, 8);	
+	return input_port_read_indexed(machine, 8);
 }
 
 WRITE8_HANDLER (mikro80_8255_porta_w )
-{	
-	switch (data ^ 0xff) {
-	  	case 0x01 : mikro80_keyboard_line = 0;break;
-	  	case 0x02 : mikro80_keyboard_line = 1;break;
-	  	case 0x04 : mikro80_keyboard_line = 2;break;
-	  	case 0x08 : mikro80_keyboard_line = 3;break;
-	  	case 0x10 : mikro80_keyboard_line = 4;break;
-	  	case 0x20 : mikro80_keyboard_line = 5;break;
-	  	case 0x40 : mikro80_keyboard_line = 6;break;
-	  	case 0x80 : mikro80_keyboard_line = 7;break;
-	}	
+{
+	mikro80_keyboard_mask = data ^ 0xff;
 }
 
 const ppi8255_interface mikro80_ppi8255_interface =
@@ -66,14 +66,14 @@ static TIMER_CALLBACK( mikro80_reset )
 MACHINE_RESET( mikro80 )
 {
 	timer_set(ATTOTIME_IN_USEC(10), NULL, 0, mikro80_reset);
-	memory_set_bank(1, 1);	
-	mikro80_keyboard_line = 0;
+	memory_set_bank(1, 1);
+	mikro80_keyboard_mask = 0;
 }
 
 
 READ8_DEVICE_HANDLER( mikro80_keyboard_r )
 {
-	return ppi8255_r(device, offset^0x03);	
+	return ppi8255_r(device, offset^0x03);
 }
 
 WRITE8_DEVICE_HANDLER( mikro80_keyboard_w )
@@ -84,15 +84,15 @@ WRITE8_DEVICE_HANDLER( mikro80_keyboard_w )
 
 WRITE8_HANDLER( mikro80_tape_w )
 {
-	cassette_output(image_from_devtype_and_index(IO_CASSETTE, 0),data & 0x01 ? 1 : -1);	
+	cassette_output(image_from_devtype_and_index(IO_CASSETTE, 0),data & 0x01 ? 1 : -1);
 }
 
 
 READ8_HANDLER( mikro80_tape_r )
 {
-	double level = cassette_input(image_from_devtype_and_index(IO_CASSETTE, 0));	 									 					
-	if (level <  0) { 
-		 	return 0x00; 
+	double level = cassette_input(image_from_devtype_and_index(IO_CASSETTE, 0));
+	if (level <  0) {
+		 	return 0x00;
  	}
-	return 0xff;	
+	return 0xff;
 }
