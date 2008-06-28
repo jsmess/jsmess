51e8385bf661aaf9eeb60f5d3b708fd837581511
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2008-06-28 18:10:45 +0000

    Fix GCC compile.
    git-svn-id: svn://messdev.no-ip.org/mess@2911 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/formats/ap_dsk35.c
+++ src/mess/formats/ap_dsk35.c
@@ -479,7 +479,7 @@ static floperr_t apple35_write_sector(floppy_image *floppy, int head, int track,
 static floperr_t apple35_read_sector_td(floppy_image *floppy, int head, int track, int sector, void *buffer, size_t buflen)
 {
 	floperr_t err;
-	UINT32 tag_offset;
+	UINT32 tag_offset = 0;
 
 	assert(buflen == 524);
 
