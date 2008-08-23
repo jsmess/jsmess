92e3238e016ccfc37c5aef90e634454880b88b5a
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-08-23 17:06:28 +0000

    Fixed SVI cassette format handling
    git-svn-id: svn://messdev.no-ip.org/mess@3093 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/formats/svi_cas.c
+++ src/mess/formats/svi_cas.c
@@ -1,11 +1,10 @@
-#include <string.h>
 #include "svi_cas.h"
 
-#define CAS_PERIOD_0		(40)
+#define CAS_PERIOD_0		(37)
 #define CAS_PERIOD_1		(18)
 #define CAS_HEADER_PERIODS (1600)
-#define CAS_EMPTY_SAMPLES (11025)
-#define ALLOCATE_BLOCK	  (1024*8)
+#define CAS_EMPTY_SAMPLES (24220)
+#define CAS_INIT_SAMPLES	(200)
 
 static const UINT8 CasHeader[17] =
 {
@@ -13,155 +12,159 @@ static const UINT8 CasHeader[17] =
 	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x7f
 };
 
-#define SMPLO	(INT16)0x8001
-#define SMPHI	(INT16)0x7fff
+#define SMPLO	-32768
+#define SMPHI	32767
 
-static int svi_cas_to_wav (const UINT8 *casdata, int caslen, INT16 **wavdata, int *wavlen)
+static int cas_size;
+
+/*******************************************************************
+   Generate samples for the tape image
+********************************************************************/
+static int svi_cas_fill_wave(INT16 *buffer, int sample_count, UINT8 *bytes)
 {
-	int cas_pos, samples_size, bit, samples_pos, size, n, i;
-	INT16 *samples, *nsamples;
-
-	if (caslen < 17) return 1;
-	if (memcmp (casdata, CasHeader, sizeof (CasHeader) ) ) return 1;
+	int cas_pos, samples_pos, n, i;
 
 	cas_pos = 17;
-	samples_size = ALLOCATE_BLOCK * 2;
-	samples = (INT16*) malloc (samples_size);
-	if (!samples)
-		return 2;
-
 	samples_pos = 0;
 
-	while (cas_pos < caslen)
+	/* write CAS_INIT_SAMPLES of silence */
+	n = CAS_INIT_SAMPLES; while (n--) buffer[samples_pos++] = 0;
+
+	while (samples_pos < sample_count && cas_pos < cas_size)
 	{
-		/* check memory for entire header (silence + header itself) */
-		size = CAS_PERIOD_0 * (CAS_HEADER_PERIODS + 1) +
-			   CAS_PERIOD_1 * (CAS_HEADER_PERIODS + 7) +
-				CAS_EMPTY_SAMPLES;
-
-		if ( (samples_pos + size) >= samples_size)
-		{
-			samples_size += size;
-			nsamples = (INT16*) realloc (samples, samples_size * 2);
-			if (!nsamples)
-				{
-				free (samples);
-				return 2;
-				}
-			else samples = nsamples;
-		}
-
-		/* write CAS_EMPTY_PERIODS of silence */
-		n = CAS_EMPTY_SAMPLES; while (n--) samples[samples_pos++] = SMPHI;
-
 		/* write CAS_HEADER_PERIODS of header */
 		for (i=0;i<CAS_HEADER_PERIODS;i++)
 		{
 			/* write a "0" */
 			n = !(i % 4) ? 21 : 18;
-			while (n--) samples[samples_pos++] = SMPHI;
-			n = 19; while (n--) samples[samples_pos++] = SMPLO;
+			while (n--) buffer[samples_pos++] = SMPLO;
+			n = 19; while (n--) buffer[samples_pos++] = SMPHI;
 			/* write a "1" */
-			n = 9; while (n--) samples[samples_pos++] = SMPHI;
-			n = 9; while (n--) samples[samples_pos++] = SMPLO;
+			n = 9; while (n--) buffer[samples_pos++] = SMPLO;
+			n = 9; while (n--) buffer[samples_pos++] = SMPHI;
 		}
 
-		/* write 0x7f */
+		/* write "0x7f" */
 		/* write a "0" */
-		n = 21; while (n--) samples[samples_pos++] = SMPHI;
-		n = 19; while (n--) samples[samples_pos++] = SMPLO;
+		n = 21; while (n--) buffer[samples_pos++] = SMPLO;
+		n = 19; while (n--) buffer[samples_pos++] = SMPHI;
 
 		for (i=0;i<7;i++)
 		{
 			/* write a "1" */
-			n = 9; while (n--) samples[samples_pos++] = SMPHI;
-			n = 9; while (n--) samples[samples_pos++] = SMPLO;
+			n = 9; while (n--) buffer[samples_pos++] = SMPLO;
+			n = 9; while (n--) buffer[samples_pos++] = SMPHI;
 		}
+
+		while (samples_pos < sample_count && cas_pos < cas_size)
+		{
+			n = 21; while (n--) buffer[samples_pos++] = SMPLO;
+			n = 19; while (n--) buffer[samples_pos++] = SMPHI;
+
+			for (i=0;i<8;i++)
+			{
+				int bit = (bytes[cas_pos] & (0x80 >> i) );
+
+				/* write this one bit */
+				if (bit)
+				{
+					/* write a "1" */
+					n = 9; while (n--) buffer[samples_pos++] = SMPLO;
+					n = 9; while (n--) buffer[samples_pos++] = SMPHI;
+				}
+				else
+				{
+					/* write a "0" */
+					n = 18; while (n--) buffer[samples_pos++] = SMPLO;
+					n = 19; while (n--) buffer[samples_pos++] = SMPHI;
+				}
+			}
+
+			cas_pos++;
+
+			/* check if we've hit a new header (or end of block) */
+			if ( (cas_pos + 17) < cas_size)
+			{
+				if (!memcmp (bytes + cas_pos, CasHeader, 17) )
+				{
+					cas_pos += 17;
+
+					/* end of block marker */
+					n = CAS_EMPTY_SAMPLES; while (n--) buffer[samples_pos++] = SMPHI;
+
+					break; /* falls back to loop above; plays header again */
+				}
+			}
+		}
+	}
+
+	/* end of block marker */
+	n = CAS_EMPTY_SAMPLES; while (n--) buffer[samples_pos++] = SMPHI;
+
+	return samples_pos;
+}
+
+
+/*******************************************************************
+   Calculate the number of samples needed for this tape image
+********************************************************************/
+static int svi_cas_to_wav_size(const UINT8 *casdata, int caslen)
+{
+	int cas_pos, samples_pos, size, i;
+
+	if (caslen < 17) return -1;
+	if (memcmp (casdata, CasHeader, sizeof (CasHeader) ) ) return -1;
+
+	cas_size = caslen;
+
+	cas_pos = 17;
+
+	samples_pos = CAS_INIT_SAMPLES;
+
+	while (cas_pos < caslen)
+	{
+		size = CAS_HEADER_PERIODS * ( CAS_PERIOD_0 + CAS_PERIOD_1 ) +
+			   ( CAS_HEADER_PERIODS / 4 ) * 3;
+
+		samples_pos += size;
+
+		samples_pos += 21 + 19 + 7 * CAS_PERIOD_1;
+
 		while (cas_pos < caslen)
 		{
+			samples_pos += 21;
+			samples_pos += 19;
+
+			for (i=0;i<8;i++)
+			{
+				int bit = (casdata[cas_pos] & (0x80 >> i) );
+
+				samples_pos += bit ? CAS_PERIOD_1 : CAS_PERIOD_0;
+			}
+
+			cas_pos++;
+
 			/* check if we've hit a new header (or end of block) */
 			if ( (cas_pos + 17) < caslen)
 			{
 				if (!memcmp (casdata + cas_pos, CasHeader, 17) )
 				{
 					cas_pos += 17;
+
+					/* end of block marker */
+					samples_pos += CAS_EMPTY_SAMPLES;
+
 					break; /* falls back to loop above; plays header again */
 				}
 			}
-
-			/* check if we've got enough memory for the next byte */
-			size = CAS_PERIOD_0 * 9;
-			if ( (samples_pos + size) >= samples_size)
-			{
-				samples_size += ALLOCATE_BLOCK;
-				nsamples = (INT16*) realloc (samples, samples_size * 2);
-				if (!nsamples)
-				{
-					free (samples);
-					return 2;
-				}
-				else samples = nsamples;
-			}
-
-			for (i=-1;i<8;i++)
-			{
-				if (i < 0) bit = 0;
-				else bit = (casdata[cas_pos] & (0x80 >> i) );
-
-				/* write this one bit */
-				if (bit)
-				{
-					/* write a "1" */
-					n = 9; while (n--) samples[samples_pos++] = SMPHI;
-					n = 9; while (n--) samples[samples_pos++] = SMPLO;
-				}
-				else
-				{
-					/* write a "0" */
-					n = (i < 0) ? 21 : 18;
-					while (n--) samples[samples_pos++] = SMPHI;
-					n = 19; while (n--) samples[samples_pos++] = SMPLO;
-				}
-			}
-
-			cas_pos++;
 		}
 	}
 
-	*wavdata = samples;
-	*wavlen = samples_pos;
+	/* end of block marker */
+	samples_pos += CAS_EMPTY_SAMPLES;
 
-	return 0;
+	return samples_pos;
 }
-
-
-
-static int svi_cas_fill_wave(INT16 *buffer, int length, UINT8 *bytes)
-{
-	INT16 *wavdata;
-	int wavlen;
-
-	if (svi_cas_to_wav(bytes, length, &wavdata, &wavlen))
-		return -1;
-
-	memcpy(buffer, wavdata, wavlen * 2);
-	free(wavdata);
-	return 0;
-}
-
-
-static int svi_cas_chunk_sample_calc(const UINT8 *bytes, int length)
-{
-	INT16 *wavdata;
-	int wavlen;
-
-	if (svi_cas_to_wav(bytes, length, &wavdata, &wavlen))
-		return -1;
-
-	free(wavdata);
-	return wavlen;
-}
-
 
 
 static const struct CassetteLegacyWaveFiller svi_legacy_fill_wave =
@@ -169,9 +172,9 @@ static const struct CassetteLegacyWaveFiller svi_legacy_fill_wave =
 	svi_cas_fill_wave,						/* fill_wave */
 	-1,										/* chunk_size */
 	0,										/* chunk_samples */
-	svi_cas_chunk_sample_calc,				/* chunk_sample_calc */
-	22050,									/* sample_frequency */
-	-1,										/* header_samples */
+	svi_cas_to_wav_size,					/* chunk_sample_calc */
+	44100,									/* sample_frequency */
+	0,										/* header_samples */
 	0										/* trailer_samples */
 };
 
