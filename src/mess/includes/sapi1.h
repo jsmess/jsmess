cb43d1a774e5d97047a2601700056762a7945b1d
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2008-09-15 13:54:09 +0000

    [SAPI1] Keyboard now works
    git-svn-id: svn://messdev.no-ip.org/mess@3269 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/includes/sapi1.h
+++ src/mess/includes/sapi1.h
@@ -13,6 +13,9 @@ extern DRIVER_INIT( sapi1 );
 extern MACHINE_START( sapi1 );
 extern MACHINE_RESET( sapi1 );
 
+extern READ8_HANDLER (sapi1_keyboard_r );
+extern WRITE8_HANDLER(sapi1_keyboard_w );
+
 /*----------- defined in video/sapi1.c -----------*/
 
 extern VIDEO_START( sapi1 );
--- src/mess/machine/sapi1.c
+++ src/mess/machine/sapi1.c
@@ -11,6 +11,7 @@
 #include "cpu/i8085/i8085.h"
 #include "includes/sapi1.h"
 
+UINT8 sapi1_keyboard_mask;
 
 /* Driver initialization */
 DRIVER_INIT(sapi1)
@@ -19,9 +20,25 @@ DRIVER_INIT(sapi1)
 
 MACHINE_RESET( sapi1 )
 {
+	sapi1_keyboard_mask = 0;
 }
 
 MACHINE_START(sapi1)
 {
+}
+
+READ8_HANDLER (sapi1_keyboard_r )
+{
+	UINT8 key = 0xff;
+	if ((sapi1_keyboard_mask & 0x01)!=0) { key &= input_port_read(machine,"LINE0"); }
+	if ((sapi1_keyboard_mask & 0x02)!=0) { key &= input_port_read(machine,"LINE1"); }
+	if ((sapi1_keyboard_mask & 0x04)!=0) { key &= input_port_read(machine,"LINE2"); }
+	if ((sapi1_keyboard_mask & 0x08)!=0) { key &= input_port_read(machine,"LINE3"); }
+	if ((sapi1_keyboard_mask & 0x10)!=0) { key &= input_port_read(machine,"LINE4"); }
+	return key;
+}
+WRITE8_HANDLER(sapi1_keyboard_w )
+{
+	sapi1_keyboard_mask = (data ^ 0xff ) & 0x1f;
 }
 
