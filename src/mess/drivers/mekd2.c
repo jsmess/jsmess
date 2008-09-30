8ba1bc69a97d18e674e8d522870860cbee0912f4
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2008-09-30 17:23:20 +0000

    [MEKD2] Added comment about layout of keys.
    git-svn-id: svn://messdev.no-ip.org/mess@3352 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mekd2.c
+++ src/mess/drivers/mekd2.c
@@ -101,6 +101,16 @@ ADDRESS_MAP_END
   - L : Load memory from magnetic tape
   - N : Trace one instruction
   - V : Set (and remove) breakpoints
+
+The keys are laid out as:
+
+  P L N V
+
+  7 8 9 A  M
+  4 5 6 B  E
+  1 2 3 C  R
+  0 F E D  G
+
  */
 static INPUT_PORTS_START( mekd2 )
 	PORT_START("KEY0")
