f2f5f26642cbffcdc70e27e80ccc0fb7e7afa375
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2008-12-26 15:23:16 +0000

    [SNES] Fixed cart
    git-svn-id: svn://messdev.no-ip.org/mess@3985 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/snes.c
+++ src/mess/drivers/snes.c
@@ -222,6 +222,8 @@ static MACHINE_DRIVER_START( snes )
 	MDRV_SOUND_CONFIG(snes_sound_interface)
 	MDRV_SOUND_ROUTE(0, "left", 1.00)
 	MDRV_SOUND_ROUTE(1, "right", 1.00)
+	
+	MDRV_CARTSLOT_ADD("cart", snes_cartslot)
 MACHINE_DRIVER_END
 
 static MACHINE_DRIVER_START( snespal )
