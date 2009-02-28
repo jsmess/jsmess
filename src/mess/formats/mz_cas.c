ce1eb6e434a90b776fd05a864ffa0f75de0c74cb
JoJo <JoJo@9849079f-6229-0410-a07f-84406ce305ba>
2009-02-28 15:58:42 +0000

    [MZ700] Recognition of alternate cassette extension
    git-svn-id: svn://messdev.no-ip.org/mess@4362 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/formats/mz_cas.c
+++ src/mess/formats/mz_cas.c
@@ -328,7 +328,7 @@ static casserr_t mz700_cas_load(cassette_image *cassette)
 
 static const struct CassetteFormat mz700_cas_format =
 {
-	"m12",
+	"m12,mzf",
 	mz700_cas_identify,
 	mz700_cas_load,
 	NULL
