68e6ff977fdb632c334789682150ca0c30e8191f
etabeta <etabeta@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-09 06:22:07 +0000

    Replace outdated doc references with updated ones
    git-svn-id: svn://messdev.no-ip.org/mess@4653 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/climess.c
+++ src/mess/climess.c
@@ -22,7 +22,7 @@ void mess_display_help(void)
 		"MESS v%s\n"
 		"Multi Emulator Super System - Copyright (C) 1997-2009 by the MESS Team\n"
 		"MESS is based on MAME Source code\n"
-		"Copyright (C) 1997-2008 by Nicola Salmoria and the MAME Team\n\n",
+		"Copyright (C) 1997-2009 by Nicola Salmoria and the MAME Team\n\n",
 		build_version);
 	mame_printf_info("%s\n", mess_disclaimer);
 	mame_printf_info(
@@ -33,7 +33,7 @@ void mess_display_help(void)
 		"        MESS -listdevices  for a full list of supported devices\n"
 		"        MESS -createconfig to create a mess.ini\n"
 		"\n"
-		"See mess.txt for help, readme.txt for options.\n");
+		"See config.txt and windows.txt for usage instructions.\n");
 }
 
 
