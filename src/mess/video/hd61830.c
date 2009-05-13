51e11de293f96ed77dacb2c626be7b779ca24e82
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-13 08:52:34 +0000

    [HD61830] Added instruction timing, reset behavior, set/clear bit instructions.
    git-svn-id: svn://messdev.no-ip.org/mess@4688 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/hd61830.c
+++ src/mess/video/hd61830.c
@@ -11,9 +11,6 @@
 
 	TODO:
 
-	- clean up code
-	- clear/set bit instructions
-	- instruction timing
 	- text mode
 
 */
@@ -29,19 +26,23 @@
 
 enum
 {
-	HD61380_REGISTER_MODE_CONTROL = 0,
-	HD61380_REGISTER_CHARACTER_PITCH,
-	HD61380_REGISTER_NUMBER_OF_CHARACTERS,
-	HD61380_REGISTER_NUMBER_OF_TIME_DIVISIONS,
-	HD61380_REGISTER_CURSOR_POSITION,
-	HD61380_REGISTER_DISPLAY_START_LOW = 8,
-	HD61380_REGISTER_DISPLAY_START_HIGH,
-	HD61380_REGISTER_CURSOR_ADDRESS_LOW,
-	HD61380_REGISTER_CURSOR_ADDRESS_HIGH,
-	HD61380_REGISTER_DISPLAY_DATA_WRITE,
-	HD61380_REGISTER_DISPLAY_DATA_READ,
-	HD61380_REGISTER_CLEAR_BIT,						/* not supported */
-	HD61380_REGISTER_SET_BIT						/* not supported */
+	HD61380_INSTRUCTION_MODE_CONTROL = 0,
+	HD61380_INSTRUCTION_CHARACTER_PITCH,
+	HD61380_INSTRUCTION_NUMBER_OF_CHARACTERS,
+	HD61380_INSTRUCTION_NUMBER_OF_TIME_DIVISIONS,
+	HD61380_INSTRUCTION_CURSOR_POSITION,
+	HD61380_INSTRUCTION_DISPLAY_START_LOW = 8,
+	HD61380_INSTRUCTION_DISPLAY_START_HIGH,
+	HD61380_INSTRUCTION_CURSOR_ADDRESS_LOW,
+	HD61380_INSTRUCTION_CURSOR_ADDRESS_HIGH,
+	HD61380_INSTRUCTION_DISPLAY_DATA_WRITE,
+	HD61380_INSTRUCTION_DISPLAY_DATA_READ,
+	HD61380_INSTRUCTION_CLEAR_BIT,					/* not supported */
+	HD61380_INSTRUCTION_SET_BIT						/* not supported */
+};
+
+static const HD61380_CYCLES[] = {
+	4, 4, 4, 4, 4, -1, -1, -1, 4, 4, 4, 4, 6, 6, 36, 36
 };
 
 #define HD61380_MODE_EXTERNAL_CG	0x01	/* not supported */
@@ -61,26 +62,26 @@ struct _hd61830_t
 	devcb_resolved_read8		in_rd_func;
 	devcb_resolved_write8		out_rd_func;
 
-	int bf;							/* busy flag, not supported */
+	int bf;						/* busy flag */
 
-	UINT8 ir;						/* instruction register */
-	UINT8 mcr;						/* mode control register */
-	UINT8 dir;						/* data input register */
-	UINT8 dor;						/* data output register */
-	UINT16 rac1;					/* refresh address counter 1 */
-	UINT16 rac2;					/* refresh address counter 2 */
-	UINT16 cac;						/* cursor address counter */
-	UINT8 lac;						/* line address counter */
+	UINT8 ir;					/* instruction register */
+	UINT8 mcr;					/* mode control register */
+	UINT8 dor;					/* data output register */
 
-	UINT16 dsa;						/* display start address */
+	UINT16 dsa;					/* display start address */
+	UINT16 cac;					/* cursor address counter */
 
-	int vp;							/* vertical character pitch */
-	int hp;							/* horizontal character pitch */
-	int hn;							/* horizontal number of characters */
-	int nx;							/* number of time divisions */
-	int cp;							/* cursor position */
+	int vp;						/* vertical character pitch */
+	int hp;						/* horizontal character pitch */
+	int hn;						/* horizontal number of characters */
+	int nx;						/* number of time divisions */
+	int cp;						/* cursor position */
 
-	const device_config *screen;	/* screen */
+	/* devices */
+	const device_config *screen;
+
+	/* timers */
+	emu_timer *busy_timer;
 };
 
 /***************************************************************************
@@ -106,12 +107,41 @@ INLINE const hd61830_interface *get_interface(const device_config *device)
 ***************************************************************************/
 
 /*-------------------------------------------------
+    TIMER_CALLBACK( busy_tick )
+-------------------------------------------------*/
+
+static TIMER_CALLBACK( busy_tick )
+{
+	const device_config *device = ptr;
+	hd61830_t *hd61830 = get_safe_token(device);
+
+	/* clear busy flag */
+	hd61830->bf = 0;
+}
+
+/*-------------------------------------------------
+    set_busy_flag - set busy flag and arm timer
+					to clear it later
+-------------------------------------------------*/
+
+static void set_busy_flag(hd61830_t *hd61830, int period)
+{
+	/* set busy flag */
+	hd61830->bf = 1;
+
+	/* adjust busy timer */
+	timer_adjust_oneshot(hd61830->busy_timer, ATTOTIME_IN_USEC(period), 0);
+}
+
+/*-------------------------------------------------
     hd61830_status_r - status read
 -------------------------------------------------*/
 
 READ8_DEVICE_HANDLER( hd61830_status_r )
 {
 	hd61830_t *hd61830 = get_safe_token(device);
+
+	if (LOG) logerror("HD61380 '%s' Status Read: %s\n", device->tag, hd61830->bf ? "busy" : "ready");
 
 	return hd61830->bf << 7;
 }
@@ -156,7 +186,7 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 
 	switch (hd61830->ir)
 	{
-	case HD61380_REGISTER_MODE_CONTROL:
+	case HD61380_INSTRUCTION_MODE_CONTROL:
 		hd61830->mcr = data;
 
 		if (LOG)
@@ -170,7 +200,7 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 		}
 		break;
 	
-	case HD61380_REGISTER_CHARACTER_PITCH:
+	case HD61380_INSTRUCTION_CHARACTER_PITCH:
 		hd61830->hp = (data & 0x07) + 1;
 		hd61830->vp = (data >> 4) + 1;
 
@@ -178,40 +208,40 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 		if (LOG) logerror("HD61380 '%s' Vertical Character Pitch: %u\n", device->tag, hd61830->vp);
 		break;
 
-	case HD61380_REGISTER_NUMBER_OF_CHARACTERS:
+	case HD61380_INSTRUCTION_NUMBER_OF_CHARACTERS:
 		hd61830->hn = (data & 0x7f) + 1;
 
 		if (LOG) logerror("HD61380 '%s' Number of Characters: %u\n", device->tag, hd61830->hn);
 		break;
 
-	case HD61380_REGISTER_NUMBER_OF_TIME_DIVISIONS:
+	case HD61380_INSTRUCTION_NUMBER_OF_TIME_DIVISIONS:
 		hd61830->nx = (data & 0x7f) + 1;
 
 		if (LOG) logerror("HD61380 '%s' Number of Time Divisions: %u\n", device->tag, hd61830->nx);
 		break;
 
-	case HD61380_REGISTER_CURSOR_POSITION:
+	case HD61380_INSTRUCTION_CURSOR_POSITION:
 		hd61830->cp = (data & 0x7f) + 1;
 
 		if (LOG) logerror("HD61380 '%s' Cursor Position: %u\n", device->tag, hd61830->cp);
 		break;
 
-	case HD61380_REGISTER_DISPLAY_START_LOW:
+	case HD61380_INSTRUCTION_DISPLAY_START_LOW:
 		hd61830->dsa = (hd61830->dsa & 0xff00) | data;
 
 		if (LOG) logerror("HD61380 '%s' Display Start Address Low %04x\n", device->tag, hd61830->dsa);
 		break;
 
-	case HD61380_REGISTER_DISPLAY_START_HIGH:
+	case HD61380_INSTRUCTION_DISPLAY_START_HIGH:
 		hd61830->dsa = (data << 8) | (hd61830->dsa & 0xff);
 
 		if (LOG) logerror("HD61380 '%s' Display Start Address High %04x\n", device->tag, hd61830->dsa);
 		break;
 
-	case HD61380_REGISTER_CURSOR_ADDRESS_LOW:
+	case HD61380_INSTRUCTION_CURSOR_ADDRESS_LOW:
 		if (BIT(hd61830->cac, 7) && !BIT(data, 7))
 		{
-			hd61830->cac = ((((hd61830->cac & 0xff00) >> 8) + 1) << 8) | data;
+			hd61830->cac = (((hd61830->cac >> 8) + 1) << 8) | data;
 		}
 		else
 		{
@@ -221,13 +251,13 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 		if (LOG) logerror("HD61380 '%s' Cursor Address Low %02x: %04x\n", device->tag, data, hd61830->cac);
 		break;
 
-	case HD61380_REGISTER_CURSOR_ADDRESS_HIGH:
+	case HD61380_INSTRUCTION_CURSOR_ADDRESS_HIGH:
 		hd61830->cac = (data << 8) | (hd61830->cac & 0xff);
 
 		if (LOG) logerror("HD61380 '%s' Cursor Address High %02x: %04x\n", device->tag, data, hd61830->cac);
 		break;
 
-	case HD61380_REGISTER_DISPLAY_DATA_WRITE:
+	case HD61380_INSTRUCTION_DISPLAY_DATA_WRITE:
 		devcb_call_write8(&hd61830->out_rd_func, hd61830->cac, data);
 
 		if (LOG) logerror("HD61380 '%s' Display Data Write %02x -> %04x row %u col %u\n", device->tag, data, hd61830->cac, hd61830->cac / 40, hd61830->cac % 40);
@@ -235,16 +265,83 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 		hd61830->cac++;
 		break;
 
-	case HD61380_REGISTER_CLEAR_BIT:
-		logerror("HD61380 '%s' Clear Bit not supported!\n", device->tag);
+	case HD61380_INSTRUCTION_CLEAR_BIT:
+		{
+		int nb = data & 0x07;
+		UINT8 data = devcb_call_read8(&hd61830->in_rd_func, hd61830->cac);
+
+		data &= ~(2 << nb);
+
+		if (LOG) logerror("HD61380 '%s' Clear Bit %u at %04x\n", device->tag, nb + 1, hd61830->cac);
+
+		devcb_call_write8(&hd61830->out_rd_func, hd61830->cac, data);
+
+		hd61830->cac++;
+		}
 		break;
 
-	case HD61380_REGISTER_SET_BIT:
-		logerror("HD61380 '%s' Set Bit not supported!\n", device->tag);
+	case HD61380_INSTRUCTION_SET_BIT:
+		{
+		int nb = data & 0x07;
+		UINT8 data = devcb_call_read8(&hd61830->in_rd_func, hd61830->cac);
+
+		data |= 2 << nb;
+
+		if (LOG) logerror("HD61380 '%s' Set Bit %u at %04x\n", device->tag, nb + 1, hd61830->cac);
+
+		devcb_call_write8(&hd61830->out_rd_func, hd61830->cac, data);
+
+		hd61830->cac++;
+		}
 		break;
 
 	default:
 		logerror("HD61830 '%s' Illegal Instruction %02x!\n", device->tag, hd61830->ir);
+		return;
+	}
+
+	/* burn cycles */
+	set_busy_flag(hd61830, HD61380_CYCLES[hd61830->ir]);
+}
+
+/*-------------------------------------------------
+    draw_scanline - draw one scanline
+-------------------------------------------------*/
+
+static void draw_scanline(hd61830_t *hd61830, bitmap_t *bitmap, const rectangle *cliprect, int y, UINT16 ra)
+{
+	int sx, x;
+
+	for (sx = 0; sx < hd61830->hn; sx++)
+	{
+		UINT8 data = devcb_call_read8(&hd61830->in_rd_func, ra++);
+
+		for (x = 0; x < hd61830->hp; x++)
+		{
+			*BITMAP_ADDR16(bitmap, y, (sx * hd61830->hp) + x) = BIT(data, 0);
+			data >>= 1;
+		}
+	}
+}
+
+/*-------------------------------------------------
+    update_graphics - draw graphics mode screen
+-------------------------------------------------*/
+
+static void update_graphics(hd61830_t *hd61830, bitmap_t *bitmap, const rectangle *cliprect)
+{
+	int y;
+
+	for (y = 0; y < hd61830->nx; y++)
+	{
+		UINT16 rac1 = hd61830->dsa + (y * hd61830->hn);
+		UINT16 rac2 = rac1 + (hd61830->nx * hd61830->hn);
+
+		/* draw upper half scanline */
+		draw_scanline(hd61830, bitmap, cliprect, y, rac1);
+
+		/* draw lower half scanline */
+		draw_scanline(hd61830, bitmap, cliprect, y + hd61830->nx, rac2);
 	}
 }
 
@@ -255,21 +352,10 @@ WRITE8_DEVICE_HANDLER( hd61830_data_w )
 void hd61830_update(const device_config *device, bitmap_t *bitmap, const rectangle *cliprect)
 {
 	hd61830_t *hd61830 = get_safe_token(device);
-	int y, sx, x;
-	UINT16 lac = hd61830->dsa;
 
-	for (y = 0; y < (hd61830->nx * 2); y++)
+	if (hd61830->mcr & HD61380_MODE_GRAPHIC)
 	{
-		for (sx = 0; sx < hd61830->hn; sx++)
-		{
-			UINT8 data = devcb_call_read8(&hd61830->in_rd_func, lac++);
-
-			for (x = 0; x < hd61830->hp; x++)
-			{
-				*BITMAP_ADDR16(bitmap, y, (sx * hd61830->hp) + x) = BIT(data, 0);
-				data >>= 1;
-			}
-		}
+		update_graphics(hd61830, bitmap, cliprect);
 	}
 }
 
@@ -290,16 +376,15 @@ static DEVICE_START( hd61830 )
 	hd61830->screen = devtag_get_device(device->machine, intf->screen_tag);
 	assert(hd61830->screen != NULL);
 
+	/* create the busy timer */
+	hd61830->busy_timer = timer_alloc(device->machine, busy_tick, (void *)device);
+
 	/* register for state saving */
 	state_save_register_device_item(device, 0, hd61830->bf);
 	state_save_register_device_item(device, 0, hd61830->ir);
 	state_save_register_device_item(device, 0, hd61830->mcr);
-	state_save_register_device_item(device, 0, hd61830->dir);
 	state_save_register_device_item(device, 0, hd61830->dor);
-	state_save_register_device_item(device, 0, hd61830->rac1);
-	state_save_register_device_item(device, 0, hd61830->rac2);
 	state_save_register_device_item(device, 0, hd61830->cac);
-	state_save_register_device_item(device, 0, hd61830->lac);
 	state_save_register_device_item(device, 0, hd61830->dsa);
 	state_save_register_device_item(device, 0, hd61830->vp);
 	state_save_register_device_item(device, 0, hd61830->hp);
@@ -314,8 +399,10 @@ static DEVICE_START( hd61830 )
 
 static DEVICE_RESET( hd61830 )
 {
-	//hd61830_t *hd61830 = get_safe_token(device);
+	hd61830_t *hd61830 = get_safe_token(device);
 
+	hd61830->mcr = hd61830->mcr & ~(HD61380_MODE_MASTER | HD61380_MODE_DISPLAY_ON);
+	hd61830->hp = 6;
 }
 
 /*-------------------------------------------------
