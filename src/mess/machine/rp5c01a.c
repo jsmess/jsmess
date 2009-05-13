46ad7d398e92abbba91eb682e21fc6c36b2d144b
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-13 11:31:43 +0000

    [KYOCERA] Implemented trsm200 RTC.
    git-svn-id: svn://messdev.no-ip.org/mess@4692 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/rp5c01a.c
+++ src/mess/machine/rp5c01a.c
@@ -11,7 +11,12 @@
 
 	TODO:
 
-	- everything
+	- divide frequencies from device clock
+	- mask out unused bits in register writes
+	- clock adjust
+	- 12 hour clock
+	- leap year counter
+	- timer reset
 
 */
 
@@ -22,7 +27,45 @@
     PARAMETERS
 ***************************************************************************/
 
-#define LOG 1
+#define LOG 0
+
+enum
+{
+	RP5C01A_REGISTER_1_SECOND = 0,
+	RP5C01A_REGISTER_10_SECOND,
+	RP5C01A_REGISTER_1_MINUTE,
+	RP5C01A_REGISTER_10_MINUTE,
+	RP5C01A_REGISTER_1_HOUR,
+	RP5C01A_REGISTER_10_HOUR,
+	RP5C01A_REGISTER_DAY_OF_THE_WEEK,
+	RP5C01A_REGISTER_1_DAY,
+	RP5C01A_REGISTER_10_DAY,
+	RP5C01A_REGISTER_1_MONTH,
+	RP5C01A_REGISTER_10_MONTH, RP5C01A_REGISTER_12_24_SELECT = RP5C01A_REGISTER_10_MONTH,
+	RP5C01A_REGISTER_1_YEAR, RP5C01A_REGISTER_LEAP_YEAR = RP5C01A_REGISTER_1_YEAR,
+	RP5C01A_REGISTER_10_YEAR,
+	RP5C01A_REGISTER_MODE,
+	RP5C01A_REGISTER_TEST,
+	RP5C01A_REGISTER_RESET
+};
+
+#define RP5C01A_MODE_TIMER_EN		0x08
+#define RP5C01A_MODE_ALARM_EN		0x04
+#define RP5C01A_MODE_MASK			0x03
+#define RP5C01A_MODE_RAM_BLOCK11	0x03
+#define RP5C01A_MODE_RAM_BLOCK10	0x02
+#define RP5C01A_MODE_ALARM			0x01
+#define RP5C01A_MODE_TIME			0x00
+
+#define RP5C01A_TEST_4				0x08
+#define RP5C01A_TEST_3				0x04
+#define RP5C01A_TEST_2				0x02
+#define RP5C01A_TEST_1				0x01
+
+#define RP5C01A_RESET_ALARM			0x08
+#define RP5C01A_RESET_TIMER			0x04
+#define RP5C01A_RESET_16_HZ			0x02
+#define RP5C01A_RESET_1_HZ			0x01
 
 /***************************************************************************
     TYPE DEFINITIONS
@@ -32,9 +75,14 @@ typedef struct _rp5c01a_t rp5c01a_t;
 struct _rp5c01a_t
 {
 	devcb_resolved_write_line	out_alarm_func;
+
+	UINT8 reg[4][13];			/* clock registers */
+
+	UINT8 mode;					/* mode register */
     
 	/* timers */
 	emu_timer *clock_timer;
+	emu_timer *alarm_timer;
 };
 
 /***************************************************************************
@@ -56,16 +104,6 @@ INLINE const rp5c01a_interface *get_interface(const device_config *device)
 	return (const rp5c01a_interface *) device->static_config;
 }
 
-INLINE UINT8 convert_to_bcd(int val)
-{
-	return ((val / 10) << 4) | (val % 10);
-}
-
-INLINE int bcd_to_integer(UINT8 val)
-{
-	return (((val & 0xf0) >> 4) * 10) + (val & 0x0f);
-}
-
 /***************************************************************************
     IMPLEMENTATION
 ***************************************************************************/
@@ -76,6 +114,8 @@ INLINE int bcd_to_integer(UINT8 val)
 
 WRITE_LINE_DEVICE_HANDLER( rp5c01a_adj_w )
 {
+//	rp5c01a_t *rp5c01a = get_safe_token(device);
+
 }
 
 /*-------------------------------------------------
@@ -84,7 +124,27 @@ WRITE_LINE_DEVICE_HANDLER( rp5c01a_adj_w )
 
 READ8_DEVICE_HANDLER( rp5c01a_r )
 {
-	return 0;
+	rp5c01a_t *rp5c01a = get_safe_token(device);
+
+	UINT8 data = 0;
+
+	switch (offset & 0x0f)
+	{
+	case RP5C01A_REGISTER_MODE:
+		data = rp5c01a->mode;
+		break;
+
+	case RP5C01A_REGISTER_TEST:
+	case RP5C01A_REGISTER_RESET:
+		/* write only */
+		break;
+
+	default:
+		data = rp5c01a->reg[rp5c01a->mode & RP5C01A_MODE_MASK][offset];
+		break;
+	}
+
+	return data;
 }
 
 /*-------------------------------------------------
@@ -93,6 +153,151 @@ READ8_DEVICE_HANDLER( rp5c01a_r )
 
 WRITE8_DEVICE_HANDLER( rp5c01a_w )
 {
+	rp5c01a_t *rp5c01a = get_safe_token(device);
+
+	int mode = rp5c01a->mode & RP5C01A_MODE_MASK;
+
+	switch (offset & 0x0f)
+	{
+	case RP5C01A_REGISTER_MODE:
+		rp5c01a->mode = data;
+
+		timer_enable(rp5c01a->clock_timer, data & RP5C01A_MODE_TIMER_EN);
+
+		if (LOG)
+		{
+			logerror("RP5C01A '%s' Mode %u\n", device->tag, data & RP5C01A_MODE_MASK);
+			logerror("RP5C01A '%s' Timer %s\n", device->tag, (data & RP5C01A_MODE_TIMER_EN ) ? "enabled" : "disabled");
+			logerror("RP5C01A '%s' Alarm %s\n", device->tag, (data & RP5C01A_MODE_ALARM_EN ) ? "enabled" : "disabled");
+		}
+		break;
+
+	case RP5C01A_REGISTER_TEST:
+		if (LOG) logerror("RP5C01A '%s' Test %u not supported!\n", device->tag, data);
+		break;
+
+	case RP5C01A_REGISTER_RESET:
+		if (data & RP5C01A_RESET_ALARM)
+		{
+			int i;
+
+			/* reset alarm registers */
+			for (i = RP5C01A_REGISTER_1_MINUTE; i < RP5C01A_REGISTER_1_MONTH; i++)
+			{
+				rp5c01a->reg[RP5C01A_MODE_ALARM][i] = 0;
+			}
+		}
+
+		timer_enable(rp5c01a->alarm_timer, !(data & RP5C01A_RESET_16_HZ));
+
+		if (LOG)
+		{
+			if (data & RP5C01A_RESET_ALARM) logerror("RP5C01A '%s' Alarm Reset\n", device->tag);
+			if (data & RP5C01A_RESET_TIMER) logerror("RP5C01A '%s' Timer Reset not supported!\n", device->tag);
+			logerror("RP5C01A '%s' 16Hz Signal %s\n", device->tag, (data & RP5C01A_RESET_16_HZ) ? "disabled" : "enabled");
+			logerror("RP5C01A '%s' 1Hz Signal %s\n", device->tag, (data & RP5C01A_RESET_1_HZ) ? "disabled" : "enabled");
+		}
+		break;
+
+	default:
+		rp5c01a->reg[mode][offset & 0x0f] = data;
+
+		if (LOG) logerror("RP5C01A '%s' Register %u Write %02x\n", device->tag, offset & 0x0f, data);
+		break;
+	}
+}
+
+/*-------------------------------------------------
+    advance_seconds - advance seconds counter
+-------------------------------------------------*/
+
+static void advance_seconds(rp5c01a_t *rp5c01a)
+{
+	int days_per_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	int seconds = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_SECOND] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_SECOND];
+	int minutes = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_MINUTE] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_MINUTE];
+	int hours = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_HOUR] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_HOUR];
+	int days = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_DAY] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_DAY];
+	int month = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_MONTH] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_MONTH];
+	int year = (rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_YEAR] * 10) + rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_YEAR];
+	int day_of_week = rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_DAY_OF_THE_WEEK];
+	int i;
+
+	seconds++;
+
+	if (seconds > 59)
+	{
+		seconds = 0;
+		minutes++;
+	}
+
+	if (minutes > 59)
+	{
+		minutes = 0;
+		hours++;
+	}
+
+	if (hours > 23)
+	{
+		hours = 0;
+		days++;
+		day_of_week++;
+	}
+
+	if (day_of_week > 6)
+	{
+		day_of_week++;
+	}
+
+	if (days > days_per_month[month - 1])
+	{
+		days = 1;
+		month++;
+	}
+
+	if (month > 12)
+	{
+		month = 1;
+		year++;
+	}
+
+	if (year > 99)
+	{
+		year = 0;
+	}
+
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_SECOND] = seconds % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_SECOND] = seconds / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_MINUTE] = minutes % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_MINUTE] = minutes / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_HOUR] = hours % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_HOUR] = hours / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_DAY] = days % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_DAY] = days / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_MONTH] = month % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_MONTH] = month / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_1_YEAR] = year % 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_10_YEAR] = year / 10;
+	rp5c01a->reg[RP5C01A_MODE_TIME][RP5C01A_REGISTER_DAY_OF_THE_WEEK] = day_of_week;
+
+	if (!(rp5c01a->mode & RP5C01A_RESET_1_HZ))
+	{
+		/* pulse 1Hz alarm signal */
+		devcb_call_write_line(&rp5c01a->out_alarm_func, 0);
+		devcb_call_write_line(&rp5c01a->out_alarm_func, 1);
+	}
+	else if (rp5c01a->mode & RP5C01A_MODE_ALARM_EN)
+	{
+		for (i = RP5C01A_REGISTER_1_MINUTE; i < RP5C01A_REGISTER_1_MONTH; i++)
+		{
+			if (rp5c01a->reg[RP5C01A_MODE_ALARM][i] != rp5c01a->reg[RP5C01A_MODE_TIME][i]) return;
+		}
+
+		/* trigger alarm */
+		devcb_call_write_line(&rp5c01a->out_alarm_func, 0);
+		devcb_call_write_line(&rp5c01a->out_alarm_func, 1);
+	}
 }
 
 /*-------------------------------------------------
@@ -101,8 +306,23 @@ WRITE8_DEVICE_HANDLER( rp5c01a_w )
 
 static TIMER_CALLBACK( clock_tick )
 {
-//	const device_config *device = ptr;
-//	rp5c01a_t *rp5c01a = get_safe_token(device);
+	const device_config *device = ptr;
+	rp5c01a_t *rp5c01a = get_safe_token(device);
+
+	advance_seconds(rp5c01a);
+}
+
+/*-------------------------------------------------
+    TIMER_CALLBACK( alarm_tick )
+-------------------------------------------------*/
+
+static TIMER_CALLBACK( alarm_tick )
+{
+	const device_config *device = ptr;
+	rp5c01a_t *rp5c01a = get_safe_token(device);
+
+	devcb_call_write_line(&rp5c01a->out_alarm_func, 0);
+	devcb_call_write_line(&rp5c01a->out_alarm_func, 1);
 }
 
 /*-------------------------------------------------
@@ -121,25 +341,16 @@ static DEVICE_START( rp5c01a )
 	rp5c01a->clock_timer = timer_alloc(device->machine, clock_tick, (void *)device);
 	timer_adjust_periodic(rp5c01a->clock_timer, attotime_zero, 0, ATTOTIME_IN_HZ(1));
 
+	rp5c01a->alarm_timer = timer_alloc(device->machine, alarm_tick, (void *)device);
+	timer_adjust_periodic(rp5c01a->alarm_timer, attotime_zero, 0, ATTOTIME_IN_HZ(16));
+	timer_enable(rp5c01a->alarm_timer, 0);
+
 	/* register for state saving */
-//    state_save_register_global(device->machine, rp5c01a->);
-}
-
-static DEVICE_RESET( rp5c01a )
-{
-/*	rp5c01a_t *rp5c01a = get_safe_token(device);
-
-	mame_system_time curtime, *systime = &curtime;
-
-	mame_get_current_datetime(device->machine, &curtime);*/
-
-	/* HACK: load time counter from system time */
-/*	rp5c01a->time_counter[0] = convert_to_bcd(systime->local_time.second);
-	rp5c01a->time_counter[1] = convert_to_bcd(systime->local_time.minute);
-	rp5c01a->time_counter[2] = convert_to_bcd(systime->local_time.hour);
-	rp5c01a->time_counter[3] = convert_to_bcd(systime->local_time.mday);
-	rp5c01a->time_counter[4] = systime->local_time.weekday;
-	rp5c01a->time_counter[4] |= (systime->local_time.month + 1) << 4;*/
+    state_save_register_global(device->machine, rp5c01a->mode);
+    state_save_register_global_array(device->machine, rp5c01a->reg[0]);
+    state_save_register_global_array(device->machine, rp5c01a->reg[1]);
+    state_save_register_global_array(device->machine, rp5c01a->reg[2]);
+    state_save_register_global_array(device->machine, rp5c01a->reg[3]);
 }
 
 /*-------------------------------------------------
@@ -158,7 +369,7 @@ DEVICE_GET_INFO( rp5c01a )
 		/* --- the following bits of info are returned as pointers to data or functions --- */
 		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME(rp5c01a);	break;
 		case DEVINFO_FCT_STOP:							/* Nothing */								break;
-		case DEVINFO_FCT_RESET:							info->reset = DEVICE_RESET_NAME(rp5c01a);	break;
+		case DEVINFO_FCT_RESET:							/* Nothing */								break;
 
 		/* --- the following bits of info are returned as NULL-terminated strings --- */
 		case DEVINFO_STR_NAME:							strcpy(info->s, "Ricoh RP5C01A");			break;
