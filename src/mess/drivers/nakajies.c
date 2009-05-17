36233711fe4bdec9c817b47add10060e6d414131
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-17 17:03:13 +0000

    [Nakajima ES-family] Added some debugging code.
    git-svn-id: svn://messdev.no-ip.org/mess@4728 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/nakajies.c
+++ src/mess/drivers/nakajies.c
@@ -123,7 +123,7 @@ I/O Map:
 
 0060 - Irq enable/disable (?)
        0xff written at start of boot sequence
-       0x7f written just before enabling interrupts
+       0x7e written just before enabling interrupts
 
 0061 - unknown
        0xFE written in irq 0xFB handler
@@ -150,11 +150,60 @@ I/O Map:
        0xf0 written during boot sequence
 
 
+IRQ 0xF8:
+The handler clears the irq active bit and copies a word from 6D79 to 6D85 and
+from 6D7B to 6D87 then enters an endless loop (with interrupts disabled).
+Perhaps power on/off related??
+
+
+IRQ 0xF9: (T400: C049A)
+Purpose unknown. IRQ handler clear bit 0 of 6DA9.
+
+
+IRQ 0xFA: (T400: C04AE)
+Purpose unknown. Expects 6D4F to be set up properly. Enables irq 0xFD. Reads
+from input port 0xB0 and resets and sets bit 0 of output port 0x61.
+
+
+IRQ 0xFB: (T400: C04D1)
+Purpose unknown. Reads from input port 0xB0, sets bit 7 of 6D28 when
+non-zero data was read.
+
+
+IRQ 0xFC: (T400: C0550)
+Purpose unknown. Reads from input port 0xC1 and 0xC0 and possibly outputs
+something to output port 0xC1 depending on data read from 0xC1.
+
+
+IRQ 0xFD: (T400: C0724)
+Purpose unknown. Clears bit 3 of 70A5.
+
+
+IRQ 0xFE: (T400: C0738)
+Purpose unknown. Expects 6D4F to be set up properly. Enables irq 0xf9 or outputs
+a byte to ports 0x40 and 0x30. No endless loop.
+
+
+IRQ 0xFF:
+This does some simple processing and then enters an endless loop (with interrupts
+disabled). Perhaps power on/off related??
+
 ******************************************************************************/
 
 #include "driver.h"
 #include "cpu/nec/nec.h"
 #include "sound/speaker.h"
+
+
+typedef struct _nakajies_state nakajies_state;
+struct _nakajies_state {
+	/* Device lookups */
+	const device_config *cpu;
+
+	/* IRQ handling */
+	UINT8	irq_enabled;
+	UINT8	irq_active;
+};
 
 
 #define X301	19660000
@@ -178,6 +227,68 @@ static ADDRESS_MAP_START( nakajies250_map, ADDRESS_SPACE_PROGRAM, 8 )
 ADDRESS_MAP_END
 
 
+/*********************************************
+  IRQ Handling
+*********************************************/
+
+static void nakajies_update_irqs( running_machine *machine )
+{
+	nakajies_state *state = machine->driver_data;
+	UINT8 irq = state->irq_enabled & state->irq_active;
+	UINT8 vector = 0xff;
+
+	logerror("nakajies_update_irqs: irq_enabled = %02x, irq_active = %02x\n", state->irq_enabled, state->irq_active );
+
+	/* Assuming irq 0xFF has the highest priority and 0xF8 the lowest */
+	while( vector >= 0xf8 && ! ( irq & 0x01 ) )
+	{
+		irq >>= 1;
+		vector -= 1;
+	}
+
+	if ( vector >= 0xf8 )
+	{
+		cpu_set_input_line_and_vector( state->cpu, 0, ASSERT_LINE, vector );
+	}
+	else
+	{
+		cpu_set_input_line( state->cpu, 0, CLEAR_LINE );
+	} 
+}
+
+
+static READ8_HANDLER( irq_clear_r )
+{
+	return 0x00;
+}
+
+
+static WRITE8_HANDLER( irq_clear_w )
+{
+	nakajies_state *state = space->machine->driver_data;
+
+	state->irq_active &= ~data;
+	nakajies_update_irqs( space->machine );
+}
+
+
+static READ8_HANDLER( irq_enable_r )
+{
+	nakajies_state *state = space->machine->driver_data;
+
+	return state->irq_enabled;
+}
+
+
+static WRITE8_HANDLER( irq_enable_w )
+{
+	nakajies_state *state = space->machine->driver_data;
+
+	state->irq_enabled = data;
+	nakajies_update_irqs( space->machine );
+}
+
+
 static READ8_HANDLER( unk_a0_r )
 {
 	return 0xff;
@@ -185,18 +296,53 @@ static READ8_HANDLER( unk_a0_r )
 
 
 static ADDRESS_MAP_START( nakajies_io_map, ADDRESS_SPACE_IO, 8 )
+	AM_RANGE( 0x0060, 0x0060 ) AM_READWRITE( irq_enable_r, irq_enable_w )
+	AM_RANGE( 0x0090, 0x0090 ) AM_READWRITE( irq_clear_r, irq_clear_w )
 	AM_RANGE( 0x00a0, 0x00a0 ) AM_READ( unk_a0_r )
 ADDRESS_MAP_END
 
 
+static INPUT_CHANGED( trigger_irq )
+{
+	nakajies_state *state = field->port->machine->driver_data;
+	UINT8 irqs = input_port_read( field->port->machine, "debug" );
+
+	state->irq_active |= irqs;
+	nakajies_update_irqs( field->port->machine );
+}
+
+
 static INPUT_PORTS_START( nakajies )
+	PORT_START( "debug" )
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_1 ) PORT_NAME( "irq 0xff" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_2 ) PORT_NAME( "irq 0xfe" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_3 ) PORT_NAME( "irq 0xfd" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_4 ) PORT_NAME( "irq 0xfc" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_5 ) PORT_NAME( "irq 0xfb" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_6 ) PORT_NAME( "irq 0xfa" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_7 ) PORT_NAME( "irq 0xf9" ) PORT_CHANGED( trigger_irq, NULL )
+	PORT_BIT( 0x80, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_8 ) PORT_NAME( "irq 0xf8" ) PORT_CHANGED( trigger_irq, NULL )
 INPUT_PORTS_END
+
+
+static MACHINE_RESET( nakajies )
+{
+	nakajies_state *state = machine->driver_data;
+
+	state->cpu = devtag_get_device( machine, "v20hl" );
+	state->irq_enabled = 0;
+	state->irq_active = 0;
+}
 
 
 static MACHINE_DRIVER_START( nakajies210 )
 	MDRV_CPU_ADD( "v20hl", V20, X301 / 2 )
 	MDRV_CPU_PROGRAM_MAP( nakajies210_map)
 	MDRV_CPU_IO_MAP( nakajies_io_map)
+
+	MDRV_DRIVER_DATA( nakajies_state )
+
+	MDRV_MACHINE_RESET( nakajies )
 
 	MDRV_SCREEN_ADD( "screen", LCD )
 	MDRV_SCREEN_REFRESH_RATE( 50 )	/* Wild guess */
@@ -217,6 +363,10 @@ static MACHINE_DRIVER_START( nakajies220 )
 	MDRV_CPU_PROGRAM_MAP( nakajies220_map)
 	MDRV_CPU_IO_MAP( nakajies_io_map)
 
+	MDRV_DRIVER_DATA( nakajies_state )
+
+	MDRV_MACHINE_RESET( nakajies )
+
 	MDRV_SCREEN_ADD( "screen", LCD )
 	MDRV_SCREEN_REFRESH_RATE( 50 )	/* Wild guess */
 	MDRV_SCREEN_FORMAT( BITMAP_FORMAT_INDEXED16 )
@@ -235,6 +385,10 @@ static MACHINE_DRIVER_START( nakajies250 )
 	MDRV_CPU_ADD( "v20hl", V20, X301 / 2 )
 	MDRV_CPU_PROGRAM_MAP( nakajies250_map)
 	MDRV_CPU_IO_MAP( nakajies_io_map)
+
+	MDRV_DRIVER_DATA( nakajies_state )
+
+	MDRV_MACHINE_RESET( nakajies )
 
 	MDRV_SCREEN_ADD( "screen", LCD )
 	MDRV_SCREEN_REFRESH_RATE( 50 )  /* Wild guess */
