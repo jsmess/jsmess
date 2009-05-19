888098733ec8d763fb0389c0dc1357e8bb4c6cfe
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-19 10:16:47 +0000

    [XOR100] Added info, loaded rom at the correct address.
    git-svn-id: svn://messdev.no-ip.org/mess@4743 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/xor100.c
+++ src/mess/drivers/xor100.c
@@ -1,20 +1,43 @@
-/***************************************************************************
+/****************************************************************************************************
 
         XOR-100-12
 
         12/05/2009 Skeleton driver.
 
-****************************************************************************/
+	Execution of this monitor rom starts at F800.
+
+	I/O sequence:	read 0A, discard result
+			read 0B, output result to 0B
+			send the sequence AA 40 4E 37 to 01, then to 03 (programming a device?)
+			L1:read 03 to see if display device is ready
+			write ascii character to 02 goto L1 until signon message is displayed
+			in same fashion display top of memory (F800)
+			display prompt character (*) wait for input
+			read 03 to see if a key is being input, if so read 02 and display it
+			if illegal input, display ? then display prompt on a new line
+
+	To summarise:	out 01 and out 03 - program a device
+			out 02 - display a character
+			in 02 - read character from keyboard
+			in 03 - get busy status of display (bit 0) and keyboard (bit 1) High=ready
+
+*****************************************************************************************************/
 
 #include "driver.h"
 #include "cpu/z80/z80.h"
 
 static ADDRESS_MAP_START(xor100_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
+	AM_RANGE( 0x0000, 0xf7ff ) AM_RAM
+	AM_RANGE( 0xf800, 0xffff ) AM_ROM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( xor100_io , ADDRESS_SPACE_IO, 8)
+static ADDRESS_MAP_START(xor100_io, ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+//	AM_RANGE( 0x01, 0x01 ) AM_WRITE(program device)
+//	AM_RANGE( 0x02, 0x02 ) AM_READWRITE(read keyboard, display character)
+//	AM_RANGE( 0x03, 0x03 ) AM_READWRITE(read status, program device)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -62,13 +85,13 @@ SYSTEM_CONFIG_END
 
 /* ROM definition */
 ROM_START( xor100 )
-    ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-  ROM_LOAD( "mon185.bin", 0x0000, 0x0800, CRC(0d0bda8d) SHA1(11c83f7cd7e6a570641b44a2f2cc5737a7dd8ae3))
+	ROM_REGION( 0x10000, "maincpu", 0 )
+	ROM_LOAD( "mon185.bin", 0xf800, 0x0800, CRC(0d0bda8d) SHA1(11c83f7cd7e6a570641b44a2f2cc5737a7dd8ae3))
 
 ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG COMPANY   FULLNAME       FLAGS */
-COMP( ????, xor100,  0,       0, 	xor100, 	xor100, 	 0,  	  xor100,  	 "Unknown",   "XOR-100-12",		GAME_NOT_WORKING)
+/*    YEAR  NAME    PARENT  COMPAT      MACHINE         INPUT            INIT     CONFIG          COMPANY            FULLNAME       FLAGS */
+COMP( ????, xor100,  0,       0, 	xor100, 	xor100, 	 0,  	  xor100,  	 "Xor Data Science", "XOR-100-12", GAME_NOT_WORKING)
 
