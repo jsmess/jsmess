e1c4908bc57eabe5363638aeaec1cccbb1775144
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-20 12:24:16 +0000

    Added a little flesh to the bones.
    git-svn-id: svn://messdev.no-ip.org/mess@4767 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/px8.c
+++ src/mess/drivers/px8.c
@@ -4,6 +4,8 @@
 
         12/05/2009 Skeleton driver.
 
+	Seems to be a CP/M computer.
+
 ****************************************************************************/
 
 #include "driver.h"
@@ -11,6 +13,8 @@
 
 static ADDRESS_MAP_START(px8_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
+	AM_RANGE( 0x0000, 0x7fff ) AM_ROM
+	AM_RANGE( 0x8000, 0xffff ) AM_RAM
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( px8_io , ADDRESS_SPACE_IO, 8)
@@ -62,11 +66,13 @@ SYSTEM_CONFIG_END
 
 /* ROM definition */
 ROM_START( px8 )
-    ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-  ROM_LOAD( "font.rom", 0x0000, 0x0800, CRC(5b52edbd) SHA1(38197edf301bb2843bea040536af545f76b3d44f))
-  ROM_LOAD( "sys.rom", 0x0000, 0x8000, CRC(bd3e4938) SHA1(5bd48abd2a563a1ae31ff137280f40c8f756e969))
-  ROM_LOAD( "px060688.epr", 0x0000, 0x8000, CRC(44308bdf) SHA1(5c4545fcf1af9931b4699436294d9b6298052a7b))
+	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
+	/* These two roms appear to be slightly different bios versions */
+	ROM_LOAD( "sys.rom", 0x0000, 0x8000, CRC(bd3e4938) SHA1(5bd48abd2a563a1ae31ff137280f40c8f756e969))
+	ROM_LOAD( "px060688.epr", 0x0000, 0x8000, CRC(44308bdf) SHA1(5c4545fcf1af9931b4699436294d9b6298052a7b))
 
+	ROM_REGION( 0x0800, "gfx1", 0 )
+	ROM_LOAD( "font.rom", 0x0000, 0x0800, CRC(5b52edbd) SHA1(38197edf301bb2843bea040536af545f76b3d44f))
 ROM_END
 
 /* Driver */
