584d3ea34d6ba0852322216df01ad4d16b74065d
duke <duke@9849079f-6229-0410-a07f-84406ce305ba>
2009-05-26 12:29:18 +0000

    [PX8] Cleaned up, added some NO_DUMP ROMs, fixed display size
    git-svn-id: svn://messdev.no-ip.org/mess@4841 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/px8.c
+++ src/mess/drivers/px8.c
@@ -1,34 +1,35 @@
 /***************************************************************************
 
-        PX-8
+    Epson PX-8
 
-        12/05/2009 Skeleton driver.
+    12/05/2009 Skeleton driver.
 
-	Seems to be a CP/M computer.
+    Seems to be a CP/M computer.
 
-****************************************************************************/
+***************************************************************************/
 
 #include "driver.h"
 #include "cpu/z80/z80.h"
 
-static ADDRESS_MAP_START(px8_mem, ADDRESS_SPACE_PROGRAM, 8)
+
+/***************************************************************************
+    ADDRESS MAPS
+***************************************************************************/
+
+static ADDRESS_MAP_START( px8_mem, ADDRESS_SPACE_PROGRAM, 8 )
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE( 0x0000, 0x7fff ) AM_ROM
-	AM_RANGE( 0x8000, 0xffff ) AM_RAM
+	AM_RANGE(0x0000, 0x7fff) AM_ROM
+	AM_RANGE(0x8000, 0xffff) AM_RAM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( px8_io , ADDRESS_SPACE_IO, 8)
+static ADDRESS_MAP_START( px8_io , ADDRESS_SPACE_IO, 8 )
 	ADDRESS_MAP_UNMAP_HIGH
 ADDRESS_MAP_END
 
-/* Input ports */
-INPUT_PORTS_START( px8 )
-INPUT_PORTS_END
 
-
-static MACHINE_RESET(px8)
-{
-}
+/***************************************************************************
+    VIDEO EMULATION
+***************************************************************************/
 
 static VIDEO_START( px8 )
 {
@@ -39,32 +40,44 @@ static VIDEO_UPDATE( px8 )
     return 0;
 }
 
+
+/***************************************************************************
+    INPUT PORTS
+***************************************************************************/
+
+INPUT_PORTS_START( px8 )
+INPUT_PORTS_END
+
+
+/***************************************************************************
+    MACHINE DRIVERS
+***************************************************************************/
+
 static MACHINE_DRIVER_START( px8 )
     /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)
+    MDRV_CPU_ADD("maincpu", Z80, XTAL_4MHz)
     MDRV_CPU_PROGRAM_MAP(px8_mem)
     MDRV_CPU_IO_MAP(px8_io)
 
-    MDRV_MACHINE_RESET(px8)
+	/* video hardware */
+	MDRV_SCREEN_ADD("screen", LCD)
+	MDRV_SCREEN_REFRESH_RATE(50)
+	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MDRV_SCREEN_SIZE(480, 64)
+	MDRV_SCREEN_VISIBLE_AREA(0, 479, 0, 63)
 
-    /* video hardware */
-    MDRV_SCREEN_ADD("screen", RASTER)
-    MDRV_SCREEN_REFRESH_RATE(50)
-    MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MDRV_SCREEN_SIZE(640, 480)
-    MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-    MDRV_PALETTE_LENGTH(2)
+	MDRV_PALETTE_LENGTH(2)
     MDRV_PALETTE_INIT(black_and_white)
 
     MDRV_VIDEO_START(px8)
     MDRV_VIDEO_UPDATE(px8)
 MACHINE_DRIVER_END
 
-static SYSTEM_CONFIG_START(px8)
-SYSTEM_CONFIG_END
 
-/* ROM definition */
+/***************************************************************************
+    ROM DEFINITIONS
+***************************************************************************/
+
 ROM_START( px8 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	/* These two roms appear to be slightly different bios versions */
@@ -73,10 +86,27 @@ ROM_START( px8 )
 
 	ROM_REGION( 0x0800, "gfx1", 0 )
 	ROM_LOAD( "font.rom", 0x0000, 0x0800, CRC(5b52edbd) SHA1(38197edf301bb2843bea040536af545f76b3d44f))
+
+	ROM_REGION(0x1000, "sub", 0)
+	ROM_LOAD("upd7508.bin", 0x0000, 0x1000, NO_DUMP)
+
+	ROM_REGION(0x1000, "slave", 0)
+	ROM_LOAD("hd6301.bin", 0x0000, 0x1000, NO_DUMP)
 ROM_END
 
-/* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG COMPANY   FULLNAME       FLAGS */
-COMP( ????, px8,  0,       0, 	px8, 	px8, 	 0,  	  px8,  	 "Epson",   "PX-8",		GAME_NOT_WORKING)
+/***************************************************************************
+    SYSTEM CONFIG
+***************************************************************************/
 
+static SYSTEM_CONFIG_START( px8 )
+	CONFIG_RAM_DEFAULT(64 * 1024) /* 64KB RAM */
+SYSTEM_CONFIG_END
+
+
+/***************************************************************************
+    GAME DRIVERS
+***************************************************************************/
+
+/*    YEAR  NAME  PARENT  COMPAT  MACHINE  INPUT  INIT  CONFIG  COMPANY  FULLNAME  FLAGS */
+COMP( 1984, px8,  0,      0,      px8,     px8,   0,    px8,    "Epson", "PX-8",   GAME_NOT_WORKING )
