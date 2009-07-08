1962bf59e6022651bca319e924de4db383ddadeb
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2009-07-08 10:18:38 +0000

    [JR200] Fixed memory map and added comment about missing MN1544 sub-CPU ROM dump.
    git-svn-id: svn://messdev.no-ip.org/mess@5181 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/jr200.c
+++ src/mess/drivers/jr200.c
@@ -1,22 +1,30 @@
 /***************************************************************************
 
-        JR-200
+    JR-200
 
-        12/05/2009 Skeleton driver.
-
-	The incorrect processor is used here.
+    12/05/2009 Skeleton driver.
 
 ****************************************************************************/
 
+/*
+
+	TODO:
+
+	- MN1544 4-bit CPU core and ROM dump
+
+*/
+
 #include "driver.h"
-#include "cpu/z80/z80.h"
+#include "cpu/m6800/m6800.h"
 
 static ADDRESS_MAP_START(jr200_mem, ADDRESS_SPACE_PROGRAM, 8)
-	ADDRESS_MAP_UNMAP_HIGH
+	AM_RANGE(0x0000, 0x7fff) AM_RAM
+	AM_RANGE(0xa000, 0xbfff) AM_ROM // basic?
+	AM_RANGE(0xc000, 0xcfff) AM_RAM // videoram, colorram
+	AM_RANGE(0xe000, 0xffff) AM_ROM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( jr200_io , ADDRESS_SPACE_IO, 8)
-	ADDRESS_MAP_UNMAP_HIGH
+static ADDRESS_MAP_START( jr200_io, ADDRESS_SPACE_IO, 8)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -39,10 +47,12 @@ static VIDEO_UPDATE( jr200 )
 
 static MACHINE_DRIVER_START( jr200 )
     /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)
+    MDRV_CPU_ADD("maincpu", M6802, XTAL_4MHz) /* MN1800A */
     MDRV_CPU_PROGRAM_MAP(jr200_mem)
     MDRV_CPU_IO_MAP(jr200_io)
-
+/*
+    MDRV_CPU_ADD("mn1544", MN1544, ?)
+*/
     MDRV_MACHINE_RESET(jr200)
 
     /* video hardware */
@@ -65,14 +75,17 @@ SYSTEM_CONFIG_END
 /* ROM definition */
 ROM_START( jr200 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_LOAD( "basic.rom", 0x0000, 0x2000, CRC(cc53eb52) SHA1(910927b98a8338ba072173d79613422a8cb796da))
-	ROM_LOAD( "jr200u.bin", 0x2000, 0x2000, CRC(37ca3080) SHA1(17d3fdedb4de521da7b10417407fa2b61f01a77a))
+	ROM_LOAD( "basic.rom",  0xa000, 0x2000, CRC(cc53eb52) SHA1(910927b98a8338ba072173d79613422a8cb796da) )
+	ROM_LOAD( "jr200u.bin", 0xe000, 0x2000, CRC(37ca3080) SHA1(17d3fdedb4de521da7b10417407fa2b61f01a77a) )
+
+	ROM_REGION( 0x10000, "mn1544", ROMREGION_ERASEFF )
+	ROM_LOAD( "mn1544.bin",  0x0000, 0x0400, NO_DUMP )
 
 	ROM_REGION( 0x0800, "char", ROMREGION_ERASEFF )
-	ROM_LOAD( "char.rom", 0x0000, 0x0800, CRC(cb641624) SHA1(6fe890757ebc65bbde67227f9c7c490d8edd84f2))
+	ROM_LOAD( "char.rom", 0x0000, 0x0800, CRC(cb641624) SHA1(6fe890757ebc65bbde67227f9c7c490d8edd84f2) )
 ROM_END
 
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG COMPANY   FULLNAME       FLAGS */
-COMP( ????, jr200,  0,       0, 	jr200, 	jr200, 	 0,  	  jr200,  	 "National",   "JR-200",		GAME_NOT_WORKING)
+COMP( 1982, jr200,  0,       0, 	jr200, 	jr200, 	 0,  	  jr200,  	 "National",   "JR-200",		GAME_NOT_WORKING )
