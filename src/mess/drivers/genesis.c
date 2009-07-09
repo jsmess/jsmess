3f558064acc97afeb3c2c2b19b347827a6a4474b
jkerk <jkerk@9849079f-6229-0410-a07f-84406ce305ba>
2009-07-09 05:11:32 +0000

    [SEGA CD] Overhauled the BIOS sets some more, with help from segaloco
    git-svn-id: svn://messdev.no-ip.org/mess@5189 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/genesis.c
+++ src/mess/drivers/genesis.c
@@ -456,88 +456,93 @@ ROM_START( 32x )
 	ROM_LOAD( "32x_s_bios.bin", 0x000000,  0x000400, CRC(bfda1fe5) SHA1(4103668c1bbd66c5e24558e73d4f3f92061a109a) )
 ROM_END
 
-/* We need proper names for these BIOS ROMs! */
+/* We need proper names for most of these BIOS ROMs! */
 ROM_START( segacd )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* Model 1 v1.10 */
+	/* v1.10 */
 	ROM_LOAD( "segacd_model1_bios_1_10_u.bin", 0x000000,  0x020000, CRC(c6d10268) SHA1(f4f315adcef9b8feb0364c21ab7f0eaf5457f3ed) )
 ROM_END
 
 ROM_START( megacd )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* Model 1 v1.00, confirmed good dump */
+	/* v1.00, confirmed good dump */
 	ROM_LOAD( "megacd_model1_bios_1_00_e.bin", 0x000000,  0x020000, CRC(529ac15a) SHA1(f891e0ea651e2232af0c5c4cb46a0cae2ee8f356) )
 ROM_END
 
 ROM_START( megacdj )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	ROM_SYSTEM_BIOS(0, "v100s", "Model 1 v1.00S")
+	ROM_SYSTEM_BIOS(0, "v100s", "v1.00S") /* also used for Asia PAL - not handled yet */
 	ROMX_LOAD( "megacd_model1_bios_1_00s_j.bin", 0x000000,  0x020000, CRC(550f30bb) SHA1(e4193c6ae44c3cea002707d2a88f1fbcced664de), ROM_BIOS(1) )
-	ROM_SYSTEM_BIOS(1, "v100p", "Model 1 v1.00P")
+	ROM_SYSTEM_BIOS(1, "v100p", "v1.00P")
 	ROMX_LOAD( "megacd_model1_bios_1_00p_j.bin", 0x000000,  0x020000, CRC(9d2da8f2) SHA1(4846f448160059a7da0215a5df12ca160f26dd69), ROM_BIOS(2) )
 ROM_END
 
 ROM_START( segacd2 )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	ROM_SYSTEM_BIOS(0, "v211", "Model 2 v2.11")
-	ROMX_LOAD( "segacd_model2_bios_2_11_u.bin", 0x000000,  0x020000, CRC(2e49d72c) SHA1(328a3228c29fba244b9db2055adc1ec4f7a87e6b), ROM_BIOS(1) )
-	ROM_SYSTEM_BIOS(1, "v200", "Model 2 v2.00")
-	ROMX_LOAD( "segacd_model2_bios_2_00_u.bin", 0x000000,  0x020000, CRC(340b4be4) SHA1(bd3ee0c8ab732468748bf98953603ce772612704), ROM_BIOS(2) )
+	ROM_SYSTEM_BIOS(0, "v211x", "Model 2 v2.11X")
+	ROMX_LOAD( "mpr-15764-t.bin", 0x000000,  0x020000, CRC(2e49d72c) SHA1(328a3228c29fba244b9db2055adc1ec4f7a87e6b), ROM_BIOS(1) )
+	ROM_SYSTEM_BIOS(1, "v200", "Model 2 v2.00") /* verified dump */
+	ROMX_LOAD( "us_scd2_930314.bin", 0x000000,  0x020000, CRC(8af65f58) SHA1(5a8c4b91d3034c1448aac4b5dc9a6484fce51636), ROM_BIOS(2) )
+	/* this is reportedly a bad dump, it has many differences from the verified dump and does not boot in Kega */
+	/* ROMX_LOAD( "segacd_model2_bios_2_00_u.bin", 0x000000,  0x020000, CRC(340b4be4) SHA1(bd3ee0c8ab732468748bf98953603ce772612704), ROM_BIOS(2) ) */
 	ROM_SYSTEM_BIOS(2, "v200w", "Model 2 v2.00W")
 	ROMX_LOAD( "segacd_model2_bios_2_00w_u.bin", 0x000000,  0x020000, CRC(9f6f6276) SHA1(5adb6c3af218c60868e6b723ec47e36bbdf5e6f0), ROM_BIOS(3) )
 ROM_END
 
 ROM_START( megacd2 )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	ROM_SYSTEM_BIOS(0, "v200w", "Model 2 v2.00W")	// confirmed good dump
+	ROM_SYSTEM_BIOS(0, "v200w", "v2.00W")	// confirmed good dump
 	ROMX_LOAD( "megacd_model2_bios_2_00w_e.bin", 0x000000,  0x020000, CRC(4d5cb8da) SHA1(f5f60f03501908962446ee02fc27d98694dd157d), ROM_BIOS(1) )
-	ROM_SYSTEM_BIOS(1, "v200", "Model 2 v2.00")
-	ROMX_LOAD( "megacd_model2_bios_2_00_e.bin", 0x000000,  0x020000, CRC(0507b590) SHA1(7063192ae9f6b696c5b81bc8f0a9fe6f0c400e58), ROM_BIOS(2) )
+	ROM_SYSTEM_BIOS(1, "v200", "v2.00")
+	ROMX_LOAD( "mpr-15512-t.bin", 0x000000,  0x020000, CRC(0507b590) SHA1(7063192ae9f6b696c5b81bc8f0a9fe6f0c400e58), ROM_BIOS(2) )
 ROM_END
 
 ROM_START( megacd2j )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	ROM_SYSTEM_BIOS(0, "mcd2", "Mega-CD 2 v2.00C")
+	ROM_SYSTEM_BIOS(0, "v200c", "v2.00C")
 	ROMX_LOAD( "megacd2_bios_2_00c_j.bin", 0x000000,  0x020000, CRC(dd6cc972) SHA1(d203cfe22c03ae479dd8ca33840cf8d9776eb3ff), ROM_BIOS(1) )
-	ROM_SYSTEM_BIOS(1, "v200", "Model 2 v2.00")
+	ROM_SYSTEM_BIOS(1, "v200", "v2.00")
 	ROMX_LOAD( "megacd_model2_bios_2_00_j.bin", 0x000000,  0x020000, CRC(f116793c) SHA1(dc68146ad1ff50faeeb2a3f685fab1a2961dabb2), ROM_BIOS(2) )
 ROM_END
 
 ROM_START( laseract )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	ROM_SYSTEM_BIOS(0, "v104", "Pioneer LaserActive v1.04")
+	ROM_SYSTEM_BIOS(0, "v104", "v1.04")
 	ROMX_LOAD( "laseractive_bios_1_04_u.bin", 0x000000,  0x020000, CRC(50cd3d23) SHA1(aa811861f8874775075bd3f53008c8aaf59b07db), ROM_BIOS(1) )
-	ROM_SYSTEM_BIOS(1, "v102", "Pioneer LaserActive v1.02")
+	ROM_SYSTEM_BIOS(1, "v102", "v1.02")
 	ROMX_LOAD( "laseractive_bios_1_02_u.bin", 0x000000,  0x020000, CRC(3b10cf41) SHA1(8af162223bb12fc19b414f126022910372790103), ROM_BIOS(2) )
 ROM_END
 
 ROM_START( laseractj )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
+	/* v1.02 */
 	ROM_LOAD( "laseractive_bios_1_02_j.bin", 0x000000,  0x020000, CRC(00eedb3a) SHA1(26237b333db4a4c6770297fa5e655ea95840d5d9) )
 ROM_END
 
 ROM_START( xeye )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* confirmed good with a chip dump */
-	ROM_LOAD( "xeye-g304.bin", 0x000000,  0x020000, CRC(290f8e33) SHA1(651f14d5a5e0ecb974a60c0f43b1d2006323fb09) )
+	/* v2.00 (US), confirmed good with a chip dump */
+	ROM_LOAD( "g304.bin", 0x000000,  0x020000, CRC(290f8e33) SHA1(651f14d5a5e0ecb974a60c0f43b1d2006323fb09) )
 ROM_END
 
 ROM_START( wmega )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* 1.00 (Japan NTSC) Sega BIOS, chip-dumped */
-	ROM_LOAD( "wondermega-g301.bin", 0x000000,  0x020000, CRC(d21fe71d) SHA1(3fc9358072f74bd24e3e297ea11b2bf15a7af891) )
+	/* v1.00 (Japan NTSC) Sega BIOS, chip-dumped */
+	ROM_LOAD( "g301.bin", 0x000000,  0x020000, CRC(d21fe71d) SHA1(3fc9358072f74bd24e3e297ea11b2bf15a7af891) )
 ROM_END
 
 ROM_START( cdx )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* CDX v2.21 */
+	/* v2.21X */
 	ROM_LOAD( "segacdx_bios_2_21_u.bin", 0x000000,  0x020000, CRC(d48c44b5) SHA1(2b125c0545afa089b617f2558e686ea723bdc06e) )
 ROM_END
 
 ROM_START( multmega )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
-	/* Multi-Mega v2.21, confirmed good dump */
-	ROM_LOAD( "multimega_bios_2_21_e.bin", 0x000000,  0x020000, CRC(34d3cce1) SHA1(73fc9c014ad803e9e7d8076b3642a8a5224b3e51) )
+	/* v2.21X */
+	ROM_LOAD( "opr-16140.bin", 0x000000,  0x020000, CRC(aacb851e) SHA1(75548ac9aaa6e81224499f9a1403b2b42433f5b7) )
+	/* the below was marked "confirmed good dump", but 0x72 and 0x73 are 0xFF, indicating a bad dump made from memory */
+	/* ROM_LOAD( "multimega_bios_2_21_e.bin", 0x000000,  0x020000, CRC(34d3cce1) SHA1(73fc9c014ad803e9e7d8076b3642a8a5224b3e51) ) */
 ROM_END
 
 /* some games use the 32x and SegaCD together to give better quality FMV */
@@ -545,7 +550,7 @@ ROM_START( 32x_scd )
 	ROM_REGION16_BE( 0x400000, "maincpu", ROMREGION_ERASE00 )
 
 	ROM_REGION16_BE( 0x400000, "gamecart", 0 ) /* 68000 Code */
-	ROM_LOAD( "segacd_model2_bios_2_11_u.bin", 0x000000,  0x020000, CRC(2e49d72c) SHA1(328a3228c29fba244b9db2055adc1ec4f7a87e6b) )
+	ROM_LOAD( "mpr-15764-t.bin", 0x000000,  0x020000, CRC(2e49d72c) SHA1(328a3228c29fba244b9db2055adc1ec4f7a87e6b) )
 
 	ROM_REGION32_BE( 0x400000, "gamecart_sh2", 0 ) /* Copy for the SH2 */
 	ROM_COPY( "gamecart", 0x0, 0x0, 0x400000)
