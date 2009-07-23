e6f120f0719df620fb2d863b3d408437fc05328a
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2009-07-23 11:00:38 +0000

    [VBOY] Added two separate displays,and input port definition
    git-svn-id: svn://messdev.no-ip.org/mess@5300 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/vboy.c
+++ src/mess/drivers/vboy.c
@@ -9,39 +9,40 @@
 #include "driver.h"
 #include "cpu/v810/v810.h"
 #include "devices/cartslot.h"
+#include "vboy.lh"
 
-UINT32 *vboy_l_frame_0;
-UINT32 *vboy_font0;
-UINT32 *vboy_l_frame_1;
-UINT32 *vboy_font1;
-UINT32 *vboy_r_frame_0;
-UINT32 *vboy_font2;
-UINT32 *vboy_r_frame_1;
-UINT32 *vboy_font3;
-UINT32 *vboy_bg_map;
-UINT32 *vboy_paramtab;
-UINT32 *vboy_world;
-UINT32 *vboy_columntab1;
-UINT32 *vboy_columntab2;
-UINT32 *vboy_objects;
+UINT16 *vboy_l_frame_0;
+UINT16 *vboy_font0;
+UINT16 *vboy_l_frame_1;
+UINT16 *vboy_font1;
+UINT16 *vboy_r_frame_0;
+UINT16 *vboy_font2;
+UINT16 *vboy_r_frame_1;
+UINT16 *vboy_font3;
+UINT16 *vboy_bg_map;
+UINT16 *vboy_paramtab;
+UINT16 *vboy_world;
+UINT16 *vboy_columntab1;
+UINT16 *vboy_columntab2;
+UINT16 *vboy_objects;
 	
 static ADDRESS_MAP_START( vboy_mem, ADDRESS_SPACE_PROGRAM, 32 )
 	ADDRESS_MAP_GLOBAL_MASK(0x07FFFFFF)
-	AM_RANGE( 0x00000000, 0x00005FFF ) AM_RAM AM_BASE(&vboy_l_frame_0) // L Frame buffer 0
-	AM_RANGE( 0x00006000, 0x00007FFF ) AM_RAM AM_SHARE(1) AM_BASE(&vboy_font0) // Font 0-511
-	AM_RANGE( 0x00008000, 0x0000DFFF ) AM_RAM AM_BASE(&vboy_l_frame_1) // L Frame buffer 1
-	AM_RANGE( 0x0000E000, 0x0000FFFF ) AM_RAM AM_SHARE(2) AM_BASE(&vboy_font1) // Font 512-1023
-	AM_RANGE( 0x00010000, 0x00015FFF ) AM_RAM AM_BASE(&vboy_r_frame_0) // R Frame buffer 0
-	AM_RANGE( 0x00016000, 0x00017FFF ) AM_RAM AM_SHARE(3) AM_BASE(&vboy_font2) // Font 1024-1535
-	AM_RANGE( 0x00018000, 0x0001DFFF ) AM_RAM AM_BASE(&vboy_r_frame_1) // R Frame buffer 1
-	AM_RANGE( 0x0001E000, 0x0001FFFF ) AM_RAM AM_SHARE(4) AM_BASE(&vboy_font3) // Font 1536-2047
+	AM_RANGE( 0x00000000, 0x00005FFF ) AM_RAM AM_BASE((UINT32**)&vboy_l_frame_0) // L Frame buffer 0
+	AM_RANGE( 0x00006000, 0x00007FFF ) AM_RAM AM_SHARE(1) AM_BASE((UINT32**)&vboy_font0) // Font 0-511
+	AM_RANGE( 0x00008000, 0x0000DFFF ) AM_RAM AM_BASE((UINT32**)&vboy_l_frame_1) // L Frame buffer 1
+	AM_RANGE( 0x0000E000, 0x0000FFFF ) AM_RAM AM_SHARE(2) AM_BASE((UINT32**)&vboy_font1) // Font 512-1023
+	AM_RANGE( 0x00010000, 0x00015FFF ) AM_RAM AM_BASE((UINT32**)&vboy_r_frame_0) // R Frame buffer 0
+	AM_RANGE( 0x00016000, 0x00017FFF ) AM_RAM AM_SHARE(3) AM_BASE((UINT32**)&vboy_font2) // Font 1024-1535
+	AM_RANGE( 0x00018000, 0x0001DFFF ) AM_RAM AM_BASE((UINT32**)&vboy_r_frame_1) // R Frame buffer 1
+	AM_RANGE( 0x0001E000, 0x0001FFFF ) AM_RAM AM_SHARE(4) AM_BASE((UINT32**)&vboy_font3) // Font 1536-2047
 	
-	AM_RANGE( 0x00020000, 0x0003BFFF ) AM_RAM AM_BASE(&vboy_bg_map) // BG Map
-	AM_RANGE( 0x0003C000, 0x0003D7FF ) AM_RAM AM_BASE(&vboy_paramtab) // Param Table
-	AM_RANGE( 0x0003D800, 0x0003DBFF ) AM_RAM AM_BASE(&vboy_world) // World 
-	AM_RANGE( 0x0003DC00, 0x0003DDFF ) AM_RAM AM_BASE(&vboy_columntab1) // Column Table 1
-	AM_RANGE( 0x0003DE00, 0x0003DFFF ) AM_RAM AM_BASE(&vboy_columntab2) // Column Table 2
-	AM_RANGE( 0x0003E000, 0x0003FFFF ) AM_RAM AM_BASE(&vboy_objects) // Objects RAM
+	AM_RANGE( 0x00020000, 0x0003BFFF ) AM_RAM AM_BASE((UINT32**)&vboy_bg_map) // BG Map
+	AM_RANGE( 0x0003C000, 0x0003D7FF ) AM_RAM AM_BASE((UINT32**)&vboy_paramtab) // Param Table
+	AM_RANGE( 0x0003D800, 0x0003DBFF ) AM_RAM AM_BASE((UINT32**)&vboy_world) // World 
+	AM_RANGE( 0x0003DC00, 0x0003DDFF ) AM_RAM AM_BASE((UINT32**)&vboy_columntab1) // Column Table 1
+	AM_RANGE( 0x0003DE00, 0x0003DFFF ) AM_RAM AM_BASE((UINT32**)&vboy_columntab2) // Column Table 2
+	AM_RANGE( 0x0003E000, 0x0003FFFF ) AM_RAM AM_BASE((UINT32**)&vboy_objects) // Objects RAM
 	
 	AM_RANGE( 0x00040000, 0x0005FFFF ) AM_RAM // VIPC	
 	
@@ -60,6 +61,23 @@ ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( vboy )
+	PORT_START("INPUT")
+		PORT_BIT( 0x8000, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_DOWN ) PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x4000, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_LEFT ) PORT_CATEGORY(1) PORT_PLAYER(1)	
+		PORT_BIT( 0x2000, IP_ACTIVE_HIGH, IPT_SELECT )			   PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x1000, IP_ACTIVE_HIGH, IPT_START ) 			   PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0800, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_UP )    PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0400, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_DOWN )  PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0200, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_LEFT )  PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0100, IP_ACTIVE_HIGH, IPT_JOYSTICKLEFT_RIGHT ) PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0080, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_RIGHT )PORT_CATEGORY(1) PORT_PLAYER(1)		
+		PORT_BIT( 0x0040, IP_ACTIVE_HIGH, IPT_JOYSTICKRIGHT_UP )   PORT_CATEGORY(1) PORT_PLAYER(1)
+		PORT_BIT( 0x0020, IP_ACTIVE_HIGH, IPT_BUTTON3 )   		   PORT_CATEGORY(1) PORT_PLAYER(1) // Left button on back
+		PORT_BIT( 0x0010, IP_ACTIVE_HIGH, IPT_BUTTON4 )   		   PORT_CATEGORY(1) PORT_PLAYER(1) // Right button on back
+		PORT_BIT( 0x0008, IP_ACTIVE_HIGH, IPT_BUTTON2 )   		   PORT_CATEGORY(1) PORT_PLAYER(1) // A button
+		PORT_BIT( 0x0004, IP_ACTIVE_HIGH, IPT_BUTTON1 )   		   PORT_CATEGORY(1) PORT_PLAYER(1) // B button
+		PORT_BIT( 0x0002, IP_ACTIVE_HIGH, IPT_UNUSED ) // Always 1
+		PORT_BIT( 0x0001, IP_ACTIVE_LOW,  IPT_UNUSED ) // Battery low
 INPUT_PORTS_END
 
 
@@ -67,54 +85,95 @@ static MACHINE_RESET(vboy)
 {
 }
 
+static bitmap_t *bg_map[14];
+
 static VIDEO_START( vboy )
 {
+	int i;
+	// Allocate memory for temporary screens
+	for(i=0;i<14;i++) {
+		bg_map[i] = auto_bitmap_alloc(machine, 512, 512, BITMAP_FORMAT_INDEXED16);
+	}
 }
 
-static void display_char(int x, int y, UINT16 ch, bitmap_t *bitmap)
+static void put_char(int x, int y, UINT16 ch, bitmap_t *bitmap)
 {
-	UINT32 *font = vboy_font0;
-	UINT16 code = ch,t0,t1;
+	UINT16 *font = vboy_font0;
+	UINT16 code = ch;
 	int i,b;
+	// determine font buffer from char code used
 	if (ch >=512)  { font = vboy_font1; code = ch - 512;  }
 	if (ch >=1024) { font = vboy_font2; code = ch - 1024; }
 	if (ch >=1536) { font = vboy_font3; code = ch - 1536; }
 	
-	for(i=0;i<4;i++) {
-		t0 = font [code*4+i] & 0xffff; 
-		t1 = (font[code*4+i] >> 16) & 0xffff;		
+	for(i=0;i<8;i++) {
+		UINT16  data = font[code*8+i]; 
 		for (b = 0; b < 8; b++)
 		{					
-			*BITMAP_ADDR16(bitmap, y+i*2,   x + b) =  ((t0 >> (b*2)) & 0x03);
-			*BITMAP_ADDR16(bitmap, y+i*2+1, x + b) =  ((t1 >> (b*2)) & 0x03);
+			*BITMAP_ADDR16(bitmap, y+i, x + b) =  ((data >> (b << 1)) & 0x03);
 		}		
-	}
-	
+	}	
 }
 
-static void display_bg_map(int num, bitmap_t *bitmap)
+static void fill_bg_map(int num, bitmap_t *bitmap)
 {
-	int i,j;
-	UINT16 c0,c1;
+	int i,j;	
+	// Fill background map
 	for (i=0;i<64;i++) {		
-		for (j=0;j<32;j++) {
-			c0 = vboy_bg_map[j+32*i] & 0xffff;
-			c1 = (vboy_bg_map[j+32*i] >> 16) & 0xffff;			
-			display_char(j*16,8*i,c0,bitmap);
-			display_char(j*16+8,8*i,c1,bitmap);
+		for (j=0;j<64;j++) {
+			UINT16 ch = vboy_bg_map[j+64*i + (num* 0x1000)];
+			put_char(j*8,i*8,ch & 0x7ff,bitmap);
 		}
 	}
 }
+
+static UINT8 display_world(int num, bitmap_t *bitmap, UINT8 right)
+{
+	UINT16 def = vboy_world[num*16];
+	UINT16 gx  = vboy_world[num*16+1];
+	UINT16 gp  = vboy_world[num*16+2];
+	UINT16 gy  = vboy_world[num*16+3];
+	UINT16 mx  = vboy_world[num*16+4];
+	UINT16 mp  = vboy_world[num*16+5];
+	UINT16 my  = vboy_world[num*16+6];
+	//UINT16 w  = vboy_world[num*16+7];
+	//UINT16 h = vboy_world[num*16+8];
+//	UINT16 param_base  = vboy_world[num*16+9];
+//	UINT16 overplane = vboy_world[num*16+10];
+	UINT8 bg_map_num = def & 0x0f;
+	
+	if (bg_map_num < 15) {
+		fill_bg_map(bg_map_num,bg_map[bg_map_num]);	
+		if (BIT(def,15) && (right==0)) {
+			// Left screen 
+			copybitmap(bitmap,bg_map[bg_map_num],mx-mp,my,gx-gp,gy,NULL);
+		}
+		if (BIT(def,14) && (right==1)) {
+			// Right screen 
+			copybitmap(bitmap,bg_map[bg_map_num],mx+mp,my,gx+gp,gy,NULL);
+		}
+	}
+	// Return END world status
+	return BIT(def,6);
+}
+
 static VIDEO_UPDATE( vboy )
 {
-	display_bg_map(0,bitmap);
+	int i;
+	UINT8 right = 0;
+	const device_config *_3d_right_screen = devtag_get_device(screen->machine, "3dright");
+	if (screen == _3d_right_screen) right = 1;
+
+	for(i=31;i>=0;i--) {
+		if (display_world(i,bitmap,right)) break;
+	}
     return 0;
 }
 static const rgb_t vboy_palette[18] = {
 	MAKE_RGB(0x00, 0x00, 0x00), // 0
-	MAKE_RGB(0x55, 0x55, 0x55), // 1
-	MAKE_RGB(0x7f, 0x7f, 0x7f), // 2 
-	MAKE_RGB(0xff, 0xff, 0xff)  // 3  
+	MAKE_RGB(0x55, 0x00, 0x00), // 1
+	MAKE_RGB(0x7f, 0x00, 0x00), // 2 
+	MAKE_RGB(0xff, 0x00, 0x00)  // 3  
 };
 
 PALETTE_INIT( vboy )
@@ -125,26 +184,36 @@ PALETTE_INIT( vboy )
 
 static MACHINE_DRIVER_START( vboy )
     /* basic machine hardware */
-    MDRV_CPU_ADD( "maincpu", V810, 21477270 )
+    MDRV_CPU_ADD( "maincpu", V810, XTAL_20MHz )
     MDRV_CPU_PROGRAM_MAP(vboy_mem)
 
     MDRV_MACHINE_RESET(vboy)
 	
-    /* video hardware */
-    MDRV_SCREEN_ADD("screen", RASTER)
-    MDRV_SCREEN_REFRESH_RATE(50)
-    MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    //MDRV_SCREEN_SIZE(384, 224)
-    //MDRV_SCREEN_VISIBLE_AREA(0, 384-1, 0, 224-1)
-    MDRV_SCREEN_SIZE(512, 512)
-    MDRV_SCREEN_VISIBLE_AREA(0, 512-1, 0, 512-1)
+    /* video hardware */    
+    MDRV_DEFAULT_LAYOUT(layout_vboy)   
+    
     MDRV_PALETTE_LENGTH(4)
     MDRV_PALETTE_INIT(vboy)
 
     MDRV_VIDEO_START(vboy)
     MDRV_VIDEO_UPDATE(vboy)
-	
+     
+    /* Left screen */
+	MDRV_SCREEN_ADD("3dleft", RASTER)
+	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MDRV_SCREEN_REFRESH_RATE(50.2)
+	MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500))
+    MDRV_SCREEN_SIZE(384, 224)
+    MDRV_SCREEN_VISIBLE_AREA(0, 384-1, 0, 224-1)
+
+    /* Right screen */
+	MDRV_SCREEN_ADD("3dright", RASTER)
+	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MDRV_SCREEN_REFRESH_RATE(50.2)
+	MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500))
+    MDRV_SCREEN_SIZE(384, 224)
+    MDRV_SCREEN_VISIBLE_AREA(0, 384-1, 0, 224-1)
+	   	
     /* cartridge */
 	MDRV_CARTSLOT_ADD("cart")
 	MDRV_CARTSLOT_EXTENSION_LIST("vb")
--- src/mess/mess.mak
+++ src/mess/mess.mak
@@ -1499,6 +1499,7 @@ $(MESS_DRIVERS)/svision.o:	$(MESS_LAYOUT)/svision.lh
 $(MESS_DRIVERS)/sym1.o:		$(MESS_LAYOUT)/sym1.lh
 $(MESS_DRIVERS)/tec1.o:		$(MESS_LAYOUT)/tec1.lh
 $(MESS_DRIVERS)/ut88.o:		$(MESS_LAYOUT)/ut88mini.lh
+$(MESS_DRIVERS)/vboy.o:		$(MESS_LAYOUT)/vboy.lh
 $(MESS_DRIVERS)/vcs80.o:	$(MESS_LAYOUT)/vcs80.lh
 $(MESS_DRIVERS)/votrpss.o:	$(MESS_LAYOUT)/votrpss.lh
 $(MESS_DRIVERS)/votrtnt.o:	$(MESS_LAYOUT)/votrtnt.lh
