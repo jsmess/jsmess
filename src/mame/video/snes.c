00335ed4bad3ffacc8f8d5f850666cd7cda53f02
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2009-08-13 16:48:36 +0000

    [SNES]: Fixed direct color gfxs in mode 7
    git-svn-id: svn://messdev.no-ip.org/mess@5422 9849079f-6229-0410-a07f-84406ce305ba

--- src/mame/video/snes.c
+++ src/mame/video/snes.c
@@ -476,6 +476,8 @@ INLINE void snes_update_line( UINT8 screen, UINT8 color_depth, UINT8 hires, UINT
 			color_shift = 0;	//n/a, pal offset is always zero
 			color_planes = 8;
 			tile_divider = 4;
+			if(snes_ppu.direct_color) //we don't know what games trigger this one...
+				fatalerror("8bpp graphics with direct color, gfx mode used = %02x",snes_ppu.mode);
 			break;
 	}
 
@@ -694,7 +696,7 @@ static void snes_update_line_mode7(UINT8 screen, UINT8 priority_a, UINT8 priorit
 		{
 			UINT16 clr;
 			/* Direct select, but only outside EXTBG! */
-			if (snes_ppu.direct_color && layer == 1)
+			if (snes_ppu.direct_color && layer == 0)
 				clr = ((colour & 0x07) << 2) | ((colour & 0x38) << 4) | ((colour & 0xc0) << 7);
 			else
 				clr = snes_cgram[colour];
