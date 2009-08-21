0551d415caa3a346e3001943afc59545c24d89bf
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2009-08-21 07:12:07 +0000

    Fixed family name.
    git-svn-id: svn://messdev.no-ip.org/mess@5478 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/i8155.c
+++ src/mess/machine/i8155.c
@@ -533,16 +533,16 @@ DEVICE_GET_INFO( i8155 )
 		/* --- the following bits of info are returned as 64-bit signed integers --- */
 		case DEVINFO_INT_INLINE_CONFIG_BYTES:			info->i = 0;								break;
 		case DEVINFO_INT_CLASS:							info->i = DEVICE_CLASS_PERIPHERAL;			break;
-		case DEVINFO_INT_TOKEN_BYTES:					info->i = sizeof(i8155_t);				break;
+		case DEVINFO_INT_TOKEN_BYTES:					info->i = sizeof(i8155_t);					break;
 
 		/* --- the following bits of info are returned as pointers to data or functions --- */
-		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME(i8155);	break;
+		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME(i8155);		break;
 		case DEVINFO_FCT_STOP:							/* Nothing */								break;
-		case DEVINFO_FCT_RESET:							info->reset = DEVICE_RESET_NAME(i8155);	break;
+		case DEVINFO_FCT_RESET:							info->reset = DEVICE_RESET_NAME(i8155);		break;
 
 		/* --- the following bits of info are returned as NULL-terminated strings --- */
 		case DEVINFO_STR_NAME:							strcpy(info->s, "Intel 8155");				break;
-		case DEVINFO_STR_FAMILY:						strcpy(info->s, "Intel 8085");				break;
+		case DEVINFO_STR_FAMILY:						strcpy(info->s, "Intel MCS-85");			break;
 		case DEVINFO_STR_VERSION:						strcpy(info->s, "1.0");						break;
 		case DEVINFO_STR_SOURCE_FILE:					strcpy(info->s, __FILE__);					break;
 		case DEVINFO_STR_CREDITS:						strcpy(info->s, "Copyright the MESS Team");	break;
