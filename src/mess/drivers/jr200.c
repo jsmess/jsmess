90593029fd1accd93d48d9bf60fc4a4f2f4d5d5c
robiza71 <robiza71@9849079f-6229-0410-a07f-84406ce305ba>
2009-09-17 12:31:10 +0000

    [JR200] added some read handler for keyboard
    git-svn-id: svn://messdev.no-ip.org/mess@5764 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/jr200.c
+++ src/mess/drivers/jr200.c
@@ -20,17 +20,23 @@
 #include "cpu/m6800/m6800.h"
 
 static UINT8 *textram;
-static UINT16 timer = 0;
 
-static READ8_HANDLER( test_r )
+static READ8_HANDLER( test1_r )
 {
-	timer++;
-	if (timer == 0x100)
-	{
-		timer = 0;
-		return 1;
-	}
-	return 0x20;
+	if (debug_global_input_code_pressed(KEYCODE_Q)) return 0x50;
+return 0x50;
+}
+
+static READ8_HANDLER( test2_r )
+{
+	if (debug_global_input_code_pressed(KEYCODE_Q)) return 0x01;
+return 0x01;
+}
+
+static READ8_HANDLER( test3_r )
+{
+	if (debug_global_input_code_pressed(KEYCODE_Q)) return 0x10;
+return 0x10;
 }
 
 /*
@@ -45,10 +51,13 @@ static READ8_HANDLER( test_r )
 
 static ADDRESS_MAP_START(jr200_mem, ADDRESS_SPACE_PROGRAM, 8)
 	AM_RANGE(0x0000, 0x000c) AM_RAM
-	AM_RANGE(0x000d, 0x000d) AM_RAM AM_READ(test_r)
+	AM_RANGE(0x000d, 0x000d) AM_RAM
 	AM_RANGE(0x000e, 0x3fff) AM_RAM
 	AM_RANGE(0xa000, 0xbfff) AM_ROM
-	AM_RANGE(0xc000, 0xcfff) AM_RAM AM_BASE(&textram)
+	AM_RANGE(0xc000, 0xc7ff) AM_RAM AM_BASE(&textram)
+	AM_RANGE(0xc801, 0xc801) AM_RAM AM_READ(test1_r)
+	AM_RANGE(0xc81c, 0xc81c) AM_RAM AM_READ(test2_r)
+	AM_RANGE(0xc81d, 0xc81d) AM_RAM AM_READ(test3_r)
 	AM_RANGE(0xe000, 0xffff) AM_ROM
 ADDRESS_MAP_END
 
@@ -107,11 +116,25 @@ static GFXDECODE_START( jr200 )
 	GFXDECODE_ENTRY( "char", 0, tiles8x8_layout, 0, 0x100 )
 GFXDECODE_END
 
+
+static INTERRUPT_GEN( jr200_irq )
+{
+	cpu_set_input_line(device, 0, HOLD_LINE);
+
+}
+
+static INTERRUPT_GEN( jr200_nmi )
+{
+	cpu_set_input_line(device, INPUT_LINE_NMI, PULSE_LINE);
+}
+
 static MACHINE_DRIVER_START( jr200 )
 	/* basic machine hardware */
 	MDRV_CPU_ADD("maincpu", M6802, XTAL_4MHz) /* MN1800A */
 	MDRV_CPU_PROGRAM_MAP(jr200_mem)
 	MDRV_CPU_IO_MAP(jr200_io)
+MDRV_CPU_VBLANK_INT("screen", jr200_irq)
+MDRV_CPU_PERIODIC_INT(jr200_nmi,60)
 /*
 	MDRV_CPU_ADD("mn1544", MN1544, ?)
 */
