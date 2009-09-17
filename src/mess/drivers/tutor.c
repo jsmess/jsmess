a3a6045f64092a6addbded3691806e87890f8405
firewave <firewave@9849079f-6229-0410-a07f-84406ce305ba>
2009-09-17 17:31:32 +0000

    some minor formating changes
    git-svn-id: svn://messdev.no-ip.org/mess@5774 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/tutor.c
+++ src/mess/drivers/tutor.c
@@ -720,10 +720,8 @@ static void tutor_parallel_getinfo(const mess_device_class *devclass, UINT32 sta
 }
 
 static SYSTEM_CONFIG_START(tutor)
-
 	/* cartridge port is not emulated */
 	CONFIG_DEVICE(tutor_parallel_getinfo)
-
 SYSTEM_CONFIG_END
 
 /*      YEAR    NAME    PARENT      COMPAT  MACHINE     INPUT   INIT    CONFIG      COMPANY     FULLNAME */
