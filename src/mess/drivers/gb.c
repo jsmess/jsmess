5c823ba5938307e7fc006e46386ee71dc86ea812
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2009-09-17 10:54:13 +0000

    [GB] Added Super GameBoy boot ROM.
    git-svn-id: svn://messdev.no-ip.org/mess@5763 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/gb.c
+++ src/mess/drivers/gb.c
@@ -690,7 +690,7 @@ ROM_END
 
 ROM_START( supergb )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-/*  ROM_LOAD( "sgb_boot.bin", 0x0000, 0x0100, NO_DUMP ) */
+	ROM_LOAD( "sgb_boot.bin", 0x0000, 0x0100, CRC(ec8a83b9) SHA1(aa2f50a77dfb4823da96ba99309085a3c6278515) )
 ROM_END
 
 ROM_START( gbpocket )
@@ -715,15 +715,14 @@ ROM_START( megaduck )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 ROM_END
 
-/*    YEAR  NAME      PARENT   COMPAT   MACHINE   INPUT    INIT  CONFIG   COMPANY     FULLNAME */
-CONS( 1990, gameboy,  0,       0,	gameboy,  gameboy, 0,    0,	 "Nintendo", "Game Boy"  , 0)
-CONS( 1994, supergb,  0,       gameboy,	supergb,  gameboy, 0,    0,	 "Nintendo", "Super Game Boy" , 0)
-CONS( 1996, gbpocket, gameboy, 0,	gbpocket, gameboy, 0,    0,	 "Nintendo", "Game Boy Pocket" , 0)
-CONS( 1997, gblight,  gameboy, 0,	gbpocket, gameboy, 0,    0,	 "Nintendo", "Game Boy Light" , 0)
-CONS( 1998, gbcolor,  gameboy, 0,	gbcolor,  gameboy, 0,    gb_cgb, "Nintendo", "Game Boy Color" , GAME_NOT_WORKING)
+/*    YEAR  NAME      PARENT   COMPAT   MACHINE   INPUT    INIT  CONFIG		COMPANY     FULLNAME */
+CONS( 1990, gameboy,  0,       0,		gameboy,  gameboy, 0,    0,			"Nintendo", "Game Boy", 0)
+CONS( 1994, supergb,  0,       gameboy,	supergb,  gameboy, 0,    0,			"Nintendo", "Super Game Boy", 0)
+CONS( 1996, gbpocket, gameboy, 0,		gbpocket, gameboy, 0,    0,			"Nintendo", "Game Boy Pocket", 0)
+CONS( 1997, gblight,  gameboy, 0,		gbpocket, gameboy, 0,    0,			"Nintendo", "Game Boy Light", 0)
+CONS( 1998, gbcolor,  gameboy, 0,		gbcolor,  gameboy, 0,    gb_cgb,	"Nintendo", "Game Boy Color", GAME_NOT_WORKING)
 
 /* Sound is not 100% yet, it generates some sounds which could be ok. Since we're lacking a real
    system there's no way to verify. Same goes for the colors of the LCD. We are no using the default
    Game Boy green colors */
 CONS( 1993, megaduck, 0,       0,       megaduck, gameboy, 0,    0,	 "Creatronic/Videojet/Timlex/Cougar",  "MegaDuck/Cougar Boy" , 0)
-
