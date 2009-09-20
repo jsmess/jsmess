3c3ba6e3dc1e3499690fec3a31dc4c76590f838e
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2009-09-20 07:59:38 +0000

    Working IMD support
    git-svn-id: svn://messdev.no-ip.org/mess@5813 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/formats/imd_dsk.c
+++ src/mess/formats/imd_dsk.c
@@ -15,6 +15,7 @@
 
 struct imddsk_tag
 {
+	UINT64 start_offset;
 	int heads;
 	int tracks;
 	int sector_size;
@@ -54,28 +55,42 @@ static int imd_get_tracks_per_disk(floppy_image *floppy)
 	return get_tag(floppy)->tracks;
 }
 
-/*static UINT64 imd_get_track_offset(floppy_image *floppy, int head, int track)
+static UINT64 imd_get_track_offset(floppy_image *floppy, int head, int track)
 {
 	return get_tag(floppy)->track_offsets[(track<<1) + head];
 }
-*/
+
 static floperr_t get_offset(floppy_image *floppy, int head, int track, int sector, int sector_is_index, UINT64 *offset)
 {
 	UINT64 offs = 0;
-	UINT8 sectors_per_track = 0;	
-
+	UINT8 header[5];
+	UINT8 sector_num;
+	int i;
+	
+	
 	if ((head < 0) || (head >= get_tag(floppy)->heads) || (track < 0) || (track >= get_tag(floppy)->tracks)
 			|| (sector < 0) )
 		return FLOPPY_ERROR_SEEKERROR;
+	
+	offs = imd_get_track_offset(floppy,head,track);
+	floppy_image_read(floppy, header, offs, 5);
 
-	
-	if (!sector_is_index) {
-		// when taking ID's return seek error if number is over counter
-		if (sector > sectors_per_track) {
-			return FLOPPY_ERROR_SEEKERROR;
-		}
-	}
-	
+	sector_num = header[3];
+	offs += 5 + sector_num; // skip header and sector numbering map
+	if(header[2] & 0x80) offs += sector_num; // skip cylinder numbering map
+	if(header[2] & 0x40) offs += sector_num; // skip head numbering map
+	get_tag(floppy)->sector_size = 1 << (header[4] + 7);
+	for(i=0;i<sector;i++) {	
+		floppy_image_read(floppy, header, offs, 1); // take sector data type
+		switch(header[0]) {
+			case 0: offs++; break;
+			case 1:
+			case 3:
+			case 5:
+			case 7: offs += get_tag(floppy)->sector_size + 1; break;
+			default: offs += 2;
+		}			
+	}		
 	if (offset)
 		*offset = offs;
 	return FLOPPY_ERROR_SUCCESS;
@@ -86,13 +101,30 @@ static floperr_t get_offset(floppy_image *floppy, int head, int track, int secto
 static floperr_t internal_imd_read_sector(floppy_image *floppy, int head, int track, int sector, int sector_is_index, void *buffer, size_t buflen)
 {
 	UINT64 offset;
-	floperr_t err;
+	floperr_t err;	
+	UINT8 header[1];
 	
 	// take sector offset
 	err = get_offset(floppy, head, track, sector, sector_is_index, &offset);
 	if (err)
 		return err;
-		
+	
+	floppy_image_read(floppy, header, offset, 1);	
+	switch(header[0]) {
+		case 0: break;
+		case 1:
+		case 3:
+		case 5:
+		case 7: 
+				floppy_image_read(floppy, buffer, offset+1, buflen);
+				break;
+			
+		default: // all data same
+				floppy_image_read(floppy, header, offset+1, 1);
+				memset(buffer,header[0],buflen);
+				break;
+	}			
+	
 	return FLOPPY_ERROR_SUCCESS;
 }
 
@@ -122,36 +154,103 @@ static floperr_t imd_get_sector_length(floppy_image *floppy, int head, int track
 
 static floperr_t imd_get_indexed_sector_info(floppy_image *floppy, int head, int track, int sector_index, int *cylinder, int *side, int *sector, UINT32 *sector_length, unsigned long *flags)
 {
-	floperr_t retVal;
 	UINT64 offset;
-
-	retVal = get_offset(floppy, head, track, sector_index, FALSE, &offset);
-
-	if (cylinder)
-		*cylinder = 0;
-	if (side)
-		*side = 0;
-	if (sector)
-		*sector = 0;
+	UINT8 header[5];
+	UINT8 hd;
+	UINT8 tr;
+	UINT32 sector_size;
+	UINT8 sector_num;
+	
+	offset = imd_get_track_offset(floppy,head,track);
+	floppy_image_read(floppy, header, offset, 5);
+	tr = header[1];
+	hd = header[2];
+	sector_num = header[3];
+	sector_size = 1 << (header[4] + 7);
+	if (sector_index >= sector_num) return FLOPPY_ERROR_SEEKERROR;
+	if (cylinder) {
+		if (head & 0x80) {
+			floppy_image_read(floppy, header, offset + 5 + sector_num+ sector_index, 1);
+			*cylinder = header[0];
+		} else {
+			*cylinder = tr;
+		}
+	}
+	if (side) {
+		if (head & 0x40) {
+			if (head & 0x80) {
+				floppy_image_read(floppy, header, offset + 5 + 2 * sector_num+sector_index, 1);
+			} else {
+				floppy_image_read(floppy, header, offset + 5 + sector_num+sector_index, 1);
+			}
+			*side = header[0];
+		} else {
+			*side = hd & 1;
+		}
+	}
+	if (sector) {
+		floppy_image_read(floppy, header, offset + 5 + sector_index, 1);
+		*sector = header[0];
+	}
 	if (sector_length) {
-		*sector_length = 0;
+		*sector_length = sector_size;
 	}
 	if (flags)
 		*flags = 0;
-	return retVal;
+	return FLOPPY_ERROR_SUCCESS;
 }
 
 
 FLOPPY_CONSTRUCT( imd_dsk_construct )
 {
 	struct FloppyCallbacks *callbacks;
-	
+	struct imddsk_tag *tag;
+	UINT8 header[0x100];
+	UINT64 pos = 0;	
+	int sector_size = 0;
+	int sector_num;
+	int i;
 	if(params)
 	{
 		// create
 		return FLOPPY_ERROR_UNSUPPORTED;
 	}
+	
+	tag = (struct imddsk_tag *) floppy_create_tag(floppy, IMD_DSK_TAG, sizeof(struct imddsk_tag));
+	if (!tag)
+		return FLOPPY_ERROR_OUTOFMEMORY;
 
+	floppy_image_read(floppy, header, pos, 1);
+	while(header[0]!=0x1a) {
+		pos++;
+		floppy_image_read(floppy, header, pos, 1);
+	}
+	tag->start_offset = pos + 1;
+	pos++;
+	tag->tracks = 0;
+	tag->heads = 1;
+	do {
+		floppy_image_read(floppy, header, pos, 5);
+		tag->track_offsets[(header[1]<<1) + (header[2] & 1)] = pos;
+		sector_num = header[3];
+		pos += 5 + sector_num; // skip header and sector numbering map
+		if(header[2] & 0x80) pos += sector_num; // skip cylinder numbering map
+		if(header[2] & 0x40) pos += sector_num; // skip head numbering map
+		sector_size = 1 << (header[4] + 7);
+		for(i=0;i<sector_num;i++) {	
+			floppy_image_read(floppy, header, pos, 1); // take sector data type
+			switch(header[0]) {
+				case 0: pos++; break;
+				case 1:
+				case 3:
+				case 5:
+				case 7: pos += sector_size + 1; break;
+				default: pos += 2;break;
+			}			
+		}	
+		tag->tracks += 1;	
+	} while(pos < floppy_image_size(floppy));
+	
 	callbacks = floppy_callbacks(floppy);
 	callbacks->read_sector = imd_read_sector;
 	callbacks->read_indexed_sector = imd_read_indexed_sector;
@@ -160,6 +259,6 @@ FLOPPY_CONSTRUCT( imd_dsk_construct )
 	callbacks->get_tracks_per_disk = imd_get_tracks_per_disk;
 	callbacks->get_indexed_sector_info = imd_get_indexed_sector_info;
 
-	return FLOPPY_ERROR_UNSUPPORTED;
+	return FLOPPY_ERROR_SUCCESS;
 }
 
