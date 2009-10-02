1081939c3aa1da41588806b7b59b3c2573f3ef16
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2009-10-02 10:12:29 +0000

    [XTVGA] Fixed bus problem
    git-svn-id: svn://messdev.no-ip.org/mess@5941 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/pc.c
+++ src/mess/machine/pc.c
@@ -1276,7 +1276,7 @@ DRIVER_INIT( pcjr )
 }
 
 
-
+/*
 static void pc_map_vga_memory(running_machine *machine, offs_t begin, offs_t end, read8_space_func rh, write8_space_func wh)
 {
 	const address_space *space = cpu_get_address_space( machine->firstcpu, ADDRESS_SPACE_PROGRAM );
@@ -1297,14 +1297,14 @@ static void pc_map_vga_memory(running_machine *machine, offs_t begin, offs_t end
 			break;
 	}
 }
-
+*/
 
 static READ8_HANDLER( input_port_0_r ) { return input_port_read(space->machine, "IN0"); }
 
 static const struct pc_vga_interface vga_interface =
 {
 	1,
-	pc_map_vga_memory,
+	NULL,
 
 	input_port_0_r,
 
