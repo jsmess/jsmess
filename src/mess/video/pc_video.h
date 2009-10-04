f5e5635a4e2ceb83e98d8195b4b2ec49e64fa620
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2009-10-04 18:17:14 +0000

    [PC] Video, removed unused code
    git-svn-id: svn://messdev.no-ip.org/mess@5961 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/pc_video.h
+++ src/mess/video/pc_video.h
@@ -25,13 +25,4 @@ WRITE8_HANDLER( pc_video_videoram_w );
 WRITE16_HANDLER( pc_video_videoram16le_w );
 WRITE32_HANDLER( pc_video_videoram32_w );
 
-/* renderers */
-void pc_render_gfx_1bpp(bitmap_t *bitmap, struct mscrtc6845 *crtc,
-	const UINT8 *vram, const UINT16 *palette, int interlace);
-void pc_render_gfx_2bpp(bitmap_t *bitmap, struct mscrtc6845 *crtc,
-	const UINT8 *vram, const UINT16 *palette, int interlace);
-void pc_render_gfx_4bpp(bitmap_t *bitmap, struct mscrtc6845 *crtc,
-	const UINT8 *vram, const UINT16 *palette, int interlace);
-
-
 #endif /* PC_VIDEO_H */
