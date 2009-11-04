23d81145f640f931d7b5478c686bcefddd8172ed
harmony <harmony@9849079f-6229-0410-a07f-84406ce305ba>
2009-11-04 21:44:11 +0000

    [PocketStation] Added controls and fixed interrupts.  User can now get to the main screen and adjust time/date settings. [Harmony]
    git-svn-id: svn://messdev.no-ip.org/mess@6298 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/pockstat.c
+++ src/mess/drivers/pockstat.c
@@ -31,7 +31,7 @@
 #include "cpu/arm7/arm7.h"
 #include "cpu/arm7/arm7core.h"
 
-#define DEFAULT_CLOCK	8000000
+#define DEFAULT_CLOCK	2000000
 
 static const int CPU_FREQ[16] =
 {
@@ -53,8 +53,11 @@ static const int CPU_FREQ[16] =
 	0x7a0000
 };
 
-#define VERBOSE_LEVEL ( 99 )
+#define VERBOSE_LEVEL ( 0 )
 
+#define ENABLE_VERBOSE_LOG (0)
+
+#if ENABLE_VERBOSE_LOG
 INLINE void ATTR_PRINTF(3,4) verboselog( running_machine *machine, int n_level, const char *s_fmt, ... )
 {
 	if( VERBOSE_LEVEL >= n_level )
@@ -67,6 +70,9 @@ INLINE void ATTR_PRINTF(3,4) verboselog( running_machine *machine, int n_level, 
 		logerror( "%s: %s", cpuexec_describe_context(machine), buf );
 	}
 }
+#else
+#define verboselog(x,y,z,...)
+#endif
 
 static UINT32 *lcd_buffer;
 
@@ -75,6 +81,7 @@ static READ32_HANDLER( ps_ftlb_r );
 static WRITE32_HANDLER( ps_ftlb_w );
 
 // Interrupt Controller
+static UINT32 ps_intc_get_interrupt_line(running_machine *machine, UINT32 line);
 static void ps_intc_set_interrupt_line(running_machine *machine, UINT32 line, int state);
 static READ32_HANDLER( ps_intc_r );
 static WRITE32_HANDLER( ps_intc_w );
@@ -90,7 +97,6 @@ static READ32_HANDLER( ps_clock_r );
 static WRITE32_HANDLER( ps_clock_w );
 
 // RTC
-static TIMER_CALLBACK( rtc_int_reset );
 static TIMER_CALLBACK( rtc_tick );
 static READ32_HANDLER( ps_rtc_r );
 static WRITE32_HANDLER( ps_rtc_w );
@@ -114,11 +120,11 @@ typedef struct
 	UINT32 mask;
 } ps_intc_regs_t;
 
-#define PS_INT_BTN_DECISION		0x00000001 // "Decision button" ???
+#define PS_INT_BTN_ACTION		0x00000001 // "Action button"
 #define PS_INT_BTN_RIGHT		0x00000002 // "Right button"
 #define PS_INT_BTN_LEFT			0x00000004 // "Left button"
 #define PS_INT_BTN_DOWN			0x00000008 // "Down button"
-#define PS_INT_BTN_ON			0x00000010 // "Button on" ???
+#define PS_INT_BTN_UP			0x00000010 // "Up button"
 #define PS_INT_UNKNOWN			0x00000020 // "Unknown"
 #define PS_INT_COM				0x00000040 // "COM" ???
 #define PS_INT_TIMER0			0x00000080 // "Timer 0"
@@ -130,6 +136,7 @@ typedef struct
 #define PS_INT_TIMER2			0x00002000 // "Timer 2"
 #define PS_INT_IRQ_MASK			0x00001fbf
 #define PS_INT_FIQ_MASK			0x00002040
+#define PS_INT_STATUS_MASK		0x0000021f
 
 #define MAX_PS_TIMERS	3
 
@@ -162,7 +169,6 @@ typedef struct
 	UINT32 time;
 	UINT32 date;
 	emu_timer *timer;
-	emu_timer *int_timer;
 } ps_rtc_regs_t;
 
 static ps_ftlb_regs_t ftlb_regs;
@@ -271,27 +277,28 @@ static WRITE32_HANDLER( ps_ftlb_w )
 	}
 }
 
+static UINT32 ps_intc_get_interrupt_line(running_machine *machine, UINT32 line)
+{
+	return intc_regs.hold & line;
+}
+
 static void ps_intc_set_interrupt_line(running_machine *machine, UINT32 line, int state)
 {
-	line &= intc_regs.enable;
 	if(line)
 	{
 		if(state)
 		{
-			// This is not right, but I can't find a better description of the difference between "hold" and "status".
-			//printf( "Setting %08x\n", line );
+			intc_regs.status |= line & PS_INT_STATUS_MASK;
+			line &= intc_regs.enable;
+			line &= ~intc_regs.mask;
 			intc_regs.hold |= line;
-			intc_regs.status |= line;
 		}
 		else
 		{
-			// This is not right, but I can't find a better description of the difference between "hold" and "status".
-			//printf( "Setting %08x\n", line );
-			intc_regs.hold &= ~line;
-			intc_regs.status  &= ~line;
+			intc_regs.status &= ~line;
 		}
 	}
-	if(intc_regs.status & PS_INT_IRQ_MASK)
+	if(intc_regs.hold & PS_INT_IRQ_MASK)
 	{
 		cpu_set_input_line(cputag_get_cpu(machine, "maincpu"), ARM7_IRQ_LINE, ASSERT_LINE);
 	}
@@ -299,7 +306,7 @@ static void ps_intc_set_interrupt_line(running_machine *machine, UINT32 line, in
 	{
 		cpu_set_input_line(cputag_get_cpu(machine, "maincpu"), ARM7_IRQ_LINE, CLEAR_LINE);
 	}
-	if(intc_regs.status & PS_INT_FIQ_MASK)
+	if(intc_regs.hold & PS_INT_FIQ_MASK)
 	{
 		cpu_set_input_line(cputag_get_cpu(machine, "maincpu"), ARM7_FIRQ_LINE, ASSERT_LINE);
 	}
@@ -357,7 +364,9 @@ static WRITE32_HANDLER( ps_intc_w )
 			break;
 		case 0x0010/4:
 			verboselog(space->machine, 0, "ps_intc_w: Interrupt Acknowledge = %08x & %08x\n", data, mem_mask );
-			ps_intc_set_interrupt_line(space->machine, data, 0);
+			intc_regs.hold &= ~data;
+			intc_regs.status &= ~data;
+			ps_intc_set_interrupt_line(space->machine, 0, 0);
 			//COMBINE_DATA(&intc_regs.acknowledge);
 			break;
 		default:
@@ -369,6 +378,7 @@ static WRITE32_HANDLER( ps_intc_w )
 static TIMER_CALLBACK( timer_tick )
 {
 	ps_intc_set_interrupt_line(machine, param == 2 ? PS_INT_TIMER2 : (param == 1 ? PS_INT_TIMER1 : PS_INT_TIMER0), 1);
+	//printf( "Timer %d is calling back\n", param );
 	timer_regs.timer[param].count = timer_regs.timer[param].period;
 	ps_timer_start(machine, param);
 }
@@ -392,7 +402,7 @@ static void ps_timer_start(running_machine *machine, int index)
 	}
 	period = attotime_mul(ATTOTIME_IN_HZ(CPU_FREQ[clock_regs.mode & 0x0f] / 2), divisor);
 	period = attotime_mul(period, timer_regs.timer[index].count);
-	timer_adjust_oneshot(timer_regs.timer[index].timer, period, 0);
+	timer_adjust_oneshot(timer_regs.timer[index].timer, period, index);
 }
 
 static READ32_HANDLER( ps_timer_r )
@@ -452,7 +462,7 @@ static WRITE32_HANDLER( ps_timer_w )
 			}
 			else
 			{
-				timer_adjust_oneshot(timer_regs.timer[offset / (0x10/4)].timer, attotime_never, 0);
+				timer_adjust_oneshot(timer_regs.timer[offset / (0x10/4)].timer, attotime_never, offset / (0x10/4));
 			}
 			break;
 		default:
@@ -497,16 +507,50 @@ static WRITE32_HANDLER( ps_clock_w )
 	}
 }
 
-static TIMER_CALLBACK( rtc_int_reset )
-{
-	ps_intc_set_interrupt_line(machine, PS_INT_RTC, 0); // Probably not right
-}
-
 static TIMER_CALLBACK( rtc_tick )
 {
-	ps_intc_set_interrupt_line(machine, PS_INT_RTC, 1);
+	//printf( "RTC is calling back\n" );
+	ps_intc_set_interrupt_line(machine, PS_INT_RTC, ps_intc_get_interrupt_line(machine, PS_INT_RTC) ? 0 : 1);
+	if(!(rtc_regs.mode & 1))
+	{
+		rtc_regs.time++;
+		if((rtc_regs.time & 0x0000000f) == 0x0000000a)
+		{
+			rtc_regs.time &= 0xfffffff0;
+			rtc_regs.time += 0x00000010;
+			if((rtc_regs.time & 0x000000ff) == 0x00000060)
+			{
+				rtc_regs.time &= 0xffffff00;
+				rtc_regs.time += 0x00000100;
+				if((rtc_regs.time & 0x00000f00) == 0x00000a00)
+				{
+					rtc_regs.time &= 0xfffff0ff;
+					rtc_regs.time += 0x00001000;
+					if((rtc_regs.time & 0x0000ff00) == 0x00006000)
+					{
+						rtc_regs.time &= 0xffff00ff;
+						rtc_regs.time += 0x00010000;
+						if((rtc_regs.time & 0x00ff0000) == 0x00240000)
+						{
+							rtc_regs.time &= 0xff00ffff;
+							rtc_regs.time += 0x01000000;
+							if((rtc_regs.time & 0x0f000000) == 0x08000000)
+							{
+								rtc_regs.time &= 0xf0ffffff;
+								rtc_regs.time |= 0x01000000;
+							}
+						}
+						else if((rtc_regs.time & 0x000f0000) == 0x000a0000)
+						{
+							rtc_regs.time &= 0xfff0ffff;
+							rtc_regs.time += 0x00100000;
+						}
+					}
+				}
+			}
+		}
+	}
 	timer_adjust_oneshot(rtc_regs.timer, ATTOTIME_IN_HZ(1), 0);
-	timer_adjust_oneshot(rtc_regs.int_timer, ATTOTIME_IN_HZ(10000), 0); // Probably not right
 }
 
 static READ32_HANDLER( ps_rtc_r )
@@ -520,11 +564,11 @@ static READ32_HANDLER( ps_rtc_r )
 			verboselog(space->machine, 0, "ps_rtc_r: RTC Control = %08x & %08x\n", rtc_regs.control, mem_mask );
 			return rtc_regs.control;
 		case 0x0008/4:
-			verboselog(space->machine, 0, "ps_rtc_r: RTC Time = %08x & %08x\n", 0x01000000, mem_mask );
-			return 0x01000000;
+			verboselog(space->machine, 0, "ps_rtc_r: RTC Time = %08x & %08x\n", rtc_regs.time, mem_mask );
+			return rtc_regs.time;
 		case 0x000c/4:
-			verboselog(space->machine, 0, "ps_rtc_r: RTC Date = %08x & %08x\n", 0x19990101, mem_mask );
-			return 0x00990101;
+			verboselog(space->machine, 0, "ps_rtc_r: RTC Date = %08x & %08x\n", rtc_regs.date, mem_mask );
+			return rtc_regs.date;
 		default:
 			verboselog(space->machine, 0, "ps_rtc_r: Unknown Register %08x & %08x\n", 0x0b800000 + (offset << 2), mem_mask );
 			break;
@@ -539,14 +583,6 @@ static WRITE32_HANDLER( ps_rtc_w )
 		case 0x0000/4:
 			verboselog(space->machine, 0, "ps_rtc_w: RTC Mode = %08x & %08x\n", data, mem_mask );
 			COMBINE_DATA(&rtc_regs.mode);
-			if(rtc_regs.mode & 1)
-			{
-				timer_adjust_oneshot(rtc_regs.timer, ATTOTIME_IN_HZ(1), 0);
-			}
-			else
-			{
-				timer_adjust_oneshot(rtc_regs.timer, attotime_never, 0);
-			}
 			break;
 		case 0x0004/4:
 			verboselog(space->machine, 0, "ps_rtc_w: RTC Control = %08x & %08x\n", data, mem_mask );
@@ -558,6 +594,47 @@ static WRITE32_HANDLER( ps_rtc_w )
 	}
 }
 
+static UINT32 lcd_control;
+
+static READ32_HANDLER( ps_lcd_r )
+{
+	switch(offset)
+	{
+		case 0x0000/4:
+			verboselog(space->machine, 0, "ps_lcd_r: LCD Control = %08x & %08x\n", lcd_control | 0x100, mem_mask );
+			return lcd_control;
+		default:
+			verboselog(space->machine, 0, "ps_lcd_r: Unknown Register %08x & %08x\n", 0x0d000000 + (offset << 2), mem_mask );
+			break;
+	}
+	return 0;
+}
+
+static WRITE32_HANDLER( ps_lcd_w )
+{
+	switch(offset)
+	{
+		case 0x0000/4:
+			verboselog(space->machine, 0, "ps_lcd_w: LCD Control = %08x & %08x\n", data, mem_mask );
+			COMBINE_DATA(&lcd_control);
+			break;
+		default:
+			verboselog(space->machine, 0, "ps_lcd_w: Unknown Register %08x = %08x & %08x\n", 0x0d000000 + (offset << 2), data, mem_mask );
+			break;
+	}
+}
+
+static INPUT_CHANGED( input_update )
+{
+	UINT32 buttons = input_port_read(field->port->machine, "BUTTONS");
+
+	ps_intc_set_interrupt_line(field->port->machine, PS_INT_BTN_ACTION, (buttons &  1) ? 1 : 0);
+	ps_intc_set_interrupt_line(field->port->machine, PS_INT_BTN_RIGHT, 	(buttons &  2) ? 1 : 0);
+	ps_intc_set_interrupt_line(field->port->machine, PS_INT_BTN_LEFT, 	(buttons &  4) ? 1 : 0);
+	ps_intc_set_interrupt_line(field->port->machine, PS_INT_BTN_DOWN, 	(buttons &  8) ? 1 : 0);
+	ps_intc_set_interrupt_line(field->port->machine, PS_INT_BTN_UP, 	(buttons & 16) ? 1 : 0);
+}
+
 static ADDRESS_MAP_START(pockstat_mem, ADDRESS_SPACE_PROGRAM, 32)
 	AM_RANGE(0x00000000, 0x000007ff) AM_RAM
 	AM_RANGE(0x04000000, 0x04003fff) AM_ROM AM_REGION("maincpu", 0)
@@ -566,6 +643,7 @@ static ADDRESS_MAP_START(pockstat_mem, ADDRESS_SPACE_PROGRAM, 32)
 	AM_RANGE(0x0a800000, 0x0a80002b) AM_READWRITE(ps_timer_r, ps_timer_w)
 	AM_RANGE(0x0b000000, 0x0b000007) AM_READWRITE(ps_clock_r, ps_clock_w)
 	AM_RANGE(0x0b800000, 0x0b80000f) AM_READWRITE(ps_rtc_r, ps_rtc_w)
+	AM_RANGE(0x0d000000, 0x0d000003) AM_READWRITE(ps_lcd_r, ps_lcd_w)
 	AM_RANGE(0x0d000100, 0x0d00017f) AM_RAM AM_BASE(&lcd_buffer)
 /*  AM_RANGE(0x02000000, 0x0201ffff) Flash ROM
     AM_RANGE(0x08000000, 0x0801ffff) Same as 0x02 above. Mirrors every 128KB.
@@ -580,6 +658,13 @@ ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( pockstat )
+	PORT_START("BUTTONS")
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON1) 		PORT_NAME("Action Button")	PORT_CHANGED(input_update, 0)
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_JOYSTICK_RIGHT) PORT_NAME("Right")			PORT_CHANGED(input_update, 0)
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_JOYSTICK_LEFT) 	PORT_NAME("Left")			PORT_CHANGED(input_update, 0)
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_JOYSTICK_DOWN) 	PORT_NAME("Down")			PORT_CHANGED(input_update, 0)
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_JOYSTICK_UP) 	PORT_NAME("Up")				PORT_CHANGED(input_update, 0)
+	PORT_BIT( 0xe0, IP_ACTIVE_HIGH, IPT_UNUSED)
 INPUT_PORTS_END
 
 static MACHINE_START( pockstat )
@@ -591,11 +676,11 @@ static MACHINE_START( pockstat )
 		timer_adjust_oneshot(timer_regs.timer[index].timer, attotime_never, index);
 	}
 
-	rtc_regs.timer = timer_alloc(machine, rtc_tick, 0);
-	timer_adjust_oneshot(rtc_regs.timer, attotime_never, index);
+	rtc_regs.time = 0x01000000;
+	rtc_regs.date = 0x19990101;
 
-	rtc_regs.int_timer = timer_alloc(machine, rtc_int_reset, 0);
-	timer_adjust_oneshot(rtc_regs.int_timer, attotime_never, index);
+	rtc_regs.timer = timer_alloc(machine, rtc_tick, 0);
+	timer_adjust_oneshot(rtc_regs.timer, ATTOTIME_IN_HZ(1), index);
 }
 
 static MACHINE_RESET( pockstat )
@@ -656,4 +741,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME      PARENT  COMPAT  MACHINE    INPUT     INIT  CONFIG     COMPANY                             FULLNAME       FLAGS */
-CONS( 1999, pockstat, 0,      0,      pockstat,  pockstat, 0,    0,  "Sony Computer Entertainment Inc.", "Sony PocketStation", GAME_NOT_WORKING)
+CONS( 1999, pockstat, 0,      0,      pockstat,  pockstat, 0,    0,  "Sony Computer Entertainment Inc.", "Sony PocketStation", GAME_NOT_WORKING )
