3337308854c2fb1764b52bc5e0c8947e3a8f0d8a
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2009-11-18 19:51:27 +0000

    [EXL100] Corrected cpu clocks.
    git-svn-id: svn://messdev.no-ip.org/mess@6417 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/exelv.c
+++ src/mess/drivers/exelv.c
@@ -561,12 +561,12 @@ INPUT_PORTS_END
 static MACHINE_DRIVER_START(exelv)
 	/* basic machine hardware */
 	/* TMS7020 CPU @ 4.91(?) MHz */
-	MDRV_CPU_ADD("maincpu", TMS7000_EXL, 4910000)
+	MDRV_CPU_ADD("maincpu", TMS7000_EXL, XTAL_4_9152MHz)
 	MDRV_CPU_PROGRAM_MAP(exelv_memmap)
 	MDRV_CPU_IO_MAP(exelv_portmap)
 	MDRV_CPU_VBLANK_INT_HACK(exelv_hblank_interrupt, 363)
 
-	MDRV_CPU_ADD("tms7040", TMS7000, 4910000)
+	MDRV_CPU_ADD("tms7040", TMS7000, XTAL_4_9152MHz)
 	MDRV_CPU_PROGRAM_MAP(exelv_tms7040_map)
 	MDRV_CPU_IO_MAP(exelv_tms7040_port)
 
