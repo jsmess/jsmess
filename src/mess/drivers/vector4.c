c4f4148f98be72c7fd990b4e428c67fa4c987aad
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2009-12-09 09:01:04 +0000

    [VECTOR4] Hooked terminal
    git-svn-id: svn://messdev.no-ip.org/mess@6591 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/vector4.c
+++ src/mess/drivers/vector4.c
@@ -8,6 +8,28 @@
 
 #include "driver.h"
 #include "cpu/z80/z80.h"
+#include "machine/terminal.h"
+
+static UINT8 received_char = 0;
+
+static WRITE8_HANDLER(vector_terminal_w)
+{
+	const device_config	*devconf = devtag_get_device(space->machine, "terminal");
+	terminal_write(devconf,0,data);
+}
+
+static READ8_HANDLER(vector_terminal_status_r)
+{
+	if (received_char!=0) return 3; // char received
+	return 1; // ready
+}
+
+static READ8_HANDLER(vector_terminal_r)
+{
+	UINT8 retVal = received_char;
+	received_char = 0;
+	return retVal;
+}
 
 static ADDRESS_MAP_START(vector4_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
@@ -15,31 +37,38 @@ static ADDRESS_MAP_START(vector4_mem, ADDRESS_SPACE_PROGRAM, 8)
 	AM_RANGE(0xe000, 0xefff) AM_ROM
 	AM_RANGE(0xf000, 0xf7ff) AM_RAM
 	AM_RANGE(0xf800, 0xf8ff) AM_ROM
-	AM_RANGE(0xf900, 0xffff) AM_RAM
+	AM_RANGE(0xfc00, 0xffff) AM_RAM
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( vector4_io , ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x02, 0x02) AM_READWRITE(vector_terminal_r,vector_terminal_w)
+	AM_RANGE(0x03, 0x03) AM_READ(vector_terminal_status_r)
 ADDRESS_MAP_END
 
 /* Input ports */
 INPUT_PORTS_START( vector4 )
+	PORT_INCLUDE(generic_terminal)
 INPUT_PORTS_END
 
 
 static MACHINE_RESET(vector4) 
 {	
+	received_char = 0;
 	cpu_set_reg(cputag_get_cpu(machine, "maincpu"), Z80_PC, 0xe000);
 }
 
-static VIDEO_START( vector4 )
+static WRITE8_DEVICE_HANDLER( vector4_kbd_put )
 {
+	received_char = data;
 }
 
-static VIDEO_UPDATE( vector4 )
+static GENERIC_TERMINAL_INTERFACE( vector4_terminal_intf )
 {
-    return 0;
-}
+	DEVCB_HANDLER(vector4_kbd_put)
+};
+
 
 static MACHINE_DRIVER_START( vector4 )
     /* basic machine hardware */
@@ -50,34 +79,25 @@ static MACHINE_DRIVER_START( vector4 )
     MDRV_MACHINE_RESET(vector4)
 	
     /* video hardware */
-    MDRV_SCREEN_ADD("screen", RASTER)
-    MDRV_SCREEN_REFRESH_RATE(50)
-    MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MDRV_SCREEN_SIZE(640, 480)
-    MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-    MDRV_PALETTE_LENGTH(2)
-    MDRV_PALETTE_INIT(black_and_white)
-
-    MDRV_VIDEO_START(vector4)
-    MDRV_VIDEO_UPDATE(vector4)
+    MDRV_IMPORT_FROM( generic_terminal )
+	MDRV_GENERIC_TERMINAL_ADD(TERMINAL_TAG,vector4_terminal_intf)
 MACHINE_DRIVER_END
-
-static SYSTEM_CONFIG_START(vector4)
-SYSTEM_CONFIG_END
 
 /* ROM definition */
 ROM_START( vector4 )
     ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-    ROM_LOAD( "vg-em-43.bin", 0xe000, 0x1000, CRC(29a0fcee) SHA1(ca44de527f525b72f78b1c084c39aa6ce21731b5))
-//  ROM_LOAD( "vg40cl_ihl.bin", 0xe000, 0x0400, CRC(dcaf79e6) SHA1(63619ddb12ff51e5862902fb1b33a6630f555ad7))
-//  ROM_LOAD( "vg40ch_ihl.bin", 0xe400, 0x0400, CRC(3ff97d70) SHA1(b401e49aa97ac106c2fd5ee72d89e683ebe34e34))
-//  ROM_LOAD( "vg-zcb50.bin", 0xe000, 0x1000, CRC(22d692ce) SHA1(cbb21b0acc98983bf5febd59ff67615d71596e36))
+	ROM_SYSTEM_BIOS( 0, "v4c", "ver 4.0c" )
+    ROMX_LOAD( "vg40cl_ihl.bin", 0xe000, 0x0400, CRC(dcaf79e6) SHA1(63619ddb12ff51e5862902fb1b33a6630f555ad7), ROM_BIOS(1))	
+	ROMX_LOAD( "vg40ch_ihl.bin", 0xe400, 0x0400, CRC(3ff97d70) SHA1(b401e49aa97ac106c2fd5ee72d89e683ebe34e34), ROM_BIOS(1))
+	ROM_SYSTEM_BIOS( 1, "v43", "ver 4.3" )
+    ROMX_LOAD( "vg-em-43.bin",   0xe000, 0x1000, CRC(29a0fcee) SHA1(ca44de527f525b72f78b1c084c39aa6ce21731b5), ROM_BIOS(2))
+	ROM_SYSTEM_BIOS( 2, "v5", "ver 5.0" )
+	ROMX_LOAD( "vg-zcb50.bin",   0xe000, 0x1000, CRC(22d692ce) SHA1(cbb21b0acc98983bf5febd59ff67615d71596e36), ROM_BIOS(3))
     ROM_LOAD( "mfdc.bin", 0xf800, 0x0100, CRC(d82a40d6) SHA1(cd1ef5fb0312cd1640e0853d2442d7d858bc3e3b))
 ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG COMPANY   FULLNAME       FLAGS */
-COMP( ????, vector4,  0,       0, 	vector4, 	vector4, 	 0,  	  vector4,  	 "Vector Graphics",   "Vector 4",		GAME_NOT_WORKING)
+/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG 	COMPANY   				FULLNAME       FLAGS */
+COMP( ????, vector4,  0,       0, 	vector4, 	vector4,	0,  	0,  	 "Vector Graphics",   "Vector 4",		GAME_NOT_WORKING)
 
