0e6292c2b4849e8e0e54fcbee3a44ed76f84603d
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2009-12-30 12:45:28 +0000

    [MESSUI] Fixed long-standing problem of messui writing partial ini files for clones and not respecting the Default System Options if they had been changed.
    git-svn-id: svn://messdev.no-ip.org/mess@6825 9849079f-6229-0410-a07f-84406ce305ba

--- src/osd/winui/mui_opts.c
+++ src/osd/winui/mui_opts.c
@@ -2289,7 +2289,8 @@ static file_error SaveSettingsFile(core_options *opts, core_options *baseopts, c
 		filerr = core_fopen(filename, OPEN_FLAG_WRITE | OPEN_FLAG_CREATE | OPEN_FLAG_CREATE_PATHS, &file);
 		if (filerr == FILERR_NONE)
 		{
-			options_output_diff_ini_file(opts, baseopts, file);
+			options_output_ini_file(opts, file);	/* required for MESS */
+//			options_output_diff_ini_file(opts, baseopts, file);
 			core_fclose(file);
 		}
 	}
