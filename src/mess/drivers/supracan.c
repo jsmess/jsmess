e69d1e7bb5a15bf3425dab0d672954e74317de86
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-02 01:21:13 +0000

    [SuperACan]: added basic video/palette hook-up
    git-svn-id: svn://messdev.no-ip.org/mess@6849 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/supracan.c
+++ src/mess/drivers/supracan.c
@@ -25,14 +25,41 @@ static UINT8 *supracan_soundram;
 static UINT16 *supracan_soundram_16;
 static UINT16 supracan_video_regs[256];
 static emu_timer *supracan_video_timer;
-
+static UINT16 *supracan_charram, *supracan_vram;
 
 static READ16_HANDLER( supracan_unk1_r );
 static WRITE16_HANDLER( supracan_soundram_w );
 static WRITE16_HANDLER( supracan_sound_w );
 static READ16_HANDLER( supracan_video_r );
 static WRITE16_HANDLER( supracan_video_w );
+static WRITE16_HANDLER( supracan_char_w );
 
+static VIDEO_START( supracan )
+{
+
+}
+
+static VIDEO_UPDATE( supracan )
+{
+	int x,y,count;
+	const gfx_element *gfx = screen->machine->gfx[0];
+
+	count = 0;
+
+	for (y=0;y<16;y++)
+	{
+		for (x=0;x<16;x++)
+		{
+			int tile;
+
+			tile = (supracan_vram[count] & 0x00ff);
+			drawgfx_opaque(bitmap,cliprect,gfx,tile,0,0,0,x*8,y*8);
+			count++;
+		}
+	}
+
+	return 0;
+}
 
 static ADDRESS_MAP_START( supracan_mem, ADDRESS_SPACE_PROGRAM, 16 )
 	AM_RANGE( 0x000000, 0x07ffff ) AM_ROM AM_REGION( "cart", 0 )
@@ -41,10 +68,10 @@ static ADDRESS_MAP_START( supracan_mem, ADDRESS_SPACE_PROGRAM, 16 )
 	AM_RANGE( 0xe8f000, 0xe8ffff ) AM_RAM_WRITE( supracan_soundram_w ) AM_BASE( &supracan_soundram_16 )
 	AM_RANGE( 0xe90000, 0xe9001f ) AM_WRITE( supracan_sound_w )
 	AM_RANGE( 0xf00000, 0xf001ff ) AM_READWRITE( supracan_video_r, supracan_video_w )
-	AM_RANGE( 0xf00200, 0xf003ff ) AM_RAM	/* Unknown, some data gets copied here during boot */
-	AM_RANGE( 0xf40000, 0xf43fff ) AM_RAM	/* Unknown, some data gets copied here during boot */
+	AM_RANGE( 0xf00200, 0xf003ff ) AM_RAM_WRITE(paletteram16_xBBBBBGGGGGRRRRR_word_w) AM_BASE_GENERIC(paletteram)
+	AM_RANGE( 0xf40000, 0xf43fff ) AM_RAM_WRITE(supracan_char_w) AM_BASE(&supracan_charram)	/* Unknown, some data gets copied here during boot */
 	AM_RANGE( 0xf44000, 0xf441ff ) AM_RAM	/* Unknown */
-	AM_RANGE( 0xf44200, 0xf443ff ) AM_RAM	/* Unknown, some data gets copied here during boot. Possibly a tilemap?? */
+	AM_RANGE( 0xf44200, 0xf443ff ) AM_RAM AM_BASE(&supracan_vram)	/* Unknown, some data gets copied here during boot. Possibly a tilemap?? */
 	AM_RANGE( 0xf44400, 0xf445ff ) AM_RAM	/* Unknown, some data gets copied here during boot. Looks like the Functech logo is here */
 	AM_RANGE( 0xf44600, 0xf44fff ) AM_RAM	/* Unknown, stuff gets written here. Zoom table?? */
 	AM_RANGE( 0xfc0000, 0xfcffff ) AM_RAM	/* System work ram */
@@ -57,6 +84,19 @@ static ADDRESS_MAP_START( supracan_sound_mem, ADDRESS_SPACE_PROGRAM, 8 )
 	AM_RANGE( 0xf000, 0xffff ) AM_RAM AM_BASE( &supracan_soundram )
 ADDRESS_MAP_END
 
+static WRITE16_HANDLER( supracan_char_w )
+{
+	COMBINE_DATA(&supracan_charram[offset]);
+
+	{
+		UINT8 *gfx = memory_region(space->machine, "ram_gfx");
+
+		gfx[offset*2+0] = (supracan_charram[offset] & 0xff00) >> 8;
+		gfx[offset*2+1] = (supracan_charram[offset] & 0x00ff) >> 0;
+
+		gfx_element_mark_dirty(space->machine->gfx[0], offset/64);
+	}
+}
 
 static INPUT_PORTS_START( supracan )
 INPUT_PORTS_END
@@ -64,6 +104,7 @@ INPUT_PORTS_END
 
 static PALETTE_INIT( supracan )
 {
+	#if 0
 	int i, r, g, b;
 
 	for( i = 0; i < 32768; i++ )
@@ -74,6 +115,7 @@ static PALETTE_INIT( supracan )
 		b = ((i >> 10) & 0x1f) << 3;
 		palette_set_color_rgb( machine, i, r, g, b );
 	}
+	#endif
 }
 
 
@@ -186,6 +228,22 @@ static MACHINE_RESET( supracan )
 	timer_adjust_oneshot( supracan_video_timer, video_screen_get_time_until_pos( machine->primary_screen, 0, 0 ), 0 );
 }
 
+static const gfx_layout supracan_gfxlayout =
+{
+	8,8,
+	RGN_FRAC(1,1),
+	8,
+	{ STEP8(0,1) },
+	{ STEP8(0,8) },
+	{ STEP8(0,8*8) },
+	8*8*8
+};
+
+
+static GFXDECODE_START( supracan )
+	GFXDECODE_ENTRY( "ram_gfx", 0, supracan_gfxlayout,   0, 1 )
+GFXDECODE_END
+
 
 static MACHINE_DRIVER_START( supracan )
 	MDRV_CPU_ADD( "maincpu", M68000, XTAL_10_738635MHz )		/* Correct frequency unknown */
@@ -203,16 +261,22 @@ static MACHINE_DRIVER_START( supracan )
 
 	MDRV_PALETTE_LENGTH( 32768 )
 	MDRV_PALETTE_INIT( supracan )
+	MDRV_GFXDECODE( supracan )
 
 	MDRV_CARTSLOT_ADD( "cart" )
 	MDRV_CARTSLOT_EXTENSION_LIST( "bin" )
 	MDRV_CARTSLOT_MANDATORY
 	MDRV_CARTSLOT_LOAD( supracan_cart )
+
+	MDRV_VIDEO_START( supracan )
+	MDRV_VIDEO_UPDATE( supracan )
 MACHINE_DRIVER_END
 
 
 ROM_START( supracan )
 	ROM_REGION( 0x80000, "cart", ROMREGION_ERASEFF )
+
+	ROM_REGION( 0x4000, "ram_gfx", ROMREGION_ERASEFF )
 ROM_END
 
 
