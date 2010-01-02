cfc473b3cc0caaf4fb2a6b4774bcd61f4e7656ba
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-02 06:30:18 +0000

    [MC6845] Reapply r6618 again.
    git-svn-id: svn://messdev.no-ip.org/mess@6854 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/video/mc6845.c
+++ src/emu/video/mc6845.c
@@ -330,9 +330,9 @@ WRITE8_DEVICE_HANDLER( mc6845_register_w )
 	}
 
 	/* display message if the Mode Control register is not zero */
-	if ((mc6845->register_address_latch == 0x08) && (mc6845->mode_control != 0))
-		if (!supports_transparent[mc6845->device_type])
-			popmessage("Mode Control %02X is not supported!!!", mc6845->mode_control);
+//	if ((mc6845->register_address_latch == 0x08) && (mc6845->mode_control != 0))
+//		if (!supports_transparent[mc6845->device_type])
+//			popmessage("Mode Control %02X is not supported!!!", mc6845->mode_control);
 
 	recompute_parameters(mc6845, FALSE);
 }
@@ -846,10 +846,10 @@ void mc6845_update(const device_config *device, bitmap_t *bitmap, const rectangl
 		if (mc6845->intf->end_update != NULL)
 			mc6845->intf->end_update(device, bitmap, cliprect, param);
 
-		popmessage(NULL);
+//		popmessage(NULL);
 	}
-	else
-		popmessage("Invalid MC6845 screen parameters - display disabled!!!");
+//	else
+//		popmessage("Invalid MC6845 screen parameters - display disabled!!!");
 }
 
 
