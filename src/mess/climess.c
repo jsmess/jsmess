297d797da35b274f2aa0a4da6f19e0980afc627f
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-07 06:00:15 +0000

    [CLI] -listmedia will indicate if a driver has no devices, or if no matching drivers were found. Fixes bug 1770.
    git-svn-id: svn://messdev.no-ip.org/mess@6964 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/climess.c
+++ src/mess/climess.c
@@ -36,7 +36,8 @@ void mess_display_help(void)
 		"See config.txt and windows.txt for usage instructions.\n");
 }
 
-
+/* New listmedia is below - left this here just in case.. */
+#if 0
 /*-------------------------------------------------
     info_listdevices - list all devices in MESS
 -------------------------------------------------*/
@@ -99,6 +100,78 @@ int info_listdevices(core_options *opts, const char *gamename)
 	exit_resource_tracking();
 	return 0;
 }
+#endif
 
 
+/*-------------------------------------------------
+    cli_info_listdevices - output the list of
+    devices referenced by a given game or set of
+    games
+-------------------------------------------------*/
 
+int info_listdevices(core_options *options, const char *gamename)
+{
+	int count = 0, devcount;
+	int drvindex;
+	machine_config *config;
+	const device_config *dev;
+	image_device_info info;
+	const char *src;
+	const char *driver_name;
+	const char *name;
+	const char *shortname;
+	char paren_shortname[16];
+
+	/* since we expand the machine driver, we need to set things up */
+	init_resource_tracking();
+
+	printf(" SYSTEM      DEVICE NAME (brief)   IMAGE FILE EXTENSIONS SUPPORTED    \n");
+	printf("----------  --------------------  ------------------------------------\n");
+
+	/* iterate over drivers */
+	for (drvindex = 0; drivers[drvindex] != NULL; drvindex++)
+		if (mame_strwildcmp(gamename, drivers[drvindex]->name) == 0)
+		{
+			/* allocate the machine config */
+			config = machine_config_alloc(drivers[drvindex]->machine_config);
+
+			driver_name = drivers[drvindex]->name;
+
+			devcount = 0;
+
+			for (dev = image_device_first(config); dev != NULL; dev = image_device_next(dev))
+			{
+				info = image_device_getinfo(config, dev);
+
+				src = info.file_extensions;
+				name = info.instance_name;
+				shortname = info.brief_instance_name;
+
+				sprintf(paren_shortname, "(%s)", shortname);
+
+				printf("%-13s%-12s%-8s   ", driver_name, name, paren_shortname);
+				driver_name = " ";
+
+				while (src && *src)
+				{
+					printf(".%-5s", src);
+					src += strlen(src) + 1;
+					devcount++;
+				}
+				printf("\n");
+			}
+			if (!devcount)
+				printf("%-13s(none)\n",driver_name);
+
+			count++;
+			machine_config_free(config);
+		}
+
+	/* clean up our tracked resources */
+	exit_resource_tracking();
+
+	if (!count)
+		printf("There are no Computers or Consoles named %s\n", gamename);
+
+	return (count > 0) ? MAMERR_NONE : MAMERR_NO_SUCH_GAME;
+}
