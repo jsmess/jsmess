fe1a9d86114f948bcbc683966d7be5d56bce0de4
npwoods <npwoods@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-10 13:41:19 +0000

    Prep for next dev cycle
    git-svn-id: svn://messdev.no-ip.org/mess@6992 9849079f-6229-0410-a07f-84406ce305ba

--- src/version.c
+++ src/version.c
@@ -4,9 +4,9 @@
 
     Version string source file for MAME.
 
-    Copyright (c) 1996-2008, Nicola Salmoria and the MAME Team.
+    Copyright (c) 1996-2010, Nicola Salmoria and the MAME Team.
     Visit http://mamedev.org for licensing and usage restrictions.
 
 ***************************************************************************/
 
-const char build_version[] = "0.136 ("__DATE__")";
+const char build_version[] = "0.136.1 ("__DATE__")";
