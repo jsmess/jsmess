04d5b199771650c861002cbd2f13fc28241bc7d6
etabeta <etabeta@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-16 13:32:30 +0000

    misc list cleanups (thx haze for pointing out gmaster issue)
    git-svn-id: svn://messdev.no-ip.org/mess@7048 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/software/gamecom_cart.c
+++ src/mess/software/gamecom_cart.c
@@ -16,48 +16,55 @@ SOFTWARE_START( set ) \
 SOFTWARE_END
 
 
-GAMECOM_ROM_LOAD( batmanr,  "batman & robin (1997).bin",                                              0x000000,  0x200000,	 CRC(b336e530) SHA1(066d5cddce9729044ad598ad3cc3489a834f5197) )
-GAMECOM_ROM_LOAD( centiped, "centipede (1999)(hasbro - tiger electronics).bin",                                          0x000000,  0x40000,	 CRC(1a041ff0) SHA1(b3d50e0cca6e2a539fba5883854aaf26424ab382) )
-GAMECOM_ROM_LOAD( dukenuke, "duke nukem 3d (1997).bin",                                               0x000000,  0x200000,	 CRC(884568d1) SHA1(9aaa1cf3b12f14201079dfbdd4c17fe868972de6) )
-GAMECOM_ROM_LOAD( fightmix, "fighters megamix (1998)(sega).bin",                                                         0x000000,  0x200000,	 CRC(061e943f) SHA1(c165e5f4b61341ae0e8a4d141b28e18108942326) )
-GAMECOM_ROM_LOAD( frogger,  "frogger (1999)(hasbro - tiger electronics).bin",                                            0x000000,  0x100000,	 CRC(10e4c5cd) SHA1(8224d3e71a3da2f4029bee8b62b4a0c108c054e4) )
-GAMECOM_ROM_LOAD( henry,    "henry (1997).bin",                                                       0x000000,  0x100000,	 CRC(75d3c8d8) SHA1(b72a03af1dec0c621f9eda89ded3845724bb34ac) )
-GAMECOM_ROM_LOAD( indy500,  "indy 500 (1997).bin",                                                    0x000000,  0x100000,	 CRC(845ed281) SHA1(117bfa1c831fbcf4a1d507cb70b6e0636c29bbd3) )
-GAMECOM_ROM_LOAD( jeopardy, "jeopardy! (1997).bin",                                                   0x000000,  0x100000,	 CRC(8c512912) SHA1(c60ac5e59c3cc092506bc47adb60b40a9415bc97) )
-GAMECOM_ROM_LOAD( lightout,  "lights out (1997).bin",                                                 0x000000,  0x8000,	 CRC(496efa62) SHA1(1d91b616caa2d35f53ec5680aada8f75e2f0ac43) )
-GAMECOM_ROM_LOAD( lostwrld, "lost world, the - jurassic park (1997).bin",                             0x000000,  0x200000,	 CRC(b422c5f8) SHA1(648badcac6c386d7ff40f917e34bd309e737c027) )
-GAMECOM_ROM_LOAD( monopoly, "monopoly (1999).bin",                                                    0x000000,  0x200000,	 CRC(d6a65a07) SHA1(8057144f6995eafff6d893383114b48f4c77abc3) )
-GAMECOM_ROM_LOAD( mktrilgy, "mortal kombat trilogy (1997)(midway).bin",                                                  0x000000,  0x1c0000,	 CRC(72893a77) SHA1(d8a403a180f4a397098c8a598dd6cd5b45ef377a) )
-GAMECOM_ROM_LOAD( quizwiz,  "quiz wiz - cyber trivia (1997).bin",                                     0x000000,  0x100000,	 CRC(0e018f6a) SHA1(c9c4435bdcea2b9ddaa2768f28f286abd382608d) )
-GAMECOM_ROM_LOAD( re2,      "resident evil 2 (1998)(capcom).bin",                                                        0x000000,  0x1c0000,	 CRC(4f09457d) SHA1(1d4d4ff13c8f7c228f30bddab174d4781b370ec9) )
-GAMECOM_ROM_LOAD( scrabble, "scrabble (1999)(hasbro - tiger electronics).bin",                                           0x000000,  0x200000,	 CRC(4d4ad909) SHA1(2735d59964fe7c1d43bd260df86b9dfbc1bfd736) )
-GAMECOM_ROM_LOAD( sonicjam, "sonic jam (1998)(sega).bin",                                                                0x000000,  0x200000,	 CRC(8b4a63f9) SHA1(393a98138c8825ed61990e1b15e1967f87068ac6) )
-GAMECOM_ROM_LOAD( tcasino,  "tiger casino (1998).bin",                                                0x000000,  0x1c0000,	 CRC(ba698142) SHA1(32ce1be96f7f367b8a0adb8cc61a8cca7d6b79dd) )
-GAMECOM_ROM_LOAD( wheelfor, "wheel of fortune (1997).bin",                                            0x000000,  0x80000,	 CRC(048347a0) SHA1(298c2b87754dd93481feea1d9e01585a1e269770) )
-GAMECOM_ROM_LOAD( wheelfo2, "wheel of fortune 2 (1998).bin",                                          0x000000,  0x80000,	 CRC(b91e2e32) SHA1(30ddd349b8bdc21ef7ada4772ab1f3a9318ab0c8) )
-GAMECOM_ROM_LOAD( arcadecl, "williams arcade classics (1997).bin",                                    0x000000,  0x100000,	 CRC(f894065d) SHA1(5e2164fa76546b2795af7b04a766e31784b82eca) )
+GAMECOM_ROM_LOAD( batmanr,  "batman & robin (1997).bin",                                           0x000000,  0x200000,	 CRC(b336e530) SHA1(066d5cddce9729044ad598ad3cc3489a834f5197) )
+GAMECOM_ROM_LOAD( centiped, "centipede (1999)(hasbro - tiger electronics).bin",                    0x000000,  0x40000,	 CRC(1a041ff0) SHA1(b3d50e0cca6e2a539fba5883854aaf26424ab382) )
+GAMECOM_ROM_LOAD( dukenuke, "duke nukem 3d (1997).bin",                                            0x000000,  0x200000,	 CRC(884568d1) SHA1(9aaa1cf3b12f14201079dfbdd4c17fe868972de6) )
+GAMECOM_ROM_LOAD( fightmix, "fighters megamix (1998)(sega).bin",                                   0x000000,  0x200000,	 CRC(061e943f) SHA1(c165e5f4b61341ae0e8a4d141b28e18108942326) )
+GAMECOM_ROM_LOAD( frogger,  "frogger (1999)(hasbro - tiger electronics).bin",                      0x000000,  0x100000,	 CRC(10e4c5cd) SHA1(8224d3e71a3da2f4029bee8b62b4a0c108c054e4) )
+GAMECOM_ROM_LOAD( henry,    "henry (1997).bin",                                                    0x000000,  0x100000,	 CRC(75d3c8d8) SHA1(b72a03af1dec0c621f9eda89ded3845724bb34ac) )
+GAMECOM_ROM_LOAD( indy500,  "indy 500 (1997).bin",                                                 0x000000,  0x100000,	 CRC(845ed281) SHA1(117bfa1c831fbcf4a1d507cb70b6e0636c29bbd3) )
+GAMECOM_ROM_LOAD( jeopardy, "jeopardy! (1997).bin",                                                0x000000,  0x100000,	 CRC(8c512912) SHA1(c60ac5e59c3cc092506bc47adb60b40a9415bc97) )
+GAMECOM_ROM_LOAD( lightout, "lights out (1997).bin",                                               0x000000,  0x8000,	 CRC(496efa62) SHA1(1d91b616caa2d35f53ec5680aada8f75e2f0ac43) )
+GAMECOM_ROM_LOAD( lostwrld, "lost world, the - jurassic park (1997).bin",                          0x000000,  0x200000,	 CRC(b422c5f8) SHA1(648badcac6c386d7ff40f917e34bd309e737c027) )
+GAMECOM_ROM_LOAD( monopoly, "monopoly (1999).bin",                                                 0x000000,  0x200000,	 CRC(d6a65a07) SHA1(8057144f6995eafff6d893383114b48f4c77abc3) )
+GAMECOM_ROM_LOAD( mktrilgy, "mortal kombat trilogy (1997)(midway).bin",                            0x000000,  0x1c0000,	 CRC(72893a77) SHA1(d8a403a180f4a397098c8a598dd6cd5b45ef377a) )
+GAMECOM_ROM_LOAD( quizwiz,  "quiz wiz - cyber trivia (1997).bin",                                  0x000000,  0x100000,	 CRC(0e018f6a) SHA1(c9c4435bdcea2b9ddaa2768f28f286abd382608d) )
+GAMECOM_ROM_LOAD( re2,      "resident evil 2 (1998)(capcom).bin",                                  0x000000,  0x1c0000,	 CRC(4f09457d) SHA1(1d4d4ff13c8f7c228f30bddab174d4781b370ec9) )
+GAMECOM_ROM_LOAD( scrabble, "scrabble (1999)(hasbro - tiger electronics).bin",                     0x000000,  0x200000,	 CRC(4d4ad909) SHA1(2735d59964fe7c1d43bd260df86b9dfbc1bfd736) )
+GAMECOM_ROM_LOAD( sonicjam, "sonic jam (1998)(sega).bin",                                          0x000000,  0x200000,	 CRC(8b4a63f9) SHA1(393a98138c8825ed61990e1b15e1967f87068ac6) )
+GAMECOM_ROM_LOAD( tcasino,  "tiger casino (1998).bin",                                             0x000000,  0x1c0000,	 CRC(ba698142) SHA1(32ce1be96f7f367b8a0adb8cc61a8cca7d6b79dd) )
+GAMECOM_ROM_LOAD( wheelfor, "wheel of fortune (1997).bin",                                         0x000000,  0x80000,	 CRC(048347a0) SHA1(298c2b87754dd93481feea1d9e01585a1e269770) )
+GAMECOM_ROM_LOAD( wheelfo2, "wheel of fortune 2 (1998).bin",                                       0x000000,  0x80000,	 CRC(b91e2e32) SHA1(30ddd349b8bdc21ef7ada4772ab1f3a9318ab0c8) )
+GAMECOM_ROM_LOAD( arcadecl, "williams arcade classics (1997).bin",                                 0x000000,  0x100000,	 CRC(f894065d) SHA1(5e2164fa76546b2795af7b04a766e31784b82eca) )
+
+GAMECOM_ROM_LOAD( internet, "internet (1997)(tiger electronics).zip",                              0x000000,  0x40000,	 CRC(7e721a84) SHA1(4c6b5b91a70397cb5e1add4ab53afe3ec0fe2072) )
+GAMECOM_ROM_LOAD( weblink,  "web link (1997)(tiger electronics).zip",                              0x000000,  0x40000,	 CRC(4c13e65b) SHA1(d4e08fc050c006ed6c1a177a1f4c41b4a68c26bb) )
+
 
 SOFTWARE_LIST_START( gamecom_cart )
-	SOFTWARE( batmanr,  0       , 1997, "Tiger Electronics", "Batman & Robin", 0, 0 )
-	SOFTWARE( centiped, 0       , 1999, "Tiger Electronics", "Centipede", 0, 0 )
-	SOFTWARE( dukenuke, 0       , 1997, "Tiger Electronics", "Duke Nukem 3D", 0, 0 )
-	SOFTWARE( fightmix, 0       , 1998, "Tiger Electronics", "Fighters Megamix", 0, 0 )
-	SOFTWARE( frogger,  0       , 1999, "Tiger Electronics", "Frogger", 0, 0 )
-	SOFTWARE( henry,    0       , 1997, "Tiger Electronics", "Henry", 0, 0 )
-	SOFTWARE( indy500,  0       , 1997, "Tiger Electronics", "Indy 500", 0, 0 )
-	SOFTWARE( jeopardy, 0       , 1997, "Tiger Electronics", "Jeopardy!", 0, 0 )
-	SOFTWARE( lightout, 0       , 1997, "Tiger Electronics", "Lights Out", 0, 0 )
-	SOFTWARE( lostwrld, 0       , 1997, "Tiger Electronics", "Lost World, The - Jurassic Park", 0, 0 )
-	SOFTWARE( monopoly, 0       , 1999, "Tiger Electronics", "Monopoly", 0, 0 )
-	SOFTWARE( mktrilgy, 0       , 1997, "Tiger Electronics", "Mortal Kombat Trilogy", 0, 0 )
-	SOFTWARE( quizwiz,  0       , 1997, "Tiger Electronics", "Quiz Wiz - Cyber Trivia", 0, 0 )
-	SOFTWARE( re2,      0       , 1998, "Tiger Electronics", "Resident Evil 2", 0, 0 )
-	SOFTWARE( scrabble, 0       , 1999, "Tiger Electronics", "Scrabble", 0, 0 )
-	SOFTWARE( sonicjam, 0       , 1998, "Tiger Electronics", "Sonic Jam", 0, 0 )
-	SOFTWARE( tcasino,  0       , 1998, "Tiger Electronics", "Tiger Casino", 0, 0 )
-	SOFTWARE( wheelfor, 0       , 1997, "Tiger Electronics", "Wheel of Fortune", 0, 0 )
-	SOFTWARE( wheelfo2, 0       , 1998, "Tiger Electronics", "Wheel of Fortune 2", 0, 0 )
-	SOFTWARE( arcadecl, 0       , 1997, "Tiger Electronics", "Williams Arcade Classics", 0, 0 )
+	SOFTWARE( batmanr,  0,        1997, "Tiger Electronics", "Batman & Robin", 0, 0 )                        /* Id: 71-709 - Developer: Tiger Electronics */
+	SOFTWARE( centiped, 0,        1999, "Tiger Electronics", "Centipede", 0, 0 )                             /* Id: 71-755 - Developer: Atari / Handheld Games */
+	SOFTWARE( dukenuke, 0,        1997, "Tiger Electronics", "Duke Nukem 3D", 0, 0 )                         /* Id: 71-712 - Developer: 3D Realms / Tiger Electronics */
+	SOFTWARE( fightmix, 0,        1998, "Tiger Electronics", "Fighters Megamix", 0, 0 )                      /* Id: 71-739 - Developer: Sega AM2 / Tiger Electronics */
+	SOFTWARE( frogger,  0,        1999, "Tiger Electronics", "Frogger", 0, 0 )                               /* Id: 71-756 - Developer: Konami/ Byte Size Sound / Al Baker & Associates */
+	SOFTWARE( henry,    0,        1997, "Tiger Electronics", "Henry - Match the Sounds Memory Game", 0, 0 )  /* Id: 71-728 - Developer: Tiger Electronics */
+	SOFTWARE( indy500,  0,        1997, "Tiger Electronics", "Indy 500", 0, 0 )                              /* Id: 71-525 - Developer: Sega AM1 / Tiger Electronics */
+	SOFTWARE( jeopardy, 0,        1997, "Tiger Electronics", "Jeopardy!", 0, 0 )                             /* Id: 71-726 - Developer: Tiger Electronics */
+	SOFTWARE( lightout, 0,        1997, "Tiger Electronics", "Lights Out", 0, 0 )                            /* Id: 71-735 - Developer: Tiger Electronics */
+	SOFTWARE( lostwrld, 0,        1997, "Tiger Electronics", "The Lost World - Jurassic Park", 0, 0 )        /* Id: 71-704 - Developer: Tiger Electronics */
+	SOFTWARE( monopoly, 0,        1999, "Tiger Electronics", "Monopoly", 0, 0 )                              /* Id: 71-752 - Developer: Hasbro Interactive */
+	SOFTWARE( mktrilgy, 0,        1997, "Tiger Electronics", "Mortal Kombat Trilogy", 0, 0 )                 /* Id: 71-711 - Developer: Midway / Tiger Electronics */
+	SOFTWARE( quizwiz,  0,        1997, "Tiger Electronics", "Quiz Wiz - Cyber Trivia", 0, 0 )               /* Id: 71-524 - Developer: Tiger Electronics */
+	SOFTWARE( re2,      0,        1998, "Tiger Electronics", "Resident Evil 2", 0, 0 )                       /* Id: 71-745 - Developer: Capcom / Tiger Electronics */
+	SOFTWARE( scrabble, 0,        1999, "Tiger Electronics", "Scrabble", 0, 0 )                              /* Id: 71-754 - Developer: Hasbro Interactive */
+	SOFTWARE( sonicjam, 0,        1998, "Tiger Electronics", "Sonic Jam", 0, 0 )                             /* Id: 71-734 - Developer: Sonic Team / S.T.I. / Tiger Electronics */
+	SOFTWARE( tcasino,  0,        1998, "Tiger Electronics", "Tiger Casino", 0, 0 )                          /* Id: 71-731 - Developer: Tiger Electronics */
+	SOFTWARE( wheelfor, 0,        1997, "Tiger Electronics", "Wheel of Fortune", 0, 0 )                      /* Id: 71-523 - Developer: Tiger Electronics */
+	SOFTWARE( wheelfo2, 0,        1998, "Tiger Electronics", "Wheel of Fortune 2", 0, 0 )                    /* Id: 71-527 - Developer: Tiger Electronics */
+	SOFTWARE( arcadecl, 0,        1997, "Tiger Electronics", "Williams Arcade Classics", 0, 0 )              /* Id: 71-722 - Developer: Williams Entertainment / Tiger Electronics */
+
+	SOFTWARE( internet, 0,        1997, "Tiger Electronics", "Game.com Internet", 0, 0 )                     /* Id: 71-529 - Developer: Tiger Electronics */
+	SOFTWARE( weblink,  0,        1997, "Tiger Electronics", "Tiger Web Link", 0, 0 )                        /* Id: 71-747 - Developer: Tiger Electronics */
 SOFTWARE_LIST_END
 
 
--- src/mess/software/gmaster_cart.c
+++ src/mess/software/gmaster_cart.c
@@ -9,7 +9,7 @@
 #include "devices/cartslot.h"
 
 
-SOFTWARE_START( bubbleboy )
+SOFTWARE_START( bublboy )
 	ROM_REGION( 0x4000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "bubbleboy.bin", 0x00000, 0x4000, CRC(edd0679d) SHA1(5f6f81b1a5ba6d21e5fb5bc38aa9ee0770b46b97) )
 SOFTWARE_END
@@ -24,7 +24,7 @@ SOFTWARE_START( dungeona )
 	ROM_LOAD( "dungeona.bin", 0x00000, 0x8000, CRC(60afb4b5) SHA1(6048a84ac5a5a51a6522c662398e44cdf2934588) )
 SOFTWARE_END
 
-SOFTWARE_START( fallblock )
+SOFTWARE_START( fallbloc )
 	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "fallblock.bin", 0x00000, 0x2000, CRC(58e409fb) SHA1(f6f7e3dae361fbd56d4a8457d81bb4f3828c48aB) )
 SOFTWARE_END
@@ -34,7 +34,7 @@ SOFTWARE_START( gobang )
 	ROM_LOAD( "gobang.bin", 0x00000, 0x4000, CRC(fda2c4e6) SHA1(959c16b1657154bb352cb4d6a3e177d7b46850c6) )
 SOFTWARE_END
 
-SOFTWARE_START( hyperspace )
+SOFTWARE_START( hspace )
 	ROM_REGION( 0x8000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "hyperspace.bin", 0x00000, 0x8000, CRC(95e0ceac) SHA1(b3f95ea3dbc881262e734a2e35feb44a1ddd5641) )
 SOFTWARE_END
@@ -59,7 +59,7 @@ SOFTWARE_START( tennism )
 	ROM_LOAD( "tennism.bin", 0x00000, 0x8000, CRC(48d425cf) SHA1(080642009569d7cfdb3d1afe0ce5315842ce4ae8) )
 SOFTWARE_END
 
-SOFTWARE_START( urbanchamp )
+SOFTWARE_START( uchamp )
 	ROM_REGION( 0x8000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "urbanchamp.bin", 0x00000, 0x8000, CRC(d53e6cac) SHA1(0bea8b120b3cb02136519010cb217fd0b819617d) )
 SOFTWARE_END
@@ -67,23 +67,23 @@ SOFTWARE_END
 
 SOFTWARE_LIST_START( gmaster_cart )
 	/* Bomb Disposer */
-	SOFTWARE( bubbleboy,   0,         1990, "Hartung", "Bubble Boy", 0, 0 )
+	SOFTWARE( bublboy,    0,         1990, "Hartung", "Bubble Boy", 0, 0 )
 	/* Car Racing */
-	SOFTWARE( cg2020,   0,         1990, "Hartung", "Continental Galaxy 2020", 0, 0 )
+	SOFTWARE( cg2020,     0,         1990, "Hartung", "Continental Galaxy 2020", 0, 0 )
 	SOFTWARE( dungeona,   0,         1990, "Hartung", "Dungeon Adventure", 0, 0 )
-	SOFTWARE( fallblock,   0,         1990, "Hartung", "Falling Block!", 0, 0 )
-	SOFTWARE( gobang,   0,         1990, "Hartung", "Go Bang", 0, 0 )
-	SOFTWARE( hyperspace,   0,         1990, "Hartung", "Hyper Space", 0, 0 )
+	SOFTWARE( fallbloc,   0,         1990, "Hartung", "Falling Block!", 0, 0 )
+	SOFTWARE( gobang,     0,         1990, "Hartung", "Go Bang", 0, 0 )
+	SOFTWARE( hspace,     0,         1990, "Hartung", "Hyper Space", 0, 0 )
 	/* Invader */
-	SOFTWARE( kungfu,   0,         1990, "Hartung", "Kung Fu Challenge", 0, 0 )
+	SOFTWARE( kungfu,     0,         1990, "Hartung", "Kung Fu Challenge", 0, 0 )
 	/* Move It */
-	SOFTWARE( pinball,   0,         1990, "Hartung", "Pin Ball", 0, 0 )
+	SOFTWARE( pinball,    0,         1990, "Hartung", "Pin Ball", 0, 0 )
 	/* S-Race */
 	/* Soccer (Fuss-Ball) */
-	SOFTWARE( scastle,   0,         1990, "Hartung", "Space Castle", 0, 0 )
+	SOFTWARE( scastle,    0,         1990, "Hartung", "Space Castle", 0, 0 )
 	/* Tank War */
-	SOFTWARE( tennism,   0,         1990, "Hartung", "Tennis Master", 0, 0 )
-	SOFTWARE( urbanchamp,   0,         1990, "Hartung", "Urban Champion", 0, 0 )
+	SOFTWARE( tennism,    0,         1990, "Hartung", "Tennis Master", 0, 0 )
+	SOFTWARE( uchamp,     0,         1990, "Hartung", "Urban Champion", 0, 0 )
 SOFTWARE_LIST_END
 
 
