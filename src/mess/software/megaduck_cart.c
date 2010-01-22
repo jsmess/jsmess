fa7875e27e21687ebdd5e1bad7d35dfe9a7f579d
incog <incog@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-22 10:26:11 +0000

    Updated Creatronic Mega Duck cartridge list.
    git-svn-id: svn://messdev.no-ip.org/mess@7124 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/software/megaduck_cart.c
+++ src/mess/software/megaduck_cart.c
@@ -19,6 +19,11 @@ SOFTWARE_START( aforce )
 	ROM_LOAD( "aforce.bin", 0x0000, 0x10000, CRC(eca6c0ad) SHA1(aa2c3774eb48df0854d4b61997653f79a810f4cc) )
 SOFTWARE_END
 
+SOFTWARE_START( bstfgtr )
+	ROM_REGION( 0x20000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "bstfgtr.bin", 0x0000, 0x20000, CRC(2adb7253) SHA1(9ff96b40e8636fad37b1b95f83f3a9584f3dfe3e) )
+SOFTWARE_END
+
 SOFTWARE_START( bftale )
 	ROM_REGION( 0x10000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "bftale.bin", 0x0000, 0x10000, CRC(211d268b) SHA1(3d62acc2db46bb7b6a2909d640dec6ce095aeec3) )
@@ -39,9 +44,29 @@ SOFTWARE_START( md4in1 )
 	ROM_LOAD( "md4in1.bin", 0x00000, 0x10000, CRC(8046ea70) SHA1(2a1c5bea602ab27ecf680c88cfe9884186b61438) )
 SOFTWARE_END
 
+SOFTWARE_START( pilewndr )
+	ROM_REGION( 0x8000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "pilewndr.bin", 0x0000, 0x8000, CRC(d71f1770) SHA1(69222a0c5c3920d0784d0e58c3407b74c1b2bfe8) )
+SOFTWARE_END
+
+SOFTWARE_START( plewndra )
+	ROM_REGION( 0x8000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "plewndra.bin", 0x0000, 0x8000, CRC(61c81e06) SHA1(13d8882371823e5dfb74191ef0addafc43aa1de4) )
+SOFTWARE_END
+
+SOFTWARE_START( puptkngt )
+	ROM_REGION( 0x10000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "puptkngt.bin", 0x00000, 0x10000, CRC(35fb1616) SHA1(4849CD6F881B5B78E1D3A08EDB727E79C77F9D39) )
+SOFTWARE_END
+
 SOFTWARE_START( railway )
 	ROM_REGION( 0x10000, CARTRIDGE_REGION_ROM, 0 )
 	ROM_LOAD( "railway.bin", 0x00000, 0x10000, CRC(2bb6aeb9) SHA1(f1a9a8365f1b78057ed5cc647fd68efafc0c49e8) )
+SOFTWARE_END
+
+SOFTWARE_START( snakeroy )
+	ROM_REGION( 0x10000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "snakeroy.bin", 0x00000, 0x10000, CRC(315ef9f2) SHA1(c76a57047f0f72252cddd7974a3b694392753469) )
 SOFTWARE_END
 
 SOFTWARE_START( srider )
@@ -63,11 +88,16 @@ SOFTWARE_END
 SOFTWARE_LIST_START( megaduck_cart )
 	SOFTWARE( arczone,  0,         1993, "Commin", "Arctic Zone", 0, 0 )
 	SOFTWARE( aforce,   0,         1993, "Commin", "Armour Force", 0, 0 )
+	SOFTWARE( bstfgtr,  0,         1993, "Sachen", "Beast Fighter", 0, 0 )
 	SOFTWARE( bftale,   0,         1993, "Commin", "Black Forest Tale", 0, 0 )
 	SOFTWARE( c5in1,    0,         1993, "Commin", "Commin 5 in 1", 0, 0 )
 	SOFTWARE( magitowr, 0,         1993, "Sachen", "Magical Tower", 0, 0 )
 	SOFTWARE( md4in1,   0,         1993, "Sachen", "Mega Duck 4 in 1 Game", 0, 0 )
+	SOFTWARE( pilewndr, 0,         1993, "Commin", "Pile Wonder", 0, 0 )
+	SOFTWARE( plewndra, pilewndr,  1993, "Commin", "Pile Wonder (alt)", 0, 0 )
+	SOFTWARE( puptkngt, 0,         1993, "Commin", "Puppet Knight", 0, 0 )
 	SOFTWARE( railway,  0,         1993, "Sachen", "Railway", 0, 0 )
+	SOFTWARE( snakeroy, 0,         1993, "Sachen", "Snake Roy", 0, 0 )
 	SOFTWARE( srider,   0,         1993, "Commin", "Street Rider", 0, 0 )
 	SOFTWARE( treasure, 0,         1993, "Commin", "Suleimans Treasure", 0, 0 )
 	SOFTWARE( brckwall, 0,         1993, "Timlex International", "The Brick Wall", 0, 0 )
