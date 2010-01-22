19dc20ad6d8d51afaa5df406a9953c5f2d91b898
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-22 07:13:23 +0000

    Fixed resources so there are no same id's (since now all is in one res file) Simplified H files.
    git-svn-id: svn://messdev.no-ip.org/mess@7121 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/osd/winui/resourcems.h
+++ src/mess/osd/winui/resourcems.h
@@ -3,76 +3,7 @@
 
 #include "resource.h"
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by mess.rc
-//
-#define ID_FILE_LOADSTATE_NEWUI         101
-#define ID_FILE_SAVESTATE               102
-#define ID_FILE_SAVESTATE_AS            103
-#define ID_FILE_SAVESCREENSHOT          104
-#define ID_FILE_EXIT_NEWUI              105
-#define ID_EDIT_PASTE                   106
-#define ID_OPTIONS_HIDEMENUBAR          107
-#define ID_OPTIONS_KEYBOARD             108
-#define ID_OPTIONS_CONFIGURATION        109
-#define ID_OPTIONS_DIPSWITCHES          110
-#define ID_OPTIONS_MISCINPUT            111
-#define ID_OPTIONS_USEMOUSE             112
-#define ID_OPTIONS_TOGGLEMENUBAR        113
-#define ID_OPTIONS_SOFTRESET            114
-#define ID_OPTIONS_HARDRESET            115
-#define ID_OPTIONS_FRAMESKIP            116
-#define ID_OPTIONS_PAUSE                117
-#define ID_OPTIONS_JOYSTICKS            118
-#define ID_OPTIONS_ANALOGCONTROLS       119
-#define ID_OPTIONS_FULLSCREEN           120
-#define ID_OPTIONS_PROFILER             121
-#define ID_OPTIONS_TOGGLEFPS            122
-#define ID_OPTIONS_DEBUGGER             123
-#define ID_KEYBOARD_EMULATED            124
-#define ID_KEYBOARD_NATURAL             125
-#define ID_KEYBOARD_CUSTOMIZE           126
-#define ID_VIDEO_ROTATE_0               127
-#define ID_VIDEO_ROTATE_90              128
-#define ID_VIDEO_ROTATE_180             129
-#define ID_VIDEO_ROTATE_270             130
-#define ID_THROTTLE_50                  131
-#define ID_THROTTLE_100                 132
-#define ID_THROTTLE_200                 133
-#define ID_THROTTLE_500                 134
-#define ID_THROTTLE_1000                135
-#define ID_THROTTLE_UNTHROTTLED         136
-#define ID_FRAMESKIP_AUTO               137
-#define ID_DEVICES                      138
-#define ID_HELP_ABOUT_NEWUI             139
-#define ID_HELP_ABOUTSYSTEM             140
-#define IDD_TAPEDIALOG                  500
-#define IDI_ICON_CART                   1000
-#define IDI_ICON_HARD                   1001
-#define IDI_ICON_CASS                   1002
-#define IDI_ICON_FLOP                   1003
-#define IDI_ICON_PRIN                   1004
-#define IDI_ICON_SERL                   1005
-#define IDI_ICON_SNAP                   1006
-#define IDR_RUNTIME_MENU                6000
-#define IDC_SLIDER                      6000
-#define IDC_PLAY_NEWUI                  6001
-#define IDC_STOP                        6002
-#define IDC_PAUSE                       6003
-#define IDC_CAPTION                     6004
-#define IDC_STATUS                      6005
-
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         40008
-#define _APS_NEXT_CONTROL_VALUE         6006
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#include "..\windows\messres.h"
 
 #define IDC_SPLITTER3				    2000
 #define IDC_SWTAB                       2001
