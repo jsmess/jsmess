797c7cc91b3ffd79a932f28c77cfde45ae180586
incog <incog@9849079f-6229-0410-a07f-84406ce305ba>
2010-01-26 03:18:15 +0000

    Added Emerson Arcadia 2001 software list
    git-svn-id: svn://messdev.no-ip.org/mess@7175 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/software/arcadia_cart.c
@@ -0,0 +1,372 @@
+/***************************************************************************
+
+  Emerson Arcadia 2001 cartridges
+
+***************************************************************************/
+
+#include "emu.h"
+#include "softlist.h"
+#include "devices/cartslot.h"
+
+
+SOFTWARE_START( 3dbwling )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "3dbwling.bin", 0x0000, 0x1000, CRC(f3c08e19) SHA1(edd33cabd3d600e9e3d3c22f326a065ea6ebe32c) )
+SOFTWARE_END
+
+SOFTWARE_START( 3dsoccer )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "3dsoccer.bin", 0x0000, 0x2000, CRC(4da68df8) SHA1(50e799c96be3613f937825c6c722521bc6df0162) )
+SOFTWARE_END
+
+SOFTWARE_START( 3dsccera )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "3dsccera.bin", 0x0000, 0x2000, CRC(1b5be22a) SHA1(d3df9ed4e09bacacde47fa4f1128383fd62ffd6e) )
+SOFTWARE_END
+
+SOFTWARE_START( alieninv )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "alieninv.bin", 0x0000, 0x1000, CRC(1f65b21e) SHA1(8d17de81fbf70db8fe32830763c6a940aa9124c3) )
+SOFTWARE_END
+
+SOFTWARE_START( astroinv )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "astroinv.bin", 0x0000, 0x1000, CRC(ddcf5b2c) SHA1(b6f693e3333607181fbe3a43dffa9f30f9656998) )
+SOFTWARE_END
+
+SOFTWARE_START( autorace )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "autorace.bin", 0x0000, 0x1000, CRC(99ac48ca) SHA1(6182219b2b8c8747f67d627a056ac947754f0978) )
+SOFTWARE_END
+
+SOFTWARE_START( baseball )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "baseball.bin", 0x0000, 0x1000, CRC(1cf08155) SHA1(f4d39aaab61e6b01c673166ae54841d450860b31) )
+SOFTWARE_END
+
+SOFTWARE_START( bsktball )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "bsktball.bin", 0x0000, 0x1000, CRC(e93938f6) SHA1(c6e3f963ea9db520bcd682a3206cc291504c9e3d) )
+SOFTWARE_END
+
+SOFTWARE_START( battle )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "battle.bin", 0x0000, 0x1000, CRC(6818c8bd) SHA1(21eadc7841e0a59671aacca93d28d81c580f16f2) )
+SOFTWARE_END
+
+SOFTWARE_START( blckjack )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "blckjack.bin", 0x0000, 0x1000, CRC(0930ce04) SHA1(a8afadfcaea847c2e998005652ace36803d6601b) )
+SOFTWARE_END
+
+SOFTWARE_START( boxing )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "boxing.bin", 0x0000, 0x1000, CRC(9240891b) SHA1(0fc3a8a33f2e89b2b32ea0a962effcf3ee430d34) )
+SOFTWARE_END
+
+SOFTWARE_START( brainqz )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "brainqz.bin", 0x0000, 0x1000, CRC(ca58b3f8) SHA1(e9560e064043946b6bc0a88087d4164adf62e773) )
+SOFTWARE_END
+
+SOFTWARE_START( breakawy )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "breakawy.bin", 0x0000, 0x1000, CRC(33c348c1) SHA1(396fe12bd34cc7cd555ad3b9b66f20e56e862385) )
+SOFTWARE_END
+
+SOFTWARE_START( capture )
+	ROM_REGION( 0x800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "capture.bin", 0x000, 0x800, CRC(aeb803e4) SHA1(107c864252647a4f3569ff501c8a9a3c7dd6067f) )
+SOFTWARE_END
+
+SOFTWARE_START( cattrax )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "cattrax.bin", 0x0000, 0x1000, CRC(fd2cf496) SHA1(b0982ed95d92a4d97a5451acbee36af10ff99a3f) )
+SOFTWARE_END
+
+SOFTWARE_START( circus )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "circus.bin", 0x0000, 0x1000, CRC(086ebc8c) SHA1(e8abeb702d9a7b7e12e1cea02946dae267b23041) )
+SOFTWARE_END
+
+SOFTWARE_START( combat )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "combat.bin", 0x0000, 0x1000, CRC(a6bffa9e) SHA1(87addcdd0fccb5f36066f21938458235d5a37bd0) )
+SOFTWARE_END
+
+SOFTWARE_START( crzycmbr )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "crzycmbr.bin", 0x0000, 0x1000, CRC(76e773fa) SHA1(0c6ce7577a121e7810ec54c5dd180a55dd0ace17) )
+SOFTWARE_END
+
+SOFTWARE_START( crzygblr )
+	ROM_REGION( 0x800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "crzygblr.bin", 0x000, 0x800, CRC(e84df2ef) SHA1(021e78bbc1f2c4ee2e7ca2fa560dd448bdd39798) )
+SOFTWARE_END
+
+SOFTWARE_START( doraemon )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "doraemon.bin", 0x0000, 0x2000, CRC(62c45881) SHA1(a16fda96d00c8b08bb362f2573a45939aa6be051) )
+SOFTWARE_END
+
+SOFTWARE_START( drslump )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "drslump.bin", 0x0000, 0x2000, CRC(927c375a) SHA1(389259f1505deef5c0fb83f9cf8b0f7fa4ed5d04) )
+SOFTWARE_END
+
+SOFTWARE_START( escape )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "escape.bin", 0x0000, 0x1000, CRC(fc7eba76) SHA1(87f81511ea5b7a9671fb945cb22bb578bedb47c9) )
+SOFTWARE_END
+
+SOFTWARE_START( football )
+	ROM_REGION( 0x1800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "football.bin", 0x0000, 0x1800, CRC(22624414) SHA1(359e938391b72742643c81228713fac804ff8244) )
+SOFTWARE_END
+
+SOFTWARE_START( funkyfsh )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "funkyfsh.bin", 0x0000, 0x2000, CRC(77c19320) SHA1(e56be2ecd8284cd43a44620d56d6615766f45451) )
+SOFTWARE_END
+
+SOFTWARE_START( golf )
+	ROM_REGION( 0x1800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "golf.bin", 0x0000, 0x1800, CRC(a23c7a01) SHA1(7c9251e42fd03b9c26a13d0e25cdf9a838319a39) )
+SOFTWARE_END
+
+SOFTWARE_START( gstennis )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "gstennis.bin", 0x0000, 0x2000, CRC(8bf2dfa9) SHA1(03cedd5fd67ff11a4ac74d513f03a2266af89446) )
+SOFTWARE_END
+
+SOFTWARE_START( gpmonaco )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "gpmonaco.bin", 0x0000, 0x1000, CRC(5e033f9c) SHA1(7dcc1387892fa7275514c91ce584f83a5d3e14d6) )
+SOFTWARE_END
+
+SOFTWARE_START( hobo )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "hobo.bin", 0x0000, 0x2000, CRC(1cec4b21) SHA1(cebb340cb0a50b2815dafc6df8f46d814015a49f) )
+SOFTWARE_END
+
+SOFTWARE_START( horserac )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "horserac.bin", 0x0000, 0x1000, CRC(560daa7f) SHA1(72dd8dea83bf9ab7d486931516e99b2209ca0a1d) )
+SOFTWARE_END
+
+SOFTWARE_START( jumpbug )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "jumpbug.bin", 0x0000, 0x2000, CRC(97060a54) SHA1(d0ace24fd1f3805e46b077e8a4770f922157c8c4) )
+SOFTWARE_END
+
+SOFTWARE_START( jumpbuga )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "jumpbuga.bin", 0x0000, 0x2000, CRC(dc0264b8) SHA1(bf3ccf7a54409f22ca4295b1df1c01bfbbe9f0a2) )
+SOFTWARE_END
+
+SOFTWARE_START( jungler )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "jungler.bin", 0x0000, 0x2000, CRC(dfb5dd55) SHA1(8d6e32ab3cc872017c597e61c076330015411435) )
+SOFTWARE_END
+
+SOFTWARE_START( msslewar )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "msslewar.bin", 0x0000, 0x1000, CRC(a615f068) SHA1(cc3e46adc8a3ed2cbfd5f9bbbf96f61f4e2ecebe) )
+SOFTWARE_END
+
+SOFTWARE_START( msgundam )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "msgundam.bin", 0x0000, 0x2000, CRC(1241f128) SHA1(4b07a88b06008ddfc3676983e826e107caf3ba0f) )
+SOFTWARE_END
+
+SOFTWARE_START( nbblemen )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "nbblemen.bin", 0x0000, 0x1000, CRC(7832f7ad) SHA1(a2f3d269e422e6ee89d58f001d39438704178e17) )
+SOFTWARE_END
+
+SOFTWARE_START( nbblemna )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "nbblemna.bin", 0x0000, 0x1000, CRC(08d8b186) SHA1(ca2efbcb1f1f580b7995aa47ab7da09fa4a1b9e2) )
+SOFTWARE_END
+
+SOFTWARE_START( oceanbat )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "oceanbat.bin", 0x0000, 0x1000, CRC(449e80cb) SHA1(444146bf89c3a0e0e010f6a30e675be3f2b023a7) )
+SOFTWARE_END
+
+SOFTWARE_START( parashtr )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "parashtr.bin", 0x0000, 0x1000, CRC(4f538848) SHA1(2e7c8e4263eec7f033019ec763effebca40ca30b) )
+SOFTWARE_END
+
+SOFTWARE_START( pleiades )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "pleiades.bin", 0x0000, 0x2000, CRC(4f19fd9b) SHA1(bdb7ce3d4552c82be3cf8db43d5ad103f9d9cc91) )
+SOFTWARE_END
+
+SOFTWARE_START( r2dtank )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "r2dtank.bin", 0x0000, 0x1000, CRC(a0626e23) SHA1(e1daef4cc1e8c51cf542d900ba7139f5f472f2c8) )
+SOFTWARE_END
+
+SOFTWARE_START( redclash )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "redclash.bin", 0x0000, 0x1000, CRC(a06f284b) SHA1(7a94f4f1768e4c62209cc03adc0fbadb954d2c24) )
+SOFTWARE_END
+
+SOFTWARE_START( robtkllr )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "robtkllr.bin", 0x0000, 0x1000, CRC(4671b7f7) SHA1(acb45a8314fe531390d1c58be23ae860fa344c94) )
+SOFTWARE_END
+
+SOFTWARE_START( route16 )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "route16.bin", 0x0000, 0x2000, CRC(8169864e) SHA1(6f6d0c0e2ae06112643c63cb6892f8c7269c98a6) )
+SOFTWARE_END
+
+SOFTWARE_START( soccer )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "soccer.bin", 0x0000, 0x1000, CRC(b0b98a92) SHA1(1a575203b78af7ac992f109add1721dda8872fc1) )
+SOFTWARE_END
+
+SOFTWARE_START( spaceatt )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spaceatt.bin", 0x0000, 0x1000, CRC(e3794a2c) SHA1(25a27466dc3ffb6013cd4f632947834002c80105) )
+SOFTWARE_END
+
+SOFTWARE_START( spaceata )
+	ROM_REGION( 0x800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spaceata.bin", 0x000, 0x800, CRC(c91828cb) SHA1(9e84d1884a002aafb84682eeb9e1cb2f29944d64) )
+SOFTWARE_END
+
+SOFTWARE_START( spaceatb )
+	ROM_REGION( 0x800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spaceata.bin", 0x000, 0x800, CRC(669632ec) SHA1(0d96d79d22237312e217d8da8e2185e9245bdf47) )
+SOFTWARE_END
+
+SOFTWARE_START( spcemssn )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spcemssn.bin", 0x0000, 0x1000, CRC(3ff224e5) SHA1(a46fa9cbc12cb83218d3936ec3f4d0a4aff8cd60) )
+SOFTWARE_END
+
+SOFTWARE_START( spcerdrs )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spcerdrs.bin", 0x0000, 0x1000, CRC(88defacf) SHA1(c2c247b57c3d3a9321f7c16e48b1d733bd7d7dbb) )
+SOFTWARE_END
+
+SOFTWARE_START( spcesqdn )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spcesqdn.bin", 0x0000, 0x1000, CRC(caa5f449) SHA1(e76efb5f2707f67775d09d2d1547a4af0254b7b1) )
+SOFTWARE_END
+
+SOFTWARE_START( spcevult )
+	ROM_REGION( 0x1800, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spcevult.bin", 0x0000, 0x1800, CRC(7666a712) SHA1(a1697ee165307792f07fdd7767122140958a4e9f) )
+SOFTWARE_END
+
+SOFTWARE_START( spiders )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "spiders.bin", 0x0000, 0x1000, CRC(bb88daea) SHA1(e2fe75ec968a586a9c4885df02481b00abef2bc7) )
+SOFTWARE_END
+
+SOFTWARE_START( starchss )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "starchss.bin", 0x0000, 0x1000, CRC(83e6e40b) SHA1(348ca876b1113827d8e7ffb9b2422e863ba92b0f) )
+SOFTWARE_END
+
+SOFTWARE_START( macross )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "macross.bin", 0x0000, 0x2000, CRC(4c885af2) SHA1(3d24ab9e42ff44a223bb53c5491dea7f41691df2) )
+SOFTWARE_END
+
+SOFTWARE_START( superbug )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "superbug.bin", 0x0000, 0x1000, CRC(fef2aae9) SHA1(1e7f7b81ecd2f766c94bd5ff17763374fb0318f1) )
+SOFTWARE_END
+
+SOFTWARE_START( superbga )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "superbga.bin", 0x0000, 0x1000, CRC(15436f39) SHA1(bf22e0fae67bc3c2147a669b194752e34fe3564d) )
+SOFTWARE_END
+
+SOFTWARE_START( tnksalot )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "tnksalot.bin", 0x0000, 0x1000, CRC(0076422e) SHA1(c431d5190e86f3dfbb3e8b1a3b9bec82bb5014ae) )
+SOFTWARE_END
+
+SOFTWARE_START( theend )
+	ROM_REGION( 0x1000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "theend.bin", 0x0000, 0x1000, CRC(e66f362d) SHA1(1148aa1525d7b46640babb50b004c79abec45d3a) )
+SOFTWARE_END
+
+SOFTWARE_START( turtles )
+	ROM_REGION( 0x2000, CARTRIDGE_REGION_ROM, 0 )
+	ROM_LOAD( "turtles.bin", 0x0000, 0x2000, CRC(306e39c1) SHA1(a806e3f248a962bb99e2515f489d45bb11fb0a0e) )
+SOFTWARE_END
+
+
+SOFTWARE_LIST_START( arcadia_cart )
+	/* 3D Attack 1983 UA Ltd. */
+	SOFTWARE( 3dbwling,   0,         1982, "UA Ltd.", "3D Bowling", 0, 0 )			/* Also known as "Bowling" or "Bowling 3D" */
+	SOFTWARE( 3dsoccer,   0,         1983, "UA Ltd.", "3D Soccer", 0, 0 )
+	SOFTWARE( 3dsccera,   3dsoccer,  1983, "UA Ltd.", "3D Soccer (alt)", 0, 0 )
+	SOFTWARE( alieninv,   0,         1983, "UA Ltd.", "Alien Invaders", 0, 0 )		/* Also known as "Alien", "Alien Invader" or "Les Envahisseurs Etrangers" */
+	SOFTWARE( astroinv,   0,         1980, "UA Ltd.", "Astro Invaders", 0, 0 )
+	SOFTWARE( autorace,   0,         198?, "UA Ltd.", "Auto Race", 0, 0 )
+	SOFTWARE( baseball,   0,         1983, "UA Ltd.", "Baseball", 0, 0 )
+	SOFTWARE( bsktball,   0,         198?, "UA Ltd.", "Basketball", 0, 0 )
+	SOFTWARE( battle,     0,         198?, "UA Ltd.", "Battle", 0, 0 )
+	SOFTWARE( blckjack,   0,         198?, "UA Ltd.", "Blackjack & Poker", 0, 0 )
+	SOFTWARE( boxing,     0,         198?, "UA Ltd.", "Boxing", 0, 0 )
+	SOFTWARE( brainqz,    0,         1982, "UA Ltd.", "Brain Quiz", 0, 0 )			/* Also known as "Math/Logic" */
+	SOFTWARE( breakawy,   0,         1982, "UA Ltd.", "Breakaway", 0, 0 )			/* Also known as "Break Away", "Evasion", "Video Breakout" or "La Desertion" */
+	SOFTWARE( capture,    0,         1982, "UA Ltd.", "Capture", 0, 0 )			/* Also known as "Othello" or "Side Attack" */
+	SOFTWARE( cattrax,    0,         1982, "UA Ltd.", "Cat Trax", 0, 0 )			/* Also known as "Cat Track" or "Cat Tracks" */
+	SOFTWARE( circus,     0,         1983, "UA Ltd.", "Circus", 0, 0 )			/* Also known as "Acrobat" or "Clown" */
+	SOFTWARE( combat,     0,         198?, "UA Ltd.", "Combat", 0, 0 )			/* Also known as "Combat Zone" */
+	SOFTWARE( crzycmbr,   0,         1982, "UA Ltd.", "Crazy Climber", 0, 0 )		/* Also known as "Crazy Wall" */
+	SOFTWARE( crzygblr,   0,         1982, "UA Ltd.", "Crazy Gobbler", 0, 0 )
+	SOFTWARE( doraemon,   0,         1983, "Bandai", "Doraemon", 0, 0 )			/* Bandai Arcadia exclusive */
+	SOFTWARE( drslump,    0,         1983, "Bandai", "Dr. Slump", 0, 0 )			/* Bandai Arcadia exclusive */
+	SOFTWARE( escape,     0,         1982, "UA Ltd.", "Escape", 0, 0 )			/* Also known as "Escape Man" */
+	SOFTWARE( football,   0,         1982, "UA Ltd.", "Football", 0, 0 )			/* Also known as "American Football" */
+	SOFTWARE( funkyfsh,   0,         1983, "UA Ltd.", "Funky Fish", 0, 0 )
+	SOFTWARE( golf,       0,         198?, "UA Ltd.", "Golf", 0, 0 )			/* Also known as "Golfer" */
+	SOFTWARE( gstennis,   0,         1982, "UA Ltd.", "Grand Slam Tennis", 0, 0 )		/* Also known as "Tennis" or "Yannick Noah Tennis" */
+	SOFTWARE( gpmonaco,   0,         1982, "UA Ltd.", "Grand Prix de Monaco", 0, 0 )	/* Also known as "Monaco Grand Prix" */
+	SOFTWARE( hobo,       0,         1983, "UA Ltd.", "Hobo", 0, 0 )
+	SOFTWARE( horserac,   0,         198?, "UA Ltd.", "Horse Racing", 0, 0 )
+	SOFTWARE( jumpbug,    0,         1982, "UA Ltd.", "Jump Bug", 0, 0 )			/* Also known as "Hoppy Bug" */
+	SOFTWARE( jumpbuga,   jumpbug,   1982, "UA Ltd.", "Jump Bug (alt)", 0, 0 )		/* Also known as "Hoppy Bug" */
+	SOFTWARE( jungler,    0,         1982, "UA Ltd.", "Jungler", 0, 0 )
+	SOFTWARE( msslewar,   0,         1982, "UA Ltd.", "Missile War", 0, 0 )			/* Also known as "Le Guerre des Missiles" */
+	SOFTWARE( msgundam,   0,         1983, "Bandai", "Mobile Soldier Gundam", 0, 0 )	/* Bandai Arcadia exclusive */
+	SOFTWARE( nbblemen,   0,         1982, "UA Ltd.", "Nibblemen", 0, 0 )			/* Also known as "Devilman the Dot Muncher" or "Super Gobbler" */
+	SOFTWARE( nbblemna,   nbblemen,  1982, "UA Ltd.", "Nibblemen (alt)", 0, 0 )		/* Also known as "Devilman the Dot Muncher" or "Super Gobbler" */
+	SOFTWARE( oceanbat,   0,         1982, "UA Ltd.", "Ocean Battle", 0, 0 )		/* Also known as "Sea Battle" or "Le Combat Naval" */
+	SOFTWARE( parashtr,   0,         1982, "UA Ltd.", "Parashooter", 0, 0 )			/* Also known as "Parachute" */
+	SOFTWARE( pleiades,   0,         1983, "UA Ltd.", "Pleiades", 0, 0 )
+	SOFTWARE( r2dtank,    0,         1981, "UA Ltd.", "R2D Tank", 0, 0 )
+	SOFTWARE( redclash,   0,         1982, "UA Ltd.", "Red Clash", 0, 0 )
+	SOFTWARE( robtkllr,   0,         1982, "UA Ltd.", "Robot Killer", 0, 0 )
+	SOFTWARE( route16,    0,         1983, "UA Ltd.", "Route 16", 0, 0 )
+	SOFTWARE( soccer,     0,         1982, "UA Ltd.", "Soccer", 0, 0 )			/* Also known as "Football" */
+	SOFTWARE( spaceatt,   0,         1982, "UA Ltd.", "Space Attack", 0, 0 )		/* Also known as "Space War" */
+	SOFTWARE( spaceata,   spaceatt,  1982, "UA Ltd.", "Space Attack (alt)", 0, 0 )		/* Prototype? Also known as "Space War" */
+	SOFTWARE( spaceatb,   spaceatt,  1982, "UA Ltd.", "Space Attack (alt 2)", 0, 0 )	/* Also known as "Space War" */
+	SOFTWARE( spcemssn,   0,         1982, "UA Ltd.", "Space Mission", 0, 0 )
+	SOFTWARE( spcerdrs,   0,         1982, "UA Ltd.", "Space Raiders", 0, 0 )		/* Also known as "Space Raider" */
+	SOFTWARE( spcesqdn,   0,         1982, "UA Ltd.", "Space Squadron", 0, 0 )		/* Also known as "L'Escadron de L'Espace" */
+	SOFTWARE( spcevult,   0,         1982, "UA Ltd.", "Space Vultures", 0, 0 )
+	SOFTWARE( spiders,    0,         1982, "UA Ltd.", "Spiders", 0, 0 )
+	SOFTWARE( starchss,   0,         1982, "UA Ltd.", "Star Chess", 0, 0 )			/* Also known as "Echec Etiles" or "Video Chess" */
+	SOFTWARE( macross,    0,         1983, "Bandai", "Macross", 0, 0 )			/* Bandai Arcadia exclusive, Also known as "Super Dimension Fortress Macross" */
+	SOFTWARE( superbug,   0,         198?, "UA Ltd.", "Super Bug", 0, 0 )
+	SOFTWARE( superbga,   superbug,  198?, "UA Ltd.", "Super Bug (alt)", 0, 0 )
+	SOFTWARE( tnksalot,   0,         1982, "UA Ltd.", "Tanks a Lot", 0, 0 )
+	SOFTWARE( theend,     0,         1982, "UA Ltd.", "The End", 0, 0 )			/* Also known as "Space Pirates" */
+	SOFTWARE( turtles,    0,         1982, "UA Ltd.", "Turtles Turpin", 0, 0 )		/* Overdumped? */
+SOFTWARE_LIST_END
+
+
+SOFTWARE_LIST( arcadia_cart, "Emerson Arcadia 2001 cartridges" )
