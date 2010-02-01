c438a1f550023f48fbdd7d2514245b353d7c09d0
etabeta <etabeta@9849079f-6229-0410-a07f-84406ce305ba>
2010-02-01 15:35:33 +0000

    Temporarily reverted rev.7222 for cococart.c, because coco.c drivers are broken otherwise
    git-svn-id: svn://messdev.no-ip.org/mess@7228 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/devices/cococart.c
+++ src/mess/devices/cococart.c
@@ -95,7 +95,10 @@ static DEVICE_START(coco_cartridge)
 	{
 		cococart->pcb = cartslot_get_pcb(cartslot);
 		if (cococart->pcb == NULL)
-			throw device_missing_dependencies();
+		{
+			return;
+			//throw device_missing_dependencies();
+		}
 
 		cococart->pcb_r = (read8_device_func) cococart->pcb->get_config_fct(COCOCARTINFO_FCT_FF40_R);
 		cococart->pcb_w = (write8_device_func) cococart->pcb->get_config_fct(COCOCARTINFO_FCT_FF40_W);
