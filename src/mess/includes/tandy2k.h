45d1b91dd9f673a2b1b5a02977bee8c6f3d31df4
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-02-22 08:15:24 +0000

    [TANDY2K] Added correct header file.
    git-svn-id: svn://messdev.no-ip.org/mess@7442 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/includes/tandy2k.h
+++ src/mess/includes/tandy2k.h
@@ -1,46 +1,48 @@
-/*****************************************************************************
- *
- * includes/tandy2k.h
- *
- ****************************************************************************/
+#ifndef __TANDY2K__
+#define __TANDY2K__
 
-#ifndef __TANDY2K_H__
-#define __TANDY2K_H__
+#define SCREEN_TAG		"screen"
+#define I80186_TAG		"u76"
+#define I8048_TAG		"m1"
+#define I8255A_TAG		"u75"
+#define I8251A_TAG		"u41"
+#define I8253_TAG		"u40"
+#define I8259A_0_TAG	"u42"
+#define I8259A_1_TAG	"u43"
+#define I8272A_TAG		"u121"
+#define CRT9007_TAG		"u16"
+#define CRT9021_TAG		"u14"
+#define WD1010_TAG		"u18"
+#define SPEAKER_TAG		"speaker"
+#define CENTRONICS_TAG	"centronics"
 
 typedef struct _tandy2k_state tandy2k_state;
 struct _tandy2k_state
 {
-	UINT8 kben;
-	UINT8 extclk;
-	UINT8 spkrdata;
-	UINT8 keylatch;
-	UINT8 rxrdy;
-	UINT8 txrdy;
-	UINT8 outspkr;
-	UINT8 pb_sel;
-	
-	
+	/* keyboard state */
+	int kben;
+	UINT16 keylatch;
+
+	/* serial state */
+	int extclk;
+	int rxrdy;
+	int txrdy;
+
+	/* PPI state */
+	int pb_sel;
+
+	/* sound state */
+	int outspkr;
+	int spkrdata;
+
 	/* devices */
-	running_device *speaker;
+	running_device *uart;
 	running_device *pit;
 	running_device *fdc;
 	running_device *pic0;
 	running_device *pic1;
-	running_device *uart;
 	running_device *centronics;
+	running_device *speaker;
 };
 
-
-#define I8272A_TAG "i8272a"
-#define I8251A_TAG "i8251a"
-#define I8253_TAG  "pit"
-#define I8255A_TAG  "pio"
-#define I8259A_0_TAG "pic0"
-#define I8259A_1_TAG "pic1"
-#define CENTRONICS_TAG "centronics"
-#define I80186_TAG "maincpu"
-#define I8048_TAG  "subcpu"
-#define SCREEN_TAG "screen"
-#define SPEAKER_TAG "speaker"
-
-#endif /* __TANDY2K_H__ */
+#endif
