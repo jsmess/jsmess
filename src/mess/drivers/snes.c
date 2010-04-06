da3c103b63102a132c463250251ed93dd3ff111e
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2010-04-06 23:00:25 +0000

    First step in supporting master cycle delays in the SNES driver
    git-svn-id: svn://messdev.no-ip.org/mess@7741 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/snes.c
+++ src/mess/drivers/snes.c
@@ -514,7 +514,7 @@ static void snes_input_read_superscope( running_machine *machine, int port )
 		state->scope[port].pause_lock = 0;
 
 	/* If we have pressed fire or cursor and we are on-screen and SuperScope is in Port2, then latch video signal.
-	Notice that we only latch Port2 because its IOBit pin is connected to bit7 of the IO Port, while Port1 has 
+	Notice that we only latch Port2 because its IOBit pin is connected to bit7 of the IO Port, while Port1 has
 	IOBit pin connected to bit6 of the IO Port, and the latter is not detected by the H/V Counters. In other
 	words, you can connect SuperScope to Port1, but there is no way SNES could detect its on-screen position */
 	if ((state->scope[port].buttons & 0xc0) && !(state->scope[port].buttons & 0x02) && port == 1)
@@ -692,7 +692,7 @@ static MACHINE_DRIVER_START( snes )
 	MDRV_DRIVER_DATA(snes_state)
 
 	/* basic machine hardware */
-	MDRV_CPU_ADD("maincpu", G65816, MCLK_NTSC/6)	/* 2.68 MHz, also 3.58 MHz */
+	MDRV_CPU_ADD("maincpu", 5A22, MCLK_NTSC)	/* 2.68 MHz, also 3.58 MHz */
 	MDRV_CPU_PROGRAM_MAP(snes_map)
 
 	MDRV_CPU_ADD("soundcpu", SPC700, 1024000)	/* 1.024 MHz */
@@ -737,7 +737,7 @@ MACHINE_DRIVER_END
 
 static MACHINE_DRIVER_START( snespal )
 	MDRV_IMPORT_FROM(snes)
-	MDRV_CPU_REPLACE("maincpu", G65816, MCLK_PAL/6)
+	MDRV_CPU_REPLACE("maincpu", 5A22, MCLK_PAL)
 
 	MDRV_SCREEN_MODIFY("screen")
 	MDRV_SCREEN_RAW_PARAMS(DOTCLK_PAL, SNES_HTOTAL, 0, SNES_SCR_WIDTH, SNES_VTOTAL_PAL, 0, SNES_SCR_HEIGHT_PAL)
