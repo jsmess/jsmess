014ee2f747c0a02745a30ff88406df90ea9d0393
firewave <firewave@9849079f-6229-0410-a07f-84406ce305ba>
2010-05-07 16:42:45 +0000

    [MAMEUI] Removed unused variable
    git-svn-id: svn://messdev.no-ip.org/mess@7986 9849079f-6229-0410-a07f-84406ce305ba

--- src/osd/winui/properties.c
+++ src/osd/winui/properties.c
@@ -1741,7 +1741,6 @@ static void SetPropEnabledControls(HWND hWnd)
 	BOOL d3d = FALSE;
 	BOOL gdi = FALSE;
 	BOOL useart = TRUE;
-	BOOL multimon = (DirectDraw_GetNumDisplays() >= 2);
 	int joystick_attached = 0;
 	int in_window = 0;
 
