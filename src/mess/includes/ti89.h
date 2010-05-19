c05cf513618612affcac19e1e4cd00e48c3eed95
etabeta <etabeta@9849079f-6229-0410-a07f-84406ce305ba>
2010-05-19 09:21:52 +0000

    missed this include file
    git-svn-id: svn://messdev.no-ip.org/mess@8083 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/includes/ti89.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ *
+ * includes/ti89.h
+ *
+ ****************************************************************************/
+
+#ifndef TI89_H_
+#define TI89_H_
+
+ enum
+ {
+	HW1 = 1,
+	HW2,
+	HW3,
+	HW4
+ };
+
+ enum
+ {
+	FLASH,
+	EPROM
+ };
+
+/***************************************************************************
++    FUNCTION PROTOTYPES
++***************************************************************************/
+
+WRITE16_HANDLER ( ti68k_io_w );
+READ16_HANDLER ( ti68k_io_r );
+WRITE16_HANDLER ( ti68k_io2_w );
+READ16_HANDLER ( ti68k_io2_r );
+WRITE16_HANDLER ( flash_w );
+READ16_HANDLER ( rom_r );
+
+static TIMER_CALLBACK( ti68k_timer_callback );
+static INPUT_CHANGED( ti68k_on_key );
+static MACHINE_RESET(ti68k);
+static VIDEO_START( ti68k );
+static VIDEO_UPDATE( ti68k );
+
+#endif /* TI89_H_ */
