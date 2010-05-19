7c2225d0bbb1377ee4f6c92b164ed117c47e68c3
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-05-19 14:33:12 +0000

    [AIM65_40] Mapped devices.
    git-svn-id: svn://messdev.no-ip.org/mess@8086 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/includes/aim65_40.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef __AIM65_40__
+#define __AIM65_40__
+
+#define M6502_TAG		"m6502"
+#define M6522_0_TAG		"m6522_0"
+#define M6522_1_TAG		"m6522_1"
+#define M6522_2_TAG		"m6522_2"
+#define M6551_TAG		"m6551"
+#define SPEAKER_TAG		"speaker"
+
+class aim65_40_state
+{
+public:
+	static void *alloc(running_machine &machine) { return auto_alloc_clear(&machine, aim65_40_state(machine)); }
+
+	aim65_40_state(running_machine &machine) { }
+
+	/* devices */
+	running_device *via0;
+	running_device *via1;
+	running_device *via2;
+	running_device *speaker;
+};
+
+#endif
