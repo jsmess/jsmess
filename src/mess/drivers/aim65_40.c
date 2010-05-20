d078c78884b58ec776db81bc402db65befb33d9c
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-05-20 10:29:35 +0000

    [AIM65_40] Fixed ROM names.
    git-svn-id: svn://messdev.no-ip.org/mess@8102 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/aim65_40.c
+++ src/mess/drivers/aim65_40.c
@@ -154,10 +154,10 @@ MACHINE_DRIVER_END
 
 ROM_START( aim65_40 )
 	ROM_REGION( 0x10000, M6502_TAG, 0 )
-	ROM_LOAD( "mon a v1.0 r32u5-11 8210.z65", 0xa000, 0x1000, CRC(8c970c67) SHA1(5c8aecb2155a10777a57d4f0f2d16b7ba7b1fb45) )
-	ROM_LOAD( "mon b v1.0 r32u6-11 8246.z66", 0xb000, 0x1000, CRC(38a1e0cd) SHA1(37c34e32ad25d27e9590ee3f325801ca311be7b1) )
-	ROM_LOAD( "r2332lp r3224-11 8224.z70",    0xc000, 0x1000, CRC(0878b399) SHA1(483e92b57d64be51643a9f6490521a8572aa2f68) ) // Assembler
-	ROM_LOAD( "i-of v1.0 r32t3-12 8152.z73",  0xf000, 0x1000, CRC(a62bec4a) SHA1(a2fc69a33dc3b7684bf3399beff7b22eaf05c843) )
+	ROM_LOAD( "mon a v1.0 r32u5-11.z65", 0xa000, 0x1000, CRC(8c970c67) SHA1(5c8aecb2155a10777a57d4f0f2d16b7ba7b1fb45) )
+	ROM_LOAD( "mon b v1.0 r32u6-11.z66", 0xb000, 0x1000, CRC(38a1e0cd) SHA1(37c34e32ad25d27e9590ee3f325801ca311be7b1) )
+	ROM_LOAD( "r2332lp r3224-11.z70",    0xc000, 0x1000, CRC(0878b399) SHA1(483e92b57d64be51643a9f6490521a8572aa2f68) ) // Assembler
+	ROM_LOAD( "i-of v1.0 r32t3-12.z73",  0xf000, 0x1000, CRC(a62bec4a) SHA1(a2fc69a33dc3b7684bf3399beff7b22eaf05c843) )
 ROM_END
 
 /***************************************************************************
