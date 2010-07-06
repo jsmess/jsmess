d225469dfd86af80393477d7809881fbd9189042
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2010-07-06 00:47:00 +0000

    Removed need for messdrv.h
    git-svn-id: svn://messdev.no-ip.org/mess@8398 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/mess.h
+++ src/mess/mess.h
@@ -11,6 +11,48 @@
 
 #include "options.h"
 
+/******************************************************************************
+ * MESS' version of the GAME() macros of MAME
+ * CONS is for consoles
+ * COMP is for computers
+ ******************************************************************************/
+
+#define CONS(YEAR,NAME,PARENT,COMPAT,MACHINE,INPUT,INIT,COMPANY,FULLNAME,FLAGS)	\
+extern const game_driver GAME_NAME(NAME) =	\
+{											\
+	__FILE__,								\
+	#PARENT,								\
+	#NAME,									\
+	FULLNAME,								\
+	#YEAR,									\
+	COMPANY,								\
+	MACHINE_DRIVER_NAME(MACHINE),			\
+	INPUT_PORTS_NAME(INPUT),				\
+	DRIVER_INIT_NAME(INIT),					\
+	ROM_NAME(NAME),							\
+	#COMPAT,								\
+	ROT0|(FLAGS),							\
+	NULL									\
+};
+
+#define COMP(YEAR,NAME,PARENT,COMPAT,MACHINE,INPUT,INIT,COMPANY,FULLNAME,FLAGS)	\
+extern const game_driver GAME_NAME(NAME) =	\
+{											\
+	__FILE__,								\
+	#PARENT,								\
+	#NAME,									\
+	FULLNAME,								\
+	#YEAR,									\
+	COMPANY,								\
+	MACHINE_DRIVER_NAME(MACHINE),			\
+	INPUT_PORTS_NAME(INPUT),				\
+	DRIVER_INIT_NAME(INIT),					\
+	ROM_NAME(NAME),							\
+	#COMPAT,								\
+	ROT0|(FLAGS),				\
+	NULL									\
+};
+
 /***************************************************************************
     CONSTANTS
 ***************************************************************************/
