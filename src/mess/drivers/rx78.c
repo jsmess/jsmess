03e6749bf08a6ac6a7abf6d8aba173df9cfb2dda
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-07-13 12:28:59 +0000

    [RX78] Added a crude screen with flashing cursor. Added F4 display.
    git-svn-id: svn://messdev.no-ip.org/mess@8486 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/rx78.c
+++ src/mess/drivers/rx78.c
@@ -11,6 +11,8 @@
 
 static ADDRESS_MAP_START(rx78_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
+	AM_RANGE(0x0000, 0x1fff) AM_ROM
+	AM_RANGE(0x2000, 0xffff) AM_RAM AM_REGION("maincpu", 0x2000)
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( rx78_io , ADDRESS_SPACE_IO, 8)
@@ -18,7 +20,7 @@ static ADDRESS_MAP_START( rx78_io , ADDRESS_SPACE_IO, 8)
 ADDRESS_MAP_END
 
 /* Input ports */
-INPUT_PORTS_START( rx78 )
+static INPUT_PORTS_START( rx78 )
 INPUT_PORTS_END
 
 
@@ -32,12 +34,60 @@ static VIDEO_START( rx78 )
 
 static VIDEO_UPDATE( rx78 )
 {
-    return 0;
+	UINT8 y,ra,chr,gfx;
+	UINT16 sy=0,ma=0xe8be,x;
+	UINT8 *RAM = memory_region(screen->machine, "maincpu");
+
+	for (y = 0; y < 23; y++)
+	{
+		for (ra = 0; ra < 8; ra++)
+		{
+			UINT16  *p = BITMAP_ADDR16(bitmap, sy++, 0);
+
+			for (x = ma; x < ma+30; x++)
+			{
+				chr = RAM[x];
+
+				/* get pattern of pixels for that character scanline */
+				gfx = RAM[0x1a27 + (chr<<3) + ra];
+
+				/* Display a scanline of a character (8 pixels) */
+				*p = ( gfx & 0x01 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x02 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x04 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x08 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x10 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x20 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x40 ) ? 1 : 0; p++;
+				*p = ( gfx & 0x80 ) ? 1 : 0; p++;
+			}
+		}
+		ma+=30;
+	}
+	return 0;
 }
+
+/* F4 Character Displayer */
+static const gfx_layout rx78_charlayout =
+{
+	8, 8,					/* 8 x 8 characters */
+	187,					/* 187 characters */
+	1,					/* 1 bits per pixel */
+	{ 0 },					/* no bitplanes */
+	/* x offsets */
+	{ 7, 6, 5, 4, 3, 2, 1, 0 },
+	/* y offsets */
+	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8 },
+	8*8					/* every char takes 8 bytes */
+};
+
+static GFXDECODE_START( rx78 )
+	GFXDECODE_ENTRY( "maincpu", 0x1a27, rx78_charlayout, 0, 1 )
+GFXDECODE_END
 
 static MACHINE_DRIVER_START( rx78 )
     /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)
+    MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)	// 4.1mhz
     MDRV_CPU_PROGRAM_MAP(rx78_mem)
     MDRV_CPU_IO_MAP(rx78_io)	
 
@@ -48,10 +98,11 @@ static MACHINE_DRIVER_START( rx78 )
     MDRV_SCREEN_REFRESH_RATE(50)
     MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
     MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MDRV_SCREEN_SIZE(640, 480)
-    MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
+    MDRV_SCREEN_SIZE(240, 184)
+    MDRV_SCREEN_VISIBLE_AREA(0, 240-1, 0, 184-1)
     MDRV_PALETTE_LENGTH(2)
     MDRV_PALETTE_INIT(black_and_white)
+	MDRV_GFXDECODE(rx78)
 
     MDRV_VIDEO_START(rx78)
     MDRV_VIDEO_UPDATE(rx78)
