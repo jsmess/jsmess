091507ebd8673c4cf910cb30ea811a70d70298f8
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-07-31 22:06:39 +0000

    [A7000] Fixed names.
    git-svn-id: svn://messdev.no-ip.org/mess@8669 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/a7000.c
+++ src/mess/drivers/a7000.c
@@ -83,5 +83,5 @@ ROM_END
 ***************************************************************************/
 
 /*    YEAR  NAME        PARENT  COMPAT  MACHINE     INPUT   INIT    COMPANY FULLNAME        FLAGS */
-CONS( 1995, a7000,      0,      0,      a7000,      a7000,	0,      "Acorn",  "Archimedes 7000",   GAME_NOT_WORKING | GAME_NO_SOUND )
-CONS( 1997, a7000p,     a7000,  0,      a7000,      a7000,	0,      "Acorn",  "Archimedes 7000+",  GAME_NOT_WORKING | GAME_NO_SOUND )
+COMP( 1995, a7000,      0,      0,      a7000,      a7000,	0,      "Acorn",  "Archimedes A7000",   GAME_NOT_WORKING | GAME_NO_SOUND )
+COMP( 1997, a7000p,     a7000,  0,      a7000,      a7000,	0,      "Acorn",  "Archimedes A7000+",  GAME_NOT_WORKING | GAME_NO_SOUND )
