b28814f3ea20c5c77b2ce3b52511ee98f3bf6f3c
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2010-07-31 14:22:25 +0000

    [BASIC MASTER JR]: Miscellaneous fixes
    git-svn-id: svn://messdev.no-ip.org/mess@8666 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/bmjr.c
+++ src/mess/drivers/bmjr.c
@@ -1,17 +1,26 @@
 /***************************************************************************
 
-        Hitachi Basic Master Jr (MB-6885)
+	Basic Master Jr (MB-6885) (c) 1982? Hitachi
 
-        13/07/2010 Skeleton driver.
+	preliminary driver by Angelo Salese
+
+	TODO:
+	- for whatever reason, BASIC won't work if you try to use it directly,
+	  it does if you enter into MON first then exit (with E)
+	- tape hook-up doesn't work yet (shouldn't be hard to fix)
+	- Break key is unemulated (tied with the NMI)
 
 ****************************************************************************/
 
 #include "emu.h"
 #include "cpu/m6800/m6800.h"
 #include "sound/beep.h"
+#include "devices/cassette.h"
+#include "sound/wave.h"
 
 static UINT8 *wram;
 static UINT8 tape_switch;
+static UINT8 xor_display;
 
 static VIDEO_START( bmjr )
 {
@@ -37,7 +46,10 @@ static VIDEO_UPDATE( bmjr )
 				{
 					int pen;
 
-					pen = (gfx_rom[tile*8+yi] >> (7-xi) & 1) ? color : 0;
+					if(xor_display)
+						pen = (gfx_rom[tile*8+yi] >> (7-xi) & 1) ? 0 : color;
+					else
+						pen = (gfx_rom[tile*8+yi] >> (7-xi) & 1) ? color : 0;
 
 					*BITMAP_ADDR16(bitmap, y*8+yi, x*8+xi) = screen->machine->pens[pen];
 				}
@@ -67,18 +79,27 @@ static WRITE8_HANDLER( key_w )
 {
 	key_mux = data & 0xf;
 
-	if(data & 0xf0)
-		printf("%02x",data & 0xf0);
+//	if(data & 0xf0)
+//		printf("%02x",data & 0xf0);
 }
+
+static running_device *bmjr_cassette;
 
 static READ8_HANDLER( ff_r )
 {
 	return 0xff;
 }
 
+static READ8_HANDLER( unk_r )
+{
+	return 0x30;
+}
+
 static READ8_HANDLER( tape_r )
 {
-	return 0xff; //TODO
+	//cassette_change_state(bmjr_cassette,CASSETTE_PLAY,CASSETTE_MASK_UISTATE);
+
+	return (cassette_input(bmjr_cassette) > 0.03) ? 0xff : 0x00;
 }
 
 static WRITE8_HANDLER( tape_w )
@@ -89,8 +110,29 @@ static WRITE8_HANDLER( tape_w )
 	}
 	else
 	{
-		// ...
+		//cassette_change_state(bmjr_cassette,CASSETTE_RECORD,CASSETTE_MASK_UISTATE);
+		cassette_output(bmjr_cassette, (data & 0x80) ? -1.0 : +1.0);
 	}
+}
+
+static READ8_HANDLER( tape_stop_r )
+{
+	tape_switch = 0;
+	//cassette_change_state(bmjr_cassette,CASSETTE_STOPPED,CASSETTE_MASK_UISTATE);
+	cassette_change_state(bmjr_cassette,CASSETTE_MOTOR_DISABLED,CASSETTE_MASK_MOTOR);
+	return 0x01;
+}
+
+static READ8_HANDLER( tape_start_r )
+{
+	tape_switch = 1;
+	cassette_change_state(bmjr_cassette,CASSETTE_MOTOR_ENABLED,CASSETTE_MASK_MOTOR);
+	return 0x01;
+}
+
+static WRITE8_HANDLER( xor_display_w )
+{
+	xor_display = data;
 }
 
 static ADDRESS_MAP_START(bmjr_mem, ADDRESS_SPACE_PROGRAM, 8)
@@ -99,18 +141,19 @@ static ADDRESS_MAP_START(bmjr_mem, ADDRESS_SPACE_PROGRAM, 8)
 	//0x0900, 0x20ff vram, modes 0x40 / 0xc0
 	//0x2100, 0x38ff vram, modes 0x44 / 0xcc
 	AM_RANGE(0x0000, 0xafff) AM_RAM AM_BASE(&wram)
-	AM_RANGE(0xb000, 0xe7ff) AM_ROM
+	AM_RANGE(0xb000, 0xdfff) AM_ROM
+	AM_RANGE(0xe000, 0xe7ff) AM_ROM
 //	AM_RANGE(0xe890, 0xe890) W MP-1710 tile color
 //	AM_RANGE(0xe891, 0xe891) W MP-1710 background color
 //	AM_RANGE(0xe892, 0xe892) W MP-1710 monochrome / color setting
-//	AM_RANGE(0xee00, 0xee00) R stop tape
-//	AM_RANGE(0xee20, 0xee20) R start tape
-//	AM_RANGE(0xee40, 0xee40) W Picture reverse (?)
+	AM_RANGE(0xee00, 0xee00) AM_READ(tape_stop_r) //R stop tape
+	AM_RANGE(0xee20, 0xee20) AM_READ(tape_start_r) //R start tape
+	AM_RANGE(0xee40, 0xee40) AM_WRITE(xor_display_w) //W Picture reverse
 	AM_RANGE(0xee80, 0xee80) AM_READWRITE(tape_r,tape_w)//RW tape input / output
 	AM_RANGE(0xeec0, 0xeec0) AM_READWRITE(key_r,key_w)//RW keyboard
 	AM_RANGE(0xef00, 0xef00) AM_READ(ff_r) //R timer
-//	AM_RANGE(0xef40, 0xef40) R unknown
-//	AM_RANGE(0xef80, 0xef80) R unknown
+	AM_RANGE(0xef40, 0xef40) AM_READ(ff_r) //R unknown
+	AM_RANGE(0xef80, 0xef80) AM_READ(unk_r) //R unknown
 //	AM_RANGE(0xefe0, 0xefe0) W screen mode
 	AM_RANGE(0xf000, 0xffff) AM_ROM
 ADDRESS_MAP_END
@@ -267,6 +310,8 @@ static MACHINE_START(bmjr)
 static MACHINE_RESET(bmjr)
 {
 	tape_switch = 0;
+	bmjr_cassette = machine->device("cassette");
+	cassette_change_state(bmjr_cassette,CASSETTE_MOTOR_DISABLED,CASSETTE_MASK_MOTOR);
 }
 
 static MACHINE_DRIVER_START( bmjr )
@@ -293,10 +338,15 @@ static MACHINE_DRIVER_START( bmjr )
     MDRV_VIDEO_START(bmjr)
     MDRV_VIDEO_UPDATE(bmjr)
 
+	MDRV_CASSETTE_ADD( "cassette", default_cassette_config )
+
 	MDRV_SPEAKER_STANDARD_MONO("mono")
 
 	MDRV_SOUND_ADD("beeper", BEEP, 0)
 	MDRV_SOUND_ROUTE(ALL_OUTPUTS,"mono",0.50)
+
+	MDRV_SOUND_WAVE_ADD("wave", "cassette")
+	MDRV_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.20)
 MACHINE_DRIVER_END
 
 /* ROM definition */
