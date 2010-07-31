b452ea872a2c732ac5a3d98672f44c0d4add298e
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2010-07-31 09:54:11 +0000

    [BASIC MASTER JR.]: Hooked up char rom, added memory map documentation
    git-svn-id: svn://messdev.no-ip.org/mess@8661 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/bmjr.c
+++ src/mess/drivers/bmjr.c
@@ -1,5 +1,5 @@
 /***************************************************************************
-   
+
         Hitachi Basic Master Jr (MB-6885)
 
         13/07/2010 Skeleton driver.
@@ -9,22 +9,7 @@
 #include "emu.h"
 #include "cpu/m6800/m6800.h"
 
-static ADDRESS_MAP_START(bmjr_mem, ADDRESS_SPACE_PROGRAM, 8)
-	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x0000, 0xAFFF) AM_RAM	// screen = 0x100 to 0x3FF
-	AM_RANGE(0xB000, 0xE7FF) AM_ROM
-	AM_RANGE(0xE800, 0xEFFF) AM_RAM	// devices?
-	AM_RANGE(0xF000, 0xFFFF) AM_ROM
-ADDRESS_MAP_END
-
-/* Input ports */
-static INPUT_PORTS_START( bmjr )
-INPUT_PORTS_END
-
-
-static MACHINE_RESET(bmjr) 
-{	
-}
+static UINT8 *wram;
 
 static VIDEO_START( bmjr )
 {
@@ -35,13 +20,59 @@ static VIDEO_UPDATE( bmjr )
     return 0;
 }
 
+static ADDRESS_MAP_START(bmjr_mem, ADDRESS_SPACE_PROGRAM, 8)
+	ADDRESS_MAP_UNMAP_HIGH
+	//0x0100, 0x03ff basic vram
+	//0x0900, 0x20ff vram, modes 0x40 / 0xc0
+	//0x2100, 0x38ff vram, modes 0x44 / 0xcc
+	AM_RANGE(0x0000, 0xafff) AM_RAM AM_BASE(&wram)
+	AM_RANGE(0xb000, 0xe7ff) AM_ROM
+//	AM_RANGE(0xe890, 0xe890) W MP-1710 tile color
+//	AM_RANGE(0xe891, 0xe891) W MP-1710 background color
+//	AM_RANGE(0xe892, 0xe892) W MP-1710 monochrome / color setting
+//	AM_RANGE(0xee00, 0xee00) R stop tape
+//	AM_RANGE(0xee20, 0xee20) R start tape
+//	AM_RANGE(0xee40, 0xee40) W Picture reverse (?)
+//	AM_RANGE(0xee80, 0xee80) RW tape input / output
+//	AM_RANGE(0xeec0, 0xeec0) RW keyboard
+//	AM_RANGE(0xef00, 0xef00) R timer
+//	AM_RANGE(0xef40, 0xef40) R unknown
+//	AM_RANGE(0xef80, 0xef80) R unknown
+//	AM_RANGE(0xefe0, 0xefe0) W screen mode
+	AM_RANGE(0xf000, 0xffff) AM_ROM
+ADDRESS_MAP_END
+
+/* Input ports */
+static INPUT_PORTS_START( bmjr )
+INPUT_PORTS_END
+
+
+static MACHINE_RESET(bmjr)
+{
+}
+
+static const gfx_layout bmjr_charlayout =
+{
+	8, 8,
+	RGN_FRAC(1,1),
+	1,
+	{ 0 },
+	{ 0, 1, 2, 3, 4, 5, 6, 7 },
+	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8 },
+	8*8
+};
+
+static GFXDECODE_START( bmjr )
+	GFXDECODE_ENTRY( "char", 0x0000, bmjr_charlayout, 0, 8 )
+GFXDECODE_END
+
 static MACHINE_DRIVER_START( bmjr )
     /* basic machine hardware */
     MDRV_CPU_ADD("maincpu",M6800, XTAL_4MHz)
     MDRV_CPU_PROGRAM_MAP(bmjr_mem)
 
     MDRV_MACHINE_RESET(bmjr)
-	
+
     /* video hardware */
     MDRV_SCREEN_ADD("screen", RASTER)
     MDRV_SCREEN_REFRESH_RATE(50)
@@ -52,6 +83,8 @@ static MACHINE_DRIVER_START( bmjr )
     MDRV_PALETTE_LENGTH(2)
     MDRV_PALETTE_INIT(black_and_white)
 
+    MDRV_GFXDECODE(bmjr)
+
     MDRV_VIDEO_START(bmjr)
     MDRV_VIDEO_UPDATE(bmjr)
 MACHINE_DRIVER_END
@@ -59,13 +92,20 @@ MACHINE_DRIVER_END
 /* ROM definition */
 ROM_START( bmjr )
     ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_LOAD( "bas.rom", 0xB000, 0x3000, CRC(2318e04e) SHA1(cdb3535663090f5bcaba20b1dbf1f34724ef6a5f))
-	ROM_LOAD( "mon.rom", 0xF000, 0x1000, CRC(776cfa3a) SHA1(be747bc40fdca66b040e0f792b05fcd43a1565ce))
-	ROM_LOAD( "prt.rom", 0xE000, 0x0800, CRC(b9aea867) SHA1(b8dd5348790d76961b6bdef41cfea371fdbcd93d))
+	ROM_LOAD( "bas.rom", 0xb000, 0x3000, BAD_DUMP CRC(2318e04e) SHA1(cdb3535663090f5bcaba20b1dbf1f34724ef6a5f)) //12k ROMs doesn't exist ...
+	ROM_LOAD( "mon.rom", 0xf000, 0x1000, CRC(776cfa3a) SHA1(be747bc40fdca66b040e0f792b05fcd43a1565ce))
+	ROM_LOAD( "prt.rom", 0xe000, 0x0800, CRC(b9aea867) SHA1(b8dd5348790d76961b6bdef41cfea371fdbcd93d))
+
+	ROM_REGION( 0x800, "char", 0 )
+	ROM_LOAD( "font.rom", 0x0000, 0x0800, BAD_DUMP CRC(258c6fd7) SHA1(d7c7dd57d6fc3b3d44f14c32182717a48e24587f)) //taken from a JP emulator
 ROM_END
 
 /* Driver */
+static DRIVER_INIT( bmjr )
+{
+
+}
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 1982, bmjr,  	0,       0, 	bmjr, 		bmjr, 	 0,  	 "Hitachi",   "Basic Master Jr",	GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1982, bmjr,  	0,       0, 	bmjr, 		bmjr, 	 bmjr,  	 "Hitachi",   "Basic Master Jr",	GAME_NOT_WORKING | GAME_NO_SOUND)
 
