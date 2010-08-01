7002448c529fc2d213a8b776648f911061a34636
jgevarya <jgevarya@9849079f-6229-0410-a07f-84406ce305ba>
2010-08-01 08:17:49 +0000

    Comment updates for zexall driver to clarify
    git-svn-id: svn://messdev.no-ip.org/mess@8673 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/zexall.c
+++ src/mess/drivers/zexall.c
@@ -1,17 +1,18 @@
 /******************************************************************************
 *
-*  Bare bones 'self contained' zexall test driver
+*  Self Contained zexall 'Z80 instruction exercizer' test driver
 *  By Jonathan Gevaryahu AKA Lord Nightmare
-*  Binary supplied by Blargg
-*  with help from Kevin 'kevtris' Horton (wrote preloader at 0000-00ff which implements the fffd-ffff serial stuff)
+*  Zexall originally written by Frank Cringle for ZX Spectrum
+*  Modularized Spectrum-independent Zexall binary supplied by Blargg
+*  Serial interface binary/preloader at 0x0000-0x00FF written by Kevin 'kevtris' Horton
 *
 *
 * mem map:
 Ram 0000-FFFF (preloaded with binary)
-Special calls take place for three ram values:
-FFFD - shared ram with output device; z80 reads from here and considers the byte at FFFF read if this value incremented
-FFFE - shared ram with output device; z80 writes an incrementing value to FFFD if there is a byte waiting at FFFF until FFFD is incremented
-FFFF - shared ram with output device; z80 writes data to be sent to output device here
+Special calls take place for three ram values (this interface was designed by kevtris):
+FFFD - 'ack' - shared ram with output device; z80 reads from here and considers the byte at FFFF read if this value incremented
+FFFE - 'req' - shared ram with output device; z80 writes an incrementing value to FFFE to indicate that there is a byte waiting at FFFF and hence requesting the output device on the other end do something about it, until FFFD is incremented by the output device to acknowledge reciept
+FFFF - 'data' - shared ram with output device; z80 writes the data to be sent to output device here
 One i/o port is used:
 0001 - bit 0 controls whether interrupt timer is enabled (1) or not (0), this is a holdover from a project of kevtris' and can be ignored.
 
@@ -20,7 +21,6 @@ One i/o port is used:
 /* Core includes */
 #include "emu.h"
 #include "cpu/z80/z80.h"
-//#include "dectalk.lh" //  hack to avoid screenless system crash
 #include "machine/terminal.h"
 
 /* Defines */
@@ -30,11 +30,11 @@ static UINT8 *main_ram;
 
 static struct
 {
-UINT8 data[8]; // what is this for? to suppress the scalar initializer warning?
-UINT8 out_data;
-UINT8 out_req;
-UINT8 out_req_last;
-UINT8 out_ack;
+	UINT8 data[8]; // unused; to suppress the scalar initializer warning
+	UINT8 out_data; // byte written to 0xFFFF
+	UINT8 out_req; // byte written to 0xFFFE
+	UINT8 out_req_last; // old value at 0xFFFE before the most recent write
+	UINT8 out_ack; // byte written to 0xFFFC
 } zexall={ {0}};
 
 /* Devices */
@@ -112,7 +112,7 @@ ADDRESS_MAP_END
 
 static ADDRESS_MAP_START(z80_io, ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x0001, 0x0001) AM_NOP // really a disable/enable for some sort of interrupt timer on kev's hardware, which is completely irrelevant for the test
+	AM_RANGE(0x0001, 0x0001) AM_NOP // really a disable/enable for some sort of interrupt timer on kev's hardware, which is completely irrelevant for the zexall test
 ADDRESS_MAP_END
 
 
@@ -137,14 +137,13 @@ static GENERIC_TERMINAL_INTERFACE( dectalk_terminal_intf )
 
 static MACHINE_DRIVER_START(zexall)
     /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu", Z80, XTAL_3_579545MHz*60)  /* Toshiba TMPZ84C00AP @ 3.579545 MHz, verified, xtal is divided by 6 */
+    MDRV_CPU_ADD("maincpu", Z80, XTAL_3_579545MHz*10)  
     MDRV_CPU_PROGRAM_MAP(z80_mem)
     MDRV_CPU_IO_MAP(z80_io)
     MDRV_QUANTUM_TIME(HZ(60))
     MDRV_MACHINE_RESET(zexall)
 
     /* video hardware */
-    //MDRV_DEFAULT_LAYOUT(layout_dectalk) // hack to avoid screenless system crash
 	MDRV_IMPORT_FROM( generic_terminal )
 	MDRV_GENERIC_TERMINAL_ADD(TERMINAL_TAG,dectalk_terminal_intf)
 
