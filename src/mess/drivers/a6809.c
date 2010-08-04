19cd39f8f83d78c098e91425b130938ad8664a6f
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2010-08-04 14:52:05 +0000

    [A6809] Connected some I/O and video to machine
    git-svn-id: svn://messdev.no-ip.org/mess@8707 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/a6809.c
+++ src/mess/drivers/a6809.c
@@ -11,11 +11,18 @@
 
 #include "emu.h"
 #include "cpu/m6809/m6809.h"
+#include "machine/6522via.h"
+#include "video/saa5050.h"
+#include "video/mc6845.h"
 
 static ADDRESS_MAP_START(a6809_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x0000,0xf7ff) AM_RAM
-	AM_RANGE(0x0400,0x07ff) AM_BASE_GENERIC(videoram)
+	AM_RANGE(0x0000,0x03ff) AM_RAM
+	AM_RANGE(0x0400,0x07ff) AM_DEVREADWRITE("saa5050", saa5050_videoram_r, saa5050_videoram_w)	
+	AM_RANGE(0x0800,0x0800) AM_DEVWRITE("mc6845", mc6845_address_w)
+	AM_RANGE(0x0801,0x0801) AM_DEVREADWRITE("mc6845", mc6845_register_r , mc6845_register_w)
+	AM_RANGE(0x0900,0x090f) AM_MIRROR(0xf0)AM_DEVREADWRITE("via", via_r, via_w)
+	AM_RANGE(0xf000,0xf7ff) AM_ROM // optional ROM
 	AM_RANGE(0xf800,0xffff) AM_ROM
 ADDRESS_MAP_END
 
@@ -32,14 +39,56 @@ static MACHINE_RESET(a6809)
 {
 }
 
-static VIDEO_START( a6809 )
+static VIDEO_UPDATE( a6809 )
+{
+	running_device *saa5050 = screen->machine->device("saa5050");
+
+	saa5050_update(saa5050, bitmap, NULL);
+	return 0;
+}
+
+static MC6845_UPDATE_ROW( a6809_update_row )
 {
 }
 
-static VIDEO_UPDATE( a6809 )
+static const mc6845_interface a6809_crtc6845_interface =
 {
-	return 0;
-}
+	"screen",
+	6 /*?*/,
+	NULL,
+	a6809_update_row,
+	NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	NULL
+};
+
+static const saa5050_interface a6809_saa5050_intf =
+{
+	"screen",
+	0,	/* starting gfxnum */
+	40, 25, 40,  /* x, y, size */
+      0 	/* rev y order */
+};
+
+static const via6522_interface via_intf =
+{
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_CPU_INPUT_LINE("maincpu", INPUT_LINE_IRQ0)
+};
 
 static MACHINE_DRIVER_START( a6809 )
 	/* basic machine hardware */
@@ -52,15 +101,22 @@ static MACHINE_DRIVER_START( a6809 )
 	/* video hardware */
 	MDRV_SCREEN_ADD("screen", RASTER)
 	MDRV_SCREEN_REFRESH_RATE(50)
-	MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-	MDRV_SCREEN_SIZE(640, 480)
-	MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-	MDRV_PALETTE_LENGTH(2)
-	MDRV_PALETTE_INIT(black_and_white)
+	MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(SAA5050_VBLANK))
 
-	MDRV_VIDEO_START(a6809)
+	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MDRV_SCREEN_SIZE(40 * 6, 25 * 10)
+	MDRV_SCREEN_VISIBLE_AREA(0, 40 * 6 - 1, 0, 25 * 10 - 1)
+	MDRV_GFXDECODE(saa5050)
+	MDRV_PALETTE_LENGTH(128)
+	MDRV_PALETTE_INIT(saa5050)
+
+	MDRV_SAA5050_ADD("saa5050", a6809_saa5050_intf)
+
 	MDRV_VIDEO_UPDATE(a6809)
+	
+	MDRV_VIA6522_ADD("via", XTAL_4MHz / 4, via_intf)
+	
+	MDRV_MC6845_ADD("mc6845", MC6845, XTAL_4MHz / 2, a6809_crtc6845_interface)
 MACHINE_DRIVER_END
 
 /* ROM definition */
@@ -71,7 +127,10 @@ ROM_START( a6809 )
 	/* This looks like some sort of prom */
 	ROM_REGION( 0x100, "proms", 0 )
 	ROM_LOAD( "acorn6809.ic11", 0x0000, 0x0100, CRC(7908317d) SHA1(e0f1e5bd3a8598d3b62bc432dd1f3892ed7e66d8) )
-	/* Character generator is missing */
+	
+	/* This is SAA5050 so same char def is taken as in p2000t driver */
+	ROM_REGION(0x01000, "gfx1",0)
+	ROM_LOAD("p2000.chr", 0x0140, 0x08c0, BAD_DUMP CRC(78c17e3e) SHA1(4e1c59dc484505de1dc0b1ba7e5f70a54b0d4ccc))
 ROM_END
 
 /* Driver */
