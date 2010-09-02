437c0e0358a43f13622aab10a8da91a7380f7279
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2010-09-02 08:18:01 +0000

    Another disclaimer fix
    git-svn-id: svn://messdev.no-ip.org/mess@9013 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/mess.c
+++ src/mess/mess.c
@@ -35,7 +35,7 @@ void mess_display_help(void)
 {
 	mame_printf_info(
 		"MESS v%s\n"
-		"Multi Emulator Super System - Copyright (C) 1997-2009 by the MESS Team\n"
+		"Multi Emulator Super System - Copyright (C) 1997-2010 by the MESS Team\n"
 		"MESS is based on MAME Source code\n"
 		"Copyright (C) 1997-2010 by Nicola Salmoria and the MAME Team\n\n",
 		build_version);
