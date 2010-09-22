bb06b8f6f560f46018bee13ed0dcee833c76cbcc
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-09-22 07:12:15 +0000

    [ABC99] Converted to modern device.
    git-svn-id: svn://messdev.no-ip.org/mess@9211 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/abc99.c
+++ src/mess/machine/abc99.c
@@ -1,77 +1,134 @@
 #include "emu.h"
 #include "abc99.h"
 #include "cpu/mcs48/mcs48.h"
+#include "machine/devhelpr.h"
 
-/*-------------------------------------------------
-    ADDRESS_MAP( abc99_map )
--------------------------------------------------*/
 
-static ADDRESS_MAP_START( abc99_map, ADDRESS_SPACE_PROGRAM, 8 )
-	AM_RANGE(0x000, 0xfff) AM_ROM
-ADDRESS_MAP_END
 
-/*-------------------------------------------------
-    ADDRESS_MAP( abc99_io_map )
--------------------------------------------------*/
+//**************************************************************************
+//	MACROS / CONSTANTS
+//**************************************************************************
 
-static ADDRESS_MAP_START( abc99_io_map, ADDRESS_SPACE_IO, 8 )
-ADDRESS_MAP_END
+#define I8035_TAG	"i8035"
 
-/*-------------------------------------------------
-    MACHINE_DRIVER( abc99 )
--------------------------------------------------*/
 
-static MACHINE_CONFIG_FRAGMENT( abc99 )
-	MDRV_CPU_ADD("i8035", I8035, XTAL_4_608MHz)
-	MDRV_CPU_PROGRAM_MAP(abc99_map)
-	MDRV_CPU_IO_MAP(abc99_io_map)
-MACHINE_CONFIG_END
 
-/*-------------------------------------------------
-    ROM( abc99 )
--------------------------------------------------*/
+//**************************************************************************
+//  DEVICE DEFINITIONS
+//**************************************************************************
+
+const device_type ABC99 = abc99_device_config::static_alloc_device_config;
+
+
+
+//**************************************************************************
+//  DEVICE CONFIGURATION
+//**************************************************************************
+
+GENERIC_DEVICE_CONFIG_SETUP(abc99, "Luxor ABC 99")
+
+
+//-------------------------------------------------
+//  device_config_complete - perform any
+//  operations now that the configuration is
+//  complete
+//-------------------------------------------------
+
+void abc99_device_config::device_config_complete()
+{
+}
+
+
+//-------------------------------------------------
+//  ROM( abc99 )
+//-------------------------------------------------
 
 ROM_START( abc99 )
-	ROM_REGION( 0x1800, "i8035", 0 )
+	ROM_REGION( 0x1800, I8035_TAG, 0 )
 	ROM_LOAD( "10681909.bin", 0x0000, 0x1000, CRC(ffe32a71) SHA1(fa2ce8e0216a433f9bbad0bdd6e3dc0b540f03b7) )
 	ROM_LOAD( "10726864.bin", 0x1000, 0x0800, CRC(e33683ae) SHA1(0c1d9e320f82df05f4804992ef6f6f6cd20623f3) )
 ROM_END
 
-/*-------------------------------------------------
-    DEVICE_START( abc99 )
--------------------------------------------------*/
 
-static DEVICE_START( abc99 )
+//-------------------------------------------------
+//  rom_region - device-specific ROM region
+//-------------------------------------------------
+
+const rom_entry *abc99_device_config::rom_region() const
+{
+	return ROM_NAME( abc99 );
+}
+
+
+//-------------------------------------------------
+//  ADDRESS_MAP( abc99_map )
+//-------------------------------------------------
+
+static ADDRESS_MAP_START( abc99_map, ADDRESS_SPACE_PROGRAM, 8 )
+	AM_RANGE(0x0000, 0x17ff) AM_ROM
+ADDRESS_MAP_END
+
+
+//-------------------------------------------------
+//  ADDRESS_MAP( abc99_io_map )
+//-------------------------------------------------
+
+static ADDRESS_MAP_START( abc99_io_map, ADDRESS_SPACE_IO, 8 )
+ADDRESS_MAP_END
+
+
+//-------------------------------------------------
+//  MACHINE_DRIVER( abc99 )
+//-------------------------------------------------
+
+static MACHINE_CONFIG_FRAGMENT( abc99 )
+	MDRV_CPU_ADD(I8035_TAG, I8035, 4608000) // ?
+	MDRV_CPU_PROGRAM_MAP(abc99_map)
+	MDRV_CPU_IO_MAP(abc99_io_map)
+MACHINE_CONFIG_END
+
+
+//-------------------------------------------------
+//  machine_config_additions - device-specific
+//	machine configurations
+//-------------------------------------------------
+
+machine_config_constructor abc99_device_config::machine_config_additions() const
+{
+	return MACHINE_CONFIG_NAME( abc99 );
+}
+
+
+
+//**************************************************************************
+//  LIVE DEVICE
+//**************************************************************************
+
+//-------------------------------------------------
+//  abc99_device - constructor
+//-------------------------------------------------
+
+abc99_device::abc99_device(running_machine &_machine, const abc99_device_config &config)
+    : device_t(_machine, config),
+      m_config(config)
+{
+
+}
+
+
+//-------------------------------------------------
+//  device_start - device-specific startup
+//-------------------------------------------------
+
+void abc99_device::device_start()
 {
 }
 
-/*-------------------------------------------------
-    DEVICE_GET_INFO( abc99 )
--------------------------------------------------*/
 
-DEVICE_GET_INFO( abc99 )
+//-------------------------------------------------
+//  device_reset - device-specific reset
+//-------------------------------------------------
+
+void abc99_device::device_reset()
 {
-	switch (state)
-	{
-		/* --- the following bits of info are returned as 64-bit signed integers --- */
-		case DEVINFO_INT_INLINE_CONFIG_BYTES:			info->i = 0;								break;
-			
-		/* --- the following bits of info are returned as pointers --- */
-		case DEVINFO_PTR_ROM_REGION:					info->romregion = rom_abc99;				break;
-		case DEVINFO_PTR_MACHINE_CONFIG:				info->machine_config = MACHINE_CONFIG_NAME(abc99); break;
-			
-		/* --- the following bits of info are returned as pointers to functions --- */
-		case DEVINFO_FCT_START:							info->start = DEVICE_START_NAME(abc99);		break;
-		case DEVINFO_FCT_STOP:							/* Nothing */								break;
-		case DEVINFO_FCT_RESET:							/* Nothing */								break;
-
-		/* --- the following bits of info are returned as NULL-terminated strings --- */
-		case DEVINFO_STR_NAME:							strcpy(info->s, "Luxor ABC-99");			break;
-		case DEVINFO_STR_FAMILY:						strcpy(info->s, "Luxor ABC");				break;
-		case DEVINFO_STR_VERSION:						strcpy(info->s, "1.0");						break;
-		case DEVINFO_STR_SOURCE_FILE:					strcpy(info->s, __FILE__);					break;
-		case DEVINFO_STR_CREDITS:						strcpy(info->s, "Copyright the MESS Team");	break;
-	}
 }
-
-DEFINE_LEGACY_DEVICE(ABC99, abc99);
--- src/mess/machine/abc99.h
+++ src/mess/machine/abc99.h
@@ -1,17 +1,89 @@
+#pragma once
+
 #ifndef __ABC99__
 #define __ABC99__
 
 #include "emu.h"
 
-/***************************************************************************
-    MACROS / CONSTANTS
-***************************************************************************/
+
+
+//**************************************************************************
+//	MACROS / CONSTANTS
+//**************************************************************************
 
 #define ABC99_TAG	"abc99"
 
-DECLARE_LEGACY_DEVICE(ABC99, abc99);
 
-#define MDRV_ABC99_ADD \
-	MDRV_DEVICE_ADD(ABC99_TAG, ABC99, 0)
+
+//**************************************************************************
+//  INTERFACE CONFIGURATION MACROS
+//**************************************************************************
+
+#define MDRV_ABC99_ADD(_tag) \
+    MDRV_DEVICE_ADD(_tag, ABC99, 0)
+
+
+
+//**************************************************************************
+//	TYPE DEFINITIONS
+//**************************************************************************
+
+// ======================> abc99_interface
+
+struct abc99_interface
+{
+};
+
+
+// ======================> abc99_device_config
+
+class abc99_device_config :   public device_config,
+                                public abc99_interface
+{
+    friend class abc99_device;
+
+    // construction/destruction
+    abc99_device_config(const machine_config &mconfig, const char *tag, const device_config *owner, UINT32 clock);
+
+public:
+    // allocators
+    static device_config *static_alloc_device_config(const machine_config &mconfig, const char *tag, const device_config *owner, UINT32 clock);
+    virtual device_t *alloc_device(running_machine &machine) const;
+	
+	// optional information overrides
+	virtual const rom_entry *rom_region() const;
+	virtual machine_config_constructor machine_config_additions() const;
+
+protected:
+    // device_config overrides
+    virtual void device_config_complete();
+};
+
+
+// ======================> abc99_device
+
+class abc99_device :  public device_t
+{
+    friend class abc99_device_config;
+
+    // construction/destruction
+    abc99_device(running_machine &_machine, const abc99_device_config &_config);
+
+public:
+
+protected:
+    // device-level overrides
+    virtual void device_start();
+    virtual void device_reset();
+
+private:
+    const abc99_device_config &m_config;
+};
+
+
+// device type definition
+extern const device_type ABC99;
+
+
 
 #endif
