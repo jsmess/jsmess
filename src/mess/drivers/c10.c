f8f6c580ecc4eac083f3cb1b97f8d8bc70cdf092
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-11-13 23:44:21 +0000

    [C10] Added rudimentary video driver.
    git-svn-id: svn://messdev.no-ip.org/mess@9573 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/c10.c
+++ src/mess/drivers/c10.c
@@ -4,16 +4,24 @@
 
         30/08/2010 Skeleton driver
 
+        Driver currently gets to a loop where it waits for an interrupt.
+        The interrupt routine presumably writes to FE69 which the loop is
+        constantly looking at.
+
 ****************************************************************************/
 
 #include "emu.h"
 #include "cpu/z80/z80.h"
 
-static UINT8 *c10_ram;
+static const UINT8 *FNT;
+static const UINT8 *videoram;
 
 static ADDRESS_MAP_START(c10_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x0000, 0xffff) AM_RAM AM_BASE(&c10_ram)
+	AM_RANGE(0x0000, 0x0fff) AM_RAMBANK("boot")
+	AM_RANGE(0x1000, 0x7fff) AM_RAM
+	AM_RANGE(0x8000, 0xbfff) AM_ROM
+	AM_RANGE(0xc000, 0xffff) AM_RAM AM_REGION("maincpu", 0xc000)
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( c10_io, ADDRESS_SPACE_IO, 8 )
@@ -24,20 +32,73 @@ ADDRESS_MAP_END
 static INPUT_PORTS_START( c10 )
 INPUT_PORTS_END
 
+/* after the first 4 bytes have been read from ROM, switch the ram back in */
+static TIMER_CALLBACK( c10_reset )
+{
+	memory_set_bank(machine, "boot", 0);
+}
+
 static MACHINE_RESET(c10)
 {
-	UINT8* bios = memory_region(machine, "maincpu");
-
-	memcpy(c10_ram,bios, 0x4000);
-	memcpy(c10_ram+0x8000,bios, 0x4000);
+	memory_set_bank(machine, "boot", 1);
+	timer_set(machine, ATTOTIME_IN_USEC(4), NULL, 0, c10_reset);
 }
 
 static VIDEO_START( c10 )
 {
+	FNT = memory_region(machine, "chargen");
+	videoram = memory_region(machine, "maincpu")+0xf0a2;
 }
 
+/* This system appears to have inline attribute bytes of unknown meaning.
+    Currently they are ignored. The word at FAB5 looks like it might be cursor location. */
 static VIDEO_UPDATE( c10 )
 {
+	//static UINT8 framecnt=0;
+	UINT8 y,ra,chr,gfx;
+	UINT16 sy=0,ma=0,x,xx;
+
+	//framecnt++;
+
+	for (y = 0; y < 25; y++)
+	{
+		for (ra = 0; ra < 10; ra++)
+		{
+			UINT16  *p = BITMAP_ADDR16(bitmap, sy++, 0);
+
+			xx = ma;
+			for (x = ma; x < ma + 80; x++)
+			{
+				gfx = 0;
+				if (ra < 9)
+				{
+					chr = videoram[xx++];
+
+				//	/* Take care of flashing characters */
+				//	if ((chr < 0x80) && (framecnt & 0x08))
+				//		chr |= 0x80;
+
+					if (chr & 0x80)  // ignore attribute bytes
+						x--;
+					else
+					{
+						gfx = FNT[(chr<<4) | ra ];
+
+						/* Display a scanline of a character */
+						*p = ( gfx & 0x80 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x40 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x20 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x10 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x08 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x04 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x02 ) ? 1 : 0; p++;
+						*p = ( gfx & 0x01 ) ? 1 : 0; p++;
+					}
+				}
+			}
+		}
+		ma+=96;
+	}
 	return 0;
 }
 
@@ -73,8 +134,8 @@ static MACHINE_CONFIG_START( c10, driver_device )
 	MDRV_SCREEN_REFRESH_RATE(60)
 	MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-	MDRV_SCREEN_SIZE(256, 192) /* border size not accurate */
-	MDRV_SCREEN_VISIBLE_AREA(0, 256 - 1, 0, 192 - 1)
+	MDRV_SCREEN_SIZE(640, 250)
+	MDRV_SCREEN_VISIBLE_AREA(0, 639, 0, 249)
 	MDRV_GFXDECODE(c10)
 
 	MDRV_PALETTE_LENGTH(2)
@@ -84,11 +145,16 @@ static MACHINE_CONFIG_START( c10, driver_device )
 	MDRV_VIDEO_UPDATE(c10)
 MACHINE_CONFIG_END
 
+DRIVER_INIT( c10 )
+{
+	UINT8 *RAM = memory_region(machine, "maincpu");
+	memory_configure_bank(machine, "boot", 0, 2, &RAM[0x0000], 0x8000);
+}
 
 /* ROM definition */
 ROM_START( c10 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_LOAD( "c10_cros.bin", 0x0000, 0x4000, CRC(2ccf5983) SHA1(52f7c497f5284bf5df9eb0d6e9142bb1869d8c24))
+	ROM_LOAD( "c10_cros.bin", 0x8000, 0x4000, CRC(2ccf5983) SHA1(52f7c497f5284bf5df9eb0d6e9142bb1869d8c24))
 	ROM_REGION( 0x2000, "chargen", 0 )
 	ROM_LOAD( "c10_char.bin", 0x0000, 0x2000, CRC(cb530b6f) SHA1(95590bbb433db9c4317f535723b29516b9b9fcbf))
 ROM_END
@@ -96,4 +162,4 @@ ROM_END
 /* Driver */
 
 /*   YEAR  NAME    PARENT  COMPAT   MACHINE  INPUT  INIT        COMPANY   FULLNAME       FLAGS */
-COMP( 1982, c10,  0,       0,	c10,	c10,	 0, 	  "Cromemco",   "C-10",		GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1982, c10,  0,       0,	c10,	c10,	 c10, 	  "Cromemco",   "C-10",		GAME_NOT_WORKING | GAME_NO_SOUND)
