f824e1f5495df283cfaae65e450e5749b7d6dfc7
robbbert <robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-11-18 10:03:38 +0000

    [MCCPM] Connected to a terminal.
    git-svn-id: svn://messdev.no-ip.org/mess@9611 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mccpm.c
+++ src/mess/drivers/mccpm.c
@@ -3,13 +3,30 @@
         mc-CP/M-Computer
 
         31/08/2010 Skeleton driver.
+        18/11/2010 Connected to a terminal
 
 ****************************************************************************/
 
 #include "emu.h"
 #include "cpu/z80/z80.h"
+#include "machine/terminal.h"
 
 static UINT8 *mccpm_ram;
+static UINT8 term_data;
+
+static WRITE8_HANDLER( mccpm_f0_w )
+{
+	running_device *terminal = space->machine->device("terminal");
+
+	terminal_write(terminal, 0, data);
+}
+
+static READ8_HANDLER( mccpm_f0_r )
+{
+	UINT8 ret = term_data;
+	term_data = 0;
+	return ret;
+}
 
 static ADDRESS_MAP_START(mccpm_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
@@ -19,10 +36,12 @@ ADDRESS_MAP_END
 static ADDRESS_MAP_START( mccpm_io , ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0xf0, 0xf0) AM_READWRITE(mccpm_f0_r,mccpm_f0_w)
 ADDRESS_MAP_END
 
 /* Input ports */
-INPUT_PORTS_START( mccpm )
+static INPUT_PORTS_START( mccpm )
+	PORT_INCLUDE(generic_terminal)
 INPUT_PORTS_END
 
 
@@ -33,40 +52,33 @@ static MACHINE_RESET(mccpm)
 	memcpy(mccpm_ram,bios, 0x1000);
 }
 
-static VIDEO_START( mccpm )
+static WRITE8_DEVICE_HANDLER( mccpm_kbd_put )
 {
+	term_data = data;
 }
 
-static VIDEO_UPDATE( mccpm )
+static GENERIC_TERMINAL_INTERFACE( mccpm_terminal_intf )
 {
-    return 0;
-}
+	DEVCB_HANDLER(mccpm_kbd_put)
+};
 
 static MACHINE_CONFIG_START( mccpm, driver_device )
-    /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)
-    MDRV_CPU_PROGRAM_MAP(mccpm_mem)
-    MDRV_CPU_IO_MAP(mccpm_io)
+	/* basic machine hardware */
+	MDRV_CPU_ADD("maincpu",Z80, XTAL_4MHz)
+	MDRV_CPU_PROGRAM_MAP(mccpm_mem)
+	MDRV_CPU_IO_MAP(mccpm_io)
 
-    MDRV_MACHINE_RESET(mccpm)
+	MDRV_MACHINE_RESET(mccpm)
 
-    /* video hardware */
-    MDRV_SCREEN_ADD("screen", RASTER)
-    MDRV_SCREEN_REFRESH_RATE(50)
-    MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MDRV_SCREEN_SIZE(640, 480)
-    MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-    MDRV_PALETTE_LENGTH(2)
-    MDRV_PALETTE_INIT(black_and_white)
+	/* video hardware */
+	MDRV_FRAGMENT_ADD( generic_terminal )
 
-    MDRV_VIDEO_START(mccpm)
-    MDRV_VIDEO_UPDATE(mccpm)
+	MDRV_GENERIC_TERMINAL_ADD("terminal", mccpm_terminal_intf)
 MACHINE_CONFIG_END
 
 /* ROM definition */
 ROM_START( mccpm )
-    ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD( "mon36.j15", 0x0000, 0x1000, CRC(9c441537) SHA1(f95bad52d9392b8fc9d9b8779b7b861672a0022b))
 ROM_END
 
