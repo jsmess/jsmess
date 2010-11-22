1a3c0a4f072d42ab9f63dbdf39d6ecd08af79928
cow <cow@9849079f-6229-0410-a07f-84406ce305ba>
2010-11-22 20:25:35 +0000

    fixing the madness that is AA :)
    git-svn-id: svn://messdev.no-ip.org/mess@9635 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/polgar.c
+++ src/mess/drivers/polgar.c
@@ -1,56 +1,16 @@
 /******************************************************************************
- Mephisto 4 + 5 Chess Computer
- 2007 Dirk V.
+ Mephisto Polgar
+ 2010 Dirk V., Cowering
 
 ******************************************************************************/
 
 /*
 
-
 CPU 65C02 P4
 Clock 4.9152 MHz
-NMI CLK 600 Hz
-IRQ Line is set to VSS
-8 KByte  SRAM Sony CXK5864-15l
-
-1-CD74HC4060E   14 Bit Counter
-1-CD74HC166E
-1-CD74HC251E
-1-SN74HC138N TI
-1-SN74HC139N TI
-1-74HC14AP Toshiba
-1-74HC02AP Toshiba
-1-74HC00AP Toshiba
-1-CD74HC259E
 
 
-$0000-$1fff   S-RAM
-$2000 LCD 4 Byte Shift Register writeonly right to left
-every 2nd char xor'd by $FF
-
-2c00-2c07 Keyboard (8to1 Multiplexer) 74HCT251
-2*8 Matrix
-Adr. 0x3407
-==0 !=0
-2c00 CL E5
-2c01 POS F6
-2c02 MEMO G7
-2c03 INFO A1
-2c04 LEV H8
-2c05 ENT B2
-2c06 >0 C3
-2c07 <9 D4
-
-$3400-$3407 LED 1-6, Buzzer, Keyboard select
-
-$2400 // Chess Board
-$2800 // Chess Board
-$3000 // Chess Board
-
-$4000-7FFF Opening Modul HG550
-$8000-$FFF ROM
-
-// lese Tastatur von 2c00 - 2c07, 
+// lese Tastatur von 2c00 - 2c07,
 2c00 Trn
 2c01 Info
 2c02 Mem
@@ -60,17 +20,22 @@ $8000-$FFF ROM
 2c06 Ent
 2c07 CL
 
-
+$1ff0
+Bit 0 LCD Command/Data  0/1
+Bit 1 LCD Enable (signals Data is valid)
+Bit 2 Beeper
+Bit 3 Beeper
+Bit 4  LED A-H
+Bit 5  LED 1-8
+Bit 6  LED 1-8
+Bit 7  LED A-H
 
 
 */
 
 #include "emu.h"
 #include "cpu/m6502/m6502.h"
-// #include"cpu/mcs51/mcs51.h"
-// #include "sound/dac.h"
 #include "sound/beep.h"
-
 // #include "milano.lh"
 #include "video/hd44780.h"
 // #include "render.h"
@@ -78,13 +43,15 @@ $8000-$FFF ROM
 // #include "uiinput.h"
 #include "machine/mboard.h"
 
-static UINT8 lcd_shift_counter;
+//static UINT8 lcd_shift_counter;
 static UINT8 led_status;
 static UINT8 lcd_char=0;
 // static UINT8 *milano_ram;
-// static UINT8 led7;
+static UINT8 led7;
 
+static const int value[4] = {0x80,0x81,0x00,0x01};
 
+static UINT8 latch_data;
 
 static const hd44780_interface polgar_display =
 {
@@ -94,54 +61,95 @@ static const hd44780_interface polgar_display =
 };
 
 
-
-
-
-static WRITE8_HANDLER ( write_lcd_i )
+static WRITE8_HANDLER ( write_io )
 {
-
+int i;
  hd44780_device * hd44780 = space->machine->device<hd44780_device>("hd44780");
  running_device *speaker = space->machine->device("beep");
-// state->hd44780->data_write(offset, data);
-//state->hd44780->control_write(0, data);
 
-if ((data&1)==1) 
- hd44780->data_write(128, lcd_char);
- else
-hd44780->control_write(128, lcd_char);
+if (BIT(data,1)) {
+	if (BIT(data,0)) {
+		hd44780->data_write(128, lcd_char);
+	} else {
+		hd44780->control_write(128, lcd_char);
+	}
+}
+
+if (BIT(data,2) || BIT(data,3)) beep_set_state(speaker,1); else beep_set_state(speaker,0);
+
+if (BIT(data,7) && BIT(data, 4))
+	{
+		for (i=0;i<8;i++)
+		output_set_led_value(i,!BIT(latch_data,i));
+	}
+if (BIT(data,6) && BIT(data,5))
+	{
+		for (i=0;i<8;i++)
+		output_set_led_value(10+i,!BIT(latch_data,7-i));
+	}
+
+	//logerror("LCD Status  Data = %d\n",data);
 
 
- if ((data&4)==4) beep_set_state(speaker,1);
- 	else beep_set_state(speaker,0);
- 	
-
-	logerror("LCD Register = %d Data = %d\n",offset,data);
 }
 
 static WRITE8_HANDLER ( write_lcd )
 {
 
- //hd44780_device * hd44780 = space->machine->device<hd44780_device>("hd44780");
-// state->hd44780->data_write(offset, data);
-//state->hd44780->control_write(0, data);
   lcd_char=data;
- //hd44780->data_write(128, data);
-//hd44780->data_write(128, data_8);
+
+ //logerror("LCD Data = %d [%c]\n",data,(data&0xff));
+
+}
+
+
+static WRITE8_HANDLER ( write_board )
+{
+  latch_data=data;
+}
+
+
+static WRITE8_HANDLER ( milano_write_led )
+{
+	UINT8 LED_offset=100;
+if (data==0xff) output_set_led_value(LED_offset+offset,1);
+	else
+		output_set_led_value(LED_offset+offset,0);
+
+	//logerror("LEDs  Offset = %d Data = %d\n",offset,data);
 }
 
 static WRITE8_HANDLER ( write_led )
 {
 	UINT8 LED_offset=100;
-	//data &= 0xff;
+	data &= 0x80;
+
+	if (data==0)led_status &= 255-(1<<offset) ; else led_status|=1<<offset;
+	if (offset<6)output_set_led_value(LED_offset+offset, led_status&1<<offset?1:0);
+	if (offset==7) led7=data& 0x80 ? 0x00 :0xff;
+	logerror("LEDs  Offset = %d Data = %d\n",offset,data);
+}
+
+/*static WRITE8_HANDLER ( write_led )
+{
+	UINT8 LED_offset=100;
 if (data==0xff) output_set_led_value(LED_offset+offset,1);
 	else
 		output_set_led_value(LED_offset+offset,0);
-	//if (data==0)led_status &= 255-(1<<offset) ; else led_status|=1<<offset;
-	// if (offset<6)output_set_led_value(LED_offset+offset, led_status&1<<offset?1:0);
-	// if (offset==7) led7=data& 0x80 ? 0x00 :0xff;
-	
-	
-//	logerror("LEDs  Offset = %d Data = %d\n",offset,data);
+	if (data==0)led_status &= 255-(1<<offset) ; else led_status|=1<<offset;
+	if (offset<6)output_set_led_value(LED_offset+offset, led_status&1<<offset?1:0);
+	 if (offset==7) led7=data& 0x80 ? 0x00 :0xff;
+
+
+	logerror("LEDs  Offset = %d Data = %d\n",offset,data);
+}
+*/
+static READ8_HANDLER(read_board)
+{
+	UINT8 data;
+  data=input_port_read(space->machine, "KEY1_8");
+  logerror("Keydata  Data = %d\n  ",data);
+return data;
 }
 
 static READ8_HANDLER(read_keys)
@@ -161,7 +169,7 @@ static READ8_HANDLER(read_keys)
 		data=input_port_read(space->machine, keynames[1][offset]);
 */
   data=input_port_read(space->machine, keynames[0][offset]);
-	logerror("Keyboard Port = %s Data = %d\n  ", ((led_status & 0x80) == 0x00) ? keynames[0][offset] : keynames[1][offset], data);
+	// logerror("Keyboard Port = %s Data = %d\n  ", ((led_status & 0x80) == 0x00) ? keynames[0][offset] : keynames[1][offset], data);
 	return data | 0x7f;
 }
 
@@ -175,7 +183,6 @@ static VIDEO_START( polgar )
 static VIDEO_UPDATE( polgar )
 {
     hd44780_device * hd44780 = screen->machine->device<hd44780_device>("hd44780");
-
     return hd44780->video_update( bitmap, cliprect );
 }
 
@@ -183,7 +190,7 @@ static PALETTE_INIT( polgar )
 {
 //    palette_set_color(machine, 0, MAKE_RGB(138, 146, 148));
 	palette_set_color(machine, 0, MAKE_RGB(195, 201, 200));
-    palette_set_color(machine, 1, MAKE_RGB(40, 30, 30));
+  palette_set_color(machine, 1, MAKE_RGB(40, 30, 30));
 }
 
 
@@ -203,39 +210,31 @@ static GFXDECODE_START( polgar )
 GFXDECODE_END
 
 
-
-
 static ADDRESS_MAP_START(polgar_mem , ADDRESS_SPACE_PROGRAM, 8)
-	AM_RANGE( 0x0000, 0x1fff) AM_RAM // AM_BASE(&milano_ram)//
+	AM_RANGE( 0x0000, 0x1fff) AM_RAM
 	AM_RANGE( 0x2400, 0x2407) AM_WRITE ( mboard_write_LED_8 )		// Chessboard
 	AM_RANGE( 0x2800, 0x2800) AM_WRITE ( mboard_write_board_8)		// Chessboard
-	AM_RANGE( 0x3000, 0x3000) AM_READ( mboard_read_board_8 )		// Chessboard	
-  AM_RANGE( 0x3400, 0x3405) AM_WRITE( write_led)	// Function LEDs 3400 TRN 3401
-  AM_RANGE( 0x2c00, 0x2c07) AM_READ( read_keys)	// CL Key
-  AM_RANGE( 0x2004, 0x2004) AM_WRITE( write_lcd_i)	// LCD Instr. Reg + Beeper
-  AM_RANGE( 0x2000, 0x2000) AM_WRITE( write_lcd)	        // LCD Char Reg.
-	//AM_RANGE( 0x6000, 0x6000) AM_RAM
-	//AM_RANGE( 0x7000, 0x7000) AM_RAM
+	AM_RANGE( 0x3000, 0x3000) AM_READ( mboard_read_board_8 )		// Chessboard
+	AM_RANGE( 0x3400, 0x3405) AM_WRITE( write_led)	// Function LEDs 3400 TRN 3401
+	AM_RANGE( 0x2c00, 0x2c07) AM_READ( read_keys)	// CL Key
+	AM_RANGE( 0x2004, 0x2004) AM_WRITE( write_io)	// LCD Instr. Reg + Beeper
+	AM_RANGE( 0x2000, 0x2000) AM_WRITE( write_lcd)	        // LCD Char Reg.
  	AM_RANGE( 0x4000, 0xffff) AM_ROM
 ADDRESS_MAP_END
 
 
-/*
+
 static ADDRESS_MAP_START(milano_mem , ADDRESS_SPACE_PROGRAM, 8)
-	AM_RANGE( 0x0000, 0x1fff) AM_RAM // AM_BASE(&milano_ram)//
-//	AM_RANGE( 0x2400, 0x2407) AM_WRITE ( write_LED_8 )		// Chessboard
-//	AM_RANGE( 0x2800, 0x2800) AM_WRITE ( write_board_8)		// Chessboard
-//	AM_RANGE( 0x3000, 0x3000) AM_READ( read_board_8 )		// Chessboard	
-	  // AM_RANGE( 0x3400, 0x3405) AM_WRITE( write_leds)	// Function LEDs 3400 TRN 3401
-//	  AM_RANGE( 0x2c00, 0x2c07) AM_READ( read_keys)	// CL Key
-//  AM_RANGE( 0x2004, 0x2004) AM_WRITE( write_lcd_i)	// LCD Instr. Reg
-//  AM_RANGE( 0x2000, 0x2000) AM_WRITE( write_lcd)	        // LCD Char Reg.
-	//AM_RANGE( 0x6000, 0x6000) AM_RAM
-	//AM_RANGE( 0x7000, 0x7000) AM_RAM
-	AM_RANGE( 0x3000, 0xffff) AM_ROM
-	// AM_RANGE( 0x4000, 0xffff) AM_ROM
+	AM_RANGE( 0x0000, 0x1f9f) AM_RAM
+	AM_RANGE( 0x1fd0, 0x1fd0) AM_WRITE ( write_board)		// Chessboard
+	AM_RANGE( 0x1fe0, 0x1fe0) AM_READ( read_board )		// Chessboard
+	AM_RANGE( 0x1fe8, 0x1fed) AM_WRITE( milano_write_led )	// Function LEDs 3400 TRN 3401
+	AM_RANGE( 0x1fd8, 0x1fdf) AM_READ( read_keys)	// CL Key
+	AM_RANGE( 0x1ff0, 0x1ff0) AM_WRITE( write_io)	// IO control
+	AM_RANGE( 0x1fc0, 0x1fc0) AM_WRITE( write_lcd)	// LCD Char Reg. (latched)
+	AM_RANGE( 0x2000, 0xffff) AM_ROM
 ADDRESS_MAP_END
-*/
+
 
 static INPUT_PORTS_START( board )
 	PORT_START("LINE2")
@@ -314,7 +313,7 @@ static INPUT_PORTS_START( board )
 	PORT_START("LINE10")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD)
 
-	PORT_START("B_WHITE") 
+	PORT_START("B_WHITE")
 	PORT_BIT(0x01,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x02,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x04,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
@@ -322,7 +321,7 @@ static INPUT_PORTS_START( board )
 	PORT_BIT(0x010, IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x020, IP_ACTIVE_HIGH, IPT_KEYBOARD)
 
-	PORT_START("B_BLACK") 
+	PORT_START("B_BLACK")
 	PORT_BIT(0x01,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x02,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x04,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
@@ -330,7 +329,7 @@ static INPUT_PORTS_START( board )
 	PORT_BIT(0x010, IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x020, IP_ACTIVE_HIGH, IPT_KEYBOARD)
 
-	PORT_START("B_BUTTONS") 
+	PORT_START("B_BUTTONS")
 	PORT_BIT(0x01,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x02,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 
@@ -354,6 +353,8 @@ static INPUT_PORTS_START( polgar )
 	PORT_BIT(0x080, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("ENT") PORT_CODE(KEYCODE_F7)
 	PORT_START("KEY1_7") //Port $2c07
 	PORT_BIT(0x080, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("CL") PORT_CODE(KEYCODE_F8)
+	PORT_START("KEY1_8")
+	PORT_BIT(0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD) PORT_NAME("A") PORT_CODE(KEYCODE_1)
 INPUT_PORTS_END
 
 static TIMER_CALLBACK( update_nmi )
@@ -368,7 +369,6 @@ static TIMER_CALLBACK( update_nmi )
 
 static MACHINE_START( polgar )
 {
-	lcd_shift_counter=3;
 	// timer_pulse(machine, ATTOTIME_IN_HZ(60), NULL, 0, update_leds);
 	timer_pulse(machine, ATTOTIME_IN_HZ(600), NULL, 0, update_nmi);
 	timer_pulse(machine, ATTOTIME_IN_HZ(100), NULL, 0, mboard_update_artwork);
@@ -378,10 +378,8 @@ static MACHINE_START( polgar )
 
 static MACHINE_RESET( polgar )
 {
-	// lcd_shift_counter = 3;
-        hd44780_device * hd44780 = machine->device<hd44780_device>("hd44780");
-      	hd44780->control_write(128, 15);		/* Initialize LCD */
-      	mboard_set_boarder_pieces();
+
+	mboard_set_border_pieces();
 	mboard_set_board();
 }
 
@@ -389,8 +387,6 @@ static MACHINE_RESET( polgar )
 static MACHINE_CONFIG_START( polgar, driver_device )
 	/* basic machine hardware */
 	MDRV_CPU_ADD("maincpu",M65C02,4915200)        /* 65C02 */
-	//MDRV_CPU_ADD("maincpu",M65C02,10000000)        /* 65C02 */
-	// MDRV_CPU_ADD("maincpu",I8032,120000000)        /* 65C02 */
 	MDRV_CPU_PROGRAM_MAP(polgar_mem)
 	MDRV_QUANTUM_TIME(HZ(60))
 	MDRV_MACHINE_START( polgar )
@@ -401,8 +397,8 @@ static MACHINE_CONFIG_START( polgar, driver_device )
   MDRV_SCREEN_ADD("screen", LCD)
 	MDRV_SCREEN_REFRESH_RATE(50)
 	MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-	MDRV_SCREEN_SIZE(100, 19)
-	MDRV_SCREEN_VISIBLE_AREA(0, 100-1, 0, 19-1)
+	MDRV_SCREEN_SIZE(100, 20)
+	MDRV_SCREEN_VISIBLE_AREA(0, 100-1, 0, 20-1)
 	MDRV_PALETTE_LENGTH(2)
 	MDRV_PALETTE_INIT(polgar)
 
@@ -414,11 +410,8 @@ static MACHINE_CONFIG_START( polgar, driver_device )
     MDRV_VIDEO_UPDATE(polgar)
 
 
-
 //	MDRV_DEFAULT_LAYOUT(layout_van16)
 	 MDRV_DEFAULT_LAYOUT(layout_lcd)
-
-
 
 
 	/* sound hardware */
@@ -427,28 +420,34 @@ static MACHINE_CONFIG_START( polgar, driver_device )
 	MDRV_SOUND_ROUTE(ALL_OUTPUTS, "mono", 1.0)
 MACHINE_CONFIG_END
 
-/*
+static MACHINE_CONFIG_DERIVED( milano, polgar )
+	/* basic machine hardware */
+  // MDRV_IMPORT_FROM( polgar )
+	MDRV_CPU_MODIFY("maincpu")
+	MDRV_CPU_PROGRAM_MAP(milano_mem)
+	//beep_set_frequency(0, 4000);
+MACHINE_CONFIG_END
 
-ROM_START(milano)    
+ROM_START(milano)
 	ROM_REGION(0x10000,"maincpu",0)
-	ROM_LOAD("milano.bin", 0x0000, 0x10000, CRC(8d02e1ef) SHA1(9972c75936613bd68cfd3fe62bd222e90e8b1083))
-	ROM_REGION( 0x00800, "hd44780", ROMREGION_ERASE )
-	ROM_LOAD( "m_chargen.bin",    0x0100, 0x0700,  BAD_DUMP CRC(4c64fc7d) SHA1(123a87b2dc7efd7afcd8e27eb9bc74f54a8f1bc9))
+	ROM_LOAD("milano.bin", 0x0000, 0x10000, CRC(0e9c8fe1) SHA1(e9176f42d86fe57e382185c703c7eff7e63ca711))
+	ROM_REGION( 0x0860, "hd44780", ROMREGION_ERASE )
+	ROM_LOAD( "44780a00.bin",    0x0000, 0x0860,  BAD_DUMP CRC(3a89024c) SHA1(5a87b68422a916d1b37b5be1f7ad0b3fb3af5a8d))
 ROM_END
 
-*/
+
 
 ROM_START(polgar) //polgar
 	ROM_REGION(0x10000,"maincpu",0)
 	ROM_LOAD("polgar.bin", 0x0000, 0x10000, CRC(88d55c0f) SHA1(e86d088ec3ac68deaf90f6b3b97e3e31b1515913))
-	ROM_REGION( 0x00800, "hd44780", ROMREGION_ERASE )
-	ROM_LOAD( "m_chargen.bin",    0x0100, 0x0700,  BAD_DUMP CRC(b497af5c) SHA1(0bddebf91dac868ffbdd7f514253943635d7fe9a))
+	ROM_REGION( 0x0860, "hd44780", ROMREGION_ERASE )
+	ROM_LOAD( "44780a00.bin",    0x0000, 0x0860,  BAD_DUMP CRC(3a89024c) SHA1(5a87b68422a916d1b37b5be1f7ad0b3fb3af5a8d))
 ROM_END
 
 
 static DRIVER_INIT( polgar )
 {
-	lcd_shift_counter = 3;
+led_status=0;
 }
 
 /***************************************************************************
@@ -459,5 +458,6 @@ static DRIVER_INIT( polgar )
 
 /*    YEAR  NAME        PARENT  COMPAT  MACHINE     INPUT       INIT        COMPANY             FULLNAME                            FLAGS */
 //CONS( 1983, mephisto,   0,      0,      mephisto,   mephisto,   mephisto, "Hegener & Glaser", "Mephisto Polgar Computer",         GAME_NOT_WORKING )
-CONS( 1986, polgar,     0,      0,      polgar,     polgar,   polgar,   "Hegener & Glaser", "Mephisto Milano Schach Computer", 0 )
+CONS( 1986, polgar,     0,      0,      polgar,     polgar,   polgar,   "Hegener & Glaser", "Mephisto Polgar Schach Computer", 0 )
+CONS( 1986, milano,     polgar,      0,      milano,     polgar,   polgar,   "Hegener & Glaser", "Mephisto Milano Schach Computer", 0 )
 // second design sold (same computer/program?)
