d9d33ad793258042b09adcfc7b87ffe6db635af6
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2010-12-13 05:38:20 +0000

    [H19] Added interrupts. Added terminal bell.
    git-svn-id: svn://messdev.no-ip.org/mess@9764 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/h19.c
+++ src/mess/drivers/h19.c
@@ -9,6 +9,12 @@
 #include "emu.h"
 #include "cpu/z80/z80.h"
 #include "video/mc6845.h"
+#include "sound/beep.h"
+
+#define H19_CLOCK (XTAL_12_288MHz / 6)
+#define H19_BEEP_FRQ (H19_CLOCK / 1024)
+// next one needs to be measured
+#define H19_IRQ_PULSE (H19_BEEP_FRQ / 2)
 
 
 class h19_state : public driver_device
@@ -17,24 +23,53 @@ public:
 	h19_state(running_machine &machine, const driver_device_config_base &config)
 		: driver_device(machine, config) { }
 
-	UINT8 *video_ram;
+	UINT8 *videoram;
 	UINT8 *charrom;
+	running_device *beeper;
 };
 
 
+static TIMER_CALLBACK( h19_beepoff )
+{
+	h19_state *state = machine->driver_data<h19_state>();
+	beep_set_state(state->beeper, 0);
+}
+
+static WRITE8_HANDLER( h19_c0_w )
+{
+	h19_state *state = space->machine->driver_data<h19_state>();
+/* Beeper control - lengths need verifying
+    offset 00-1F = keyclick
+    offset 20-3F = terminal bell */
+
+	UINT16 length = 0;
+
+	if (offset & 0x20)
+		length = 200;
+	else
+		length = 4;
+
+	timer_set(space->machine, ATTOTIME_IN_MSEC(length), NULL, 0, h19_beepoff);
+	beep_set_state(state->beeper, 1);
+}
 
 static ADDRESS_MAP_START(h19_mem, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0x1fff) AM_ROM
 	AM_RANGE(0x2000, 0xf7ff) AM_RAM
-	AM_RANGE(0xf800, 0xffff) AM_RAM AM_BASE_MEMBER(h19_state, video_ram)
+	AM_RANGE(0xf800, 0xffff) AM_RAM AM_BASE_MEMBER(h19_state, videoram)
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( h19_io , ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x60, 0x60) AM_DEVWRITE("crtc", mc6845_address_w)
-	AM_RANGE(0x61, 0x61) AM_DEVREADWRITE("crtc", mc6845_register_r , mc6845_register_w)
+	AM_RANGE(0x60, 0x60) AM_MIRROR(0x1E) AM_DEVWRITE("crtc", mc6845_address_w)
+	AM_RANGE(0x61, 0x61) AM_MIRROR(0x1E) AM_DEVREADWRITE("crtc", mc6845_register_r, mc6845_register_w)
+	AM_RANGE(0xc0, 0xff) AM_WRITE(h19_c0_w)
+	//AM_RANGE(0x00, 0x3F) powerup logic
+	//AM_RANGE(0x40, 0x5F) serial control
+	//AM_RANGE(0x80, 0x9F) keyboard data
+	//AM_RANGE(0xA0, 0xBF) keyboard control
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -44,6 +79,9 @@ INPUT_PORTS_END
 
 static MACHINE_RESET(h19)
 {
+	h19_state *state = machine->driver_data<h19_state>();
+	state->beeper = machine->device("beep");
+	beep_set_frequency(state->beeper, H19_BEEP_FRQ);
 }
 
 static VIDEO_START( h19 )
@@ -70,8 +108,8 @@ static MC6845_UPDATE_ROW( h19_update_row )
 	{
 		UINT8 inv=0;
 		if (x == cursor_x) inv=0xff;
-		mem = (ma + x) & 0xfff;
-		chr = state->video_ram[mem];
+		mem = (ma + x) & 0x7ff;
+		chr = state->videoram[mem];
 
 		if (chr & 0x80)
 		{
@@ -104,7 +142,7 @@ static const mc6845_interface h19_crtc6845_interface =
 	DEVCB_NULL,
 	DEVCB_NULL,
 	DEVCB_NULL,
-	DEVCB_NULL,
+	DEVCB_CPU_INPUT_LINE("maincpu", INPUT_LINE_NMI), // frame pulse
 	NULL
 };
 
@@ -129,9 +167,10 @@ GFXDECODE_END
 
 static MACHINE_CONFIG_START( h19, h19_state )
 	/* basic machine hardware */
-	MDRV_CPU_ADD("maincpu",Z80, XTAL_12_288MHz / 6) // From schematics
+	MDRV_CPU_ADD("maincpu",Z80, H19_CLOCK) // From schematics
 	MDRV_CPU_PROGRAM_MAP(h19_mem)
 	MDRV_CPU_IO_MAP(h19_io)
+	MDRV_DEVICE_PERIODIC_INT(irq0_line_hold, H19_IRQ_PULSE)
 
 	MDRV_MACHINE_RESET(h19)
 
@@ -150,6 +189,11 @@ static MACHINE_CONFIG_START( h19, h19_state )
 
 	MDRV_VIDEO_START( h19 )
 	MDRV_VIDEO_UPDATE( h19 )
+
+	/* sound hardware */
+	MDRV_SPEAKER_STANDARD_MONO("mono")
+	MDRV_SOUND_ADD("beep", BEEP, 0)
+	MDRV_SOUND_ROUTE(ALL_OUTPUTS, "mono", 1.00)
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -174,10 +218,9 @@ ROM_START( h19 )
 	ROM_LOAD( "2716_444-37_h19keyb.bin", 0x0000, 0x0800, CRC(5c3e6972) SHA1(df49ce64ae48652346a91648c58178a34fb37d3c))
 	// Watzman keyboard
 	ROM_LOAD( "keybd.bin", 0x0000, 0x0800, CRC(58dc8217) SHA1(1b23705290bdf9fc6342065c6a528c04bff67b13))
-
 ROM_END
 
-/* Driver */
+/* Driver (year is either 1978 or 1979) */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 19??, h19,     0,       0,	h19,	h19,	 0, 	"Heath Inc", "Heathkit H-19", GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1978, h19,     0,       0,	h19,	h19,	 0, 	"Heath Inc", "Heathkit H-19", GAME_NOT_WORKING | GAME_NO_SOUND)
