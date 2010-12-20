e4ab64502c2ee44a6597d78251b1494a80cb136a
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2010-12-20 19:14:07 +0000

    [MSBC1] Fixed ROM names, added PCB layout.
    git-svn-id: svn://messdev.no-ip.org/mess@9797 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/msbc1.c
+++ src/mess/drivers/msbc1.c
@@ -1,77 +1,119 @@
-/***************************************************************************
+/*
 
-        Onmibyte MSBC-1
+Omnibyte MSBC-1
 
-        11/12/2009 Skeleton driver.
+PCB Layout
+----------
 
-****************************************************************************/
+REV B
+
+|-----------------------------------------------------------|
+|		CN1		CN2		CN3		CN4			SW1		CN5		|
+|															|
+|			3.6864MHz	3.6864MHz	ROM1					|
+|									ROM0					|
+|		SIO			SIO		SW3 SW2							|
+|								PIT							|
+|				12.5MHz					41256 41256 41256	|
+|										41256 41256 41256	|
+|	8MHz						CPU			  41245 41256	|
+|															|
+|												8419		|
+|							SW4								|
+|-|-------------CN6----------------|----|-------CN7-------|-|
+  |--------------------------------|	|-----------------|
+
+Notes:
+    Relevant IC's shown.
+
+    CPU		- Motorola MC68000R12
+	SIO		- Mostek MK68564N-5A Serial Input/Output
+	PIT		- Motorola MC68230L10 Parallel Interface/Timer
+	8419	- National Semiconductor DP8419N DRAM Controller
+	41256	- Fujitsu MB81256-12 256Kx1 RAM
+	ROM0	- AMD AM27128ADC 16Kx8 ROM "47-2818-36G1"
+	ROM1	- AMD AM27128ADC 16Kx8 ROM "47-2818-36G2"
+	SW1		- push button
+	SW2		- rotary hex DIP
+	SW3		- rotary hex DIP
+	SW4		- DIP8
+	CN1		-
+	CN2		- 
+	CN3		-
+	CN4		-
+	CN5		-
+	CN6		-
+	CN7		-
+
+*/
+
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/m68000/m68000.h"
+#include "machine/terminal.h"
+#include "includes/msbc1.h"
 
-
-class msbc1_state : public driver_device
-{
-public:
-	msbc1_state(running_machine &machine, const driver_device_config_base &config)
-		: driver_device(machine, config) { }
-
-};
-
-
-static ADDRESS_MAP_START(msbc1_mem, ADDRESS_SPACE_PROGRAM, 16)
+static ADDRESS_MAP_START( msbc1_mem, ADDRESS_SPACE_PROGRAM, 16, msbc1_state )
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x00000000, 0x00007fff) AM_ROM AM_REGION("user1",0)
-	AM_RANGE(0x00f80000, 0x00f87fff) AM_ROM AM_REGION("user1",0)
+	AM_RANGE(0x000000, 0x03ffff) AM_RAM
+	AM_RANGE(0xf80000, 0xf87fff) AM_ROM AM_REGION(MC68000R12_TAG, 0)
 ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( msbc1 )
+	PORT_INCLUDE(generic_terminal)
 INPUT_PORTS_END
 
-
-static MACHINE_RESET(msbc1)
+void msbc1_state::machine_reset()
 {
+	void *ram = cpu_get_address_space(machine->firstcpu, ADDRESS_SPACE_PROGRAM)->get_write_ptr(0);
+	UINT8 *rom = memory_region(machine, MC68000R12_TAG);
+
+	memcpy(ram, rom, 8);
+
+	machine->firstcpu->reset();
 }
 
-static VIDEO_START( msbc1 )
+static GENERIC_TERMINAL_INTERFACE( terminal_intf )
 {
-}
-
-static VIDEO_UPDATE( msbc1 )
-{
-    return 0;
-}
+	DEVCB_NULL
+};
 
 static MACHINE_CONFIG_START( msbc1, msbc1_state )
     /* basic machine hardware */
-    MDRV_CPU_ADD("maincpu",M68000, XTAL_8MHz)
+    MDRV_CPU_ADD(MC68000R12_TAG, M68000, XTAL_12_5MHz)
     MDRV_CPU_PROGRAM_MAP(msbc1_mem)
 
-    MDRV_MACHINE_RESET(msbc1)
-
     /* video hardware */
-    MDRV_SCREEN_ADD("screen", RASTER)
-    MDRV_SCREEN_REFRESH_RATE(50)
-    MDRV_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MDRV_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MDRV_SCREEN_SIZE(640, 480)
-    MDRV_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-    MDRV_PALETTE_LENGTH(2)
-    MDRV_PALETTE_INIT(black_and_white)
+	MDRV_FRAGMENT_ADD( generic_terminal )
 
-    MDRV_VIDEO_START(msbc1)
-    MDRV_VIDEO_UPDATE(msbc1)
+	// devices
+	MDRV_GENERIC_TERMINAL_ADD(TERMINAL_TAG, terminal_intf)
 MACHINE_CONFIG_END
 
 /* ROM definition */
 ROM_START( msbc1 )
-    ROM_REGION( 0x8000, "user1", ROMREGION_ERASEFF )
-	ROM_LOAD16_BYTE( "msbc1.u19", 0x0000, 0x4000, CRC(4814b9e1) SHA1(d96cf75084c6588cb33513830c6beeeffc2de853))
-	ROM_LOAD16_BYTE( "msbc1.u18", 0x0001, 0x4000, CRC(14f25d47) SHA1(964bc49c1dd9e9680c0d3d89ff3794c5044bea62))
+    ROM_REGION16_BE( 0x8000, MC68000R12_TAG, 0 )
+	ROM_LOAD16_BYTE( "47-2818-36g1.u18", 0x0000, 0x4000, CRC(14f25d47) SHA1(964bc49c1dd9e9680c0d3d89ff3794c5044bea62) )
+	ROM_LOAD16_BYTE( "47-2818-36g2.u19", 0x0001, 0x4000, CRC(4814b9e1) SHA1(d96cf75084c6588cb33513830c6beeeffc2de853) )
+
+	ROM_REGION( 0x100, "plds", 0 )
+	ROM_LOAD( "p21.1.u22", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p22.0.u37", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p23.0.u38", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p24.0.u43", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p25.0.u44", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p26.0.u45", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p27.0.u46", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p28.1.u49", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p29.0.u58", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p30.0.u60", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p31.0.u61", 0x000, 0x100, NO_DUMP )
+	ROM_LOAD( "p32.0.u76", 0x000, 0x100, NO_DUMP )
 ROM_END
 
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 19??, msbc1,  0,       0, 	msbc1,	msbc1,	 0,			 "Omnibyte",   "MSBC-1",		GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1985, msbc1,  0,       0, 	msbc1,	msbc1,	 0,			 "Omnibyte",   "MSBC-1",		GAME_NOT_WORKING | GAME_NO_SOUND)
---
+++ src/mess/includes/msbc1.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef __MSBC1__
+#define __MSBC1__
+
+#define MC68000R12_TAG	"u50"
+#define MK68564_0_TAG	"u14"
+#define MK68564_1_TAG	"u15"
+#define MC68230L10_TAG	"u16"
+#define TERMINAL_TAG	"terminal"
+
+class msbc1_state : public driver_device
+{
+public:
+	msbc1_state(running_machine &machine, const driver_device_config_base &config)
+		: driver_device(machine, config) { }
+
+	virtual void machine_reset();
+};
+
+#endif
