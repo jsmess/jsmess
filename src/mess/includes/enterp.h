68f94f4a5b0360802271309cc03a6df824ff3b93
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2010-12-20 16:06:33 +0000

    Added missing H file [AtariAce]
    git-svn-id: svn://messdev.no-ip.org/mess@9794 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/includes/enterp.h
@@ -0,0 +1,38 @@
+#ifndef __ENTERP_H__
+#define __ENTERP_H__
+
+
+/* there are 64us per line, although in reality
+   about 50 are visible. */
+#define ENTERPRISE_SCREEN_WIDTH (50*16)
+
+/* there are 312 lines per screen, although in reality
+   about 35*8 are visible */
+#define ENTERPRISE_SCREEN_HEIGHT	(35*8)
+
+
+#define NICK_PALETTE_SIZE	256
+
+
+class ep_state : public driver_device
+{
+public:
+	ep_state(running_machine &machine, const driver_device_config_base &config)
+		: driver_device(machine, config) { }
+
+	UINT8 exdos_card_value;  /* state of the wd1770 irq/drq lines */
+	UINT8 keyboard_line;     /* index of keyboard line to read */
+	struct _NICK_STATE *nick;
+};
+
+
+/*----------- defined in video/epnick.c -----------*/
+
+PALETTE_INIT( epnick );
+VIDEO_START( epnick );
+VIDEO_UPDATE( epnick );
+
+WRITE8_HANDLER( epnick_reg_w );
+
+
+#endif /* __ENTERP_H__ */
