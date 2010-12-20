0e15d9cf909d439b30650bf3c24b956b225c54f7
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2010-12-20 14:31:04 +0000

    Fix 64-bit compile error.
    git-svn-id: svn://messdev.no-ip.org/mess@9793 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/amigakbd.c
+++ src/mess/machine/amigakbd.c
@@ -57,7 +57,7 @@ static TIMER_CALLBACK( kbd_update_callback )
 
 static INPUT_CHANGED( kbd_update )
 {
-	int	index = (int)param, i;
+	int	index = (int)(FPTR)param, i;
 	UINT32	oldvalue = oldval * field->mask, newvalue = newval * field->mask;
 	UINT32	delta = oldvalue ^ newvalue;
 
