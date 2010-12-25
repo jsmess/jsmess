cc9162bf2649d82070c856f53155e1a47f396bc5
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2010-12-25 16:01:46 +0000

    [OSBEXEC] Adding a bit of meat to the skeleton.
    git-svn-id: svn://messdev.no-ip.org/mess@9818 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/osbexec.c
+++ src/mess/drivers/osbexec.c
@@ -2,6 +2,32 @@
 
     Osborne Executive driver file
 
+Screen is 80x24
+
+On boot at least bios seems to be enabled.
+Boot sequence:
+- set stack to and initialize Fxxx
+- write stuff to/initialize 2000-27FF
+- set PA to 80
+- initialize some more i/o
+- set PA to C0
+- test C000-CFFF
+- Set PA to 80
+- i/o things
+- Set PA to C0
+- Clear VRAM with 0x20
+- Write "PERFORMING SELF-TEST" to VRAM
+- Set PA to 80
+- (bp 0307)
+- Test 2000-27FF
+- Set SP to 27FF
+- Set PA to 80
+- Test 4000-FFFF
+- Set PA to 81
+- Test 4000-EFFF
+- (bp 2c6)
+- Test i/o
+
 
 ***************************************************************************/
 
@@ -15,21 +41,75 @@
 #include "machine/6821pia.h"
 #include "machine/pit8253.h"
 #include "devices/messram.h"
-#include "includes/osbexec.h"
 
 
 #define MAIN_CLOCK	23961600
 
 
+class osbexec_state : public driver_device
+{
+public:
+	osbexec_state(running_machine &machine, const driver_device_config_base &config)
+		: driver_device(machine, config) { }
+
+	region_info	*fontram_region;
+	region_info *vram_region;
+	UINT8	*fontram;
+	UINT8	*vram;
+	emu_timer *video_timer;
+
+	/* PIA 0 (UD12) */
+	UINT8	pia0_porta;
+
+
+	void set_banks(running_machine *machine)
+	{
+		UINT8 *messram_ptr = messram_get_ptr( machine->device("messram") );
+
+		if ( pia0_porta & 0x01 )
+			messram_ptr += 0x10000;
+
+		memory_set_bankptr( machine, "4000", messram_ptr + 0x4000 );
+		memory_set_bankptr( machine, "c000", messram_ptr + 0xc000 );
+		memory_set_bankptr( machine, "e000", messram_ptr + 0xe000 );
+
+		if ( pia0_porta & 0x80 )
+			memory_set_bankptr( machine, "0000", memory_region(machine, "maincpu") );
+
+		if ( pia0_porta & 0x40 )
+			memory_set_bankptr(machine, "c000", vram_region->base() );
+	}
+
+};
+
+
+static WRITE8_HANDLER( osbexec_0000_w )
+{
+	osbexec_state *state = space->machine->driver_data<osbexec_state>();
+
+	/* Font RAM writing is enabled when ROM bank is enabled */
+	if ( state->pia0_porta & 0x80 )
+	{
+		if ( offset < 0x1000 )
+			state->fontram[ offset ] = data;
+	}
+}
+
+
 static ADDRESS_MAP_START( osbexec_mem, ADDRESS_SPACE_PROGRAM, 8 )
-	AM_RANGE( 0x0000, 0x1FFF ) AM_ROM
+	AM_RANGE( 0x0000, 0x1FFF ) AM_READ_BANK("0000") AM_WRITE( osbexec_0000_w )	/* ROM and maybe also banked ram */
+	AM_RANGE( 0x2000, 0x3FFF ) AM_RAM											/* Banked? RAM */
+	AM_RANGE( 0x4000, 0xBFFF ) AM_RAMBANK("4000")								/* Banked ram */
+	AM_RANGE( 0xC000, 0xDFFF ) AM_RAMBANK("c000")								/* Video ram / Banked RAM */
+	AM_RANGE( 0xE000, 0xEFFF ) AM_RAMBANK("e000")								/* Banked ram */
+	AM_RANGE( 0xF000, 0xFFFF ) AM_RAM											/* 4KB of non-banked RAM for system stack etc */
 ADDRESS_MAP_END
 
 
 static ADDRESS_MAP_START( osbexec_io, ADDRESS_SPACE_IO, 8 )
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	/* 0x00 - 0x01/0x03 - 6821 PIA @UD12 */
+	AM_RANGE( 0x00, 0x03 ) AM_DEVREADWRITE( "pia_0", pia6821_r, pia6821_w )		/* 6821 PIA UD12 */
 	/* 0x04 - 0x07 - 8253 @UD1 */
 	/* 0x08 - 0x0B - MB8877 @UB17 */
 	/* 0x0c - 0x0f - SIO @UD4 */
@@ -134,6 +214,74 @@ static PALETTE_INIT( osbexec )
 
 
 /*
+  UD12 - 6821 PIA
+
+  Port A:
+  PA7 - ROM BANK ENA
+  PA6 - VRAM BANK ENA
+  PA5 - BANK6ENA
+  PA4 - BANK5ENA
+  PA3 - BANK4ENA
+  PA2 - BANK3ENA
+  PA1 - BANK2ENA
+  PA0 - BANK1ENA
+*/
+
+static READ8_DEVICE_HANDLER( osbexec_pia0_a_r )
+{
+	osbexec_state *state = device->machine->driver_data<osbexec_state>();
+
+	return state->pia0_porta;
+}
+
+
+static WRITE8_DEVICE_HANDLER( osbexec_pia0_a_w )
+{
+	osbexec_state *state = device->machine->driver_data<osbexec_state>();
+
+	logerror("osbexec_pia0_a_w: %02x\n", data );
+
+	state->pia0_porta = data;
+
+	state->set_banks( device->machine );
+}
+
+
+static const pia6821_interface osbexec_pia0_config =
+{
+	DEVCB_HANDLER( osbexec_pia0_a_r ),	/* in_a_func */			/* port A - banking */
+	DEVCB_NULL,							/* in_b_func */			/* modem / speaker */
+	DEVCB_NULL,							/* in_ca1_func */		/* DMA IRQ */
+	DEVCB_NULL,							/* in_cb1_func */		/* Vblank (rtc irq) */
+	DEVCB_NULL,							/* in_ca2_func */
+	DEVCB_NULL,							/* in_cb2_func */
+	DEVCB_HANDLER( osbexec_pia0_a_w ),	/* out_a_func */		/* port A - banking */
+	DEVCB_NULL,							/* out_b_func */		/* modem / speaker */
+	DEVCB_NULL,							/* out_ca2_func */		/* Keyboard strobe */
+	DEVCB_NULL,							/* out_cb2_func */		/* 60/50 */
+	DEVCB_NULL,							/* irq_a_func */		/* IRQ */
+	DEVCB_NULL							/* irq_b_func */		/* IRQ */
+};
+
+
+static const pia6821_interface osbexec_pia1_config =
+{
+	DEVCB_NULL,							/* in_a_func */
+	DEVCB_NULL,							/* in_b_func */
+	DEVCB_NULL,							/* in_ca1_func */
+	DEVCB_NULL,							/* in_cb1_func */
+	DEVCB_NULL,							/* in_ca2_func */
+	DEVCB_NULL,							/* in_cb2_func */
+	DEVCB_NULL,							/* out_a_func */
+	DEVCB_NULL,							/* out_b_func */
+	DEVCB_NULL,							/* out_ca2_func */
+	DEVCB_NULL,							/* out_cb2_func */
+	DEVCB_NULL,							/* irq_a_func */
+	DEVCB_NULL							/* irq_b_func */
+};
+
+
+/*
  * The Osborne Executive supports the following disc formats: (TODO: Verify)
  * - Osborne single density: 40 tracks, 10 sectors per track, 256-byte sectors (100 KByte)
  * - Osborne double density: 40 tracks, 5 sectors per track, 1024-byte sectors (200 KByte)
@@ -189,13 +337,78 @@ static const floppy_config osbexec_floppy_config =
 };
 
 
+static TIMER_CALLBACK( osbexec_video_callback )
+{
+	osbexec_state *state = machine->driver_data<osbexec_state>();
+	running_device *pia_0 = machine->device("pia_0");
+	int y = machine->primary_screen->vpos();
+
+	/* Start of frame */
+	if ( y == 0 )
+	{
+		/* Clear CB1 on PIA @ UD12 */
+		pia6821_cb1_w( pia_0, 0 );
+	}
+	else if ( y == 240 )
+	{
+		/* Set CB1 on PIA @ UD12 */
+		pia6821_cb1_w( pia_0, 1 );
+	}
+	if ( y < 240 )
+	{
+		UINT16 row_addr = ( y / 10 ) * 128;
+		UINT16 *p = BITMAP_ADDR16( machine->generic.tmpbitmap, y, 0 );
+		UINT8 char_line = y % 10;
+
+		for ( int x = 0; x < 64; x++ )
+		{
+			UINT8 ch = state->vram[ row_addr + x ];
+			UINT8 attr = state->vram[ 0x1000 + row_addr + x ];
+			UINT8 fg_col = ( attr & 0x80 ) ? 1 : 2;
+			UINT8 font_bits = state->fontram[ ( ( attr & 0x10 ) ? 0x800 : 0 ) + ch * 16 + char_line ];
+
+			/* Check for underline */
+			if ( ( attr & 0x40 ) && char_line == 9 )
+				font_bits = 0xFF;
+
+			for ( int b = 0; b < 8; b++ )
+			{
+				p[ x * 8 + b ] = ( font_bits & 0x80 ) ? fg_col : 0;
+				font_bits <<= 1;
+			}
+		}
+	}
+
+	timer_adjust_oneshot( state->video_timer, machine->primary_screen->time_until_pos( y + 1, 0 ), 0 );
+}
+
+
 static DRIVER_INIT( osbexec )
 {
+	osbexec_state *state = machine->driver_data<osbexec_state>();
+
+	state->fontram_region = machine->region_alloc( "fontram", 0x1000, 0 );
+	state->vram_region = machine->region_alloc( "vram", 0x2000, 0 );
+	state->vram = state->vram_region->base();
+	state->fontram = state->fontram_region->base();
+
+
+	memset( state->fontram, 0x00, 0x1000 );
+	memset( state->vram, 0x00, 0x2000 );
+
+	state->video_timer = timer_alloc( machine, osbexec_video_callback, NULL );
 }
 
 
 static MACHINE_RESET( osbexec )
 {
+	osbexec_state *state = machine->driver_data<osbexec_state>();
+
+	state->pia0_porta = 0xC0;		/* Enable ROM and VRAM on reset */
+
+	state->set_banks( machine );
+
+	timer_adjust_oneshot( state->video_timer, machine->primary_screen->time_until_pos( 0, 0 ), 0 );
 }
 
 
@@ -208,7 +421,7 @@ static MACHINE_CONFIG_START( osbexec, osbexec_state )
 
 	MDRV_SCREEN_ADD("screen", RASTER)
 	MDRV_SCREEN_FORMAT( BITMAP_FORMAT_INDEXED16 )
-	MDRV_SCREEN_RAW_PARAMS( MAIN_CLOCK/4, 512, 0, 416, 260, 0, 240 )	/* Most likely not correct */
+	MDRV_SCREEN_RAW_PARAMS( MAIN_CLOCK/2, 640, 0, 512, 260, 0, 240 )	/* May not be correct */
 	MDRV_VIDEO_START( generic_bitmapped )
 	MDRV_VIDEO_UPDATE( generic_bitmapped )
 	MDRV_PALETTE_LENGTH( 3 )
@@ -220,8 +433,8 @@ static MACHINE_CONFIG_START( osbexec, osbexec_state )
 
 //	MDRV_PIT8253_ADD( "pit", osbexec_pit_config )
 
-//	MDRV_PIA6821_ADD( "pia_0", osbexec_pia0_config )
-//	MDRV_PIA6821_ADD( "pia_1", osbexec_pia1_config )
+	MDRV_PIA6821_ADD( "pia_0", osbexec_pia0_config )
+	MDRV_PIA6821_ADD( "pia_1", osbexec_pia1_config )
 
 //	MDRV_Z80SIO0_ADD( "sio", MAIN_CLOCK/6, osbexec_sio_config )
 
@@ -231,7 +444,7 @@ static MACHINE_CONFIG_START( osbexec, osbexec_state )
 
 	/* internal ram */
 	MDRV_RAM_ADD("messram")
-	MDRV_RAM_DEFAULT_SIZE("68K")	/* 64KB Main RAM and 4Kbit video attribute RAM */
+	MDRV_RAM_DEFAULT_SIZE("128K")	/* 128KB Main RAM */
 MACHINE_CONFIG_END
 
 
