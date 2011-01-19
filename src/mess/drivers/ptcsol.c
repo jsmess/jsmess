2725eb64c6f6c7395cc91a135c5753dbfbf52ce1
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-01-19 13:36:51 +0000

    [SOL20] Cassette works now. Format is identical to the sorcerer, and tapes are interchangeable.
    git-svn-id: svn://messdev.no-ip.org/mess@10105 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/ptcsol.c
+++ src/mess/drivers/ptcsol.c
@@ -32,6 +32,18 @@
 #include "imagedev/cassette.h"
 #include "machine/ay31015.h"
 
+typedef struct {
+	struct {
+		int length;		/* time cassette level is at input.level */
+		int level;		/* cassette level */
+		int bit;		/* bit being read */
+	} input;
+	struct {
+		int length;		/* time cassette level is at output.level */
+		int level;		/* cassette level */
+		int bit;		/* bit to to output */
+	} output;
+} cass_data_t;
 
 class sol20_state : public driver_device
 {
@@ -39,21 +51,24 @@ public:
 	sol20_state(running_machine &machine, const driver_device_config_base &config)
 		: driver_device(machine, config),
 	m_maincpu(*this, "maincpu"),
-	m_cass_1(*this, "cassette1"),
-	m_cass_2(*this, "cassette2"),
+	m_cass1(*this, "cassette1"),
+	m_cass2(*this, "cassette2"),
 	m_uart_c(*this, "uart_c"),
 	m_uart_s(*this, "uart_s"),
 	m_terminal(*this, TERMINAL_TAG)
 	{ }
 
 	required_device<cpu_device> m_maincpu;
-	required_device<device_t> m_cass_1;
-	required_device<device_t> m_cass_2;
+	required_device<device_t> m_cass1;
+	required_device<device_t> m_cass2;
 	required_device<device_t> m_uart_c;
 	required_device<device_t> m_uart_s;
 	required_device<device_t> m_terminal;
 	DECLARE_READ8_MEMBER( sol20_fa_r );
+	DECLARE_READ8_MEMBER( sol20_fb_r );
 	DECLARE_READ8_MEMBER( sol20_fc_r );
+	DECLARE_WRITE8_MEMBER( sol20_fa_w );
+	DECLARE_WRITE8_MEMBER( sol20_fb_w );
 	DECLARE_WRITE8_MEMBER( sol20_fe_w );
 	DECLARE_WRITE8_MEMBER( sol20_kbd_put );
 	UINT8 m_sol20_fa;
@@ -62,19 +77,170 @@ public:
 	const UINT8 *FNT;
 	const UINT8 *m_videoram;
 	UINT8 m_framecnt;
+	emu_timer *m_cassette_timer;
+	cass_data_t m_cass_data;
 };
 
+
+/* timer to read cassette waveforms */
+
+
+static device_t *cassette_device_image(running_machine *machine)
+{
+	sol20_state *state = machine->driver_data<sol20_state>();
+	if (state->m_sol20_fa & 0x40)
+		return machine->device("cassette2");
+	else
+		return machine->device("cassette1");
+}
+
+
+// identical to sorcerer
+static TIMER_CALLBACK(sol20_cassette_tc)
+{
+	sol20_state *state = machine->driver_data<sol20_state>();
+	UINT8 cass_ws = 0;
+	switch (state->m_sol20_fa & 0x20)
+	{
+		case 0x20:				/* Cassette 300 baud */
+
+			/* loading a tape - this is basically the same as the super80.
+                           We convert the 1200/2400 Hz signal to a 0 or 1, and send it to the uart. */
+
+			state->m_cass_data.input.length++;
+
+			cass_ws = (cassette_input(cassette_device_image(machine)) > +0.02) ? 1 : 0;
+
+			if (cass_ws != state->m_cass_data.input.level)
+			{
+				state->m_cass_data.input.level = cass_ws;
+				state->m_cass_data.input.bit = ((state->m_cass_data.input.length < 0x6) || (state->m_cass_data.input.length > 0x20)) ? 1 : 0;
+				state->m_cass_data.input.length = 0;
+				ay31015_set_input_pin( state->m_uart_c, AY31015_SI, state->m_cass_data.input.bit );
+			}
+
+			/* saving a tape - convert the serial stream from the uart, into 1200 and 2400 Hz frequencies.
+                           Synchronisation of the frequency pulses to the uart is extremely important. */
+
+			state->m_cass_data.output.length++;
+			if (!(state->m_cass_data.output.length & 0x1f))
+			{
+				cass_ws = ay31015_get_output_pin( state->m_uart_c, AY31015_SO );
+				if (cass_ws != state->m_cass_data.output.bit)
+				{
+					state->m_cass_data.output.bit = cass_ws;
+					state->m_cass_data.output.length = 0;
+				}
+			}
+
+			if (!(state->m_cass_data.output.length & 3))
+			{
+				if (!((state->m_cass_data.output.bit == 0) && (state->m_cass_data.output.length & 4)))
+				{
+					state->m_cass_data.output.level ^= 1;			// toggle output state, except on 2nd half of low bit
+					cassette_output(cassette_device_image(machine), state->m_cass_data.output.level ? -1.0 : +1.0);
+				}
+			}
+			return;
+
+		case 0x00:			/* Cassette 1200 baud */
+			/* loading a tape */
+			state->m_cass_data.input.length++;
+
+			cass_ws = (cassette_input(cassette_device_image(machine)) > +0.02) ? 1 : 0;
+
+			if (cass_ws != state->m_cass_data.input.level || state->m_cass_data.input.length == 10)
+			{
+				state->m_cass_data.input.bit = ((state->m_cass_data.input.length < 10) || (state->m_cass_data.input.length > 0x20)) ? 1 : 0;
+				if ( cass_ws != state->m_cass_data.input.level )
+				{
+					state->m_cass_data.input.length = 0;
+					state->m_cass_data.input.level = cass_ws;
+				}
+				ay31015_set_input_pin( state->m_uart_c, AY31015_SI, state->m_cass_data.input.bit );
+			}
+
+			/* saving a tape - convert the serial stream from the uart, into 600 and 1200 Hz frequencies. */
+
+			state->m_cass_data.output.length++;
+			if (!(state->m_cass_data.output.length & 7))
+			{
+				cass_ws = ay31015_get_output_pin( state->m_uart_c, AY31015_SO );
+				if (cass_ws != state->m_cass_data.output.bit)
+				{
+					state->m_cass_data.output.bit = cass_ws;
+					state->m_cass_data.output.length = 0;
+				}
+			}
+
+			if (!(state->m_cass_data.output.length & 7))
+			{
+				if (!((state->m_cass_data.output.bit == 0) && (state->m_cass_data.output.length & 8)))
+				{
+					state->m_cass_data.output.level ^= 1;			// toggle output state, except on 2nd half of low bit
+					cassette_output(cassette_device_image(machine), state->m_cass_data.output.level ? -1.0 : +1.0);
+				}
+			}
+			return;
+	}
+}
 
 
 READ8_MEMBER( sol20_state::sol20_fa_r )
 {
-	return m_sol20_fa;
+	/* set unused bits high */
+	UINT8 data = 0x26;
+
+	ay31015_set_input_pin( m_uart_c, AY31015_SWE, 0 );
+	data |= ay31015_get_output_pin( m_uart_c, AY31015_TBMT ) ? 0x80 : 0;
+	data |= ay31015_get_output_pin( m_uart_c, AY31015_DAV  ) ? 0x40 : 0;
+	data |= ay31015_get_output_pin( m_uart_c, AY31015_OR   ) ? 0x10 : 0;
+	data |= ay31015_get_output_pin( m_uart_c, AY31015_FE   ) ? 0x08 : 0;
+	ay31015_set_input_pin( m_uart_c, AY31015_SWE, 1 );
+
+	return data | (m_sol20_fa & 1);
+}
+
+READ8_MEMBER( sol20_state::sol20_fb_r)
+{
+	UINT8 data = ay31015_get_received_data( m_uart_c );
+	ay31015_set_input_pin( m_uart_c, AY31015_RDAV, 0 );
+	ay31015_set_input_pin( m_uart_c, AY31015_RDAV, 1 );
+	return data;
 }
 
 READ8_MEMBER( sol20_state::sol20_fc_r )
 {
-	m_sol20_fa = 0xff;
+	m_sol20_fa |= 1;
 	return m_sol20_fc;
+}
+
+WRITE8_MEMBER( sol20_state::sol20_fa_w )
+{
+	m_sol20_fa &= 1;
+	m_sol20_fa |= (data & 0xf0);
+
+	/* cassette 1 motor */
+	cassette_change_state(m_cass1,
+		(BIT(data, 7)) ? CASSETTE_MOTOR_ENABLED : CASSETTE_MOTOR_DISABLED, CASSETTE_MASK_MOTOR);
+
+	/* cassette 2 motor */
+	cassette_change_state(m_cass2,
+		(BIT(data, 6)) ? CASSETTE_MOTOR_ENABLED : CASSETTE_MOTOR_DISABLED, CASSETTE_MASK_MOTOR);
+
+	if (data & 0xc0)
+		timer_adjust_periodic(m_cassette_timer, attotime_zero, 0, ATTOTIME_IN_HZ(19200));
+	else
+		timer_adjust_oneshot(m_cassette_timer, attotime_zero, 0);
+
+	// bit 5 baud rate */
+	ay31015_set_receiver_clock( m_uart_c, (BIT(data, 5)) ? 4800.0 : 19200.0);
+	ay31015_set_transmitter_clock( m_uart_c, (BIT(data, 5)) ? 4800.0 : 19200.0);
+}
+
+WRITE8_MEMBER( sol20_state::sol20_fb_w )
+{
+	ay31015_set_transmit_data( m_uart_c, data );
 }
 
 WRITE8_MEMBER( sol20_state::sol20_fe_w )
@@ -94,7 +260,8 @@ ADDRESS_MAP_END
 static ADDRESS_MAP_START( sol20_io , ADDRESS_SPACE_IO, 8, sol20_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0xfa, 0xfa) AM_READ(sol20_fa_r)
+	AM_RANGE(0xfa, 0xfa) AM_READWRITE(sol20_fa_r,sol20_fa_w)
+	AM_RANGE(0xfb, 0xfb) AM_READWRITE(sol20_fb_r,sol20_fb_w)
 	AM_RANGE(0xfc, 0xfc) AM_READ(sol20_fc_r)
 	AM_RANGE(0xfe, 0xfe) AM_WRITE(sol20_fe_w)
 /*  AM_RANGE(0xf8, 0xf8) serial status in (bit 6=data av, bit 7=tmbe)
@@ -137,13 +304,28 @@ static TIMER_CALLBACK( sol20_boot )
 	memory_set_bank(machine, "boot", 0);
 }
 
+static MACHINE_START( sol20 )
+{
+	sol20_state *state = machine->driver_data<sol20_state>();
+	state->m_cassette_timer = timer_alloc(machine, sol20_cassette_tc, NULL);
+}
+
 static MACHINE_RESET( sol20 )
 {
 	sol20_state *state = machine->driver_data<sol20_state>();
-	state->m_sol20_fa=0xff;
 	state->m_sol20_fe=0;
+	state->m_sol20_fa=1;
+	// set hard-wired uart pins
+	ay31015_set_input_pin( state->m_uart_c, AY31015_CS, 0 );
+	ay31015_set_input_pin( state->m_uart_c, AY31015_NB1, 1);
+	ay31015_set_input_pin( state->m_uart_c, AY31015_NB2, 1);
+	ay31015_set_input_pin( state->m_uart_c, AY31015_TSB, 1);
+	ay31015_set_input_pin( state->m_uart_c, AY31015_EPS, 1);
+	ay31015_set_input_pin( state->m_uart_c, AY31015_NP,  1);
+	ay31015_set_input_pin( state->m_uart_c, AY31015_CS, 1 );
+	// boot-bank
 	memory_set_bank(machine, "boot", 1);
-	timer_set(machine, ATTOTIME_IN_USEC(10), NULL, 0, sol20_boot);
+	timer_set(machine, ATTOTIME_IN_USEC(9), NULL, 0, sol20_boot);
 }
 
 static DRIVER_INIT( sol20 )
@@ -228,6 +410,7 @@ static MACHINE_CONFIG_START( sol20, sol20_state )
 	MCFG_CPU_PROGRAM_MAP(sol20_mem)
 	MCFG_CPU_IO_MAP(sol20_io)
 
+	MCFG_MACHINE_START(sol20)
 	MCFG_MACHINE_RESET(sol20)
 
 	/* video hardware */
