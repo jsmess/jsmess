df51c727ee51ec29bda2b25fbf6d258767af45c6
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2011-02-19 18:53:04 +0000

    [uPD7220]: Added RDAT command, A5105 now interprets the BASIC commands (scroll down still doesn't work because it wants some FIFO delaying)
    git-svn-id: svn://messdev.no-ip.org/mess@10510 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/upd7220.c
+++ src/mess/video/upd7220.c
@@ -16,11 +16,11 @@
         - character
         - mixed
     - commands
-        - WDAT
+    	- WDAT, modifiers are untested
         - FIGS
         - FIGD
         - GCHRD
-        - RDAT
+        - RDAT, needs a delayed FIFO (seen in A5105 when it scrolls up)
         - DMAR
         - DMAW
     - read-modify-write cycle
@@ -480,49 +480,6 @@ static void reset_figs_param(upd7220_t *upd7220)
 }
 
 /*-------------------------------------------------
-    write_vram - write in the device memory space
--------------------------------------------------*/
-
-static void write_vram(upd7220_t *upd7220,UINT8 type, UINT8 mod)
-{
-	int i;
-	if(type == 1)
-	{
-		logerror("uPD7220 invalid type 1 WDAT parameter\n");
-		return;
-	}
-
-	if(mod)
-	{
-		printf("uPD7220 unimplemented WDAT modifier %02x\n",mod);
-		return;
-	}
-
-	for(i=0;i<upd7220->figs_dc + 1;i++)
-	{
-		switch(mod & 3)
-		{
-			case 0x00: //replace
-				switch(type)
-				{
-					case 0: upd7220->vram[upd7220->ead+i] = ((upd7220->pr[1]) | (upd7220->pr[2] << 8)) & upd7220->mask; break;
-					case 2:	upd7220->vram[upd7220->ead+i] = (((upd7220->pr[1] & upd7220->mask) & 0xff) | (upd7220->vram[upd7220->ead] & 0xff00)); break;
-					case 3: upd7220->vram[upd7220->ead+i] = (((upd7220->pr[1] & upd7220->mask) << 8) | (upd7220->vram[upd7220->ead] & 0x00ff)); break;
-				}
-				break;
-			case 0x01: //complement
-				break;
-			case 0x02: //reset to zero
-				break;
-			case 0x03: //set to one
-				break;
-		}
-	}
-
-	reset_figs_param(upd7220);
-}
-
-/*-------------------------------------------------
     advance_ead - advance EAD pointer
 -------------------------------------------------*/
 
@@ -582,6 +539,101 @@ static void advance_ead(upd7220_t *upd7220)
 	}
 
 	EAD &= 0x3ffff;
+}
+
+/*-------------------------------------------------
+    write_vram - read in the device memory space
+-------------------------------------------------*/
+
+static void read_vram(upd7220_t *upd7220,UINT8 type, UINT8 mod)
+{
+	int i;
+
+	if(type == 1)
+	{
+		logerror("uPD7220 invalid type 1 RDAT parameter\n");
+		return;
+	}
+
+	if(mod)
+		logerror("uPD7220 RDAT used with mod = %02x?\n",mod);
+
+	for(i=0;i<upd7220->figs_dc;i++)
+	{
+		switch(type)
+		{
+			case 0:
+				queue(upd7220, upd7220->vram[upd7220->ead] & 0xff, 0);
+				queue(upd7220, upd7220->vram[upd7220->ead] >> 8, 0);
+				break;
+			case 2:
+				queue(upd7220, upd7220->vram[upd7220->ead] & 0xff, 0);
+				break;
+			case 3:
+				queue(upd7220, upd7220->vram[upd7220->ead] >> 8, 0);
+				break;
+		}
+
+		advance_ead(upd7220);
+	}
+}
+
+/*-------------------------------------------------
+    write_vram - write in the device memory space
+-------------------------------------------------*/
+
+static void write_vram(upd7220_t *upd7220,UINT8 type, UINT8 mod)
+{
+	int i;
+	if(type == 1)
+	{
+		logerror("uPD7220 invalid type 1 WDAT parameter\n");
+		return;
+	}
+
+	if(mod)
+		printf("uPD7220 check WDAT modifier %02x\n",mod);
+
+	for(i=0;i<upd7220->figs_dc + 1;i++)
+	{
+		switch(mod & 3)
+		{
+			case 0x00: //replace
+				switch(type)
+				{
+					case 0: upd7220->vram[upd7220->ead] = ((upd7220->pr[1]) | (upd7220->pr[2] << 8)) & upd7220->mask; break;
+					case 2:	upd7220->vram[upd7220->ead] = (((upd7220->pr[1] & upd7220->mask) & 0xff) | (upd7220->vram[upd7220->ead] & 0xff00)); break;
+					case 3: upd7220->vram[upd7220->ead] = (((upd7220->pr[1] & upd7220->mask) << 8) | (upd7220->vram[upd7220->ead] & 0x00ff)); break;
+				}
+				break;
+			case 0x01: //complement
+				switch(type)
+				{
+					case 0: upd7220->vram[upd7220->ead] ^= ((upd7220->pr[1]) | (upd7220->pr[2] << 8)) & upd7220->mask; break;
+					case 2:	upd7220->vram[upd7220->ead] ^= (((upd7220->pr[1] & upd7220->mask) & 0xff); break;
+					case 3: upd7220->vram[upd7220->ead] ^= (((upd7220->pr[1] & upd7220->mask) << 8); break;
+				}
+				break;
+			case 0x02: //reset to zero
+				switch(type)
+				{
+					case 0: upd7220->vram[upd7220->ead] &= ~(((upd7220->pr[1]) | (upd7220->pr[2] << 8)) & upd7220->mask); break;
+					case 2:	upd7220->vram[upd7220->ead] &= ~(((upd7220->pr[1] & upd7220->mask) & 0xff); break;
+					case 3: upd7220->vram[upd7220->ead] &= ~(((upd7220->pr[1] & upd7220->mask) << 8); break;
+				}
+				break;
+			case 0x03: //set to one
+				switch(type)
+				{
+					case 0: upd7220->vram[upd7220->ead] |= ((upd7220->pr[1]) | (upd7220->pr[2] << 8)) & upd7220->mask; break;
+					case 2:	upd7220->vram[upd7220->ead] |= (((upd7220->pr[1] & upd7220->mask) & 0xff); break;
+					case 3: upd7220->vram[upd7220->ead] |= (((upd7220->pr[1] & upd7220->mask) << 8); break;
+				}
+				break;
+		}
+
+		advance_ead(upd7220);
+	}
 }
 
 /*-------------------------------------------------
@@ -844,7 +896,7 @@ static void process_fifo(device_t *device)
 			//printf("%02x = %02x %02x (%c) %04x\n",upd7220->cr,upd7220->pr[2],upd7220->pr[1],upd7220->pr[1],EAD);
 
 			write_vram(upd7220,(upd7220->cr & 0x18) >> 3,upd7220->cr & 3);
-			advance_ead(upd7220);
+			reset_figs_param(upd7220);
 		}
 		break;
 
@@ -895,7 +947,12 @@ static void process_fifo(device_t *device)
 		break;
 
 	case COMMAND_RDAT: /* read data from display memory */
-		logerror("uPD7220 '%s' Unimplemented command RDAT\n", device->tag());
+		fifo_set_direction(upd7220, FIFO_READ);
+
+		read_vram(upd7220,(upd7220->cr & 0x18) >> 3,upd7220->cr & 3);
+		reset_figs_param(upd7220);
+
+		upd7220->sr |= UPD7220_SR_DATA_READY;
 		break;
 
 	case COMMAND_CURD: /* cursor address read */
