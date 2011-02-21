c7e024b95e8924af17abffbac56b57e218b254b2
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-02-21 01:58:44 +0000

    [PHUNSY] Modernised. Added F4 display. Added speaker.
    git-svn-id: svn://messdev.no-ip.org/mess@10529 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/phunsy.c
+++ src/mess/drivers/phunsy.c
@@ -5,10 +5,12 @@
         04/11/2010 Skeleton driver.
 
 ****************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/s2650/s2650.h"
 #include "machine/terminal.h"
+#include "sound/speaker.h"
 
 
 #define LOG	1
@@ -18,9 +20,22 @@ class phunsy_state : public driver_device
 {
 public:
 	phunsy_state(running_machine &machine, const driver_device_config_base &config)
-		: driver_device(machine, config) { }
+		: driver_device(machine, config),
+	m_maincpu(*this, "maincpu"),
+	m_terminal(*this, TERMINAL_TAG),
+	m_speaker(*this, "speaker")
+	{ }
 
-	UINT8		*video_ram;
+	required_device<cpu_device> m_maincpu;
+	required_device<device_t> m_terminal;
+	required_device<device_t> m_speaker;
+	DECLARE_READ8_MEMBER( phunsy_data_r );
+	DECLARE_READ8_MEMBER( phunsy_sense_r );
+	DECLARE_WRITE8_MEMBER( phunsy_1800_w );
+	DECLARE_WRITE8_MEMBER( phunsy_ctrl_w );
+	DECLARE_WRITE8_MEMBER( phunsy_data_w );
+	DECLARE_WRITE8_MEMBER( phunsy_kbd_put );
+	UINT8		*videoram;
 	UINT8		data_out;
 	UINT8		keyboard_input;
 	UINT8		q_bank;
@@ -29,74 +44,66 @@ public:
 };
 
 
-static WRITE8_HANDLER( phunsy_1800_w )
+WRITE8_MEMBER( phunsy_state::phunsy_1800_w )
 {
-	phunsy_state *state = space->machine->driver_data<phunsy_state>();
-
-	if ( state->u_bank == 0 )
-		state->ram_1800[offset] = data;
+	if ( u_bank == 0 )
+		ram_1800[offset] = data;
 }
 
 
-static ADDRESS_MAP_START(phunsy_mem, ADDRESS_SPACE_PROGRAM, 8)
+static ADDRESS_MAP_START(phunsy_mem, ADDRESS_SPACE_PROGRAM, 8, phunsy_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x0000, 0x07ff) AM_ROM
 	AM_RANGE( 0x0800, 0x0fff) AM_RAM
-	AM_RANGE( 0x1000, 0x17ff) AM_RAM	AM_BASE_MEMBER( phunsy_state, video_ram ) // Video RAM
+	AM_RANGE( 0x1000, 0x17ff) AM_RAM AM_BASE( videoram ) // Video RAM
 	AM_RANGE( 0x1800, 0x1fff) AM_RAM_WRITE( phunsy_1800_w ) AM_ROMBANK("bank1")	// Banked RAM/ROM
 	AM_RANGE( 0x4000, 0xffff) AM_RAMBANK("bank2") // Banked RAM
 ADDRESS_MAP_END
 
 
-static WRITE8_HANDLER( phunsy_ctrl_w )
+WRITE8_MEMBER( phunsy_state::phunsy_ctrl_w )
 {
-	phunsy_state *state = space->machine->driver_data<phunsy_state>();
-
 	if (LOG)
-		logerror("%s: phunsy_ctrl_w %02x\n", space->machine->describe_context(), data);
+		logerror("%s: phunsy_ctrl_w %02x\n", machine->describe_context(), data);
 
-	state->u_bank = data >> 4;
-	state->q_bank = data & 0x0F;
+	u_bank = data >> 4;
+	q_bank = data & 0x0F;
 
-	switch( state->u_bank )
+	switch( u_bank )
 	{
 	case 0x00:	/* RAM */
-		memory_set_bankptr( space->machine, "bank1", state->ram_1800 );
+		memory_set_bankptr( machine, "bank1", ram_1800 );
 		break;
 	case 0x01:	/* MDCR program */
 	case 0x02:	/* Disassembler */
 	case 0x03:	/* Label handler */
-		memory_set_bankptr( space->machine, "bank1", space->machine->region("maincpu")->base() + ( 0x800 * state->u_bank ) );
+		memory_set_bankptr( machine, "bank1", machine->region("maincpu")->base() + ( 0x800 * u_bank ) );
 		break;
 	default:	/* Not used */
 		break;
 	}
 
-	memory_set_bankptr( space->machine, "bank2", space->machine->region("ram_4000")->base() + 0x4000 * state->q_bank );
+	memory_set_bankptr( machine, "bank2", machine->region("ram_4000")->base() + 0x4000 * q_bank );
 }
 
 
-static WRITE8_HANDLER( phunsy_data_w )
+WRITE8_MEMBER( phunsy_state::phunsy_data_w )
 {
-	phunsy_state *state = space->machine->driver_data<phunsy_state>();
-
 	if (LOG)
-		logerror("%s: phunsy_data_w %02x\n", space->machine->describe_context(), data);
+		logerror("%s: phunsy_data_w %02x\n", machine->describe_context(), data);
 
-	state->data_out = data;
+	data_out = data;
 
 	/* b0 - TTY out */
 	/* b1 - select MDCR / keyboard */
 	/* b2 - Clear keyboard strobe signal */
 	if ( data & 0x04 )
 	{
-		state->keyboard_input |= 0x80;
+		keyboard_input |= 0x80;
 	}
 
-	/* b3 - speaker output */
-	if ( data & 0x08 )
-	{
-	}
+	/* b3 - speaker output (manual says it is bit 1)*/
+	speaker_level_w(m_speaker, BIT(data, 1));
 
 	/* b4 - -REV MDCR output */
 	/* b5 - -FWD MDCR output */
@@ -105,15 +112,14 @@ static WRITE8_HANDLER( phunsy_data_w )
 }
 
 
-static READ8_HANDLER( phunsy_data_r )
+READ8_MEMBER( phunsy_state::phunsy_data_r )
 {
-	phunsy_state *state = space->machine->driver_data<phunsy_state>();
 	UINT8 data;
 
 	if (LOG)
-		logerror("%s: phunsy_data_r\n", space->machine->describe_context());
+		logerror("%s: phunsy_data_r\n", machine->describe_context());
 
-	if ( state->data_out & 0x02 )
+	if ( data_out & 0x02 )
 	{
 		/* MDCR selected */
 		/* b0 - TTY input */
@@ -131,20 +137,20 @@ static READ8_HANDLER( phunsy_data_r )
 		/* Keyboard selected */
 		/* b0-b6 - ASCII code from keyboard */
 		/* b7    - strobe signal */
-		data = state->keyboard_input;
+		data = keyboard_input;
 	}
 
 	return data;
 }
 
 
-static READ8_HANDLER( phunsy_sense_r )
+READ8_MEMBER( phunsy_state::phunsy_sense_r )
 {
 	return 0;
 }
 
 
-static ADDRESS_MAP_START( phunsy_io , ADDRESS_SPACE_IO, 8)
+static ADDRESS_MAP_START( phunsy_io, ADDRESS_SPACE_IO, 8, phunsy_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( S2650_CTRL_PORT, S2650_CTRL_PORT ) AM_WRITE( phunsy_ctrl_w )
 	AM_RANGE( S2650_DATA_PORT,S2650_DATA_PORT) AM_READWRITE( phunsy_data_r, phunsy_data_w )
@@ -153,21 +159,19 @@ ADDRESS_MAP_END
 
 
 /* Input ports */
-INPUT_PORTS_START( phunsy )
+static INPUT_PORTS_START( phunsy )
 INPUT_PORTS_END
 
 
-static WRITE8_DEVICE_HANDLER( phunsy_kbd_put )
+WRITE8_MEMBER( phunsy_state::phunsy_kbd_put )
 {
-	phunsy_state *state = device->machine->driver_data<phunsy_state>();
-
-	state->keyboard_input = data;
+	keyboard_input = data;
 }
 
 
 static GENERIC_TERMINAL_INTERFACE( phunsy_terminal_intf )
 {
-	DEVCB_HANDLER(phunsy_kbd_put)
+	DEVCB_DRIVER_MEMBER(phunsy_state, phunsy_kbd_put)
 };
 
 
@@ -208,8 +212,8 @@ static VIDEO_START( phunsy )
 static VIDEO_UPDATE( phunsy )
 {
 	phunsy_state *state = screen->machine->driver_data<phunsy_state>();
-	UINT8	*gfx = screen->machine->region( "gfx" )->base();
-	UINT8	*v = state->video_ram;
+	UINT8	*gfx = screen->machine->region( "chargen" )->base();
+	UINT8	*v = state->videoram;
 
 	for ( int h = 0; h < 32; h++ )
 	{
@@ -269,19 +273,37 @@ static VIDEO_UPDATE( phunsy )
 			v++;
 		}
 	}
-    return 0;
+	return 0;
 }
+
+/* F4 Character Displayer */
+static const gfx_layout phunsy_charlayout =
+{
+	8, 8,					/* 8 x 8 characters */
+	128,					/* 128 characters */
+	1,					/* 1 bits per pixel */
+	{ 0 },					/* no bitplanes */
+	/* x offsets */
+	{ 0, 1, 2, 3, 4, 5, 6, 7 },
+	/* y offsets */
+	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8 },
+	8*8					/* every char takes 8 bytes */
+};
+
+static GFXDECODE_START( phunsy )
+	GFXDECODE_ENTRY( "chargen", 0x0000, phunsy_charlayout, 1, 3 )
+GFXDECODE_END
 
 
 static MACHINE_CONFIG_START( phunsy, phunsy_state )
-    /* basic machine hardware */
+	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu",S2650, XTAL_1MHz)
 	MCFG_CPU_PROGRAM_MAP(phunsy_mem)
 	MCFG_CPU_IO_MAP(phunsy_io)
 
 	MCFG_MACHINE_RESET(phunsy)
 
-    /* video hardware */
+	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
 	/* Display (page 12 of pdf)
        - 8Mhz clock
@@ -291,13 +313,21 @@ static MACHINE_CONFIG_START( phunsy, phunsy_state )
     */
 	MCFG_SCREEN_RAW_PARAMS(XTAL_8MHz, 480, 0, 64*6, 313, 0, 256)
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MCFG_GFXDECODE(phunsy)
 	MCFG_PALETTE_LENGTH(8)
 	MCFG_PALETTE_INIT(phunsy)
 
 	MCFG_VIDEO_START(phunsy)
 	MCFG_VIDEO_UPDATE(phunsy)
 
-	MCFG_GENERIC_TERMINAL_ADD("ascii_keyboard",phunsy_terminal_intf)
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_MONO("mono")
+	//MCFG_SOUND_WAVE_ADD("wave", "cassette")
+	//MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.25)
+	MCFG_SOUND_ADD("speaker", SPEAKER_SOUND, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.50)
+
+	MCFG_GENERIC_TERMINAL_ADD(TERMINAL_TAG,phunsy_terminal_intf)
 MACHINE_CONFIG_END
 
 
@@ -308,9 +338,11 @@ ROM_START( phunsy )
 	ROM_LOAD( "pdcr.bin",        0x0800, 0x0800, CRC(74bf9d0a) SHA1(8d2f673615215947f033571f1221c6aa99c537e9))
 	ROM_LOAD( "dass.bin",        0x1000, 0x0800, CRC(13380140) SHA1(a999201cb414abbf1e10a7fcc1789e3e000a5ef1))
 	ROM_LOAD( "labhnd.bin",      0x1800, 0x0800, CRC(1d5a106b) SHA1(a20d09e32e21cf14db8254cbdd1d691556b473f0))
-	ROM_REGION( 0x0400, "gfx", ROMREGION_ERASEFF )
+
+	ROM_REGION( 0x0400, "chargen", ROMREGION_ERASEFF )
 	ROM_LOAD( "ph_char1.bin", 0x0000, 0x0200, CRC(a7e567fc) SHA1(b18aae0a2d4f92f5a7e22640719bbc4652f3f4ee))
 	ROM_LOAD( "ph_char2.bin", 0x0200, 0x0200, CRC(3d5786d3) SHA1(8cf87d83be0b5e4becfa9fd6e05b01250a2feb3b))
+
 	/* 16 x 16KB RAM banks */
 	ROM_REGION( 0x40000, "ram_4000", ROMREGION_ERASEFF )
 ROM_END
@@ -318,5 +350,5 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 1980, phunsy,  0,       0,	phunsy, 	phunsy, 	 phunsy,		 "J.F.P. Philipse",   "PHUNSY",		GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1980, phunsy,  0,       0,	phunsy, 	phunsy, 	 phunsy, "J.F.P. Philipse",   "PHUNSY", GAME_NOT_WORKING )
 
