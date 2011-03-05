405848891ab6c92ea8d3b30fa02e56691cd8c36e
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2011-03-05 12:35:19 +0000

    Synced with MAME.
    git-svn-id: svn://messdev.no-ip.org/mess@10677 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/cpu/mcs51/mcs51.c
+++ src/emu/cpu/mcs51/mcs51.c
@@ -1243,7 +1243,7 @@ INLINE void serial_receive(mcs51_state_t *mcs51_state)
 {
 	int mode = (GET_SM0<<1) | GET_SM1;
 
-	if(GET_ES && GET_REN) {
+	if (GET_REN) {
 		switch(mode) {
 			//8 bit shifter ( + start,stop bit ) - baud set by clock freq / 12
 			case 0:
@@ -1809,12 +1809,11 @@ static void mcs51_set_irq_line(mcs51_state_t *mcs51_state, int irqline, int stat
 {
 	/* From the manual:
      *
-     * <cite>In operatiom all the interrupt tlags are latched into the
-     * interrupt control system during State 5 of every ma-
-     * chine cycle. The samples are polled during the follow-
-     * ing machine cycle.</cite>
+     * <cite>In operation all the interrupt flags are latched into the
+     * interrupt control system during State 5 of every machine cycle.
+     * The samples are polled during the following machine cycle.</cite>
      *
-     * ==> Since we do not emulate substates, this assumes that the signal is present
+     * ==> Since we do not emulate sub-states, this assumes that the signal is present
      * for at least one cycle (12 states)
      *
      */
