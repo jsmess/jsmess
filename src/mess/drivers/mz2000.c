6533f2673465e6aa4892524cd124e276c07f43f0
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2011-03-15 01:40:57 +0000

    [MZ-2000]: More works
    git-svn-id: svn://messdev.no-ip.org/mess@10817 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mz2000.c
+++ src/mess/drivers/mz2000.c
@@ -2,7 +2,7 @@
 
 	Sharp MZ-2000
 
-	skeleton driver
+	preliminary driver by Angelo Salese
 
 	Basically a simpler version of Sharp MZ-2500
 
@@ -15,11 +15,11 @@
 #include "machine/i8255a.h"
 #include "machine/wd17xx.h"
 #include "machine/pit8253.h"
-#include "sound/2203intf.h"
 #include "sound/beep.h"
+#include "sound/wave.h"
 #include "machine/rp5c15.h"
 
-//#include "imagedev/cassette.h"
+#include "imagedev/cassette.h"
 #include "imagedev/flopdrv.h"
 #include "imagedev/flopimg.h"
 #include "formats/basicdsk.h"
@@ -35,6 +35,11 @@ public:
 	UINT8 ipl_enable;
 	UINT8 tvram_enable;
 	UINT8 gvram_enable;
+	UINT8 gvram_bank;
+
+	UINT8 key_mux,pio_latchb;
+
+	UINT8 old_portc;
 };
 
 static VIDEO_START( mz2000 )
@@ -113,6 +118,22 @@ static WRITE8_HANDLER( mz2000_tvram_w )
 	tvram[offset] = data;
 }
 
+static READ8_HANDLER( mz2000_gvram_r )
+{
+	mz2000_state *state = space->machine->driver_data<mz2000_state>();
+	UINT8 *gvram = space->machine->region("gvram")->base();
+
+	return gvram[offset+state->gvram_bank*0x4000];
+}
+
+static WRITE8_HANDLER( mz2000_gvram_w )
+{
+	mz2000_state *state = space->machine->driver_data<mz2000_state>();
+	UINT8 *gvram = space->machine->region("gvram")->base();
+
+	gvram[offset+state->gvram_bank*0x4000] = data;
+}
+
 
 static READ8_HANDLER( mz2000_mem_r )
 {
@@ -124,10 +145,15 @@ static READ8_HANDLER( mz2000_mem_r )
 	if(page_mem == 0 && state->ipl_enable)
 		return mz2000_ipl_r(space,offset & 0xfff);
 
-	if(page_mem >= 8)
+	if(((page_mem & 8) == 0) && state->ipl_enable == 0) // if ipl is enabled, 0x1000 - 0x7fff accesses to dummy region
+		return mz2000_wram_r(space,offset);
+
+	if(page_mem & 8)
 	{
 		if(page_mem == 0xd && state->tvram_enable)
 			return mz2000_tvram_r(space,offset & 0xfff);
+		else if(page_mem >= 0xc && state->gvram_enable)
+			return mz2000_gvram_r(space,offset & 0x3fff);
 		else
 			return mz2000_wram_r(space,offset);
 	}
@@ -142,39 +168,194 @@ static WRITE8_HANDLER( mz2000_mem_w )
 
 	page_mem = (offset & 0xf000) >> 12;
 
-	if(page_mem >= 8)
+	if((page_mem & 8) == 0 && state->ipl_enable == 0)
+		mz2000_wram_w(space,offset,data);
+
+	if(page_mem & 8)
 	{
 		if(page_mem == 0xd && state->tvram_enable)
 			mz2000_tvram_w(space,offset & 0xfff,data);
+		else if(page_mem >= 0xc && state->gvram_enable)
+			mz2000_gvram_w(space,offset & 0x3fff,data);
 		else
 			mz2000_wram_w(space,offset,data);
 	}
 }
 
+static WRITE8_HANDLER( mz2000_gvram_bank_w )
+{
+	mz2000_state *state = space->machine->driver_data<mz2000_state>();
 
+	state->gvram_bank = data & 3;
+}
+
+static WRITE8_HANDLER( mz2000_fdc_w )
+{
+	device_t* dev = space->machine->device("mb8877a");
+
+	switch(offset+0xdc)
+	{
+		case 0xdc:
+			wd17xx_set_drive(dev,data & 3);
+			floppy_mon_w(floppy_get_device(space->machine, data & 3), (data & 0x80) ? CLEAR_LINE : ASSERT_LINE);
+			floppy_drive_set_ready_state(floppy_get_device(space->machine, data & 3), 1,0);
+			break;
+		case 0xdd:
+			wd17xx_set_side(dev,(data & 1));
+			break;
+	}
+}
 
 static ADDRESS_MAP_START(mz2000_map, ADDRESS_SPACE_PROGRAM, 8)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x0000, 0xffff ) AM_READWRITE(mz2000_mem_r,mz2000_mem_w)
-	// RAM is 0x10000
-	// TVRAM is 0x1000
-	// GVRAM is 0x4000
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START(mz2000_io, ADDRESS_SPACE_IO, 8)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-//	AM_RANGE(0xd8, 0xdd) disk i/o identical to Sharp MZ-2500
+	AM_RANGE(0xd8, 0xdb) AM_DEVREADWRITE("mb8877a", wd17xx_r, wd17xx_w)
+	AM_RANGE(0xdc, 0xdd) AM_WRITE(mz2000_fdc_w)
 	AM_RANGE(0xe0, 0xe3) AM_DEVREADWRITE("i8255_0", i8255a_r, i8255a_w)
 //	AM_RANGE(0xe4, 0xe7) i8253
 	AM_RANGE(0xe8, 0xeb) AM_DEVREADWRITE("z80pio_1", z80pio_ba_cd_r, z80pio_ba_cd_w)
 //	AM_RANGE(0xf0, 0xf3) clear 8253, same as Sharp MZ-2500
 //	AM_RANGE(0xf4, 0xf7) CRTC
+	AM_RANGE(0xf7, 0xf7) AM_WRITE(mz2000_gvram_bank_w)
 ADDRESS_MAP_END
 
 
 /* Input ports */
 static INPUT_PORTS_START( mz2000 )
+	PORT_START("KEY0")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F1") PORT_CODE(KEYCODE_F1)
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F2") PORT_CODE(KEYCODE_F2)
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F3") PORT_CODE(KEYCODE_F3)
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F4") PORT_CODE(KEYCODE_F4)
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F5") PORT_CODE(KEYCODE_F5)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F6") PORT_CODE(KEYCODE_F6)
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F7") PORT_CODE(KEYCODE_F7)
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F8") PORT_CODE(KEYCODE_F8)
+
+	PORT_START("KEY1")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F9") PORT_CODE(KEYCODE_F9)
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F10") PORT_CODE(KEYCODE_F10)
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("8 (PAD)") PORT_CODE(KEYCODE_8_PAD)
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("9 (PAD)") PORT_CODE(KEYCODE_9_PAD)
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(", (PAD)") //PORT_CODE(KEYCODE_SLASH_PAD)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(". (PAD)") PORT_CODE(KEYCODE_DEL_PAD)
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("+ (PAD)") PORT_CODE(KEYCODE_PLUS_PAD)
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("- (PAD)") PORT_CODE(KEYCODE_MINUS_PAD)
+
+	PORT_START("KEY2")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("0 (PAD)") PORT_CODE(KEYCODE_0_PAD)
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("1 (PAD)") PORT_CODE(KEYCODE_1_PAD)
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("2 (PAD)") PORT_CODE(KEYCODE_2_PAD)
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("3 (PAD)") PORT_CODE(KEYCODE_3_PAD)
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("4 (PAD)") PORT_CODE(KEYCODE_4_PAD)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("5 (PAD)") PORT_CODE(KEYCODE_5_PAD)
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("6 (PAD)") PORT_CODE(KEYCODE_6_PAD)
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("7 (PAD)") PORT_CODE(KEYCODE_7_PAD)
+
+	PORT_START("KEY3")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("TAB") PORT_CODE(KEYCODE_TAB)
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Space") PORT_CODE(KEYCODE_SPACE) PORT_CHAR(' ')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("RETURN") PORT_CODE(KEYCODE_ENTER) PORT_CHAR(27)
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Up") PORT_CODE(KEYCODE_UP)
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Down") PORT_CODE(KEYCODE_DOWN)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Left") PORT_CODE(KEYCODE_LEFT)
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Right") PORT_CODE(KEYCODE_RIGHT)
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("BREAK") //PORT_CODE(KEYCODE_ESC)
+
+	PORT_START("KEY4")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("/") //PORT_CODE(KEYCODE_SLASH)
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("A") PORT_CODE(KEYCODE_A) PORT_CHAR('A')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("B") PORT_CODE(KEYCODE_B) PORT_CHAR('B')
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("C") PORT_CODE(KEYCODE_C) PORT_CHAR('C')
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("D") PORT_CODE(KEYCODE_D) PORT_CHAR('D')
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("E") PORT_CODE(KEYCODE_E) PORT_CHAR('E')
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("F") PORT_CODE(KEYCODE_F) PORT_CHAR('F')
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("G") PORT_CODE(KEYCODE_G) PORT_CHAR('G')
+
+	PORT_START("KEY5")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("H") PORT_CODE(KEYCODE_H) PORT_CHAR('H')
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("I") PORT_CODE(KEYCODE_I) PORT_CHAR('I')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("J") PORT_CODE(KEYCODE_J) PORT_CHAR('J')
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("K") PORT_CODE(KEYCODE_K) PORT_CHAR('K')
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("L") PORT_CODE(KEYCODE_L) PORT_CHAR('L')
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("M") PORT_CODE(KEYCODE_M) PORT_CHAR('M')
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("N") PORT_CODE(KEYCODE_N) PORT_CHAR('N')
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("O") PORT_CODE(KEYCODE_O) PORT_CHAR('O')
+
+	PORT_START("KEY6")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("P") PORT_CODE(KEYCODE_P) PORT_CHAR('P')
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Q") PORT_CODE(KEYCODE_Q) PORT_CHAR('Q')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_R) PORT_CHAR('R')
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("S") PORT_CODE(KEYCODE_S) PORT_CHAR('S')
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("T") PORT_CODE(KEYCODE_T) PORT_CHAR('T')
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("U") PORT_CODE(KEYCODE_U) PORT_CHAR('U')
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("V") PORT_CODE(KEYCODE_V) PORT_CHAR('V')
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("W") PORT_CODE(KEYCODE_W) PORT_CHAR('W')
+
+	PORT_START("KEY7")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("X") PORT_CODE(KEYCODE_X) PORT_CHAR('X')
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Y") PORT_CODE(KEYCODE_Y) PORT_CHAR('Y')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("Z") PORT_CODE(KEYCODE_Z) PORT_CHAR('Z')
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("^")
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) //Yen symbol
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("_")
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(".")
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(",")
+
+	PORT_START("KEY8")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("0") PORT_CODE(KEYCODE_0) PORT_CHAR('0')
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("1") PORT_CODE(KEYCODE_1) PORT_CHAR('1')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("2") PORT_CODE(KEYCODE_2) PORT_CHAR('2')
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("3") PORT_CODE(KEYCODE_3) PORT_CHAR('3')
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("4") PORT_CODE(KEYCODE_4) PORT_CHAR('4')
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("5") PORT_CODE(KEYCODE_5) PORT_CHAR('5')
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("6") PORT_CODE(KEYCODE_6) PORT_CHAR('6')
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("7") PORT_CODE(KEYCODE_7) PORT_CHAR('7')
+
+	PORT_START("KEY9")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("8") PORT_CODE(KEYCODE_8) PORT_CHAR('8')
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("9") PORT_CODE(KEYCODE_9) PORT_CHAR('9')
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(":")
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME(";")
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("-") PORT_CODE(KEYCODE_MINUS)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("@")
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("[")
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_UNUSED)
+
+	PORT_START("KEYA")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("]")
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("COPY")
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("CLR")
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("INST")
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("BACKSPACE") PORT_CODE(KEYCODE_BACKSPACE)
+	PORT_BIT(0x20,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("ESC")
+	PORT_BIT(0x40,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("* (PAD)") PORT_CODE(KEYCODE_ASTERISK)
+	PORT_BIT(0x80,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("/ (PAD)") PORT_CODE(KEYCODE_SLASH_PAD)
+
+	PORT_START("KEYB")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("GRPH")
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("SLOCK")
+	PORT_BIT(0x04,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("SHIFT") PORT_CODE(KEYCODE_LSHIFT)
+	PORT_BIT(0x08,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("KANA")
+	PORT_BIT(0x10,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("CTRL")
+	PORT_BIT(0xe0,IP_ACTIVE_LOW,IPT_UNUSED)
+
+	PORT_START("KEYC")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("KJ1")
+	PORT_BIT(0x02,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("KJ2")
+	PORT_BIT(0xfc,IP_ACTIVE_LOW,IPT_UNUSED)
+
+	PORT_START("KEYD")
+	PORT_BIT(0x01,IP_ACTIVE_LOW,IPT_KEYBOARD) PORT_NAME("LOGO KEY")
+	PORT_BIT(0xfe,IP_ACTIVE_LOW,IPT_UNUSED)
+
+	PORT_START("UNUSED")
+	PORT_BIT(0xff,IP_ACTIVE_LOW,IPT_UNUSED )
 INPUT_PORTS_END
 
 
@@ -185,6 +366,9 @@ static MACHINE_RESET(mz2000)
 	state->ipl_enable = 1;
 	state->tvram_enable = 0;
 	state->gvram_enable = 0;
+
+	beep_set_frequency(machine->device("beeper"),4096);
+	beep_set_state(machine->device("beeper"),0);
 }
 
 
@@ -213,9 +397,18 @@ static READ8_DEVICE_HANDLER( mz2000_portb_r )
 {
 	/*
 	x--- ---- break key
+	-x-- ---- read tape data
+	--x- ---- no tape signal
+	---x ---- no tape write signal
+	---- x--- end of tape reached
 	---- ---x "blank" control
 	*/
-	return 0xff;
+	UINT8 tape_read_data = 0;
+
+	if (cassette_input(device->machine->device("cassette")) > 0.03)
+		tape_read_data |= 0x40;
+
+	return 0xbf | tape_read_data;
 }
 
 static READ8_DEVICE_HANDLER( mz2000_portc_r )
@@ -250,6 +443,7 @@ static WRITE8_DEVICE_HANDLER( mz2000_portb_w )
 
 static WRITE8_DEVICE_HANDLER( mz2000_portc_w )
 {
+	mz2000_state *state = device->machine->driver_data<mz2000_state>();
 	/*
 		x--- ---- tape data write
 		-x-- ---- tape rec
@@ -261,7 +455,19 @@ static WRITE8_DEVICE_HANDLER( mz2000_portc_w )
     */
 	printf("C W %02x\n",data);
 
-	// ...
+	if(((state->old_portc & 2) == 0) && data & 2)
+	{
+		state->ipl_enable = 0;
+		/* correct? */
+		cputag_set_input_line(device->machine, "maincpu", INPUT_LINE_RESET, PULSE_LINE);
+	}
+
+	if(((state->old_portc & 8) == 0) && data & 8)
+		state->ipl_enable = 1;
+
+	beep_set_state(device->machine->device("beeper"),data & 0x04);
+
+	state->old_portc = data;
 }
 
 static I8255A_INTERFACE( ppi8255_intf )
@@ -280,18 +486,75 @@ static WRITE8_DEVICE_HANDLER( mz2000_pio1_porta_w )
 	state->tvram_enable = ((data & 0xc0) == 0xc0);
 	state->gvram_enable = ((data & 0xc0) == 0x80);
 
+	state->key_mux = data & 0x1f;
+
 	//printf("%02x PIO\n",data);
+}
+
+static READ8_DEVICE_HANDLER( mz2000_pio1_porta_r )
+{
+	mz2000_state *state = device->machine->driver_data<mz2000_state>();
+	static const char *const keynames[] = { "KEY0", "KEY1", "KEY2", "KEY3",
+	                                        "KEY4", "KEY5", "KEY6", "KEY7",
+	                                        "KEY8", "KEY9", "KEYA", "KEYB",
+	                                        "KEYC", "KEYD", "UNUSED", "UNUSED" };
+
+	if(((state->key_mux & 0x10) == 0x00) || ((state->key_mux & 0x0f) == 0x0f)) //status read
+	{
+		int res,i;
+
+		res = 0xff;
+		for(i=0;i<0xe;i++)
+			res &= input_port_read(device->machine, keynames[i]);
+
+		state->pio_latchb = res;
+
+		return res;
+	}
+
+	state->pio_latchb = input_port_read(device->machine, keynames[state->key_mux & 0xf]);
+
+	return input_port_read(device->machine, keynames[state->key_mux & 0xf]);
 }
 
 static Z80PIO_INTERFACE( mz2000_pio1_intf )
 {
 	DEVCB_NULL,
-	DEVCB_NULL, //HANDLER( mz2500_pio1_porta_r ),
+	DEVCB_HANDLER( mz2000_pio1_porta_r ),
 	DEVCB_HANDLER( mz2000_pio1_porta_w ),
 	DEVCB_NULL,
-	DEVCB_NULL, //HANDLER( mz2500_pio1_porta_r ),
+	DEVCB_HANDLER( mz2000_pio1_porta_r ),
 	DEVCB_NULL,
 	DEVCB_NULL
+};
+
+static const wd17xx_interface mz2000_mb8877a_interface =
+{
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	{FLOPPY_0, FLOPPY_1, FLOPPY_2, FLOPPY_3}
+};
+
+static FLOPPY_OPTIONS_START( mz2000 )
+	FLOPPY_OPTION( img2d, "2d", "2D disk image", basicdsk_identify_default, basicdsk_construct_default,
+		HEADS([2])
+		TRACKS([80])
+		SECTORS([16])
+		SECTOR_LENGTH([256])
+		FIRST_SECTOR_ID([1]))
+FLOPPY_OPTIONS_END
+
+static const floppy_config mz2000_floppy_config =
+{
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	FLOPPY_STANDARD_3_5_DSHD,
+	FLOPPY_OPTIONS_NAME(default),
+	NULL
 };
 
 static MACHINE_CONFIG_START( mz2000, mz2000_state )
@@ -304,6 +567,11 @@ static MACHINE_CONFIG_START( mz2000, mz2000_state )
 
 	MCFG_I8255A_ADD( "i8255_0", ppi8255_intf )
 	MCFG_Z80PIO_ADD( "z80pio_1", 4000000, mz2000_pio1_intf )
+
+	MCFG_MB8877_ADD("mb8877a",mz2000_mb8877a_interface)
+	MCFG_FLOPPY_4_DRIVES_ADD(mz2000_floppy_config)
+
+	MCFG_CASSETTE_ADD( "cassette", default_cassette_config )
 
 	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
@@ -320,8 +588,14 @@ static MACHINE_CONFIG_START( mz2000, mz2000_state )
 	MCFG_VIDEO_START(mz2000)
 	MCFG_SCREEN_UPDATE(mz2000)
 
-MACHINE_CONFIG_END
+	MCFG_SPEAKER_STANDARD_MONO("mono")
 
+	MCFG_SOUND_WAVE_ADD("wave", "cassette")
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.20)
+
+	MCFG_SOUND_ADD("beeper", BEEP, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS,"mono",0.50)
+MACHINE_CONFIG_END
 
 
 
@@ -335,10 +609,11 @@ ROM_START( mz2000 )
 
 	ROM_REGION( 0x1000, "tvram", ROMREGION_ERASE00 )
 
+	ROM_REGION( 0x10000, "gvram", ROMREGION_ERASE00 )
+
 	ROM_REGION( 0x1000, "chargen", 0 )
 	ROM_LOAD( "mzfont.rom", 0x0000, 0x0800, CRC(0631efc3) SHA1(99b206af5c9845995733d877e9e93e9681b982a8) )
 ROM_END
-
 
 
 /* Driver */
