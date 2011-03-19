a64eec56319c102415e6a5aa9f701f55f10b9912
sronco <sronco@9849079f-6229-0410-a07f-84406ce305ba>
2011-03-19 16:40:14 +0000

    [REX6000] Added quickload for load rex/ds2 images and some minors fixes.
    git-svn-id: svn://messdev.no-ip.org/mess@10866 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/rex6000.c
+++ src/mess/drivers/rex6000.c
@@ -2,7 +2,7 @@
 
         Xircom / Intel REX 6000
 
-        18/06/2009 Skeleton driver.
+        Driver by Sandro Ronco
 
 ****************************************************************************/
 
@@ -11,6 +11,7 @@
 #include "emu.h"
 #include "cpu/z80/z80.h"
 #include "machine/tc8521.h"
+#include "imagedev/snapquik.h"
 #include "rendlay.h"
 
 #define MAKE_BANK_ADDRESS(lo, hi)		(((lo)<<13) | ((hi)<<21))
@@ -27,6 +28,8 @@ public:
 
 	UINT8 m_bank[4];
 	UINT8 m_lcd_base[2];
+	UINT8 m_lcd_enabled;
+	UINT8 m_lcd_cmd;
 
 	UINT32 m_lcd_addr;
 	UINT8 *m_bank_base;
@@ -41,6 +44,8 @@ public:
 	DECLARE_WRITE8_MEMBER( bankswitch_w );
 	DECLARE_READ8_MEMBER( lcd_base_r );
 	DECLARE_WRITE8_MEMBER( lcd_base_w );
+	DECLARE_READ8_MEMBER( lcd_io_r );
+	DECLARE_WRITE8_MEMBER( lcd_io_w );
 	DECLARE_READ8_MEMBER( irq_r );
 	DECLARE_WRITE8_MEMBER( irq_w );
 	DECLARE_READ8_MEMBER( touchscreen_r );
@@ -88,6 +93,24 @@ WRITE8_MEMBER( rex6000_state::lcd_base_w )
 	//printf("Set LCD base at %x\n", m_lcd_addr);
 }
 
+READ8_MEMBER( rex6000_state::lcd_io_r )
+{
+	return (offset == 0) ? m_lcd_enabled : m_lcd_cmd;
+}
+
+WRITE8_MEMBER( rex6000_state::lcd_io_w )
+{
+	switch (offset)
+	{
+		case 0:
+			m_lcd_enabled = data;
+			break;
+		case 1:
+			m_lcd_cmd = data;
+			break;
+	}
+}
+
 READ8_MEMBER( rex6000_state::irq_r )
 {
 	switch (offset)
@@ -95,7 +118,7 @@ READ8_MEMBER( rex6000_state::irq_r )
 		case 0: //irq flag
 			/*
                 ---- ---x   input port
-                ---x ----   low battery
+                ---x ----   sec timer
                 --x- ----   timer
             */
 			return m_irq_flag;
@@ -168,6 +191,7 @@ static ADDRESS_MAP_START( rex6000_io, ADDRESS_SPACE_IO, 8, rex6000_state)
 	AM_RANGE( 0x22, 0x23 ) AM_READWRITE(lcd_base_r, lcd_base_w)
 	AM_RANGE( 0x30, 0x3f ) AM_DEVREADWRITE_LEGACY("rtc", tc8521_r, tc8521_w)
 	AM_RANGE( 0x40, 0x47 ) AM_NOP	//SIO
+	AM_RANGE( 0x50, 0x51 ) AM_READWRITE(lcd_io_r, lcd_io_w)
 	AM_RANGE( 0x68, 0x6c ) AM_READ(touchscreen_r)
 	//AM_RANGE( 0x00, 0xff ) AM_RAM
 ADDRESS_MAP_END
@@ -187,8 +211,8 @@ INPUT_PORTS_START( rex6000 )
 	PORT_BIT(0x01, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Home")	PORT_CODE(KEYCODE_ENTER)		PORT_CHANGED(trigger_irq, 0)
 	PORT_BIT(0x02, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Back")	PORT_CODE(KEYCODE_BACKSPACE)	PORT_CHANGED(trigger_irq, 0)
 	PORT_BIT(0x04, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Select")		PORT_CODE(KEYCODE_SPACE)		PORT_CHANGED(trigger_irq, 0)
-	PORT_BIT(0x08, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Left")	PORT_CODE(KEYCODE_LEFT)			PORT_CHANGED(trigger_irq, 0)
-	PORT_BIT(0x10, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Right")	PORT_CODE(KEYCODE_RIGHT)		PORT_CHANGED(trigger_irq, 0)
+	PORT_BIT(0x08, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Up")		PORT_CODE(KEYCODE_UP)			PORT_CHANGED(trigger_irq, 0)
+	PORT_BIT(0x10, IP_ACTIVE_HIGH, IPT_KEYPAD)  PORT_NAME("Down")	PORT_CODE(KEYCODE_DOWN)			PORT_CHANGED(trigger_irq, 0)
 	PORT_BIT(0x40, IP_ACTIVE_HIGH, IPT_BUTTON1) PORT_NAME("Pen")	PORT_CODE(MOUSECODE_BUTTON1)	PORT_CHANGED(trigger_irq, 0)
 
 	PORT_START("PENX")
@@ -210,17 +234,24 @@ bool rex6000_state::screen_update(screen_device &screen, bitmap_t &bitmap, const
 {
 	UINT8 *lcd_base = m_bank_base + m_lcd_addr;
 
-	for (int y=0; y<120; y++)
-		for (int x=0; x<30; x++)
-		{
-			UINT8 data = lcd_base[y*30 + x];
-
-			for (int b=0; b<8; b++)
+	if (m_lcd_enabled)
+	{
+		for (int y=0; y<120; y++)
+			for (int x=0; x<30; x++)
 			{
-				*BITMAP_ADDR16(&bitmap, y, (x * 8) + b) = BIT(data, 7);
-				data <<= 1;
+				UINT8 data = lcd_base[y*30 + x];
+
+				for (int b=0; b<8; b++)
+				{
+					*BITMAP_ADDR16(&bitmap, y, (x * 8) + b) = BIT(data, 7);
+					data <<= 1;
+				}
 			}
-		}
+	}
+	else
+	{
+		bitmap_fill(&bitmap, &cliprect, 0);
+	}
 
 	return 0;
 }
@@ -234,11 +265,44 @@ static TIMER_DEVICE_CALLBACK( irq_timer )
 	cpu_set_input_line(state->m_maincpu, 0, HOLD_LINE);
 }
 
+static TIMER_DEVICE_CALLBACK( sec_timer )
+{
+	rex6000_state *state = timer.machine->driver_data<rex6000_state>();
+
+	state->m_irq_flag |= 0x10;
+
+	cpu_set_input_line(state->m_maincpu, 0, HOLD_LINE);
+}
+
+
 static PALETTE_INIT( rex6000 )
 {
 	palette_set_color(machine, 0, MAKE_RGB(138, 146, 148));
 	palette_set_color(machine, 1, MAKE_RGB(92, 83, 88));
 }
+
+static QUICKLOAD_LOAD(rex6000)
+{
+	static const char magic[] = "ApplicationName:Addin";
+	running_machine *machine = image.device().machine;
+	UINT32 img_start = 0;
+	UINT8 *data;
+
+	data = (UINT8*)auto_alloc_array(machine, UINT8, image.length());
+	image.fread(data, image.length());
+
+	if(strncmp((const char*)data, magic, 21))
+		return IMAGE_INIT_FAIL;
+
+	img_start = strlen((const char*)data) + 5;
+	img_start += 0xa0;	//skip the icon (40x32 pixel)
+
+	memcpy((UINT8*)machine->region("flash")->base() + 0x100000, data + img_start, image.length() - img_start);
+	auto_free(machine, data);
+
+	return IMAGE_INIT_PASS;
+}
+
 
 /* F4 Character Displayer */
 static const gfx_layout rex6000_bold_charlayout =
@@ -300,6 +364,7 @@ static MACHINE_CONFIG_START( rex6000, rex6000_state )
     MCFG_CPU_IO_MAP(rex6000_io)
 
 	MCFG_TIMER_ADD_PERIODIC("irq_timer", irq_timer, attotime::from_msec(25))
+	MCFG_TIMER_ADD_PERIODIC("sec_timer", sec_timer, attotime::from_seconds(1))
 
     /* video hardware */
     MCFG_SCREEN_ADD("screen", LCD)
@@ -312,6 +377,9 @@ static MACHINE_CONFIG_START( rex6000, rex6000_state )
     MCFG_PALETTE_LENGTH(2)
     MCFG_PALETTE_INIT(rex6000)
 	MCFG_GFXDECODE(rex6000)
+
+	/* quickload */
+	MCFG_QUICKLOAD_ADD("quickload", rex6000, "rex,ds2", 0)
 
 	MCFG_TC8521_ADD("rtc", rex6000_tc8521_interface)
 MACHINE_CONFIG_END
