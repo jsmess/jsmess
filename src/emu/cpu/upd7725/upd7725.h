4323b048c2273559365b0b68ba6c4574ab315041
jgevarya <jgevarya@9849079f-6229-0410-a07f-84406ce305ba>
2011-03-24 12:01:49 +0000

    revert 10911, did not link properly. If anyone wants to help me figure out why, I'm all ears.
    git-svn-id: svn://messdev.no-ip.org/mess@10912 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/cpu/upd7725/upd7725.h
+++ src/emu/cpu/upd7725/upd7725.h
@@ -12,17 +12,6 @@
 #define __UPD7725_H__
 
 //**************************************************************************
-//  ENUMERATIONS
-//**************************************************************************
-
-// input lines
-enum
-{
-	NECDSP_INPUT_LINE_INT = 0
-	// add more here as needed
-};
-
-//**************************************************************************
 //  TYPE DEFINITIONS
 //**************************************************************************
 
@@ -30,26 +19,9 @@ class necdsp_device;
 class upd7725_device;
 class upd96050_device;
 
-// ======================> necdsp_interface
-
-struct necdsp_interface
-{
-	devcb_read_line		m_in_int_func;
-	//devcb_read8		m_in_si_func;
-	//devcb_read_line	m_in_sck_func;
-	//devcb_read_line	m_in_sien_func;
-	//devcb_read_line	m_in_soen_func;
-	//devcb_read_line	m_in_dack_func;
-	devcb_write_line	m_out_p0_func;
-	devcb_write_line	m_out_p1_func;
-	//devcb_write8		m_out_so_func;
-	//devcb_write_line	m_out_sorq_func;
-	//devcb_write_line	m_out_drq_func;
-};
-
 // ======================> necdsp_device_config
 
-class necdsp_device_config :	public cpu_device_config, public necdsp_interface
+class necdsp_device_config :	public cpu_device_config
 {
 	friend class necdsp_device;
 
@@ -63,9 +35,6 @@ public:
 	virtual device_t *alloc_device(running_machine &machine) const;
 
 protected:
-	// device_config overrides
-	virtual void device_config_complete();
-
 	// device_config_execute_interface overrides
 	virtual UINT32 execute_min_cycles() const;
 	virtual UINT32 execute_max_cycles() const;
@@ -129,7 +98,6 @@ protected:
 
 	// device_execute_interface overrides
 	virtual void execute_run();
-	virtual void execute_set_input(int inputnum, int state);
 
 	// device_state_interface overrides
 	virtual void state_import(const device_state_entry &entry);
@@ -211,12 +179,9 @@ private:
   void stack_pull();
 
   int m_icount;
-  int m_irq; // old irq line state, for detecting rising edges.
 
   address_space *m_program, *m_data;
   direct_read_data *m_direct;
-  
-  const necdsp_device_config &m_config;
 };
 
 class upd7725_device : public necdsp_device
@@ -246,19 +211,6 @@ extern const device_type UPD96050;
 //**************************************************************************
 //  ENUMERATIONS
 //**************************************************************************
-
-// device callbacks
-	devcb_resolved_read_line	m_in_int_func;
-	//devcb_resolved_read8		m_in_si_func;
-	//devcb_resolved_read_line	m_in_sck_func;
-	//devcb_resolved_read_line	m_in_sien_func;
-	//devcb_resolved_read_line	m_in_soen_func;
-	//devcb_resolved_read_line	m_in_dack_func;
-	devcb_resolved_write_line	m_out_p0_func;
-	devcb_resolved_write_line	m_out_p1_func;
-	//devcb_resolved_write8		m_out_so_func;
-	//devcb_resolved_write_line	m_out_sorq_func;
-	//devcb_resolved_write_line	m_out_drq_func;
 
 // registers
 enum
