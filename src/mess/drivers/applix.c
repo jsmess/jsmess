6837a22ffdb243e3a1bdee50647c1d4f5894a28e
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-03-28 13:28:46 +0000

    [APPLIX] Fixed rom order.
    git-svn-id: svn://messdev.no-ip.org/mess@10962 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/applix.c
+++ src/mess/drivers/applix.c
@@ -20,12 +20,14 @@ public:
 	applix_state(running_machine &machine, const driver_device_config_base &config)
 		: driver_device(machine, config) { }
 
+	UINT8* m_base;
+
 };
 
 
 static ADDRESS_MAP_START(applix_mem, AS_PROGRAM, 16)
 	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x000000, 0x4fffff) AM_RAM
+	AM_RANGE(0x000000, 0x4fffff) AM_RAM AM_BASE_MEMBER(applix_state, m_base)
 	AM_RANGE(0x500000, 0x5fffff) AM_ROM
 ADDRESS_MAP_END
 
@@ -37,10 +39,10 @@ INPUT_PORTS_END
 
 static MACHINE_RESET(applix)
 {
-	//applix_state *state = machine->driver_data<applix_state>();
+	applix_state *state = machine->driver_data<applix_state>();
 	UINT8* RAM = machine->region("maincpu")->base();
-	memcpy(RAM, RAM+0x500000, 8);
-	//machine->device("maincpu")->reset();
+	memcpy(state->m_base, RAM+0x500000, 16);
+	machine->device("maincpu")->reset();
 }
 
 static VIDEO_START( applix )
@@ -76,11 +78,11 @@ MACHINE_CONFIG_END
 
 /* ROM definition */
 ROM_START( applix )
-	ROM_REGION16_BE(0x1000000, "maincpu", 0)
-	ROM_LOAD16_BYTE( "1616oslv.044", 0x500000, 0x10000, CRC(ef619994) SHA1(ff16fe9e2c99a1ffc855baf89278a97a2a2e881a) )
-	ROM_LOAD16_BYTE( "1616oshv.044", 0x500001, 0x10000, CRC(4a1a90d3) SHA1(4df504bbf6fc5dad76c29e9657bfa556500420a6) )
-	ROM_LOAD16_BYTE( "1616oslv.045", 0x520000, 0x10000, CRC(951bd441) SHA1(e0a38c8d0d38d84955c1de3f6a7d56ce06b063f6) )
-	ROM_LOAD16_BYTE( "1616oshv.045", 0x520001, 0x10000, CRC(9dfb3224) SHA1(5223833a357f90b147f25826c01713269fc1945f) )
+	ROM_REGION(0x1000000, "maincpu", 0)
+	ROM_LOAD16_BYTE( "1616oshv.044", 0x500000, 0x10000, CRC(4a1a90d3) SHA1(4df504bbf6fc5dad76c29e9657bfa556500420a6) )
+	ROM_LOAD16_BYTE( "1616oslv.044", 0x500001, 0x10000, CRC(ef619994) SHA1(ff16fe9e2c99a1ffc855baf89278a97a2a2e881a) )
+	ROM_LOAD16_BYTE( "1616oshv.045", 0x520000, 0x10000, CRC(9dfb3224) SHA1(5223833a357f90b147f25826c01713269fc1945f) )
+	ROM_LOAD16_BYTE( "1616oslv.045", 0x520001, 0x10000, CRC(951bd441) SHA1(e0a38c8d0d38d84955c1de3f6a7d56ce06b063f6) )
 	ROM_LOAD( "1616osv.045",  0x540000, 0x20000, CRC(b9f75432) SHA1(278964e2a02b1fe26ff34f09dc040e03c1d81a6d) )
 	ROM_LOAD( "1616ssdv.022", 0x560000, 0x08000, CRC(6d8e413a) SHA1(fc27d92c34f231345a387b06670f36f8c1705856) )
 ROM_END
