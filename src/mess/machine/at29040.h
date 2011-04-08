c192f24d1606946a9a21b8d2efe9ff8b57b28fa7
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-04-08 07:12:12 +0000

    Removed not used files, for at29040 there is rewritten implementation (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@11111 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/at29040.h
+++
@@ -1,7 +0,0 @@
-void at29c040a_init_data_ptr(int id, UINT8 *data_ptr);
-void at29c040a_init(running_machine &machine,int id);
-int at29c040a_file_load(int id, emu_file *file);
-int at29c040a_file_save(int id, emu_file *file);
-UINT8 at29c040a_r(int id, offs_t offset);
-void at29c040a_w(int id, offs_t offset, UINT8 data);
-int at29c040a_get_dirty_flag(int id);
--- src/mess/mess.mak
+++ src/mess/mess.mak
@@ -513,7 +513,6 @@ $(MESSOBJ)/shared.a: \
 	$(MESS_MACHINE)/74145.o		\
 	$(MESS_MACHINE)/8530scc.o	\
 	$(MESS_MACHINE)/adc080x.o	\
-	$(MESS_MACHINE)/at29040.o	\
 	$(MESS_MACHINE)/at45dbxx.o	\
 	$(MESS_MACHINE)/ay31015.o	\
 	$(MESS_MACHINE)/ctronics.o	\
