5290e1d4c638bc09bdb66fab3f86f16885d66660
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-04-20 09:05:03 +0000

    Fix for x68030 regression (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@11251 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/x68k.c
+++ src/mess/drivers/x68k.c
@@ -2640,8 +2640,8 @@ static MACHINE_START( x68030 )
 	x68k_state *state = machine.driver_data<x68k_state>();
 	/*  Install RAM handlers  */
 	state->m_spriteram = (UINT16*)machine.region("user1")->base();
-	space->install_legacy_read_handler(0x000000,0xbffffb,0xffffffff,0,FUNC(x68k_rom0_r));
-	space->install_legacy_write_handler(0x000000,0xbffffb,0xffffffff,0,FUNC(x68k_rom0_w));
+	space->install_legacy_read_handler(0x000000,0xbffffb,0xffffffff,0,FUNC(x68k_rom0_r),0xffffffff);
+	space->install_legacy_write_handler(0x000000,0xbffffb,0xffffffff,0,FUNC(x68k_rom0_w),0xffffffff);
 	space->install_readwrite_bank(0x000000,ram_get_size(machine.device(RAM_TAG))-1,0xffffffff,0,"bank1");
 	// mirror? Human68k 3.02 explicitly adds 0x3000000 to some pointers
 	space->install_readwrite_bank(0x3000000,0x3000000+ram_get_size(machine.device(RAM_TAG))-1,0xffffffff,0,"bank5");
