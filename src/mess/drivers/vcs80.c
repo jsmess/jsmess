10a4f4e8a8c6e759efe408be3385d98ad1e29abf
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2011-04-26 18:03:48 +0000

    [VCS80] Modernized driver. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@11340 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/vcs80.c
+++ src/mess/drivers/vcs80.c
@@ -8,50 +8,45 @@
 
 ****************************************************************************/
 
-#include "emu.h"
 #include "includes/vcs80.h"
-#include "cpu/z80/z80.h"
-#include "cpu/z80/z80daisy.h"
-#include "machine/z80pio.h"
-#include "machine/ram.h"
 #include "vcs80.lh"
 
 /* Read/Write Handlers */
 
-static READ8_DEVICE_HANDLER( vcs80_z80pio_r )
+READ8_MEMBER( vcs80_state::pio_r )
 {
 	switch (offset)
 	{
-	case 0: return z80pio_c_r(device, 1);
-	case 1: return z80pio_c_r(device, 0);
-	case 2: return z80pio_d_r(device, 1);
-	case 3: return z80pio_d_r(device, 0);
+	case 0: return z80pio_c_r(m_pio, 1);
+	case 1: return z80pio_c_r(m_pio, 0);
+	case 2: return z80pio_d_r(m_pio, 1);
+	case 3: return z80pio_d_r(m_pio, 0);
 	}
 
 	return 0;
 }
 
-static WRITE8_DEVICE_HANDLER( vcs80_z80pio_w )
+WRITE8_MEMBER( vcs80_state::pio_w )
 {
 	switch (offset)
 	{
-	case 0: z80pio_c_w(device, 1, data); break;
-	case 1: z80pio_c_w(device, 0, data); break;
-	case 2: z80pio_d_w(device, 1, data); break;
-	case 3: z80pio_d_w(device, 0, data); break;
+	case 0: z80pio_c_w(m_pio, 1, data); break;
+	case 1: z80pio_c_w(m_pio, 0, data); break;
+	case 2: z80pio_d_w(m_pio, 1, data); break;
+	case 3: z80pio_d_w(m_pio, 0, data); break;
 	}
 }
 
 /* Memory Maps */
 
-static ADDRESS_MAP_START( vcs80_mem, AS_PROGRAM, 8 )
+static ADDRESS_MAP_START( vcs80_mem, AS_PROGRAM, 8, vcs80_state )
 	AM_RANGE(0x0000, 0x01ff) AM_ROM
 	AM_RANGE(0x0400, 0x07ff) AM_RAM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( vcs80_io, AS_IO, 8 )
+static ADDRESS_MAP_START( vcs80_io, AS_IO, 8, vcs80_state )
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x04, 0x07) AM_DEVREADWRITE(Z80PIO_TAG, vcs80_z80pio_r, vcs80_z80pio_w)
+	AM_RANGE(0x04, 0x07) AM_READWRITE(pio_r, pio_w)
 ADDRESS_MAP_END
 
 /* Input Ports */
@@ -100,12 +95,12 @@ static TIMER_DEVICE_CALLBACK( vcs80_keyboard_tick )
 		state->m_keylatch &= 7;
 	}
 
-	z80pio_pa_w(state->m_z80pio, 0, state->m_keyclk << 7);
+	z80pio_pa_w(state->m_pio, 0, state->m_keyclk << 7);
 
 	state->m_keyclk = !state->m_keyclk;
 }
 
-static READ8_DEVICE_HANDLER( pio_port_a_r )
+READ8_MEMBER( vcs80_state::pio_pa_r )
 {
 	/*
 
@@ -122,25 +117,23 @@ static READ8_DEVICE_HANDLER( pio_port_a_r )
 
     */
 
-	vcs80_state *state = device->machine().driver_data<vcs80_state>();
-
 	UINT8 data = 0;
 
 	/* keyboard and led latch */
-	data |= state->m_keylatch;
+	data |= m_keylatch;
 
 	/* keyboard rows */
-	data |= BIT(input_port_read(device->machine(), "ROW0"), state->m_keylatch) << 4;
-	data |= BIT(input_port_read(device->machine(), "ROW1"), state->m_keylatch) << 5;
-	data |= BIT(input_port_read(device->machine(), "ROW2"), state->m_keylatch) << 6;
+	data |= BIT(input_port_read(machine(), "ROW0"), m_keylatch) << 4;
+	data |= BIT(input_port_read(machine(), "ROW1"), m_keylatch) << 5;
+	data |= BIT(input_port_read(machine(), "ROW2"), m_keylatch) << 6;
 
 	/* demultiplexer clock */
-	data |= (state->m_keyclk << 7);
+	data |= (m_keyclk << 7);
 
 	return data;
 }
 
-static WRITE8_DEVICE_HANDLER( pio_port_b_w )
+WRITE8_MEMBER( vcs80_state::pio_pb_w )
 {
 	/*
 
@@ -157,10 +150,8 @@ static WRITE8_DEVICE_HANDLER( pio_port_b_w )
 
     */
 
-	vcs80_state *state = device->machine().driver_data<vcs80_state>();
-
 	UINT8 led_data = BITSWAP8(data & 0x7f, 7, 5, 6, 4, 3, 2, 1, 0);
-	int digit = state->m_keylatch;
+	int digit = m_keylatch;
 
 	/* skip middle digit */
 	if (digit > 3) digit++;
@@ -171,11 +162,11 @@ static WRITE8_DEVICE_HANDLER( pio_port_b_w )
 static Z80PIO_INTERFACE( pio_intf )
 {
 	DEVCB_CPU_INPUT_LINE(Z80_TAG, INPUT_LINE_IRQ0),	/* callback when change interrupt status */
-	DEVCB_HANDLER(pio_port_a_r),	/* port A read callback */
+	DEVCB_DRIVER_MEMBER(vcs80_state, pio_pa_r),	/* port A read callback */
 	DEVCB_NULL,						/* port A write callback */
 	DEVCB_NULL,						/* portA ready active callback */
 	DEVCB_NULL,						/* port B read callback */
-	DEVCB_HANDLER(pio_port_b_w),	/* port B write callback */
+	DEVCB_DRIVER_MEMBER(vcs80_state, pio_pb_w),	/* port B write callback */
 	DEVCB_NULL						/* portB ready active callback */
 };
 
@@ -189,32 +180,24 @@ static const z80_daisy_config vcs80_daisy_chain[] =
 
 /* Machine Initialization */
 
-static MACHINE_START(vcs80)
+void vcs80_state::machine_start()
 {
-	vcs80_state *state = machine.driver_data<vcs80_state>();
-
-	/* find devices */
-	state->m_z80pio = machine.device(Z80PIO_TAG);
-
-	z80pio_astb_w(state->m_z80pio, 1);
-	z80pio_bstb_w(state->m_z80pio, 1);
+	z80pio_astb_w(m_pio, 1);
+	z80pio_bstb_w(m_pio, 1);
 
 	/* register for state saving */
-	state->save_item(NAME(state->m_keylatch));
-	state->save_item(NAME(state->m_keyclk));
+	save_item(NAME(m_keylatch));
+	save_item(NAME(m_keyclk));
 }
 
 /* Machine Driver */
 
 static MACHINE_CONFIG_START( vcs80, vcs80_state )
-
 	/* basic machine hardware */
     MCFG_CPU_ADD(Z80_TAG, Z80, XTAL_5MHz/2) /* U880D */
     MCFG_CPU_PROGRAM_MAP(vcs80_mem)
     MCFG_CPU_IO_MAP(vcs80_io)
 	MCFG_CPU_CONFIG(vcs80_daisy_chain)
-
-    MCFG_MACHINE_START(vcs80)
 
 	/* keyboard timer */
 	MCFG_TIMER_ADD_PERIODIC("keyboard", vcs80_keyboard_tick, attotime::from_hz(1000))
@@ -244,7 +227,7 @@ DIRECT_UPDATE_HANDLER( vcs80_direct_update_handler )
 	vcs80_state *state = machine.driver_data<vcs80_state>();
 
 	/* _A0 is connected to PIO PB7 */
-	z80pio_pb_w(state->m_z80pio, 0, (!BIT(address, 0)) << 7);
+	z80pio_pb_w(state->m_pio, 0, (!BIT(address, 0)) << 7);
 
 	return address;
 }
--- src/mess/includes/vcs80.h
+++ src/mess/includes/vcs80.h
@@ -1,5 +1,15 @@
+#pragma once
+
 #ifndef __VCS80__
 #define __VCS80__
+
+#define ADDRESS_MAP_MODERN
+
+#include "emu.h"
+#include "cpu/z80/z80.h"
+#include "cpu/z80/z80daisy.h"
+#include "machine/z80pio.h"
+#include "machine/ram.h"
 
 #define SCREEN_TAG		"screen"
 #define Z80_TAG			"z80"
@@ -9,14 +19,22 @@ class vcs80_state : public driver_device
 {
 public:
 	vcs80_state(running_machine &machine, const driver_device_config_base &config)
-		: driver_device(machine, config) { }
+		: driver_device(machine, config),
+		  m_pio(*this, Z80PIO_TAG)
+	{ }
+
+	required_device<z80pio_device> m_pio;
+
+	virtual void machine_start();
+
+	DECLARE_READ8_MEMBER( pio_r );
+	DECLARE_WRITE8_MEMBER( pio_w );
+	DECLARE_READ8_MEMBER( pio_pa_r );
+	DECLARE_WRITE8_MEMBER( pio_pb_w );
 
 	/* keyboard state */
 	int m_keylatch;
 	int m_keyclk;
-
-	/* devices */
-	device_t *m_z80pio;
 };
 
 #endif
