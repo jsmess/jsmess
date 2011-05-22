035c269fe460a9fb1bfcd56a49f5fd73ee53e8d2
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-05-22 08:52:50 +0000

    [ACRNSYS1] Modernised.
    git-svn-id: svn://messdev.no-ip.org/mess@11616 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/acrnsys1.c
+++ src/mess/drivers/acrnsys1.c
@@ -7,6 +7,7 @@
  Driver by Dirk Best
 
 ******************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/m6502/m6502.h"
@@ -19,8 +20,16 @@ class acrnsys1_state : public driver_device
 {
 public:
 	acrnsys1_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+	m_maincpu(*this, "maincpu"),
+	m_ttl74145(*this, "ic8_7445")
+	{ }
 
+	required_device<cpu_device> m_maincpu;
+	required_device<device_t> m_ttl74145;
+	DECLARE_READ8_MEMBER(ins8154_b1_port_a_r);
+	DECLARE_WRITE8_MEMBER(ins8154_b1_port_a_w);
+	DECLARE_WRITE8_MEMBER(acrnsys1_led_segment_w);
 };
 
 
@@ -29,30 +38,29 @@ public:
     KEYBOARD HANDLING
 ***************************************************************************/
 
-static READ8_DEVICE_HANDLER( ins8154_b1_port_a_r )
+READ8_MEMBER( acrnsys1_state::ins8154_b1_port_a_r )
 {
-	device_t *ttl74145 = device->machine().device("ic8_7445");
-	UINT8 key_line = ttl74145_r(ttl74145, 0, 0);
+	UINT8 key_line = ttl74145_r(m_ttl74145, 0, 0);
 
 	switch (key_line)
 	{
-	case 1 << 0: return input_port_read(device->machine(), "keyboard_0");
-	case 1 << 1: return input_port_read(device->machine(), "keyboard_1");
-	case 1 << 2: return input_port_read(device->machine(), "keyboard_2");
-	case 1 << 3: return input_port_read(device->machine(), "keyboard_3");
-	case 1 << 4: return input_port_read(device->machine(), "keyboard_4");
-	case 1 << 5: return input_port_read(device->machine(), "keyboard_5");
-	case 1 << 6: return input_port_read(device->machine(), "keyboard_6");
-	case 1 << 7: return input_port_read(device->machine(), "keyboard_7");
+	case 1 << 0: return input_port_read(machine(), "X0");
+	case 1 << 1: return input_port_read(machine(), "X1");
+	case 1 << 2: return input_port_read(machine(), "X2");
+	case 1 << 3: return input_port_read(machine(), "X3");
+	case 1 << 4: return input_port_read(machine(), "X4");
+	case 1 << 5: return input_port_read(machine(), "X5");
+	case 1 << 6: return input_port_read(machine(), "X6");
+	case 1 << 7: return input_port_read(machine(), "X7");
 	}
 
 	/* should never reach this */
 	return 0xff;
 }
 
-static WRITE8_DEVICE_HANDLER( ins8154_b1_port_a_w )
+WRITE8_MEMBER( acrnsys1_state::ins8154_b1_port_a_w )
 {
-	ttl74145_w(device->machine().device("ic8_7445"), 0, data & 0x07);
+	ttl74145_w(m_ttl74145, 0, data & 0x07);
 }
 
 
@@ -60,10 +68,9 @@ static WRITE8_DEVICE_HANDLER( ins8154_b1_port_a_w )
     LED DISPLAY
 ***************************************************************************/
 
-static WRITE8_DEVICE_HANDLER( acrnsys1_led_segment_w )
+WRITE8_MEMBER( acrnsys1_state::acrnsys1_led_segment_w )
 {
-	device_t *ttl74145 = device->machine().device("ic8_7445");
-	UINT8 key_line = ttl74145_r(ttl74145, 0, 0);
+	UINT8 key_line = ttl74145_r(m_ttl74145, 0, 0);
 
 	output_set_digit_value(key_line, data);
 }
@@ -73,9 +80,9 @@ static WRITE8_DEVICE_HANDLER( acrnsys1_led_segment_w )
     ADDRESS MAPS
 ***************************************************************************/
 
-static ADDRESS_MAP_START( acrnsys1_map, AS_PROGRAM, 8 )
+static ADDRESS_MAP_START( acrnsys1_map, AS_PROGRAM, 8, acrnsys1_state )
 	AM_RANGE(0x0000, 0x03ff) AM_RAM
-	AM_RANGE(0x0e00, 0x0e7f) AM_MIRROR(0x100) AM_DEVREADWRITE("b1", ins8154_r, ins8154_w)
+	AM_RANGE(0x0e00, 0x0e7f) AM_MIRROR(0x100) AM_DEVREADWRITE_LEGACY("b1", ins8154_r, ins8154_w)
 	AM_RANGE(0x0e80, 0x0eff) AM_MIRROR(0x100) AM_RAM
 	AM_RANGE(0xf800, 0xffff) AM_ROM
 ADDRESS_MAP_END
@@ -86,49 +93,49 @@ ADDRESS_MAP_END
 ***************************************************************************/
 
 static INPUT_PORTS_START( acrnsys1 )
-	PORT_START("keyboard_0")
+	PORT_START("X0")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("8") PORT_CODE(KEYCODE_8) PORT_CHAR('8')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("M") PORT_CODE(KEYCODE_M) PORT_CHAR('M')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("0") PORT_CODE(KEYCODE_0) PORT_CHAR('0')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_1")
+	PORT_START("X1")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("9") PORT_CODE(KEYCODE_9) PORT_CHAR('9')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("G") PORT_CODE(KEYCODE_G) PORT_CHAR('G')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("1") PORT_CODE(KEYCODE_1) PORT_CHAR('1')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_2")
+	PORT_START("X2")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("A") PORT_CODE(KEYCODE_A) PORT_CHAR('A')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("P") PORT_CODE(KEYCODE_P) PORT_CHAR('P')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("2") PORT_CODE(KEYCODE_2) PORT_CHAR('2')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_3")
+	PORT_START("X3")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("B") PORT_CODE(KEYCODE_B) PORT_CHAR('B')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("S") PORT_CODE(KEYCODE_S) PORT_CHAR('S')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("3") PORT_CODE(KEYCODE_3) PORT_CHAR('3')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_4")
+	PORT_START("X4")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C") PORT_CODE(KEYCODE_C) PORT_CHAR('C')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("L") PORT_CODE(KEYCODE_L) PORT_CHAR('L')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("4") PORT_CODE(KEYCODE_4) PORT_CHAR('4')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_5")
+	PORT_START("X5")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("D") PORT_CODE(KEYCODE_D) PORT_CHAR('D')
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_C) PORT_CHAR('R')
+	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_R) PORT_CHAR('R')
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("5") PORT_CODE(KEYCODE_5) PORT_CHAR('5')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_6")
+	PORT_START("X6")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("E") PORT_CODE(KEYCODE_E) PORT_CHAR('E')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME(UTF8_UP) PORT_CODE(KEYCODE_UP)   PORT_CHAR(UCHAR_MAMEKEY(UP))
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("6") PORT_CODE(KEYCODE_6) PORT_CHAR('6')
 	PORT_BIT(0xc7, IP_ACTIVE_LOW, IPT_UNUSED)
 
-	PORT_START("keyboard_7")
+	PORT_START("X7")
 	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("F") PORT_CODE(KEYCODE_F) PORT_CHAR('F')
 	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME(UTF8_DOWN) PORT_CODE(KEYCODE_DOWN) PORT_CHAR(UCHAR_MAMEKEY(DOWN))
 	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("7") PORT_CODE(KEYCODE_7) PORT_CHAR('7')
@@ -139,7 +146,7 @@ static INPUT_PORTS_START( acrnsys1 )
 	PORT_BIT(0xfe, IP_ACTIVE_LOW, IPT_UNUSED)
 
 	PORT_START("switch")
-	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Switch") PORT_CODE(KEYCODE_F3) PORT_CHAR(UCHAR_MAMEKEY(F1))
+	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Switch") PORT_CODE(KEYCODE_F1) PORT_CHAR(UCHAR_MAMEKEY(F1))
 	PORT_BIT(0xfe, IP_ACTIVE_LOW, IPT_UNUSED)
 
 	PORT_START("config")
@@ -156,10 +163,10 @@ INPUT_PORTS_END
 
 static const ins8154_interface ins8154_b1 =
 {
-	DEVCB_HANDLER(ins8154_b1_port_a_r),
-	DEVCB_HANDLER(ins8154_b1_port_a_w),
+	DEVCB_DRIVER_MEMBER(acrnsys1_state, ins8154_b1_port_a_r),
+	DEVCB_DRIVER_MEMBER(acrnsys1_state, ins8154_b1_port_a_w),
 	DEVCB_NULL,
-	DEVCB_HANDLER(acrnsys1_led_segment_w),
+	DEVCB_DRIVER_MEMBER(acrnsys1_state, acrnsys1_led_segment_w),
 	DEVCB_NULL
 };
 
