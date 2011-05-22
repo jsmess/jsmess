b35cb4a8c7e7d5c7a4e75a408772e53997ec8d20
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-05-22 12:02:50 +0000

    [TEC1] added JMON (not working)
    git-svn-id: svn://messdev.no-ip.org/mess@11620 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/tec1.c
+++ src/mess/drivers/tec1.c
@@ -47,9 +47,12 @@ ToDo:
 - The 10ms debounce is not emulated.
 - Needs proper artwork.
 
-Note (2011-04-10)
-I've enabled the tec1b rom at the request of a user, but it doesn't seem
-to do anything useful. He will investigate..
+
+JMON ToDo:
+- Add cassette circuitry
+- Fix keyboard
+- Add LCD display (2 rows by 16 characters)
+
 
 ***************************************************************************/
 #define ADDRESS_MAP_MODERN
@@ -242,6 +245,24 @@ static ADDRESS_MAP_START( tec1_io, AS_IO, 8, tec1_state )
 ADDRESS_MAP_END
 
 
+static ADDRESS_MAP_START( tecjmon_map, AS_PROGRAM, 8, tec1_state )
+	ADDRESS_MAP_GLOBAL_MASK(0x3fff)
+	AM_RANGE(0x0000, 0x07ff) AM_ROM
+	AM_RANGE(0x0800, 0x37ff) AM_RAM
+	AM_RANGE(0x3800, 0x3fff) AM_ROM
+ADDRESS_MAP_END
+
+static ADDRESS_MAP_START( tecjmon_io, AS_IO, 8, tec1_state )
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x00, 0x00) AM_READ(tec1_kbd_r)
+	AM_RANGE(0x01, 0x01) AM_WRITE(tec1_digit_w)
+	AM_RANGE(0x02, 0x02) AM_WRITE(tec1_segment_w)
+	//AM_RANGE(0x03, 0x03) AM_READ(latch_r)
+	//AM_RANGE(0x04, 0x04) AM_WRITE(lcd_en_w)
+	//AM_RANGE(0x84, 0x84) AM_WRITE(lcd_2nd_w)
+ADDRESS_MAP_END
+
+
 /**************************************************************************
 
     Keyboard Layout
@@ -305,6 +326,21 @@ static MACHINE_CONFIG_START( tec1, tec1_state )
 	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.50)
 MACHINE_CONFIG_END
 
+static MACHINE_CONFIG_START( tecjmon, tec1_state )
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu", Z80, XTAL_3_579545MHz / 2)
+	MCFG_CPU_PROGRAM_MAP(tecjmon_map)
+	MCFG_CPU_IO_MAP(tecjmon_io)
+
+	/* video hardware */
+	MCFG_DEFAULT_LAYOUT(layout_tec1)
+
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_MONO("mono")
+	MCFG_SOUND_ADD("speaker", SPEAKER_SOUND, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.50)
+MACHINE_CONFIG_END
+
 
 /***************************************************************************
 
@@ -322,5 +358,12 @@ ROM_START(tec1)
 	ROMX_LOAD("mon2.rom",   0x0000, 0x0800, CRC(082fd7e7) SHA1(7659add30ca22b15a03d1cbac0892a5c25e47ecd), ROM_BIOS(3))
 ROM_END
 
+ROM_START(tecjmon)
+	ROM_REGION(0x10000, "maincpu", ROMREGION_ERASEFF)
+	ROM_LOAD("jmon.rom",    0x0000, 0x0800, CRC(202c47a2) SHA1(701588ec5640d633d90d94b2ccd6f65422e19a70) )
+	ROM_LOAD("util.rom",    0x3800, 0x0800, CRC(7c19700d) SHA1(dc5b3ade66bb11c54430056966ed99cdd299d82b) )
+ROM_END
+
 /*    YEAR  NAME      PARENT  COMPAT  MACHINE     INPUT    INIT       COMPANY  FULLNAME */
 COMP( 1984, tec1,     0,      0,      tec1,       tec1,    0,		"Talking Electronics magazine",  "TEC-1" , 0 )
+COMP( 1984, tecjmon,  tec1,   0,      tecjmon,    tec1,    0,		"Talking Electronics magazine",  "TEC-1A with JMON" , GAME_NOT_WORKING )
