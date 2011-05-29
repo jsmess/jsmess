cc30e438a942534cdd6723b347bd420c37db4ae7
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-05-29 05:02:50 +0000

    MESSUI - Screen property sheet - fixed Number of Screens slider.
    git-svn-id: svn://messdev.no-ip.org/mess@11696 9849079f-6229-0410-a07f-84406ce305ba

--- src/osd/winui/properties.c
+++ src/osd/winui/properties.c
@@ -2521,6 +2521,7 @@ static void BuildDataMap(void)
 	datamap_set_trackbar_range(properties_datamap, IDC_AUDIO_LATENCY, 1,  5, 1);
 	datamap_set_trackbar_range(properties_datamap, IDC_VOLUME,      -32,  0, 1);
 	datamap_set_trackbar_range(properties_datamap, IDC_SECONDSTORUN, 0,  60, 1);
+	datamap_set_trackbar_range(properties_datamap, IDC_NUMSCREENS, 1,  4, 1);
 	datamap_set_trackbar_range(properties_datamap, IDC_PRESCALE, 1,  10, 1);
 	datamap_set_trackbar_range(properties_datamap, IDC_GAMMA, 0.0,  2.0, 0.1);
 	datamap_set_trackbar_range(properties_datamap, IDC_BRIGHTCORRECT, 0.00,  2.0, (float)0.1);
