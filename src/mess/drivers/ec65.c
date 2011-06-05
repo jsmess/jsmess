02753bf29e2476577fb050322f1668ab7f5b2d7a
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-05 09:07:12 +0000

    [EC65] Modernised.
    git-svn-id: svn://messdev.no-ip.org/mess@11814 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/ec65.c
+++ src/mess/drivers/ec65.c
@@ -5,6 +5,7 @@
         16/07/2009 Initial driver.
 
 ****************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/m6502/m6502.h"
@@ -28,26 +29,32 @@ public:
 	ec65_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag) { }
 
-	UINT8 *m_video_ram;
+	DECLARE_READ8_MEMBER(ec65_via_read_a);
+	DECLARE_READ8_MEMBER(ec65_read_ca1 );
+	DECLARE_READ8_MEMBER(ec65_via_read_b);
+	DECLARE_WRITE8_MEMBER(ec65_via_write_a);
+	DECLARE_WRITE8_MEMBER(ec65_via_write_b);
+	UINT8 *m_p_chargen;
+	UINT8 *m_p_videoram;
 };
 
-static ADDRESS_MAP_START(ec65_mem, AS_PROGRAM, 8)
+static ADDRESS_MAP_START(ec65_mem, AS_PROGRAM, 8, ec65_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0xdfff) AM_RAM
-	AM_RANGE(0xe000, 0xe003) AM_DEVREADWRITE_MODERN(PIA6821_TAG, pia6821_device, read, write)
-	AM_RANGE(0xe010, 0xe010) AM_DEVREADWRITE_MODERN(ACIA6850_TAG, acia6850_device, status_read, control_write)
-	AM_RANGE(0xe011, 0xe011) AM_DEVREADWRITE_MODERN(ACIA6850_TAG, acia6850_device, data_read, data_write)
-	AM_RANGE(0xe100, 0xe10f) AM_DEVREADWRITE_MODERN(VIA6522_0_TAG, via6522_device, read, write)
-	AM_RANGE(0xe110, 0xe11f) AM_DEVREADWRITE_MODERN(VIA6522_1_TAG, via6522_device, read, write)
-	AM_RANGE(0xe130, 0xe133) AM_DEVREADWRITE(ACIA6551_TAG,  acia_6551_r, acia_6551_w )
-	AM_RANGE(0xe140, 0xe140) AM_DEVWRITE_MODERN(MC6845_TAG, mc6845_device, address_w)
-	AM_RANGE(0xe141, 0xe141) AM_DEVREADWRITE_MODERN(MC6845_TAG, mc6845_device, register_r , register_w)
+	AM_RANGE(0xe000, 0xe003) AM_DEVREADWRITE(PIA6821_TAG, pia6821_device, read, write)
+	AM_RANGE(0xe010, 0xe010) AM_DEVREADWRITE(ACIA6850_TAG, acia6850_device, status_read, control_write)
+	AM_RANGE(0xe011, 0xe011) AM_DEVREADWRITE(ACIA6850_TAG, acia6850_device, data_read, data_write)
+	AM_RANGE(0xe100, 0xe10f) AM_DEVREADWRITE(VIA6522_0_TAG, via6522_device, read, write)
+	AM_RANGE(0xe110, 0xe11f) AM_DEVREADWRITE(VIA6522_1_TAG, via6522_device, read, write)
+	AM_RANGE(0xe130, 0xe133) AM_DEVREADWRITE_LEGACY(ACIA6551_TAG,  acia_6551_r, acia_6551_w )
+	AM_RANGE(0xe140, 0xe140) AM_DEVWRITE(MC6845_TAG, mc6845_device, address_w)
+	AM_RANGE(0xe141, 0xe141) AM_DEVREADWRITE(MC6845_TAG, mc6845_device, register_r , register_w)
 	AM_RANGE(0xe400, 0xe7ff) AM_RAM // 1KB on-board RAM
-	AM_RANGE(0xe800, 0xefff) AM_RAM AM_BASE_MEMBER(ec65_state,m_video_ram)
+	AM_RANGE(0xe800, 0xefff) AM_RAM AM_BASE(m_p_videoram)
 	AM_RANGE(0xf000, 0xffff) AM_ROM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START(ec65k_mem, AS_PROGRAM, 8)
+static ADDRESS_MAP_START(ec65k_mem, AS_PROGRAM, 8, ec65_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0xefff) AM_RAM
 	AM_RANGE(0xf000, 0xffff) AM_ROM
@@ -81,35 +88,35 @@ static ACIA6850_INTERFACE( ec65_acia_intf )
 	DEVCB_NULL
 };
 
-static READ8_DEVICE_HANDLER(ec65_via_read_a)
+READ8_MEMBER( ec65_state::ec65_via_read_a)
 {
 	return 0;
 }
-static READ8_DEVICE_HANDLER( ec65_read_ca1 )
+READ8_MEMBER( ec65_state::ec65_read_ca1 )
 {
 	return 0;
 }
-static READ8_DEVICE_HANDLER(ec65_via_read_b)
+READ8_MEMBER( ec65_state::ec65_via_read_b)
 {
 	return 0xff;
 }
-static WRITE8_DEVICE_HANDLER( ec65_via_write_a )
+WRITE8_MEMBER( ec65_state::ec65_via_write_a )
 {
 }
-static WRITE8_DEVICE_HANDLER( ec65_via_write_b )
+WRITE8_MEMBER( ec65_state::ec65_via_write_b )
 {
 }
 
 static const via6522_interface ec65_via_1_intf=
 {
 	DEVCB_NULL,   /* in_a_func */
-	DEVCB_HANDLER(ec65_via_read_b),   /* in_b_func */
-	DEVCB_NULL,     /* in_ca1_func */
+	DEVCB_DRIVER_MEMBER(ec65_state, ec65_via_read_b),   /* in_b_func */
+	DEVCB_NULL,                       /* in_ca1_func */
 	DEVCB_NULL,                       /* in_cb1_func */
 	DEVCB_NULL,                       /* in_ca2_func */
 	DEVCB_NULL,                       /* in_cb2_func */
-	DEVCB_HANDLER(ec65_via_write_a),  /* out_a_func */
-	DEVCB_HANDLER(ec65_via_write_b),  /* out_b_func */
+	DEVCB_DRIVER_MEMBER(ec65_state, ec65_via_write_a),  /* out_a_func */
+	DEVCB_DRIVER_MEMBER(ec65_state, ec65_via_write_b),  /* out_b_func */
 	DEVCB_NULL,                       /* out_ca1_func */
 	DEVCB_NULL,                       /* out_cb1_func */
 	DEVCB_NULL,                       /* out_ca2_func */
@@ -119,9 +126,9 @@ static const via6522_interface ec65_via_1_intf=
 
 static const via6522_interface ec65_via_0_intf=
 {
-	DEVCB_HANDLER(ec65_via_read_a),      /* in_a_func */
+	DEVCB_DRIVER_MEMBER(ec65_state, ec65_via_read_a),      /* in_a_func */
 	DEVCB_NULL,      /* in_b_func */
-	DEVCB_HANDLER(ec65_read_ca1),      /* in_ca1_func */
+	DEVCB_DRIVER_MEMBER(ec65_state, ec65_read_ca1),      /* in_ca1_func */
 	DEVCB_NULL,      /* in_cb1_func */
 	DEVCB_NULL,      /* in_ca2_func */
 	DEVCB_NULL,      /* in_cb2_func */
@@ -146,6 +153,8 @@ static MACHINE_RESET(ec65)
 
 static VIDEO_START( ec65 )
 {
+	ec65_state *state = machine.driver_data<ec65_state>();
+	state->m_p_chargen = machine.region("chargen")->base();
 }
 
 static SCREEN_UPDATE( ec65 )
@@ -158,30 +167,31 @@ static SCREEN_UPDATE( ec65 )
 static MC6845_UPDATE_ROW( ec65_update_row )
 {
 	ec65_state *state = device->machine().driver_data<ec65_state>();
-	UINT8 *charrom = device->machine().region("chargen")->base();
-	int column, bit;
+	UINT8 chr,gfx,inv;
+	UINT16 mem,x;
+	UINT16 *p = BITMAP_ADDR16(bitmap, y, 0);
 
-	for (column = 0; column < x_count; column++)
+	for (x = 0; x < x_count; x++)
 	{
-		UINT8 code = state->m_video_ram[ma + column];
-		UINT16 addr = code << 4 | (ra & 0x0f);
-		UINT8 data = charrom[(addr & 0xfff)];
-		if (column == cursor_x)
-		{
-			data = 0xff;
-		}
+		inv = (x == cursor_x) ? 0xff : 0;
+		mem = (ma + x) & 0x7ff;
+		chr = state->m_p_videoram[mem];
 
-		for (bit = 0; bit < 8; bit++)
-		{
-			int x = (column * 8) + bit;
-			int color = BIT(data, 7) ? 1 : 0;
+		/* get pattern of pixels for that character scanline */
+		gfx = state->m_p_chargen[(chr<<4) | (ra & 0x0f)] ^ inv;
 
-			*BITMAP_ADDR16(bitmap, y, x) = color;
-
-			data <<= 1;
-		}
+		/* Display a scanline of a character */
+		*p++ = BIT(gfx, 7);
+		*p++ = BIT(gfx, 6);
+		*p++ = BIT(gfx, 5);
+		*p++ = BIT(gfx, 4);
+		*p++ = BIT(gfx, 3);
+		*p++ = BIT(gfx, 2);
+		*p++ = BIT(gfx, 1);
+		*p++ = BIT(gfx, 0);
 	}
 }
+
 static const mc6845_interface ec65_crtc6845_interface =
 {
 	"screen",
@@ -217,20 +227,20 @@ GFXDECODE_END
 
 static MACHINE_CONFIG_START( ec65, ec65_state )
 
-    /* basic machine hardware */
-    MCFG_CPU_ADD("maincpu",M6502, XTAL_4MHz / 4)
-    MCFG_CPU_PROGRAM_MAP(ec65_mem)
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu",M6502, XTAL_4MHz / 4)
+	MCFG_CPU_PROGRAM_MAP(ec65_mem)
 
-    MCFG_MACHINE_RESET(ec65)
+	MCFG_MACHINE_RESET(ec65)
 
-    /* video hardware */
+	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(60)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
 	MCFG_SCREEN_SIZE(640, 200)
 	MCFG_SCREEN_VISIBLE_AREA(0, 640 - 1, 0, 200 - 1)
-    MCFG_SCREEN_UPDATE(ec65)
+	MCFG_SCREEN_UPDATE(ec65)
 
 	MCFG_GFXDECODE(ec65)
 	MCFG_PALETTE_LENGTH(2)
@@ -238,9 +248,9 @@ static MACHINE_CONFIG_START( ec65, ec65_state )
 
 	MCFG_MC6845_ADD(MC6845_TAG, MC6845, XTAL_16MHz / 8, ec65_crtc6845_interface)
 
-    MCFG_VIDEO_START(ec65)
+	MCFG_VIDEO_START(ec65)
 
-    /* devices */
+	/* devices */
 	MCFG_PIA6821_ADD( PIA6821_TAG, ec65_pia_interface )
 	MCFG_ACIA6850_ADD(ACIA6850_TAG, ec65_acia_intf)
 	MCFG_VIA6522_ADD(VIA6522_0_TAG, XTAL_4MHz / 4, ec65_via_0_intf)
@@ -250,20 +260,20 @@ MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_START( ec65k, ec65_state )
 
-    /* basic machine hardware */
-    MCFG_CPU_ADD("maincpu",G65816, XTAL_4MHz) // can use 4,2 or 1 MHz
-    MCFG_CPU_PROGRAM_MAP(ec65k_mem)
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu",G65816, XTAL_4MHz) // can use 4,2 or 1 MHz
+	MCFG_CPU_PROGRAM_MAP(ec65k_mem)
 
-    MCFG_MACHINE_RESET(ec65)
+	MCFG_MACHINE_RESET(ec65)
 
-    /* video hardware */
+	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(60)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
 	MCFG_SCREEN_SIZE(640, 200)
 	MCFG_SCREEN_VISIBLE_AREA(0, 640 - 1, 0, 200 - 1)
-    MCFG_SCREEN_UPDATE(ec65)
+	MCFG_SCREEN_UPDATE(ec65)
 
 	MCFG_GFXDECODE(ec65)
 	MCFG_PALETTE_LENGTH(2)
@@ -271,13 +281,14 @@ static MACHINE_CONFIG_START( ec65k, ec65_state )
 
 	MCFG_MC6845_ADD(MC6845_TAG, MC6845, XTAL_16MHz / 8, ec65_crtc6845_interface)
 
-    MCFG_VIDEO_START(ec65)
+	MCFG_VIDEO_START(ec65)
 MACHINE_CONFIG_END
 
 /* ROM definition */
 ROM_START( ec65 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD( "ec65.ic6", 0xf000, 0x1000, CRC(acd928ed) SHA1(e02a688a057ff77294717cf7b887425fed0b1153))
+
 	ROM_REGION( 0x1000, "chargen", 0 )
 	ROM_LOAD( "chargen.ic19", 0x0000, 0x1000, CRC(9b56a28d) SHA1(41c04fd9fb542c50287bc0e366358a61fc4b0cd4))	// Located on VDU card
 ROM_END
@@ -285,12 +296,13 @@ ROM_END
 ROM_START( ec65k )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	ROM_LOAD( "ec65k.ic19",  0xf000, 0x1000, CRC(5e5a890a) SHA1(daa006f2179fd156833e11c73b37881cafe5dede))
+
 	ROM_REGION( 0x1000, "chargen", 0 )
 	ROM_LOAD( "chargen.ic19", 0x0000, 0x1000, CRC(9b56a28d) SHA1(41c04fd9fb542c50287bc0e366358a61fc4b0cd4))	// Located on VDU card
 ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 1985, ec65,  0,       0,		ec65,	ec65,	 0, 		 "Elektor Electronics",   "EC-65",		GAME_NOT_WORKING | GAME_NO_SOUND)
-COMP( 1985, ec65k, ec65,    0,		ec65k,	ec65,	 0, 		 "Elektor Electronics",   "EC-65K",		GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1985, ec65,   0,       0,      ec65,      ec65,    0,    "Elektor Electronics", "EC-65", GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1985, ec65k,  ec65,    0,      ec65k,     ec65,    0,    "Elektor Electronics", "EC-65K", GAME_NOT_WORKING | GAME_NO_SOUND)
 
