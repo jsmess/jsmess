bee0ad74757209373d2f8a492c7ecd5011b4c4c8
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-05 07:36:26 +0000

    [DMS86] - connected terminal, you can type into it. Added a list of monitor commands.
    git-svn-id: svn://messdev.no-ip.org/mess@11811 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/dms86.c
+++ src/mess/drivers/dms86.c
@@ -4,29 +4,89 @@
 
         11/01/2010 Skeleton driver.
 
+Monitor commands:
+A
+B boot to HiNet
+D dump memory to screen
+I in port
+L memory test
+O out port (e.g. O 84 77)
+P
+S write a byte to memory
+T
+X display registers
+
+
+Note that bit 3 of port 82 is tested at boot. If low, the computer bypasses
+the monitor and goes straight to "Joining HiNet".
+
 ****************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/i86/i86.h"
+#include "machine/terminal.h"
 
 
 class dms86_state : public driver_device
 {
 public:
 	dms86_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+	m_maincpu(*this, "maincpu"),
+	m_terminal(*this, TERMINAL_TAG)
+	{ }
 
+	required_device<cpu_device> m_maincpu;
+	required_device<device_t> m_terminal;
+	DECLARE_READ16_MEMBER( dms86_82_r );
+	DECLARE_READ16_MEMBER( dms86_84_r );
+	DECLARE_READ16_MEMBER( dms86_86_r );
+	DECLARE_READ16_MEMBER( dms86_9a_r );
+	DECLARE_WRITE8_MEMBER( kbd_put );
+	UINT8 *m_ram;
+	UINT8 m_term_data;
 };
 
+READ16_MEMBER( dms86_state::dms86_82_r )
+{
+// HiNet / Monitor switch
 
-static ADDRESS_MAP_START(dms86_mem, AS_PROGRAM, 16)
+	return 0xff;
+}
+
+READ16_MEMBER( dms86_state::dms86_84_r )
+{
+	UINT8 ret = m_term_data;
+	m_term_data = 0;
+	return ret;
+}
+
+READ16_MEMBER( dms86_state::dms86_86_r )
+{
+	return 4 | (m_term_data ? 1 : 0);
+}
+
+READ16_MEMBER( dms86_state::dms86_9a_r )
+{
+	return 0;
+}
+
+
+static ADDRESS_MAP_START(dms86_mem, AS_PROGRAM, 16, dms86_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x00000, 0x1ffff) AM_RAM
 	AM_RANGE(0xfe000, 0xfffff) AM_ROM AM_REGION("user1",0)
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( dms86_io , AS_IO, 16)
+static ADDRESS_MAP_START(dms86_io, AS_IO, 16, dms86_state)
 	ADDRESS_MAP_UNMAP_HIGH
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x82, 0x83) AM_READ(dms86_82_r)
+	AM_RANGE(0x84, 0x85) AM_READ(dms86_84_r) AM_DEVWRITE8_LEGACY(TERMINAL_TAG, terminal_write, 0xff)
+	AM_RANGE(0x86, 0x87) AM_READ(dms86_86_r)
+	AM_RANGE(0x9A, 0x9B) AM_READ(dms86_9a_r)
+	AM_RANGE(0x9c, 0x9d) AM_DEVWRITE8_LEGACY(TERMINAL_TAG, terminal_write, 0xff)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -38,36 +98,27 @@ static MACHINE_RESET(dms86)
 {
 }
 
-static VIDEO_START( dms86 )
+WRITE8_MEMBER( dms86_state::kbd_put )
 {
+	m_term_data = data;
 }
 
-static SCREEN_UPDATE( dms86 )
+static GENERIC_TERMINAL_INTERFACE( terminal_intf )
 {
-    return 0;
-}
+	DEVCB_DRIVER_MEMBER(dms86_state, kbd_put)
+};
+
 
 static MACHINE_CONFIG_START( dms86, dms86_state )
-    /* basic machine hardware */
-    MCFG_CPU_ADD("maincpu",I8086, XTAL_9_8304MHz)
-    MCFG_CPU_PROGRAM_MAP(dms86_mem)
-    MCFG_CPU_IO_MAP(dms86_io)
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu",I8086, XTAL_9_8304MHz)
+	MCFG_CPU_PROGRAM_MAP(dms86_mem)
+	MCFG_CPU_IO_MAP(dms86_io)
 
-    MCFG_MACHINE_RESET(dms86)
+	MCFG_MACHINE_RESET(dms86)
 
-    /* video hardware */
-    MCFG_SCREEN_ADD("screen", RASTER)
-    MCFG_SCREEN_REFRESH_RATE(50)
-    MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-    MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-    MCFG_SCREEN_SIZE(640, 480)
-    MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-    MCFG_SCREEN_UPDATE(dms86)
-
-    MCFG_PALETTE_LENGTH(2)
-    MCFG_PALETTE_INIT(black_and_white)
-
-    MCFG_VIDEO_START(dms86)
+	MCFG_FRAGMENT_ADD( generic_terminal )
+	MCFG_GENERIC_TERMINAL_ADD(TERMINAL_TAG, terminal_intf)
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -79,6 +130,6 @@ ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT     COMPANY   FULLNAME       FLAGS */
-COMP( 1982, dms86,  0,       0, 	dms86,	dms86,	 0, 	 "Digital Microsystems",   "DMS-86",		GAME_NOT_WORKING | GAME_NO_SOUND)
+/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT     COMPANY              FULLNAME       FLAGS */
+COMP( 1982, dms86,  0,       0,      dms86,     dms86,    0,   "Digital Microsystems", "DMS-86", GAME_NOT_WORKING | GAME_NO_SOUND)
 
