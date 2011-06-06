503d4f2453949ef64166ac50a5e916b01cf2ad4f
mooglyguy <mooglyguy@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-06 22:13:44 +0000

    Forgot to uncheck apple2.c during my last two checkins
    git-svn-id: svn://messdev.no-ip.org/mess@11851 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/apple2.c
+++ src/mess/video/apple2.c
@@ -226,12 +226,11 @@ static void apple2_hires_draw(running_machine &machine, bitmap_t *bitmap, const 
 	int row, col, b;
 	int offset;
 	int columns;
-	UINT8 vram_row[81];
+	UINT8 vram_row[82];
 	UINT16 v;
 	UINT16 *p;
 	UINT32 w;
-	UINT16 artifact_buf[14];
-	UINT8 artifact_idx = 0;
+	UINT16 *artifact_map_ptr;
 
 	/* sanity checks */
 	if (beginrow < cliprect->min_y)
@@ -245,7 +244,7 @@ static void apple2_hires_draw(running_machine &machine, bitmap_t *bitmap, const 
 	columns		= ((effective_a2(state) & (VAR_DHIRES|VAR_80COL)) == (VAR_DHIRES|VAR_80COL)) ? 80 : 40;
 
 	vram_row[0] = 0;
-	vram_row[columns] = 0;
+	vram_row[columns + 1] = 0;
 
 	for (row = beginrow; row <= endrow; row++)
 	{
@@ -256,12 +255,12 @@ static void apple2_hires_draw(running_machine &machine, bitmap_t *bitmap, const 
 			switch(columns)
 			{
 				case 40:
-					vram_row[col] = vram[offset];
+					vram_row[1+col] = vram[offset];
 					break;
 
 				case 80:
-					vram_row[(col*2)+0] = vram[offset + 0x10000];
-					vram_row[(col*2)+1] = vram[offset + 0x00000];
+					vram_row[1+(col*2)+0] = vram[offset + 0x10000];
+					vram_row[1+(col*2)+1] = vram[offset + 0x00000];
 					break;
 
 				default:
@@ -271,8 +270,6 @@ static void apple2_hires_draw(running_machine &machine, bitmap_t *bitmap, const 
 		}
 
 		p = BITMAP_ADDR16(bitmap, row, 0);
-		bool artifacting = false;
-		int x = 0;
 
 		for (col = 0; col < columns; col++)
 		{
@@ -283,53 +280,34 @@ static void apple2_hires_draw(running_machine &machine, bitmap_t *bitmap, const 
 			switch(columns)
 			{
 				case 40:
-				{
-					w = vram_row[col+0];
-					artifact_idx = 0;
+					artifact_map_ptr = &state->m_hires_artifact_map[((vram_row[col+1] & 0x80) >> 7) * 16];
 					for (b = 0; b < 7; b++)
 					{
-						v = (w & 1);
-						w >>= 1;
-						artifact_buf[artifact_idx++] = v ? WHITE : BLACK;
-						artifact_buf[artifact_idx++] = v ? WHITE : BLACK;
-					}
-
-					int start = 0;
-
-					if (vram_row[col] & 0x80)
-					{
-						if (x < 560)
-						{
-							UINT16 last = *(p - 1);
-							*(p++) = last;
-							x++;
-						}
-						artifacting = true;
-						start = 1;
-					}
-
-					for(int a = 0; a < 14 - start; a++)
-					{
-						if(x < 560)
-						{
-							*(p++) = artifact_buf[a];
-							x++;
-						}
+						v = artifact_map_ptr[((w >> (b + 7-1)) & 0x07) | (((b ^ col) & 0x01) << 3)];
+						*(p++) = v;
+						*(p++) = v;
 					}
 					break;
-				}
 
 				case 80:
-				{
-					w = vram_row[col+0];
-					for (b = 0; b < 7; b++)
+					if (state->m_monochrome_dhr)
 					{
-						v = (w & 1);
-						w >>= 1;
-						*(p++) = v ? WHITE : BLACK;
+						for (b = 0; b < 7; b++)
+						{
+							v = (w & 1);
+							w >>= 1;
+							*(p++) = v ? WHITE : BLACK;
+						}
+					}
+					else
+					{
+						for (b = 0; b < 7; b++)
+						{
+							v = state->m_dhires_artifact_map[((((w >> (b + 7-1)) & 0x0F) * 0x11) >> (((2-(col*7+b))) & 0x03)) & 0x0F];
+							*(p++) = v;
+						}
 					}
 					break;
-				}
 
 				default:
 					fatalerror("Invalid column count");
