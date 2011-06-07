28580b483e02396afa2c8c28a4f67a54ced25b0a
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-07 13:55:39 +0000

    [IQ151] Added bios-rom selection. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@11854 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/iq151.c
+++ src/mess/drivers/iq151.c
@@ -7,6 +7,9 @@
         07/June/2011 Added screen & keyboard (by looking at the Z80 code)
 
 
+This computer depends on RAM just happening to be certain values at powerup.
+If the conditions are not met, it may crash.
+
 Monitor Commands:
 C
 D Dump memory, any key to dump more, Return to finish
@@ -22,8 +25,6 @@ X Display/Edit registers
 
 ToDo:
 - Add whatever devices may exist.
-
-- Sort out the various roms
 
 - Line 32 does not scroll, should it show?
   (could be reserved for a status line in a terminal mode)
@@ -96,9 +97,9 @@ READ8_MEMBER(iq151_state::keyboard_r)
 static ADDRESS_MAP_START(iq151_mem, AS_PROGRAM, 8, iq151_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x0000, 0x07ff ) AM_RAMBANK("boot")
-	AM_RANGE( 0x0000, 0xc7ff ) AM_RAM
-	AM_RANGE( 0xc800, 0xcfff ) AM_ROM // c800 has to be FF before we can boot
-	AM_RANGE( 0xd000, 0xebff ) AM_RAM
+	AM_RANGE( 0x0800, 0xdfff ) AM_RAM AM_REGION("maincpu", 0x0800) // puts FF into C800 so we can boot
+	AM_RANGE( 0xe000, 0xe7ff ) AM_ROM
+	AM_RANGE( 0xe800, 0xebff ) AM_RAM
 	AM_RANGE( 0xec00, 0xefff ) AM_RAM AM_BASE(m_p_videoram)
 	AM_RANGE( 0xf000, 0xffff ) AM_ROM
 ADDRESS_MAP_END
@@ -325,10 +326,15 @@ ROM_START( iq151 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	/* A number of bios versions here. The load address is shown for each */
 	ROM_LOAD( "iq151_disc2_12_5_1987_v4_0.rom", 0xe000, 0x0800, CRC(b189b170) SHA1(3e2ca80934177e7a32d0905f5a0ad14072f9dabf))
-	ROM_LOAD( "iq151_monitor_cpm.rom", 0xf000, 0x1000, CRC(26f57013) SHA1(4df396edc375dd2dd3c82c4d2affb4f5451066f1))
-	ROM_LOAD( "iq151_monitor_cpm_old.rom", 0xf000, 0x1000, CRC(6743e1b7) SHA1(ae4f3b1ba2511a1f91c4e8afdfc0e5aeb0fb3a42))
-	ROM_LOAD( "iq151_monitor_disasm.rom", 0xf000, 0x1000, CRC(45c2174e) SHA1(703e3271a124c3ef9330ae399308afd903316ab9))
-	ROM_LOAD( "iq151_monitor_orig.rom", 0xf000, 0x1000, CRC(acd10268) SHA1(4d75c73f155ed4dc2ac51a9c22232f869cca95e2))
+
+	ROM_SYSTEM_BIOS( 0, "orig", "Original" )
+	ROMX_LOAD( "iq151_monitor_orig.rom", 0xf000, 0x1000, CRC(acd10268) SHA1(4d75c73f155ed4dc2ac51a9c22232f869cca95e2),ROM_BIOS(1))
+	ROM_SYSTEM_BIOS( 1, "disasm", "Disassembler" )
+	ROMX_LOAD( "iq151_monitor_disasm.rom", 0xf000, 0x1000, CRC(45c2174e) SHA1(703e3271a124c3ef9330ae399308afd903316ab9),ROM_BIOS(2))
+	ROM_SYSTEM_BIOS( 2, "cpm", "CPM" )
+	ROMX_LOAD( "iq151_monitor_cpm.rom", 0xf000, 0x1000, CRC(26f57013) SHA1(4df396edc375dd2dd3c82c4d2affb4f5451066f1),ROM_BIOS(3))
+	ROM_SYSTEM_BIOS( 3, "cpmold", "CPM (old)" )
+	ROMX_LOAD( "iq151_monitor_cpm_old.rom", 0xf000, 0x1000, CRC(6743e1b7) SHA1(ae4f3b1ba2511a1f91c4e8afdfc0e5aeb0fb3a42),ROM_BIOS(4))
 
 	ROM_REGION( 0x0c00, "chargen", ROMREGION_INVERT )
 	ROM_LOAD( "iq151_video32font.rom", 0x0000, 0x0400, CRC(395567a7) SHA1(18800543daf4daed3f048193c6ae923b4b0e87db))
