17edba27af9d993de9ab521261ea717f512762f8
etabeta <etabeta@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-09 15:52:07 +0000

    consistency fix
    git-svn-id: svn://messdev.no-ip.org/mess@11884 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/argo.c
+++ src/mess/drivers/argo.c
@@ -360,4 +360,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY           FULLNAME       FLAGS */
-COMP( 1986, argo,  0,      0,       argo,     argo,    argo,     "unknown",   "Argo", GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1986, argo,  0,      0,       argo,     argo,    argo,     "<unknown>",   "Argo", GAME_NOT_WORKING | GAME_NO_SOUND)
