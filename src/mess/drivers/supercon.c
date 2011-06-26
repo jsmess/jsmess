d6712538b525f701fcd10106797a0e230c2314c4
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-06-26 03:51:17 +0000

    SUPERCON - Modernised. Fixed artwork issue.
    git-svn-id: svn://messdev.no-ip.org/mess@12064 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/supercon.c
+++ src/mess/drivers/supercon.c
@@ -16,21 +16,35 @@
 
 
 ******************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/m6502/m6502.h"
 #include "sound/beep.h"
-
 #include "supercon.lh"
 
+#define MACHINE_RESET_MEMBER(name) void name::machine_reset()
 
 class supercon_state : public driver_device
 {
 public:
 	supercon_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+	m_maincpu(*this, "maincpu"),
+	m_beep(*this, BEEPER_TAG)
+	{ }
 
-	UINT8 *m_ram;
+	required_device<cpu_device> m_maincpu;
+	required_device<device_t> m_beep;
+	DECLARE_READ8_MEMBER(supercon_port1_r);
+	DECLARE_READ8_MEMBER(supercon_port2_r);
+	DECLARE_READ8_MEMBER(supercon_port3_r);
+	DECLARE_READ8_MEMBER(supercon_port4_r);
+	DECLARE_WRITE8_MEMBER(supercon_port1_w);
+	DECLARE_WRITE8_MEMBER(supercon_port2_w);
+	DECLARE_WRITE8_MEMBER(supercon_port3_w);
+	DECLARE_WRITE8_MEMBER(supercon_port4_w);
+	static const UINT8 m_border_pieces[12];
 	emu_timer* m_timer_update_irq;
 	emu_timer* m_timer_mouse_click;
 	int m_emu_started;
@@ -51,6 +65,12 @@ public:
 	int m_confirm_board_click;
 	int m_board[64];
 	int m_save_board[64];
+	void set_board();
+	void set_pieces();
+	void set_border_pieces();
+	void clear_pieces();
+	void update_leds();
+	virtual void machine_reset();
 };
 
 
@@ -82,18 +102,19 @@ enum
 
 #define MAIN_CLOCK 4000000 /* 4 MHz */
 
-#define FALSE	0
-#define TRUE	1
-
 #define NOT_VALID	99
 
 #define NO_ACTION	0
 #define	TAKE		1
-#define SET			2
+#define SET		2
 
 /* artwork board */
-static const int start_board[64] =
+
+const UINT8 supercon_state::m_border_pieces[12] = {WK,WQ,WR,WB,WN,WP,BK,BQ,BR,BB,BN,BP};
+
+void supercon_state::set_board()
 {
+	static const int start_board[64] = {
 	BR, BN, BB, BQ, BK, BB, BN, BR,
 	BP, BP, BP, BP, BP, BP, BP, BP,
 	EM, EM, EM, EM, EM, EM, EM, EM,
@@ -101,41 +122,34 @@ static const int start_board[64] =
 	EM, EM, EM, EM, EM, EM, EM, EM,
 	EM, EM, EM, EM, EM, EM, EM, EM,
 	WP, WP, WP, WP, WP, WP, WP, WP,
-	WR, WN, WB, WQ, WK, WB, WN, WR
-};
+	WR, WN, WB, WQ, WK, WB, WN, WR };
 
-static const UINT8 border_pieces[12] = {WK,WQ,WR,WB,WN,WP,BK,BQ,BR,BB,BN,BP,};
+	for (UINT8 i=0; i<64; i++)
+		m_board[i]=start_board[i];
+}
 
-
-static void set_board( supercon_state *state )
+void supercon_state::set_pieces()
 {
 	int i;
 	for (i=0;i<64;i++)
-		state->m_board[i]=start_board[i];
+		output_set_indexed_value("P", i, m_board[i]);
 }
 
-static void set_pieces (supercon_state *state)
-{
-	int i;
-	for (i=0;i<64;i++)
-		output_set_indexed_value("P", i, state->m_board[i]);
-}
-
-static void set_boarder_pieces (void)
+void supercon_state::set_border_pieces()
 {
 	UINT8 i;
 
 	for (i=0;i<12;i++)
-		output_set_indexed_value("Q", i, border_pieces[i]);
+		output_set_indexed_value("Q", i, m_border_pieces[i]);
 }
 
-static void clear_pieces(supercon_state *state)
+void supercon_state::clear_pieces()
 {
 	int i;
 	for (i=0;i<64;i++)
 	{
 		output_set_indexed_value("P", i, EM);
-		state->m_board[i]=EM;
+		m_board[i]=EM;
 	}
 }
 
@@ -162,23 +176,23 @@ static int get_first_bit(UINT8 data)
 }
 
 
-static void update_leds( supercon_state *state )
+void supercon_state::update_leds()
 {
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		if (BIT(state->m_LED_18, i))
+		if (BIT(m_LED_18, i))
 			output_set_led_value(i + 1, 1);
 		else
 			output_set_led_value(i + 1, 0);
 
-		if (BIT(state->m_LED_AH, i))
+		if (BIT(m_LED_AH, i))
 			output_set_led_value(i + 9, 1);
 		else
 			output_set_led_value(i + 9, 0);
 
-		if (BIT(state->m_LED_ST, i))
+		if (BIT(m_LED_ST, i))
 			output_set_led_value(i + 17, 1);
 		else
 			output_set_led_value(i + 17, 0);
@@ -191,13 +205,13 @@ static void mouse_update(running_machine &machine)
 	UINT8 port_input; // m_left;
 	int i;
 
-/* Boarder pieces and moving pice */
+/* border pieces and moving piece */
 
 	port_input=input_port_read(machine, "B_WHITE");
 	if (port_input)
 	{
 		i=get_first_bit(port_input);
-		state->m_moving_piece=border_pieces[i];
+		state->m_moving_piece=state->m_border_pieces[i];
 		output_set_value("MOVING",state->m_moving_piece);
 		return;
 	}
@@ -207,7 +221,7 @@ static void mouse_update(running_machine &machine)
 	if (port_input)
 	{
 		i=get_first_bit(port_input);
-		state->m_moving_piece=border_pieces[6+i];
+		state->m_moving_piece=state->m_border_pieces[6+i];
 		output_set_value("MOVING",state->m_moving_piece);
 		return;
 	}
@@ -234,7 +248,7 @@ static DRIVER_INIT(supercon)
 	state->m_LED_AH=0;
 	state->m_LED_ST=0;
 
-	state->m_wait_time =  attotime::from_hz(4);
+	state->m_wait_time = attotime::from_hz(4);
 	state->m_save_key_data = 0xff;
 
 	state->m_moving_piece=0;
@@ -242,25 +256,24 @@ static DRIVER_INIT(supercon)
 
 /* Read 1C000 */
 
-static READ8_HANDLER( supercon_port1_r )
+READ8_MEMBER( supercon_state::supercon_port1_r )
 {
 	LOG(("Read from %04x \n",0x1C00));
-    return 0xff;
+	return 0xff;
 }
 
 /* Read 1D000 */
 
-static READ8_HANDLER( supercon_port2_r )
+READ8_MEMBER( supercon_state::supercon_port2_r )
 {
 	LOG(("Read from %04x \n",0x1D00));
-    return 0xff;
+	return 0xff;
 }
 
 /* Read 1E00 */
 
-static READ8_HANDLER( supercon_port3_r )
+READ8_MEMBER( supercon_state::supercon_port3_r )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
 	int i;
 	UINT8 key_data=0;
 
@@ -271,53 +284,53 @@ static READ8_HANDLER( supercon_port3_r )
 
 /* remove last bit (only if it was not already set) */
 
-	if ( state->m_data_1F00 & LED_LINE_AH )
+	if ( m_data_1F00 & LED_LINE_AH )
 	{
-		if (state->m_last_LED_value != state->m_LED_AH)
-			state->m_LED_AH=state->m_LED_AH & ~state->m_data_1E00;
+		if (m_last_LED_value != m_LED_AH)
+			m_LED_AH=m_LED_AH & ~m_data_1E00;
 	}
-	else if ( state->m_data_1F00 & LED_LINE_ST)
+	else if ( m_data_1F00 & LED_LINE_ST)
 	{
-		if (state->m_last_LED_value != state->m_LED_ST)
-			state->m_LED_ST=state->m_LED_ST & ~state->m_data_1E00;
+		if (m_last_LED_value != m_LED_ST)
+			m_LED_ST=m_LED_ST & ~m_data_1E00;
 	}
-	else if ( state->m_data_1F00 & LED_LINE_18 )
+	else if ( m_data_1F00 & LED_LINE_18 )
 	{
-		if (state->m_last_LED_value != state->m_LED_18)
-			state->m_LED_18=state->m_LED_18 & ~state->m_data_1E00;
+		if (m_last_LED_value != m_LED_18)
+			m_LED_18=m_LED_18 & ~m_data_1E00;
 	}
 
 
-	LOG(("LED_18 from %02x \n",state->m_LED_18));
-	LOG(("LED_AH from %02x \n",state->m_LED_AH));
-	LOG(("LED_ST from %02x \n",state->m_LED_ST));
+	LOG(("LED_18 from %02x \n",m_LED_18));
+	LOG(("LED_AH from %02x \n",m_LED_AH));
+	LOG(("LED_ST from %02x \n",m_LED_ST));
 
-	if (state->m_led_update)			/*No LED Update if Port 1C00,1D00 was read */
-		update_leds(state);
+	if (m_led_update)			/*No LED Update if Port 1C00,1D00 was read */
+		update_leds();
 
-	state->m_remove_led_flag=TRUE;
-	state->m_led_update=TRUE;
+	m_remove_led_flag=TRUE;
+	m_led_update=TRUE;
 
-	state->m_LED_18=0;
-	state->m_LED_AH=0;
-	state->m_LED_ST=0;
+	m_LED_18=0;
+	m_LED_AH=0;
+	m_LED_ST=0;
 
 
 /* Start */
 
-	if (!state->m_emu_started)
+	if (!m_emu_started)
 		return 0xbf;
 	else
-		state->m_timer_update_irq->adjust( attotime::zero, 0, attotime::from_hz(598) );  //HACK adjust timer after start ???
+		m_timer_update_irq->adjust( attotime::zero, 0, attotime::from_hz(598) );  //HACK adjust timer after start ???
 
 
 /* Buttons */
 
-	i=get_first_bit(state->m_data_1E00);
+	i=get_first_bit(m_data_1E00);
 	if (i==NOT_VALID)
 		return 0xff;
 
-	key_data=input_port_read(space->machine(), status_lines[i]);
+	key_data=input_port_read(machine(), status_lines[i]);
 
 	if (key_data != 0xc0)
 	{
@@ -327,20 +340,19 @@ static READ8_HANDLER( supercon_port3_r )
 
 		if (i==0 && key_data==0x80)
 		{
-			set_board(state);
-			set_pieces(state);
+			set_board();
+			set_pieces();
 
-			state->m_emu_started=FALSE;
+			m_emu_started=FALSE;
 		}
 
 /* Button: Clear Board -> remove all pieces */
 
 		if (i==3 && key_data==0x80)
-			clear_pieces(state);
+			clear_pieces();
 
 		if (key_data != 0xff )
 			return key_data;
-
 	}
 
 	return 0xc0;
@@ -348,9 +360,8 @@ static READ8_HANDLER( supercon_port3_r )
 
 /* Read Port $1F00 */
 
-static READ8_HANDLER( supercon_port4_r )
+READ8_MEMBER( supercon_state::supercon_port4_r )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
 	int i_18, i_AH;
 	UINT8 key_data = 0x00;
 
@@ -361,21 +372,21 @@ static READ8_HANDLER( supercon_port4_r )
 
 /* Board buttons */
 
-	i_18=get_first_bit(state->m_data_1E00);
+	i_18=get_first_bit(m_data_1E00);
 	if (i_18==NOT_VALID)
 		return 0xff;
 
 /* if a button was pressed wait til timer -timer_mouse_click- is fired */
 
-	if (state->m_selecting)
-		return state->m_save_key_data;
+	if (m_selecting)
+		return m_save_key_data;
 	else
 	{
-		set_pieces(state);
-		output_set_value("MOVING",state->m_moving_piece);
+		set_pieces();
+		output_set_value("MOVING",m_moving_piece);
 	}
 
-	key_data=input_port_read(space->machine(), board_lines[i_18]);
+	key_data=input_port_read(machine(), board_lines[i_18]);
 
 	if (key_data != 0xff)
 	{
@@ -383,30 +394,30 @@ static READ8_HANDLER( supercon_port4_r )
 
 /* Only if valid data and mouse button is pressed */
 
-		if (key_data && input_port_read(space->machine(), "BUTTON_L"))
+		if (key_data && input_port_read(machine(), "BUTTON_L"))
 		{
 
 /* Set or remove pieces */
 
 			i_AH=7-get_first_cleared_bit(key_data);
-			LOG(("Press -> AH: %d 18: %d Piece: %d\n",i_AH,i_18,state->m_board[i_18*8 + i_AH]););
+			LOG(("Press -> AH: %d 18: %d Piece: %d\n",i_AH,i_18,m_board[i_18*8 + i_AH]););
 
-			state->m_current_field=&state->m_board[i_18*8 + i_AH];
+			m_current_field=&m_board[i_18*8 + i_AH];
 
-			if (state->m_moving_piece)
+			if (m_moving_piece)
 			{
-				*state->m_current_field = state->m_moving_piece;
-				state->m_moving_piece = EM;
+				*m_current_field = m_moving_piece;
+				m_moving_piece = EM;
 			}
 			else
 			{
-				state->m_moving_piece = *state->m_current_field;
-				*state->m_current_field = EM;
+				m_moving_piece = *m_current_field;
+				*m_current_field = EM;
 			}
-			state->m_selecting=TRUE;				/* Flag is removed in timer -timer_mouse_click- */
-			state->m_save_key_data=key_data;		/* return same key_data til flag selecting is removed */
+			m_selecting=TRUE;				/* Flag is removed in timer -timer_mouse_click- */
+			m_save_key_data=key_data;		/* return same key_data til flag selecting is removed */
 
-			state->m_timer_mouse_click->adjust(state->m_wait_time, 0);
+			m_timer_mouse_click->adjust(m_wait_time, 0);
 
 			return key_data;
 		}
@@ -417,91 +428,80 @@ static READ8_HANDLER( supercon_port4_r )
 
 /* Write Port $1C00 */
 
-static WRITE8_HANDLER( supercon_port1_w )
+WRITE8_MEMBER( supercon_state::supercon_port1_w )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
 	LOG(("Write from %04x data: %02x\n",0x1C00,data));
-	state->m_led_update=FALSE;
+	m_led_update=FALSE;
 }
 
 /* Write Port $1D00 */
 
-static WRITE8_HANDLER( supercon_port2_w )
+WRITE8_MEMBER( supercon_state::supercon_port2_w )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
-
 	LOG(("Write from %04x data: %02x\n",0x1D00,data));
-	state->m_led_update=FALSE;
+	m_led_update=FALSE;
 }
 
 /* Write Port $1E00 */
 
-static WRITE8_HANDLER( supercon_port3_w )
+WRITE8_MEMBER( supercon_state::supercon_port3_w )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
-
 	if (data)
 		LOG(("Write from %04x data: %02x\n",0x1E00,data));
 
 	if (data)
 	{
-		state->m_data_1E00=data;
+		m_data_1E00=data;
 
 /* Set bits for LED's */
 
-		if ( state->m_data_1F00)
+		if ( m_data_1F00)
 		{
 
-			if (state->m_data_1F00 & LED_LINE_AH )
+			if (m_data_1F00 & LED_LINE_AH )
 			{
-				state->m_last_LED = &state->m_LED_AH;				/* save last value */
-				state->m_last_LED_value = *state->m_last_LED;
+				m_last_LED = &m_LED_AH;				/* save last value */
+				m_last_LED_value = *m_last_LED;
 
-				state->m_LED_AH=state->m_LED_AH | state->m_data_1E00;
+				m_LED_AH=m_LED_AH | m_data_1E00;
 			}
-			else if (state->m_data_1F00 & LED_LINE_ST )
+			else if (m_data_1F00 & LED_LINE_ST )
 			{
-				state->m_last_LED = &state->m_LED_ST;
-				state->m_last_LED_value = *state->m_last_LED;
+				m_last_LED = &m_LED_ST;
+				m_last_LED_value = *m_last_LED;
 
-				state->m_LED_ST=state->m_LED_ST | state->m_data_1E00;
+				m_LED_ST=m_LED_ST | m_data_1E00;
 			}
-			else if (state->m_data_1F00 &  LED_LINE_18)
+			else if (m_data_1F00 &  LED_LINE_18)
 			{
-				state->m_last_LED = &state->m_LED_18;
-				state->m_last_LED_value = *state->m_last_LED;
+				m_last_LED = &m_LED_18;
+				m_last_LED_value = *m_last_LED;
 
-				state->m_LED_18=state->m_LED_18 | state->m_data_1E00;
+				m_LED_18=m_LED_18 | m_data_1E00;
 			}
 		}
-
 	}
-
 }
 
 /* Write Port $1F00 */
 
-static WRITE8_HANDLER( supercon_port4_w )
+WRITE8_MEMBER( supercon_state::supercon_port4_w )
 {
-	supercon_state *state = space->machine().driver_data<supercon_state>();
-	device_t *speaker = space->machine().device(BEEPER_TAG);
-
 	if (data)
 		LOG(("Write from %04x data: %02x\n",0x1F00,data));
 
 	if (data)
-		state->m_data_1F00=data;
+		m_data_1F00=data;
 
 /* Bit 7 is set -> Buzzer on */
 
-	if ( state->m_data_1F00 & 0x80 )
+	if ( m_data_1F00 & 0x80 )
 	{
-		beep_set_state(speaker,1);
-		state->m_emu_started=TRUE;
+		beep_set_state(m_beep, 1);
+		m_emu_started=TRUE;
 	}
 	else
-		beep_set_state(speaker,0);
-
+		beep_set_state(m_beep ,0);
 }
 
 static TIMER_CALLBACK( mouse_click )
@@ -529,52 +529,46 @@ static TIMER_CALLBACK( update_irq )
 
 static void board_presave(supercon_state *state)
 {
-    int i;
-    for (i=0;i<64;i++)
-        state->m_save_board[i]=state->m_board[i];
+	int i;
+	for (i=0;i<64;i++)
+		state->m_save_board[i]=state->m_board[i];
 }
 
 static void board_postload(supercon_state *state)
 {
-    int i;
-    for (i=0;i<64;i++)
-        state->m_board[i]=state->m_save_board[i];
+	int i;
+	for (i=0;i<64;i++)
+		state->m_board[i]=state->m_save_board[i];
 
-    set_pieces(state);
+	state->set_pieces();
 }
 
 static MACHINE_START( supercon )
 {
 	supercon_state *state = machine.driver_data<supercon_state>();
-
 	state->m_timer_update_irq = machine.scheduler().timer_alloc(FUNC(update_irq));
 	state->m_timer_update_irq->adjust( attotime::zero, 0, attotime::from_hz(1000) );
-
 	state->m_timer_mouse_click =  machine.scheduler().timer_alloc(FUNC(mouse_click),NULL);
-
-
-    state->save_item(NAME(state->m_save_board));
-    machine.save().register_postload(save_prepost_delegate(FUNC(board_postload),state));
-    machine.save().register_presave(save_prepost_delegate(FUNC(board_presave),state));
-
+	state->save_item(NAME(state->m_save_board));
+	machine.save().register_postload(save_prepost_delegate(FUNC(board_postload),state));
+	machine.save().register_presave(save_prepost_delegate(FUNC(board_presave),state));
 }
 
-static MACHINE_RESET( supercon )
+MACHINE_RESET_MEMBER( supercon_state )
 {
-	supercon_state *state = machine.driver_data<supercon_state>();
-	set_board(state);
-	set_pieces(state);
-	set_boarder_pieces();
+	set_board();
+	set_pieces();
+	set_border_pieces();
 
-	state->m_emu_started=FALSE;
+	m_emu_started=FALSE;
 }
 
 /* Address maps */
 
-static ADDRESS_MAP_START(supercon_mem, AS_PROGRAM, 8)
-	AM_RANGE( 0x0000, 0x0fff) AM_RAM AM_BASE_MEMBER(supercon_state, m_ram )
+static ADDRESS_MAP_START(supercon_mem, AS_PROGRAM, 8, supercon_state)
+	AM_RANGE( 0x0000, 0x0fff) AM_RAM
 	AM_RANGE( 0x2000, 0x7fff) AM_ROM
-    AM_RANGE( 0x8000, 0xffff) AM_ROM
+	AM_RANGE( 0x8000, 0xffff) AM_ROM
 
 	AM_RANGE( 0x1C00, 0x1C00) AM_WRITE ( supercon_port1_w )
 	AM_RANGE( 0x1D00, 0x1D00) AM_WRITE ( supercon_port2_w )
@@ -585,25 +579,16 @@ static ADDRESS_MAP_START(supercon_mem, AS_PROGRAM, 8)
 	AM_RANGE( 0x1D00, 0x1D00) AM_READ ( supercon_port2_r )
 	AM_RANGE( 0x1E00, 0x1E00) AM_READ ( supercon_port3_r )
 	AM_RANGE( 0x1F00, 0x1F00) AM_READ ( supercon_port4_r )
-
-
 ADDRESS_MAP_END
 
-/*static ADDRESS_MAP_START(supercon_io, AS_IO, 8)
-
-
-ADDRESS_MAP_END
- */
 /* Input ports */
 
 static INPUT_PORTS_START( supercon )
-
 	PORT_START("MOUSE_X")
 	PORT_BIT( 0xffff, 0x00, IPT_MOUSE_X) PORT_SENSITIVITY(100)
 
 	PORT_START("MOUSE_Y")
 	PORT_BIT( 0xffff, 0x00, IPT_MOUSE_Y ) PORT_SENSITIVITY(100)
-
 
 	PORT_START("BUTTON_L")
 	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_BUTTON2) PORT_CODE(MOUSECODE_BUTTON1) PORT_NAME("left button")
@@ -723,7 +708,6 @@ static INPUT_PORTS_START( supercon )
 	PORT_BIT(0x040, IP_ACTIVE_LOW, IPT_KEYBOARD)
 	PORT_BIT(0x080, IP_ACTIVE_LOW, IPT_KEYBOARD)
 
-
 	PORT_START("B_WHITE")
 	PORT_BIT(0x01,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
 	PORT_BIT(0x02,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
@@ -742,30 +726,25 @@ static INPUT_PORTS_START( supercon )
 
 	PORT_START("B_CLR")
 	PORT_BIT(0x01,  IP_ACTIVE_HIGH, IPT_KEYBOARD)
-
 INPUT_PORTS_END
 
 /* Machine driver */
 static MACHINE_CONFIG_START( supercon, supercon_state )
-
-    /* basic machine hardware */
+	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu",M6502,MAIN_CLOCK)
-    MCFG_CPU_PROGRAM_MAP(supercon_mem)
+	MCFG_CPU_PROGRAM_MAP(supercon_mem)
 
 	MCFG_MACHINE_START( supercon )
-	MCFG_MACHINE_RESET( supercon )
 
-    /* video hardware */
-
-	//MCFG_DEFAULT_LAYOUT(layout_supercon)
+	/* video hardware */
+	MCFG_DEFAULT_LAYOUT(layout_supercon)
 
 	/* sound hardware */
 	MCFG_SPEAKER_STANDARD_MONO("mono")
 	MCFG_SOUND_ADD(BEEPER_TAG, BEEP, 0)
-	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 1.0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.50)
 
 	MCFG_TIMER_ADD_PERIODIC("artwork_timer", update_artwork, attotime::from_hz(20))
-
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -774,11 +753,9 @@ ROM_START(supercon)
 	ROM_REGION(0x10000,"maincpu",0)
 	ROM_LOAD("supercon_low.bin",  0x0000,  0x8000, CRC(b853cf6e) SHA1(1a759072a5023b92c07f1fac01b7a21f7b5b45d0 ))
 	ROM_LOAD("supercon_high.bin", 0x8000,  0x8000, CRC(c8f82331) SHA1(f7fd039f9a3344db9749931490ded9e9e309cfbe ))
-
-
 ROM_END
 
 /* Driver */
 
 /*    YEAR  NAME          PARENT  COMPAT  MACHINE    INPUT       INIT      COMPANY  FULLNAME                     FLAGS */
-CONS( 1983, supercon,     0,      0,      supercon,  supercon,   supercon, "Novag", "SuperConstellation",       GAME_SUPPORTS_SAVE)
+CONS( 1983, supercon,     0,      0,      supercon,  supercon,   supercon, "Novag", "SuperConstellation", GAME_SUPPORTS_SAVE)
