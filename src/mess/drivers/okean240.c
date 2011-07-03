55968f80cc6016754badb61d03037d948fda75e0
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-07-03 09:18:52 +0000

    OKEAN240 - moved the test rom into its own driver; removed compile-time define; test rom has 2 screens, others only have 1.
    git-svn-id: svn://messdev.no-ip.org/mess@12137 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/okean240.c
+++ src/mess/drivers/okean240.c
@@ -27,13 +27,7 @@ ppaC0 : K580ww55
   portC=vid.scroll.x
 
 
-NOTES ABOUT THE TEST ROM (-bios 1):
-- The test rom tests various things including video memory. The test results
-are sent to the comport (for us, the generic terminal). Therefore it would
-be useful to have the 2 screens side-by-side. Unfortunately they will not
-co-exist, instead causing a crash at start ("Mandatory artwork is missing").
-So, if you want to see the test rom results, you'll have to compile a modified
-driver (uncomment the define below).
+NOTE ABOUT THE TEST ROM (okean240t):
 - You need to press a key every so often.
 
 
@@ -47,13 +41,11 @@ ToDo:
 - Add memory banking (perhaps port C1)
 
 Usage of terminal:
-- okean240 bios 0 - the keyboard
-- okean240a bios 0 - not used
-- (either) bios 1 - the keyboard (part of the testing), & the screen if compiled specially
+- okean240 - the keyboard
+- okean240a - not used
+- okean240t - the keyboard & screen
 
 ****************************************************************************/
-//#define OKEAN240_USING_TESTROM
-
 #define ADDRESS_MAP_MODERN
 
 #include "emu.h"
@@ -90,7 +82,7 @@ public:
 	virtual void video_start();
 };
 
-// bios 0 requires bit 4 to change
+// okean240 requires bit 4 to change
 READ8_MEMBER( okean240_state::okean240_kbd_status_r )
 {
 	if (m_term_data)
@@ -207,9 +199,6 @@ static ADDRESS_MAP_START(okean240_io, AS_IO, 8, okean240_state)
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	AM_RANGE(0x40, 0x42) AM_READWRITE(okean240_keyboard_r,okean240_keyboard_w)
 	AM_RANGE(0x80, 0x80) AM_READ(okean240_kbd_status_r)
-#ifdef OKEAN240_USING_TESTROM
-	AM_RANGE(0xa0, 0xa0) AM_DEVWRITE_LEGACY(TERMINAL_TAG, terminal_write)
-#endif
 	AM_RANGE(0xa0, 0xa0) AM_READ(term_r)
 	AM_RANGE(0xa1, 0xa1) AM_READ(term_status_r)
 	AM_RANGE(0xc0, 0xc0) AM_WRITE(scroll_w)
@@ -219,9 +208,6 @@ static ADDRESS_MAP_START(okean240a_io, AS_IO, 8, okean240_state)
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	AM_RANGE(0x40, 0x42) AM_READWRITE(okean240a_keyboard_r,okean240_keyboard_w)
 	AM_RANGE(0x80, 0x80) AM_READ(okean240a_kbd_status_r)
-#ifdef OKEAN240_USING_TESTROM
-	AM_RANGE(0xa0, 0xa0) AM_DEVWRITE_LEGACY(TERMINAL_TAG, terminal_write)
-#endif
 	AM_RANGE(0xa0, 0xa0) AM_READ(term_r)
 	AM_RANGE(0xa1, 0xa1) AM_READ(term_status_r)
 	AM_RANGE(0xc0, 0xc0) AM_WRITE(scroll_w)
@@ -235,6 +221,16 @@ static ADDRESS_MAP_START(okean240a_io, AS_IO, 8, okean240_state)
 	// AM_RANGE(0xa0, 0xa1)=comport.data
 	// AM_RANGE(0xc0, 0xdf)=ppaC0.data
 	// AM_RANGE(0xe0, 0xff)=ppaE0.data
+ADDRESS_MAP_END
+
+static ADDRESS_MAP_START(okean240t_io, AS_IO, 8, okean240_state)
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x40, 0x42) AM_READWRITE(okean240_keyboard_r,okean240_keyboard_w)
+	AM_RANGE(0x80, 0x80) AM_READ(okean240_kbd_status_r)
+	AM_RANGE(0xa0, 0xa0) AM_DEVWRITE_LEGACY(TERMINAL_TAG, terminal_write)
+	AM_RANGE(0xa0, 0xa0) AM_READ(term_r)
+	AM_RANGE(0xa1, 0xa1) AM_READ(term_status_r)
+	AM_RANGE(0xc0, 0xc0) AM_WRITE(scroll_w)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -379,7 +375,7 @@ WRITE8_MEMBER( okean240_state::kbd_put )
 	m_term_data = data;
 }
 
-static GENERIC_TERMINAL_INTERFACE( okean240_terminal_intf )
+static GENERIC_TERMINAL_INTERFACE( terminal_intf )
 {
 	DEVCB_DRIVER_MEMBER(okean240_state, kbd_put)
 };
@@ -394,7 +390,6 @@ VIDEO_START_MEMBER( okean240_state )
 {
 }
 
-#ifndef OKEAN240_USING_TESTROM
 static SCREEN_UPDATE( okean240 )
 {
 	okean240_state *state = screen->machine().driver_data<okean240_state>();
@@ -411,19 +406,19 @@ static SCREEN_UPDATE( okean240 )
 			gfx = state->m_p_videoram[x|ma] | state->m_p_videoram[x|ma|0x100];
 
 			/* Display a scanline of a character */
-			*p++ = BIT(gfx, 0);
-			*p++ = BIT(gfx, 1);
-			*p++ = BIT(gfx, 2);
-			*p++ = BIT(gfx, 3);
-			*p++ = BIT(gfx, 4);
-			*p++ = BIT(gfx, 5);
-			*p++ = BIT(gfx, 6);
-			*p++ = BIT(gfx, 7);
+			*p++ = BIT(gfx, 0) + 2;
+			*p++ = BIT(gfx, 1) + 2;
+			*p++ = BIT(gfx, 2) + 2;
+			*p++ = BIT(gfx, 3) + 2;
+			*p++ = BIT(gfx, 4) + 2;
+			*p++ = BIT(gfx, 5) + 2;
+			*p++ = BIT(gfx, 6) + 2;
+			*p++ = BIT(gfx, 7) + 2;
 		}
 	}
 	return 0;
 }
-#endif
+
 
 /* F4 Character Displayer */
 static const gfx_layout okean240_charlayout =
@@ -447,6 +442,13 @@ static GFXDECODE_START( okean240a )
 	GFXDECODE_ENTRY( "maincpu", 0xef63, okean240_charlayout, 0, 1 )
 GFXDECODE_END
 
+// Palette 0 & 1 are for terminal and are defined in terminal.c
+static PALETTE_INIT( okean240 )
+{
+	palette_set_color(machine, 2, RGB_BLACK); /* black */
+	palette_set_color(machine, 3, RGB_WHITE); /* white */
+}
+
 static MACHINE_CONFIG_START( okean240, okean240_state )
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu",I8080, XTAL_12MHz / 6)
@@ -454,22 +456,18 @@ static MACHINE_CONFIG_START( okean240, okean240_state )
 	MCFG_CPU_IO_MAP(okean240_io)
 
 	/* video hardware */
-#ifdef OKEAN240_USING_TESTROM
-	MCFG_FRAGMENT_ADD( generic_terminal )
-#else
 	MCFG_SCREEN_ADD("screen", RASTER)
 	MCFG_SCREEN_REFRESH_RATE(50)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
 	MCFG_SCREEN_SIZE(256, 256)
 	MCFG_SCREEN_VISIBLE_AREA(0, 255, 0, 255)
-	MCFG_PALETTE_LENGTH(2)
-	MCFG_PALETTE_INIT(black_and_white)
+	MCFG_PALETTE_LENGTH(4)
+	MCFG_PALETTE_INIT(okean240)
 	MCFG_SCREEN_UPDATE(okean240)
-#endif
 	MCFG_GFXDECODE(okean240)
 
-	MCFG_GENERIC_TERMINAL_ADD(TERMINAL_TAG, okean240_terminal_intf)
+	MCFG_GENERIC_TERMINAL_ADD(TERMINAL_TAG, terminal_intf)
 MACHINE_CONFIG_END
 
 static MACHINE_CONFIG_DERIVED( okean240a, okean240 )
@@ -478,25 +476,28 @@ static MACHINE_CONFIG_DERIVED( okean240a, okean240 )
 	MCFG_GFXDECODE(okean240a)
 MACHINE_CONFIG_END
 
+static MACHINE_CONFIG_DERIVED( okean240t, okean240 )
+	MCFG_CPU_MODIFY("maincpu")
+	MCFG_CPU_IO_MAP(okean240t_io)
+	MCFG_FRAGMENT_ADD( generic_terminal )
+MACHINE_CONFIG_END
+
 /* ROM definition */
 ROM_START( okean240 )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_SYSTEM_BIOS(0, "Normal", "Normal")
-	ROMX_LOAD( "monitor.bin", 0xe000, 0x2000, CRC(587799bc) SHA1(1f677afa96722ca4ed2643eaca243548845fc854), ROM_BIOS(1))
-	ROMX_LOAD( "cpm80.bin",   0xc000, 0x2000, CRC(7378e4f9) SHA1(c3c06c6f2e953546452ca6f82140a79d0e4953b4), ROM_BIOS(1))
-
-	ROM_SYSTEM_BIOS(1, "Test", "Test")
-	ROMX_LOAD( "test.bin",    0xe000, 0x0800, CRC(e9e2b7b9) SHA1(e4e0b6984a2514b6ba3e97500d487ea1a68b7577), ROM_BIOS(2))
+	ROM_LOAD( "monitor.bin", 0xe000, 0x2000, CRC(587799bc) SHA1(1f677afa96722ca4ed2643eaca243548845fc854) )
+	ROM_LOAD( "cpm80.bin",   0xc000, 0x2000, CRC(7378e4f9) SHA1(c3c06c6f2e953546452ca6f82140a79d0e4953b4) )
 ROM_END
 
 ROM_START( okean240a )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_SYSTEM_BIOS(0, "Normal", "Normal")
-	ROMX_LOAD( "fddmonitor.bin", 0xe000, 0x2000, CRC(bcac5ca0) SHA1(602ab824704d3d5d07b3787f6227ff903c33c9d5), ROM_BIOS(1))
-	ROMX_LOAD( "fddcpm80.bin",   0xc000, 0x2000, CRC(b89a7e16) SHA1(b8f937c04f430be18e48f296ed3ef37194171204), ROM_BIOS(1))
+	ROM_LOAD( "fddmonitor.bin", 0xe000, 0x2000, CRC(bcac5ca0) SHA1(602ab824704d3d5d07b3787f6227ff903c33c9d5) )
+	ROM_LOAD( "fddcpm80.bin",   0xc000, 0x2000, CRC(b89a7e16) SHA1(b8f937c04f430be18e48f296ed3ef37194171204) )
+ROM_END
 
-	ROM_SYSTEM_BIOS(1, "Test", "Test")
-	ROMX_LOAD( "test.bin",    0xe000, 0x0800, CRC(e9e2b7b9) SHA1(e4e0b6984a2514b6ba3e97500d487ea1a68b7577), ROM_BIOS(2))
+ROM_START( okean240t )
+	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
+	ROM_LOAD( "test.bin",    0xe000, 0x0800, CRC(e9e2b7b9) SHA1(e4e0b6984a2514b6ba3e97500d487ea1a68b7577) )
 ROM_END
 
 /* Driver */
@@ -504,4 +505,4 @@ ROM_END
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT      INIT         COMPANY     FULLNAME       FLAGS */
 COMP( 1986, okean240,  0,       0,   okean240,  okean240,  okean240,  "<unknown>", "Okeah-240", GAME_NOT_WORKING | GAME_NO_SOUND)
 COMP( 1986, okean240a, okean240,0,   okean240a, okean240a, okean240,  "<unknown>", "Ocean-240 with fdd", GAME_NOT_WORKING | GAME_NO_SOUND)
-
+COMP( 1986, okean240t, okean240,0,   okean240t, okean240,  okean240,  "<unknown>", "Ocean-240 Test Rom", GAME_NO_SOUND)
