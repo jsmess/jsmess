9c131a4d6a76ee12d7d32111f1d229f5afbcf834
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-07-17 13:51:26 +0000

    SLC1 - committing WIP.
    git-svn-id: svn://messdev.no-ip.org/mess@12259 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/slc1.c
+++ src/mess/drivers/slc1.c
@@ -16,7 +16,7 @@ Hardware
 ToDo:
 - Everything
 
-This source was copied from tec1.c, so most of the code is wrong.
+
 
 ***************************************************************************/
 #define ADDRESS_MAP_MODERN
@@ -41,10 +41,8 @@ public:
 	required_device<cpu_device> m_maincpu;
 	required_device<device_t> m_speaker;
 	emu_timer *m_kbd_timer;
-	DECLARE_READ8_MEMBER( slc1_kbd_r );
-	DECLARE_WRITE8_MEMBER( slc1_digit_w );
-	DECLARE_WRITE8_MEMBER( digit_w );
-	DECLARE_WRITE8_MEMBER( slc1_segment_w );
+	DECLARE_READ8_MEMBER( io_r );
+	DECLARE_WRITE8_MEMBER( io_w );
 	UINT8 m_kbd;
 	UINT8 m_segment;
 	UINT8 m_digit;
@@ -64,23 +62,34 @@ public:
 
 ***************************************************************************/
 
-WRITE8_MEMBER( slc1_state::slc1_segment_w )
+WRITE8_MEMBER( slc1_state::io_w )
 {
-	m_segment = BITSWAP8(data, 4, 2, 1, 6, 7, 5, 3, 0);
+	UINT8 upper = offset >> 8;
+//	if (upper == 0x2f) return;
+
+
+	speaker_level_w(m_speaker, (upper == 0x29));
+
+//	output_set_led_value(0, BIT(data, 4));
+
+	// This computer sets each segment, one at a time. */
+
+	if ((offset & 0xff) < 8)
+	{
+		static const UINT8 segments[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+		UINT8 digit = data & 7;
+		UINT8 segment = segments[offset];
+		UINT8 segdata = output_get_digit_value(digit);
+
+		if (BIT(data, 7))
+			segdata |= segment;
+		else
+			segdata &= ~segment;
+
+		output_set_digit_value(digit, segdata);
+	}
 }
 
-WRITE8_MEMBER( slc1_state::digit_w )
-{
-	output_set_digit_value(offset, data);
-}	
-
-WRITE8_MEMBER( slc1_state::slc1_digit_w )
-{
-	if ((data & 0xfd) == 0x29)
-		speaker_level_w(m_speaker, BIT(data, 1));
-
-	m_digit = data & 0x3f;
-}
 
 
 /***************************************************************************
@@ -89,9 +98,14 @@ WRITE8_MEMBER( slc1_state::slc1_digit_w )
 
 ***************************************************************************/
 
-READ8_MEMBER( slc1_state::slc1_kbd_r )
+READ8_MEMBER( slc1_state::io_r )
 {
-	return m_kbd | input_port_read(machine(), "SHIFT");
+	UINT8 data = 0xff, upper = (offset >> 8) & 7;
+
+	if (upper == 5) data &= input_port_read(machine(), "X0");
+	if (upper == 6) data &= input_port_read(machine(), "X1");
+	if (upper == 7) data &= input_port_read(machine(), "X2");
+	return data;
 }
 
 
@@ -108,7 +122,6 @@ MACHINE_START_MEMBER( slc1_state )
 
 MACHINE_RESET_MEMBER( slc1_state )
 {
-	m_kbd = 0;
 }
 
 
@@ -121,19 +134,14 @@ MACHINE_RESET_MEMBER( slc1_state )
 
 static ADDRESS_MAP_START( slc1_map, AS_PROGRAM, 8, slc1_state )
 	ADDRESS_MAP_UNMAP_HIGH
-	ADDRESS_MAP_GLOBAL_MASK(0xffff)
+	ADDRESS_MAP_GLOBAL_MASK(0x4fff)
 	AM_RANGE(0x0000, 0x0fff) AM_ROM
-	AM_RANGE(0x5000, 0x53ff) AM_RAM
+	AM_RANGE(0x5000, 0x53ff) AM_RAM AM_MIRROR(0xc00)
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( slc1_io, AS_IO, 8, slc1_state )
 	ADDRESS_MAP_UNMAP_HIGH
-	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x00, 0x05) AM_WRITE(digit_w)
-	AM_RANGE(0x07, 0x07) AM_WRITE(slc1_digit_w)
-	AM_RANGE(0xf6, 0xf6) AM_READ(slc1_kbd_r)
-	AM_RANGE(0xf5, 0xf5) AM_WRITE(slc1_digit_w)
-	AM_RANGE(0xf4, 0xf4) AM_WRITE(slc1_segment_w)
+	AM_RANGE(0x0000, 0xffff) AM_READWRITE(io_r,io_w)
 ADDRESS_MAP_END
 
 
@@ -144,38 +152,26 @@ ADDRESS_MAP_END
 ***************************************************************************/
 
 static INPUT_PORTS_START( slc1 )
-	PORT_START("LINE0") /* KEY ROW 0 */
-	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("0") PORT_CODE(KEYCODE_0)	PORT_CHAR('0')
-	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("4") PORT_CODE(KEYCODE_4)	PORT_CHAR('4')
-	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("8") PORT_CODE(KEYCODE_8)	PORT_CHAR('8')
-	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C") PORT_CODE(KEYCODE_C)	PORT_CHAR('C')
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("+") PORT_CODE(KEYCODE_EQUALS)	PORT_CHAR('=')
+	PORT_START("X0")
+	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
+	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("3 B D 4 T") PORT_CODE(KEYCODE_3)
+	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("2 A C 3 L") PORT_CODE(KEYCODE_2)
+	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("1 9 B 2 S") PORT_CODE(KEYCODE_1)
+	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("0 8 A 1 B") PORT_CODE(KEYCODE_0)
 
-	PORT_START("LINE1") /* KEY ROW 1 */
-	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("1") PORT_CODE(KEYCODE_1)	PORT_CHAR('1')
-	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("5") PORT_CODE(KEYCODE_5)	PORT_CHAR('5')
-	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("9") PORT_CODE(KEYCODE_9)	PORT_CHAR('9')
-	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("D") PORT_CODE(KEYCODE_D)	PORT_CHAR('D')
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("-") PORT_CODE(KEYCODE_MINUS)	PORT_CHAR('-')
+	PORT_START("X1")
+	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
+	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("7 F H 8 GO") PORT_CODE(KEYCODE_7)
+	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("6 E G 7 BL") PORT_CODE(KEYCODE_6)
+	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("5 D F 6 K DEL") PORT_CODE(KEYCODE_5)
+	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("4 C E 5 D INS") PORT_CODE(KEYCODE_4)
 
-	PORT_START("LINE2") /* KEY ROW 2 */
-	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("2") PORT_CODE(KEYCODE_2)	PORT_CHAR('2')
-	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("6") PORT_CODE(KEYCODE_6)	PORT_CHAR('6')
-	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("A") PORT_CODE(KEYCODE_A)	PORT_CHAR('A')
-	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("E") PORT_CODE(KEYCODE_E)	PORT_CHAR('E')
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("GO") PORT_CODE(KEYCODE_ENTER)	PORT_CHAR(13)
-
-	PORT_START("LINE3") /* KEY ROW 3 */
-	PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("3") PORT_CODE(KEYCODE_3)	PORT_CHAR('3')
-	PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("7") PORT_CODE(KEYCODE_7)	PORT_CHAR('7')
-	PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("B") PORT_CODE(KEYCODE_B)	PORT_CHAR('B')
-	PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("F") PORT_CODE(KEYCODE_F)	PORT_CHAR('F')
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("AD") PORT_CODE(KEYCODE_J)	PORT_CHAR('J')
-
-	PORT_START("SHIFT")
-	PORT_BIT(0x1f, IP_ACTIVE_LOW, IPT_UNUSED)
-	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Shift") PORT_CODE(KEYCODE_LSHIFT)
-	PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+	PORT_START("X2")
+	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
+	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Seq C BG") PORT_CODE(KEYCODE_Q)
+	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("+-1 A SS") PORT_CODE(KEYCODE_W)
+	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Fu St DP") PORT_CODE(KEYCODE_E)
+	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("ADR Z BP") PORT_CODE(KEYCODE_R)
 INPUT_PORTS_END
 
 
