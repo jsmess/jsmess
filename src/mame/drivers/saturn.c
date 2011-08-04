bde4c84881f75c084518550ad3629da26bb5b41e
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2011-08-04 21:29:46 +0000

    Fixed regression
    git-svn-id: svn://messdev.no-ip.org/mess@12480 9849079f-6229-0410-a07f-84406ce305ba

--- src/mame/drivers/saturn.c
+++ src/mame/drivers/saturn.c
@@ -666,8 +666,9 @@ static void scu_dma_direct(address_space *space, UINT8 dma_ch)
 	saturn_state *state = space->machine().driver_data<saturn_state>();
 	UINT32 tmp_src,tmp_dst,tmp_size;
 	UINT8 single_dma_step;
+	UINT8 cd_transfer_flag;
 
-	if(state->m_scu.src_add[dma_ch] == 0)
+	if(state->m_scu.src_add[dma_ch] == 0 || state->m_scu.dst_add[dma_ch] != 4)
 	{
 	if(LOG_SCU) printf("DMA lv %d transfer START\n"
 			             "Start %08x End %08x Size %04x\n",dma_ch,state->m_scu.src[dma_ch],state->m_scu.dst[dma_ch],state->m_scu.size[dma_ch]);
@@ -696,8 +697,9 @@ static void scu_dma_direct(address_space *space, UINT8 dma_ch)
 	if(!(DWUP(dma_ch))) tmp_dst = state->m_scu.dst[dma_ch];
 
 	single_dma_step = state->m_scu.dst_add[dma_ch];
+	cd_transfer_flag = state->m_scu.src_add[dma_ch] == 0 && state->m_scu.src[dma_ch] == 0x05818000;
 
-	if(single_dma_step > 4)
+	if(single_dma_step > 4 && !cd_transfer_flag)
 		single_dma_step = 4;
 
 	if(single_dma_step == 2)
@@ -706,7 +708,7 @@ static void scu_dma_direct(address_space *space, UINT8 dma_ch)
 	for (; state->m_scu.size[dma_ch] > 0; state->m_scu.size[dma_ch]-=single_dma_step)
 	{
 		/* Many games directly accesses CD-ROM register 0x05818000, it must be a dword access with current implementation otherwise it won't work */
-		if(state->m_scu.src_add[dma_ch] == 0)
+		if(cd_transfer_flag)
 		{
 			space->write_dword(state->m_scu.dst[dma_ch],  space->read_dword(state->m_scu.src[dma_ch]  ));
 			if(state->m_scu.dst_add[dma_ch] == 8)
@@ -790,7 +792,7 @@ static void scu_dma_indirect(address_space *space,UINT8 dma_ch)
 		if(indirect_src & 0x80000000)
 			job_done = 1;
 
-		if(state->m_scu.src_add[dma_ch] == 0)
+		if(state->m_scu.src_add[dma_ch] == 0 || state->m_scu.dst_add[dma_ch] != 4)
 		{
 			if(LOG_SCU) printf("DMA lv %d indirect mode transfer START\n"
 					           "Index %08x Start %08x End %08x Size %04x\n",dma_ch,tmp_src,indirect_src,indirect_dst,indirect_size);
