84a8c705b349616a79433a0cf53dd3cb367ff7bb
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-08-10 14:05:19 +0000

    Modernised various non-working drivers.
    git-svn-id: svn://messdev.no-ip.org/mess@12555 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/vt240.c
+++ src/mess/drivers/vt240.c
@@ -8,6 +8,7 @@
     mc7105 - failing the rom check. If you skip that, it is waiting on i/o.
 
 ****************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/i8085/i8085.h"
@@ -21,17 +22,25 @@ public:
 	vt240_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag) { }
 
+	DECLARE_READ8_MEMBER(v240_00_r);
 };
 
 
-static ADDRESS_MAP_START(vt240_mem, AS_PROGRAM, 8)
+READ8_MEMBER( vt240_state::v240_00_r )
+{
+	return 4 | (machine().rand() & 0x20);
+}
+
+static ADDRESS_MAP_START(vt240_mem, AS_PROGRAM, 8, vt240_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0x7fff) AM_ROM
 	AM_RANGE(0x8000, 0x87ff) AM_RAM
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( vt240_io , AS_IO, 8)
+static ADDRESS_MAP_START(vt240_io, AS_IO, 8, vt240_state)
 	ADDRESS_MAP_UNMAP_HIGH
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0x00, 0x00) AM_READ(v240_00_r)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -67,12 +76,10 @@ static MACHINE_CONFIG_START( vt240, vt240_state )
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
 	MCFG_SCREEN_SIZE(640, 480)
 	MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
+	MCFG_VIDEO_START(vt240)
 	MCFG_SCREEN_UPDATE(vt240)
-
 	MCFG_PALETTE_LENGTH(2)
 	MCFG_PALETTE_INIT(black_and_white)
-
-	MCFG_VIDEO_START(vt240)
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -85,15 +92,14 @@ ROM_START( mc7105 )
 	ROM_REGION( 0x20000, "subcpu", ROMREGION_ERASEFF )
 	ROM_LOAD16_BYTE( "029.bin", 0x00000, 0x8000, CRC(4a6db217) SHA1(47637325609ea19ffab61fe31e2700d72fa50729))
 	ROM_LOAD16_BYTE( "031.bin", 0x00001, 0x8000, CRC(47129579) SHA1(39de9e2e26f90c5da5e72a09ff361c1a94b9008a))
-
 	ROM_LOAD16_BYTE( "030.bin", 0x10000, 0x8000, CRC(05fd7b75) SHA1(2ad8c14e76accfa1b9b8748c58e9ebbc28844a47))
 	ROM_LOAD16_BYTE( "032.bin", 0x10001, 0x8000, CRC(e81d93c4) SHA1(982412a7a6e65d6f6b4f66bd093e54ee16f31384))
 ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    CONFIG COMPANY   FULLNAME       FLAGS */
-//COMP( 1983, vt240,  0,       0,   vt220,  vt220,   0,          "Digital Equipment Corporation",   "VT240",      GAME_NOT_WORKING | GAME_NO_SOUND)
-//COMP( 1983, vt241,  0,       0,   vt220,  vt220,   0,          "Digital Equipment Corporation",   "VT241",      GAME_NOT_WORKING | GAME_NO_SOUND)
-COMP( 1983, mc7105,  0,       0,	vt240,	vt240,	 0, 		 "Elektronika",   "MC7105",		GAME_NOT_WORKING | GAME_NO_SOUND)
+/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY                      FULLNAME       FLAGS */
+//COMP( 1983, vt240,  0,      0,       vt220,     vt220,   0,  "Digital Equipment Corporation", "VT240", GAME_NOT_WORKING | GAME_NO_SOUND)
+//COMP( 1983, vt241,  0,      0,       vt220,     vt220,   0,  "Digital Equipment Corporation", "VT241", GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 1983, mc7105, 0,      0,       vt240,     vt240,   0,  "Elektronika",                  "MC7105", GAME_NOT_WORKING | GAME_NO_SOUND)
 
