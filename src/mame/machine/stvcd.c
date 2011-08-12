fbceba86cbff86d790b7b79c624e5b4b9e0c99eb
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2011-08-12 01:56:18 +0000

    GCC 4.6 (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@12577 9849079f-6229-0410-a07f-84406ce305ba

--- src/mame/machine/stvcd.c
+++ src/mame/machine/stvcd.c
@@ -1146,9 +1146,9 @@ static void cd_exec_command(running_machine &machine)
 
 		case 0x7100:	// Read directory entry
 			CDROM_LOG(("%s:CD: Read Directory Entry\n",   machine.describe_context()))
-			UINT32 read_dir;
+//			UINT32 read_dir;
 
-			read_dir = ((cr3&0xff)<<16)|cr4;
+//			read_dir = ((cr3&0xff)<<16)|cr4;
 
 			if((cr3 >> 8) < 0x24)
 				cddevice = &filters[cr3 >> 8];
