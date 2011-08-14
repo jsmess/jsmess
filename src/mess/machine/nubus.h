4b7bb7b7196c3e63f08b580d8e0b1e4a4523d529
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2011-08-14 02:59:14 +0000

    mac: m2hires and vikbw ignore the top 4 address bits and thus are compatible with macii -bios 1 [R. Belmont]
    git-svn-id: svn://messdev.no-ip.org/mess@12597 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/nubus.h
+++ src/mess/machine/nubus.h
@@ -91,9 +91,9 @@ public:
 	static void static_set_cputag(device_t &device, const char *tag);
 
 	void add_nubus_card(device_nubus_card_interface *card);
-    void install_device(offs_t start, offs_t end, read8_delegate rhandler, write8_delegate whandler);
-    void install_device(offs_t start, offs_t end, read16_delegate rhandler, write16_delegate whandler);
-    void install_device(offs_t start, offs_t end, read32_delegate rhandler, write32_delegate whandler);
+    void install_device(offs_t start, offs_t end, read8_delegate rhandler, write8_delegate whandler, UINT32 mask=0xffffffff);
+    void install_device(offs_t start, offs_t end, read16_delegate rhandler, write16_delegate whandler, UINT32 mask=0xffffffff);
+    void install_device(offs_t start, offs_t end, read32_delegate rhandler, write32_delegate whandler, UINT32 mask=0xffffffff);
 	void install_bank(offs_t start, offs_t end, offs_t mask, offs_t mirror, const char *tag, UINT8 *data);
     void set_irq_line(int slot, int state);
 
@@ -144,7 +144,7 @@ public:
 	void set_nubus_device();
 
     // helper functions for card devices
-    void install_declaration_rom(device_t *dev, const char *romregion);
+    void install_declaration_rom(device_t *dev, const char *romregion, bool mirror_all_mb = false);
     void install_bank(offs_t start, offs_t end, offs_t mask, offs_t mirror, const char *tag, UINT8 *data);
 
     UINT32 get_slotspace() { return 0xf0000000 | (m_slot<<24); }
