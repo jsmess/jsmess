9cbb38e0abc12277ca1db2fc9d2f0a05c109364e
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-05 14:18:03 +0000

    OSBORNE1 - modernised
    git-svn-id: svn://messdev.no-ip.org/mess@12798 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/includes/osborne1.h
+++ src/mess/includes/osborne1.h
@@ -7,33 +7,65 @@
 #ifndef OSBORNE1_H_
 #define OSBORNE1_H_
 
+#include "emu.h"
+#include "cpu/z80/z80.h"
+#include "cpu/z80/z80daisy.h"
+#include "sound/beep.h"
 #include "machine/6821pia.h"
+#include "machine/6850acia.h"
+#include "machine/ram.h"
+#include "machine/wd17xx.h"
+#include "imagedev/flopdrv.h"
+#include "formats/basicdsk.h"
+
 
 class osborne1_state : public driver_device
 {
 public:
 	osborne1_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+	m_maincpu(*this, "maincpu"),
+	m_pia0(*this, "pia_0"),
+	m_pia1(*this, "pia_1"),
+	m_fdc(*this, "mb8877"),
+	m_beep(*this, BEEPER_TAG),
+	m_ram(*this, RAM_TAG)
+	{ }
 
-	UINT8	m_bank2_enabled;
-	UINT8	m_bank3_enabled;
+	required_device<cpu_device> m_maincpu;
+	required_device<pia6821_device> m_pia0;
+	required_device<pia6821_device> m_pia1;
+	required_device<device_t> m_fdc;
+	required_device<device_t> m_beep;
+	required_device<device_t> m_ram;
+	DECLARE_WRITE8_MEMBER(osborne1_0000_w);
+	DECLARE_WRITE8_MEMBER(osborne1_1000_w);
+	DECLARE_READ8_MEMBER(osborne1_2000_r);
+	DECLARE_WRITE8_MEMBER(osborne1_2000_w);
+	DECLARE_WRITE8_MEMBER(osborne1_3000_w);
+	DECLARE_WRITE8_MEMBER(osborne1_videoram_w);
+	DECLARE_WRITE8_MEMBER(osborne1_bankswitch_w);
+	DECLARE_WRITE_LINE_MEMBER(ieee_pia_irq_a_func);
+	DECLARE_WRITE8_MEMBER(video_pia_out_cb2_dummy);
+	DECLARE_WRITE8_MEMBER(video_pia_port_a_w);
+	DECLARE_WRITE8_MEMBER(video_pia_port_b_w);
+	DECLARE_WRITE_LINE_MEMBER(video_pia_irq_a_func);
+	bool m_bank2_enabled;
+	bool m_bank3_enabled;
 	UINT8	*m_bank4_ptr;
 	UINT8	*m_empty_4K;
 	/* IRQ states */
-	int		m_pia_0_irq_state;
-	int		m_pia_1_irq_state;
+	bool m_pia_0_irq_state;
+	bool m_pia_1_irq_state;
 	/* video related */
 	UINT8	m_new_start_x;
 	UINT8	m_new_start_y;
-	emu_timer	*m_video_timer;
-	UINT8	*m_charrom;
-	UINT8	m_charline;
-	UINT8	m_start_y;
+	emu_timer *m_video_timer;
+	UINT8	*m_p_chargen;
 	/* bankswitch setting */
 	UINT8	m_bankswitch;
-	UINT8	m_in_irq_handler;
-	/* beep state */
-	UINT8	m_beep;
+	bool m_in_irq_handler;
+	bool m_beep_state;
 };
 
 
@@ -41,15 +73,6 @@ public:
 
 extern const pia6821_interface osborne1_ieee_pia_config;
 extern const pia6821_interface osborne1_video_pia_config;
-
-WRITE8_HANDLER( osborne1_0000_w );
-WRITE8_HANDLER( osborne1_1000_w );
-READ8_HANDLER( osborne1_2000_r );
-WRITE8_HANDLER( osborne1_2000_w );
-WRITE8_HANDLER( osborne1_3000_w );
-WRITE8_HANDLER( osborne1_videoram_w );
-
-WRITE8_HANDLER( osborne1_bankswitch_w );
 
 DRIVER_INIT( osborne1 );
 MACHINE_RESET( osborne1 );
