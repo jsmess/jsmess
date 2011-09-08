e1681421145fe258a5c45466e8db26dfdd0a041f
wfp <wfp@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-08 18:37:54 +0000

    msx/msx2: Started creating a software list for MSX and MSX2 cartridges.  [Wilbert Pol]
    git-svn-id: svn://messdev.no-ip.org/mess@12816 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/msx.c
+++ src/mess/machine/msx.c
@@ -137,6 +137,7 @@ DEVICE_IMAGE_LOAD (msx_cart)
 				{ "NEOS MR6401",		false,		SLOT_ASCII8 },
 				{ "BS6202",				false,		SLOT_ASCII8 },
 				{ "BS6101",				false,		SLOT_ASCII8 },
+				{ "BS6101-16",			false,		SLOT_ASCII16 },
 			};
 
 			for ( int i = 0; i < ARRAY_LENGTH(mapper_types) && type < 0; i++ )
