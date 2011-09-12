f38e6f7833452885cfd517cb5c3fe345c212ca89
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-12 22:56:02 +0000

    UPD7220 quick fix for address bus, should fix Compis again
    git-svn-id: svn://messdev.no-ip.org/mess@12831 9849079f-6229-0410-a07f-84406ce305ba

--- src/mame/machine/archimds.c
+++ src/mame/machine/archimds.c
@@ -495,6 +495,7 @@ static READ32_HANDLER( ioc_ctrl_r )
 		}
 
 		case KART:	// keyboard read
+			printf("R\n");
 			archimedes_request_irq_b(space->machine(), ARCHIMEDES_IRQB_KBD_XMIT_EMPTY);
 			break;
 
@@ -561,6 +562,7 @@ static WRITE32_HANDLER( ioc_ctrl_w )
 			break;
 
 		case KART:
+			printf("%02x\n",data);
 			#if 0
 			if(data == 0x0d)
 				printf("\n");
