beb333bde7081664934e98115b5b977a2b5f68aa
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-13 19:53:09 +0000

    [TDV2324] Cleanup and PIC/PIT/FDC. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@12837 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/tdv2324.c
+++ src/mess/drivers/tdv2324.c
@@ -87,34 +87,10 @@
 */
 
 
-#define ADDRESS_MAP_MODERN
 
-#include "emu.h"
-#include "cpu/i8085/i8085.h"
-#include "cpu/m6800/m6800.h"
-#include "machine/s1410.h"
-#include "video/tms9927.h"
+#include "includes/tdv2324.h"
 
 
-class tdv2324_state : public driver_device
-{
-public:
-	tdv2324_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag),
-		  m_maincpu(*this, "maincpu"),
-		  m_subcpu(*this, "subcpu"),
-		  m_fdccpu(*this, "fdccpu")
-		{ }
-
-	required_device<cpu_device> m_maincpu;
-	required_device<cpu_device> m_subcpu;
-	required_device<cpu_device> m_fdccpu;
-	DECLARE_READ8_MEMBER( tdv2324_main_io_e6 );
-	virtual void video_start();
-	virtual bool screen_update(screen_device &screen, bitmap_t &bitmap, const rectangle &cliprect);
-};
-
-/* Read/Write handlers */
 
 // Not sure what this is for, i/o read at 0xE6 on maincpu, post fails if it does not return bit 4 set
 READ8_MEMBER( tdv2324_state::tdv2324_main_io_e6 )
@@ -122,15 +98,28 @@ READ8_MEMBER( tdv2324_state::tdv2324_main_io_e6 )
 	return 0x10; // TODO: this should actually return something meaningful, for now is enough to pass early boot test
 }
 
-/* Memory Maps */
+
+
+//**************************************************************************
+//  ADDRESS MAPS
+//**************************************************************************
+
+//-------------------------------------------------
+//  ADDRESS_MAP( tdv2324_mem )
+//-------------------------------------------------
 
 static ADDRESS_MAP_START( tdv2324_mem, AS_PROGRAM, 8, tdv2324_state )
-	AM_RANGE(0x0000, 0x07ff) AM_MIRROR(0x0800) AM_ROM AM_REGION("maincpu", 0)
+	AM_RANGE(0x0000, 0x07ff) AM_MIRROR(0x0800) AM_ROM AM_REGION(I8085A_0_TAG, 0)
 	/* when copying code to 4000 area it runs right off the end of rom;
 	 * I'm not sure if its supposed to mirror or read as open bus */
 	AM_RANGE(0x4000, 0x5fff) AM_RAM // 0x4000 has the boot code copied to it, 5fff and down are the stack
 	AM_RANGE(0x6000, 0x6fff) AM_RAM // used by the relocated boot code; shared?
 ADDRESS_MAP_END
+
+
+//-------------------------------------------------
+//  ADDRESS_MAP( tdv2324_io )
+//-------------------------------------------------
 
 static ADDRESS_MAP_START( tdv2324_io, AS_IO, 8, tdv2324_state )
 	//ADDRESS_MAP_GLOBAL_MASK(0xff)
@@ -140,12 +129,22 @@ static ADDRESS_MAP_START( tdv2324_io, AS_IO, 8, tdv2324_state )
 	AM_RANGE(0xE6,0xE6) AM_READ(tdv2324_main_io_e6) // bit 4 is a status for something, post will not continue until it returns 1
 ADDRESS_MAP_END
 
+
+//-------------------------------------------------
+//  ADDRESS_MAP( tdv2324_sub_mem )
+//-------------------------------------------------
+
 static ADDRESS_MAP_START( tdv2324_sub_mem, AS_PROGRAM, 8, tdv2324_state )
-	AM_RANGE(0x0000, 0x3fff) AM_ROM AM_REGION("subcpu", 0)
+	AM_RANGE(0x0000, 0x3fff) AM_ROM AM_REGION(I8085A_1_TAG, 0)
 	AM_RANGE(0x4000, 0x47ff) AM_RAM // ram exists at least here, acts as stack?
 	AM_RANGE(0x5000, 0x503f) AM_RAM // subcpu reads here (5000-5021) without writing first, implies this ram is shared
 	AM_RANGE(0x6000, 0x7fff) AM_RAM // it floodfills this area with 0; is this a framebuffer? or shared ram?
 ADDRESS_MAP_END
+
+
+//-------------------------------------------------
+//  ADDRESS_MAP( tdv2324_sub_io )
+//-------------------------------------------------
 
 static ADDRESS_MAP_START( tdv2324_sub_io, AS_IO, 8, tdv2324_state )
 	//ADDRESS_MAP_GLOBAL_MASK(0xff)
@@ -154,35 +153,63 @@ static ADDRESS_MAP_START( tdv2324_sub_io, AS_IO, 8, tdv2324_state )
 	AM_RANGE(0x30, 0x3f) AM_DEVREADWRITE_LEGACY("tms9937", tms9927_r, tms9927_w)
 ADDRESS_MAP_END
 
+
+//-------------------------------------------------
+//  ADDRESS_MAP( tdv2324_fdc_mem )
+//-------------------------------------------------
+
 static ADDRESS_MAP_START( tdv2324_fdc_mem, AS_PROGRAM, 8, tdv2324_state )
 	AM_RANGE(0x0000, 0x001f) AM_RAM // on-6802-die ram (optinally battery backed)
 	AM_RANGE(0x0020, 0x007f) AM_RAM // on-6802-die ram
 	AM_RANGE(0x0080, 0x07ff) AM_RAM // the fdc cpu explicitly tests this area with A5,5A pattern after the rom check
-	AM_RANGE(0xe000, 0xffff) AM_ROM AM_REGION("fdccpu", 0)
+	AM_RANGE(0xe000, 0xffff) AM_ROM AM_REGION(MC68802P_TAG, 0)
 ADDRESS_MAP_END
 
-/* Input Ports */
+
+
+//**************************************************************************
+//  INPUT PORTS
+//**************************************************************************
+
+//-------------------------------------------------
+//  INPUT_PORTS( tdv2324 )
+//-------------------------------------------------
 
 static INPUT_PORTS_START( tdv2324 )
 INPUT_PORTS_END
 
-/* Video */
+
+
+//**************************************************************************
+//  VIDEO
+//**************************************************************************
+
+//-------------------------------------------------
+//  VIDEO_START( tdv2324 )
+//-------------------------------------------------
 
 void tdv2324_state::video_start()
 {
 }
+
+//-------------------------------------------------
+//  SCREEN_UPDATE( tdv2324 )
+//-------------------------------------------------
 
 bool tdv2324_state::screen_update(screen_device &screen, bitmap_t &bitmap, const rectangle &cliprect)
 {
 	return 0;
 }
 
-static const tms9927_interface vtac_intf =
-{
-	"screen",
-	8,
-	NULL
-};
+
+
+//**************************************************************************
+//  DEVICE CONFIGURATION
+//**************************************************************************
+
+//-------------------------------------------------
+//  I8085_CONFIG( i8085_intf )
+//-------------------------------------------------
 
 static I8085_CONFIG( i8085_intf )
 {
@@ -192,6 +219,11 @@ static I8085_CONFIG( i8085_intf )
 	DEVCB_NULL	/* SOD changed callback (I8085A only) */
 };
 
+
+//-------------------------------------------------
+//  I8085_CONFIG( i8085_sub_intf )
+//-------------------------------------------------
+
 static I8085_CONFIG( i8085_sub_intf )
 {
 	DEVCB_NULL,			/* STATUS changed callback */
@@ -200,29 +232,135 @@ static I8085_CONFIG( i8085_sub_intf )
 	DEVCB_NULL	/* SOD changed callback (I8085A only) */
 };
 
-/* Machine Drivers */
+
+//-------------------------------------------------
+//  pic8259_interface pic_intf
+//-------------------------------------------------
+
+static const struct pic8259_interface pic_intf =
+{
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL
+};
+
+
+//-------------------------------------------------
+//  pit8253_config pit0_intf
+//-------------------------------------------------
+
+static const struct pit8253_config pit0_intf =
+{
+	{
+		{
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}, {
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}, {
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}
+	}
+};
+
+
+//-------------------------------------------------
+//  pit8253_config pit1_intf
+//-------------------------------------------------
+
+static const struct pit8253_config pit1_intf =
+{
+	{
+		{
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}, {
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}, {
+			0,
+			DEVCB_NULL,
+			DEVCB_NULL
+		}
+	}
+};
+
+
+//-------------------------------------------------
+//  wd17xx_interface fdc_intf
+//-------------------------------------------------
+
+static const floppy_interface tdv2324_floppy_interface =
+{
+    DEVCB_NULL,
+    DEVCB_NULL,
+    DEVCB_NULL,
+    DEVCB_NULL,
+    DEVCB_NULL,
+    FLOPPY_STANDARD_8_DSDD,
+    LEGACY_FLOPPY_OPTIONS_NAME(default),
+    "floppy_8",
+	NULL
+};
+
+static const wd17xx_interface fdc_intf =
+{
+	DEVCB_NULL,
+	DEVCB_NULL,
+	DEVCB_NULL,
+	{ FLOPPY_0, NULL, NULL, NULL }
+};
+
+
+//-------------------------------------------------
+//  tms9927_interface vtac_intf
+//-------------------------------------------------
+
+static const tms9927_interface vtac_intf =
+{
+	SCREEN_TAG,
+	8,
+	NULL
+};
+
+
+
+//**************************************************************************
+//  MACHINE CONFIGURATION
+//**************************************************************************
+
+//-------------------------------------------------
+//  MACHINE_CONFIG( tdv2324 )
+//-------------------------------------------------
 
 static MACHINE_CONFIG_START( tdv2324, tdv2324_state )
-	/* basic system hardware */
-	MCFG_CPU_ADD("maincpu", I8085A, 8700000/2) // ???
+	// basic system hardware
+	MCFG_CPU_ADD(I8085A_0_TAG, I8085A, 8700000/2) // ???
 	MCFG_CPU_PROGRAM_MAP(tdv2324_mem)
 	MCFG_CPU_IO_MAP(tdv2324_io)
 	MCFG_CPU_CONFIG(i8085_intf)
 
-	MCFG_CPU_ADD("subcpu", I8085A, 8000000/2) // ???
+	MCFG_CPU_ADD(I8085A_1_TAG, I8085A, 8000000/2) // ???
 	MCFG_CPU_PROGRAM_MAP(tdv2324_sub_mem)
 	MCFG_CPU_IO_MAP(tdv2324_sub_io)
 	MCFG_CPU_CONFIG(i8085_sub_intf)
 
-	MCFG_CPU_ADD("fdccpu", M6802, 4000000)
+	MCFG_CPU_ADD(MC68802P_TAG, M6802, 4000000)
 	MCFG_CPU_PROGRAM_MAP(tdv2324_fdc_mem)
 
-	/* video hardware */
-	MCFG_SCREEN_ADD( "screen", RASTER )
-	MCFG_SCREEN_REFRESH_RATE( 50 )
-	MCFG_SCREEN_FORMAT( BITMAP_FORMAT_INDEXED16 )
-	MCFG_SCREEN_SIZE( 800, 400 )
-	MCFG_SCREEN_VISIBLE_AREA( 0, 800-1, 0, 400-1 )
+	// video hardware
+	MCFG_SCREEN_ADD(SCREEN_TAG, RASTER)
+	MCFG_SCREEN_REFRESH_RATE(50)
+	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
+	MCFG_SCREEN_SIZE(800, 400)
+	MCFG_SCREEN_VISIBLE_AREA(0, 800-1, 0, 400-1)
 
 	MCFG_PALETTE_LENGTH(2)
 	MCFG_PALETTE_INIT(monochrome_green)
@@ -230,23 +368,37 @@ static MACHINE_CONFIG_START( tdv2324, tdv2324_state )
 	MCFG_TMS9927_ADD("tms9937", XTAL_25_39836MHz, vtac_intf)
 
 	// devices
+	MCFG_PIC8259_ADD(P8259A_TAG, pic_intf)
+	MCFG_PIT8253_ADD(P8253_5_0_TAG, pit0_intf)
+	MCFG_PIT8253_ADD(P8253_5_1_TAG, pit1_intf)
+	MCFG_FD1797_ADD(FD1797PL02_TAG, fdc_intf)
+	MCFG_LEGACY_FLOPPY_DRIVE_ADD(FLOPPY_0, tdv2324_floppy_interface)
+	MCFG_HARDDISK_ADD("harddisk0")
 	MCFG_S1410_ADD() // 104521-F ROM
 
 	// software list
 	MCFG_SOFTWARE_LIST_ADD("flop_list", "tdv2324")
 MACHINE_CONFIG_END
 
-/* ROMs */
+
+
+//**************************************************************************
+//  ROMS
+//**************************************************************************
+
+//-------------------------------------------------
+//  ROM( tdv2324 )
+//-------------------------------------------------
 
 ROM_START( tdv2324 )
-	ROM_REGION( 0x800, "maincpu", 0 )
-	ROM_LOAD( "962058-1.bin", 0x000, 0x800, CRC(3771aece) SHA1(36d3f03235f327d6c8682e5c167aed6dddfaa6ec) )
+	ROM_REGION( 0x800, I8085A_0_TAG, 0 )
+	ROM_LOAD( "962058-1.21g", 0x000, 0x800, CRC(3771aece) SHA1(36d3f03235f327d6c8682e5c167aed6dddfaa6ec) )
 
-	ROM_REGION( 0x4000, "subcpu", 0 )
+	ROM_REGION( 0x4000, I8085A_1_TAG, 0 )
 	ROM_LOAD( "962107-1.bin", 0x0000, 0x4000, CRC(29c1a139) SHA1(f55fa9075fdbfa6a3e94e5120270179f754d0ea5) )
 
-	ROM_REGION( 0x2000, "fdccpu", 0 )
-	ROM_LOAD( "962014-1.bin", 0x0000, 0x2000, CRC(d01c32cd) SHA1(1f00f5f5ff0c035eec6af820b5acb6d0c207b6db) )
+	ROM_REGION( 0x2000, MC68802P_TAG, 0 )
+	ROM_LOAD( "962014-1.13c", 0x0000, 0x2000, CRC(d01c32cd) SHA1(1f00f5f5ff0c035eec6af820b5acb6d0c207b6db) )
 
 	ROM_REGION( 0x4000, "chargen", 0 )
 	ROM_LOAD( "965268-1.bin", 0x0000, 0x4000, CRC(7222a85f) SHA1(e94074b68d90698734ab1fc38d156407846df47c) )
@@ -254,7 +406,11 @@ ROM_END
 
 #define rom_tdv2324m7 rom_tdv2324
 
-/* System Drivers */
 
-//    YEAR  NAME        PARENT  COMPAT  MACHINE     INPUT       INIT    COMPANY           FULLNAME                FLAGS
+
+//**************************************************************************
+//  SYSTEM DRIVERS
+//**************************************************************************
+
+//    YEAR  NAME      PARENT  COMPAT  MACHINE   INPUT     INIT  COMPANY     FULLNAME     FLAGS
 COMP( 1983, tdv2324,		0,		0,		tdv2324,		tdv2324,		0,		"Tandberg",		"TDV 2324",		GAME_NOT_WORKING|GAME_NO_SOUND)
---
+++ src/mess/includes/tdv2324.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#ifndef __TDV2324__
+#define __TDV2324__
+
+#define ADDRESS_MAP_MODERN
+
+#include "emu.h"
+#include "cpu/i8085/i8085.h"
+#include "cpu/m6800/m6800.h"
+#include "formats/basicdsk.h"
+#include "imagedev/flopdrv.h"
+#include "imagedev/harddriv.h"
+#include "machine/pit8253.h"
+#include "machine/pic8259.h"
+#include "machine/s1410.h"
+#include "machine/wd17xx.h"
+#include "video/tms9927.h"
+
+
+
+//**************************************************************************
+//  MACROS / CONSTANTS
+//**************************************************************************
+
+#define I8085A_0_TAG		"17f"
+#define MK3887N4_TAG		"15d"
+#define P8259A_TAG			"17d"
+#define P8253_5_0_TAG		"17c"
+#define P8253_5_1_TAG		"18c"
+
+#define I8085A_1_TAG		"subcpu"
+#define TMS9937NL_TAG		"tms9937"
+
+#define MC68802P_TAG		"12b"
+#define FD1797PL02_TAG		"fd1797"
+
+#define SCREEN_TAG			"screen"
+
+
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+class tdv2324_state : public driver_device
+{
+public:
+	tdv2324_state(const machine_config &mconfig, device_type type, const char *tag)
+		: driver_device(mconfig, type, tag),
+		  m_maincpu(*this, I8085A_0_TAG),
+		  m_subcpu(*this, I8085A_1_TAG),
+		  m_fdccpu(*this, MC68802P_TAG)
+	{ }
+
+	required_device<cpu_device> m_maincpu;
+	required_device<cpu_device> m_subcpu;
+	required_device<cpu_device> m_fdccpu;
+	
+	virtual void video_start();
+	virtual bool screen_update(screen_device &screen, bitmap_t &bitmap, const rectangle &cliprect);
+
+	DECLARE_READ8_MEMBER( tdv2324_main_io_e6 );
+};
+
+
+#endif
