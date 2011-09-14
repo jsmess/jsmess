9e4ce185c8c52be8c812b399e0185c75e39c6f17
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-14 13:02:43 +0000

    FP1100 - modernised
    git-svn-id: svn://messdev.no-ip.org/mess@12844 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/fp1100.c
+++ src/mess/drivers/fp1100.c
@@ -2,11 +2,48 @@
 
     Casio FP-1100
 
+    Info found at various sites:
+
+    Casio FP1000 and FP1100 are "pre-PC" personal computers, with Cassette,
+    Floppy Disk, Printer and 2 cart/expansion slots. They had 32K ROM, 64K
+    main RAM, 80x25 text display, 320x200, 640x200, 640x400 graphics display.
+    Floppy disk is 2x 5 1/4.
+
+    The FP1000 had 16K videoram and monochrome only. The monitor had a switch
+    to invert the display (swap foreground and background colours).
+
+    The FP1100 had 48K videoram and 8 colours.
+
+    Processors: Z80 @ 4MHz, uPD7801G @ 2MHz
+
+    Came with Basic built in, and you could run CP/M 2.2 from the floppy disk.
+
+    The keyboard is a separate unit. Sound capabilities are unknown.
+
     TODO:
     - irq sources and communications;
     - unimplemented instruction PER triggered
 
+unimplemented instruction: PER
+'sub' (00C0): unmapped i/o memory write to 00 = 00 & FF
+'sub' (00C8): unmapped i/o memory read from 02 & FF
+'sub' (03CF): unmapped i/o memory write to 00 = 00 & FF
+'sub' (010E): unmapped i/o memory write to 00 = 40 & FF
+'sub' (0113): unmapped i/o memory read from 02 & FF
+'sub' (0116): unmapped i/o memory read from 02 & FF
+'sub' (03CF): unmapped i/o memory write to 00 = 40 & FF
+'sub' (06A6): unmapped program memory write to F000 = 00 & FF
+'sub' (06B7): unmapped i/o memory write to 00 = 40 & FF
+'sub' (FF93): unmapped program memory write to F000 = F8 & FF
+'sub' (FFB1): unmapped i/o memory write to 00 = 40 & FF
+'sub' (FF8F): unmapped i/o memory write to 00 = 60 & FF
+'sub' (FF93): unmapped program memory write to F000 = 80 & FF
+'sub' (FFB1): unmapped i/o memory write to 00 = 40 & FF
+'sub' (06E4): unmapped i/o memory write to 00 = 47 & FF
+'sub' (06EA): unmapped program memory write to F000 = 70 & FF
+
 ****************************************************************************/
+#define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/z80/z80.h"
@@ -20,6 +57,20 @@ public:
 	fp1100_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag) { }
 
+	DECLARE_READ8_MEMBER(fp1100_mem_r);
+	DECLARE_WRITE8_MEMBER(fp1100_mem_w);
+	DECLARE_WRITE8_MEMBER(main_bank_w);
+	DECLARE_WRITE8_MEMBER(irq_mask_w);
+	DECLARE_WRITE8_MEMBER(main_to_sub_w);
+	DECLARE_READ8_MEMBER(sub_to_main_r);
+	DECLARE_READ8_MEMBER(unk_r);
+	DECLARE_WRITE8_MEMBER(slot_bank_w);
+	DECLARE_READ8_MEMBER(slot_id_r);
+	DECLARE_READ8_MEMBER(fp1100_vram_r);
+	DECLARE_WRITE8_MEMBER(fp1100_vram_w);
+	DECLARE_READ8_MEMBER(main_to_sub_r);
+	DECLARE_WRITE8_MEMBER(sub_to_main_w);
+	DECLARE_WRITE8_MEMBER(portc_w);
 	UINT8 *m_wram;
 	UINT8 *m_vram;
 	UINT8 m_mem_bank;
@@ -48,88 +99,73 @@ static SCREEN_UPDATE( fp1100 )
 	return 0;
 }
 
-static READ8_HANDLER( fp1100_mem_r )
+READ8_MEMBER( fp1100_state::fp1100_mem_r )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	if(state->m_mem_bank == 0 && offset <= 0x8fff)
+	if(m_mem_bank == 0 && offset <= 0x8fff)
 	{
-		UINT8 *ipl = space->machine().region("ipl")->base();
+		UINT8 *ipl = machine().region("ipl")->base();
 		return ipl[offset];
 	}
 
-	return state->m_wram[offset];
+	return m_wram[offset];
 }
 
-static WRITE8_HANDLER( fp1100_mem_w )
+WRITE8_MEMBER( fp1100_state::fp1100_mem_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	state->m_wram[offset] = data;
+	m_wram[offset] = data;
 }
 
-static WRITE8_HANDLER( main_bank_w )
+WRITE8_MEMBER( fp1100_state::main_bank_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	state->m_mem_bank = data & 2; //(1) RAM (0) ROM
-	state->m_slot_num = (state->m_slot_num & 3) | ((data & 1) << 2);
+	m_mem_bank = data & 2; //(1) RAM (0) ROM
+	m_slot_num = (m_slot_num & 3) | ((data & 1) << 2);
 }
 
-static WRITE8_HANDLER( irq_mask_w )
+WRITE8_MEMBER( fp1100_state::irq_mask_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
+	//if((irq_mask & 0x80) != (data & 0x80))
+	//  cputag_set_input_line(machine(), "sub", UPD7810_INTF2, HOLD_LINE);
 
-	//if((state->irq_mask & 0x80) != (data & 0x80))
-	//  cputag_set_input_line(space->machine(), "sub", UPD7810_INTF2, HOLD_LINE);
-
-	state->irq_mask = data;
-	printf("%02x\n",data);
+	irq_mask = data;
+	///printf("%02x\n",data);
 
 }
 
-static WRITE8_HANDLER( main_to_sub_w )
+WRITE8_MEMBER( fp1100_state::main_to_sub_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	space->machine().scheduler().synchronize(); // force resync
-	cputag_set_input_line(space->machine(), "sub", UPD7810_INTF2, ASSERT_LINE);
-	state->m_sub_latch = data;
+	machine().scheduler().synchronize(); // force resync
+	cputag_set_input_line(machine(), "sub", UPD7810_INTF2, ASSERT_LINE);
+	m_sub_latch = data;
 }
 
-static READ8_HANDLER( sub_to_main_r )
+READ8_MEMBER( fp1100_state::sub_to_main_r )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	space->machine().scheduler().synchronize(); // force resync
-//  cputag_set_input_line_and_vector(space->machine(), "maincpu", 0, CLEAR_LINE, 0xf0);
-	return state->m_main_latch;
+	machine().scheduler().synchronize(); // force resync
+//  cputag_set_input_line_and_vector(machine(), "maincpu", 0, CLEAR_LINE, 0xf0);
+	return m_main_latch;
 }
 
-static READ8_HANDLER( unk_r )
+READ8_MEMBER( fp1100_state::unk_r )
 {
 	return 0;
 }
 
-static WRITE8_HANDLER( slot_bank_w )
+WRITE8_MEMBER( fp1100_state::slot_bank_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	state->m_slot_num = (data & 3) | (state->m_slot_num & 4);
+	m_slot_num = (data & 3) | (m_slot_num & 4);
 }
 
-static READ8_HANDLER( slot_id_r )
+READ8_MEMBER( fp1100_state::slot_id_r )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-	return state->m_slot[state->m_slot_num & 7].id;
+	return m_slot[m_slot_num & 7].id;
 }
 
-static ADDRESS_MAP_START(fp1100_map, AS_PROGRAM, 8 )
+static ADDRESS_MAP_START(fp1100_map, AS_PROGRAM, 8, fp1100_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0xffff) AM_READWRITE(fp1100_mem_r,fp1100_mem_w)
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START(fp1100_io, AS_IO, 8 )
+static ADDRESS_MAP_START(fp1100_io, AS_IO, 8, fp1100_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0xff00, 0xff00) AM_READ(slot_id_r) AM_WRITE(slot_bank_w)
 	AM_RANGE(0xff80, 0xff80) AM_READ(sub_to_main_r) AM_WRITE(irq_mask_w)
@@ -137,61 +173,51 @@ static ADDRESS_MAP_START(fp1100_io, AS_IO, 8 )
 	AM_RANGE(0xffc0, 0xffc0) AM_READ(unk_r) AM_WRITE(main_to_sub_w)
 ADDRESS_MAP_END
 
-static READ8_HANDLER( fp1100_vram_r )
+READ8_MEMBER( fp1100_state::fp1100_vram_r )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	return state->m_vram[offset];
+	return m_vram[offset];
 }
 
-static WRITE8_HANDLER( fp1100_vram_w )
+WRITE8_MEMBER( fp1100_state::fp1100_vram_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	state->m_vram[offset] = data ^ 0xff;
+	m_vram[offset] = data ^ 0xff;
 }
 
-static READ8_HANDLER( main_to_sub_r )
+READ8_MEMBER( fp1100_state::main_to_sub_r )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	space->machine().scheduler().synchronize(); // force resync
-	cputag_set_input_line(space->machine(), "sub", UPD7810_INTF2, CLEAR_LINE);
-	return state->m_sub_latch;
+	machine().scheduler().synchronize(); // force resync
+	cputag_set_input_line(machine(), "sub", UPD7810_INTF2, CLEAR_LINE);
+	return m_sub_latch;
 }
 
-static WRITE8_HANDLER( sub_to_main_w )
+WRITE8_MEMBER( fp1100_state::sub_to_main_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
-
-	space->machine().scheduler().synchronize(); // force resync
-//  cputag_set_input_line_and_vector(space->machine(), "maincpu", 0, ASSERT_LINE, 0xf0);
-	state->m_main_latch = data;
+	machine().scheduler().synchronize(); // force resync
+//  cputag_set_input_line_and_vector(machine(), "maincpu", 0, ASSERT_LINE, 0xf0);
+	m_main_latch = data;
 }
 
-static ADDRESS_MAP_START(fp1100_slave_map, AS_PROGRAM, 8 )
+static ADDRESS_MAP_START(fp1100_slave_map, AS_PROGRAM, 8, fp1100_state )
 	AM_RANGE(0xff80, 0xffff) AM_RAM		/* upd7801 internal RAM */
 	AM_RANGE(0x0000, 0x0fff) AM_ROM AM_REGION("sub_ipl",0x0000)
 	AM_RANGE(0x1000, 0x1fff) AM_ROM AM_REGION("sub_ipl",0x1000)
-	AM_RANGE(0x2000, 0xdfff) AM_READWRITE(fp1100_vram_r,fp1100_vram_w) AM_BASE_MEMBER(fp1100_state,m_vram) //vram B/R/G
-	AM_RANGE(0xe000, 0xe000) AM_DEVWRITE_MODERN("crtc", mc6845_device, address_w)
-	AM_RANGE(0xe001, 0xe001) AM_DEVWRITE_MODERN("crtc", mc6845_device, register_w)
+	AM_RANGE(0x2000, 0xdfff) AM_READWRITE(fp1100_vram_r,fp1100_vram_w) AM_BASE(m_vram) //vram B/R/G
+	AM_RANGE(0xe000, 0xe000) AM_DEVWRITE("crtc", mc6845_device, address_w)
+	AM_RANGE(0xe001, 0xe001) AM_DEVWRITE("crtc", mc6845_device, register_w)
 	AM_RANGE(0xe400, 0xe400) AM_READ_PORT("DSW") AM_WRITENOP // key mux write
 	AM_RANGE(0xe800, 0xe800) AM_READ(main_to_sub_r) AM_WRITE(sub_to_main_w)
-	AM_RANGE(0xf000, 0xffff) AM_ROM AM_REGION("sub_ipl",0x2000)
+	AM_RANGE(0xf000, 0xff7f) AM_ROM AM_REGION("sub_ipl",0x2000)
 ADDRESS_MAP_END
 
-static WRITE8_HANDLER( portc_w )
+WRITE8_MEMBER( fp1100_state::portc_w )
 {
-	fp1100_state *state = space->machine().driver_data<fp1100_state>();
+	if((!(m_upd7801.portc & 8)) && data & 8)
+		cputag_set_input_line_and_vector(machine(), "maincpu", 0, HOLD_LINE,0xf8); // TODO
 
-	if((!(state->m_upd7801.portc & 8)) && data & 8)
-		cputag_set_input_line_and_vector(space->machine(), "maincpu", 0, HOLD_LINE,0xf8); // TODO
-
-	state->m_upd7801.portc = data;
+	m_upd7801.portc = data;
 }
 
-static ADDRESS_MAP_START(fp1100_slave_io, AS_IO, 8 )
+static ADDRESS_MAP_START(fp1100_slave_io, AS_IO, 8, fp1100_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x01, 0x01) AM_READNOP //key R
 	AM_RANGE(0x02, 0x02) AM_WRITE(portc_w)
@@ -294,21 +320,19 @@ static MACHINE_RESET(fp1100)
 	}
 }
 
-#if 0
 static const gfx_layout fp1100_chars_8x8 =
 {
 	8,8,
-	RGN_FRAC(1,1),
+	256,
 	1,
 	{ 0 },
-	{ STEP8(0,1) },
+	{ 7, 6, 5, 4, 3, 2, 1, 0 },
 	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8 },
 	8*8
 };
-#endif
 
 static GFXDECODE_START( fp1100 )
-//  GFXDECODE_ENTRY( "chargen", 0x0000, fp1100_chars_8x8, 0, 8 )
+	GFXDECODE_ENTRY( "chargen", 0x0000, fp1100_chars_8x8, 0, 1 )
 GFXDECODE_END
 
 static const UPD7810_CONFIG fp1100_slave_cpu_config = { TYPE_7801, NULL };
@@ -360,14 +384,11 @@ static MACHINE_CONFIG_START( fp1100, fp1100_state )
 	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
 	MCFG_SCREEN_SIZE(640, 480)
 	MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
+	MCFG_VIDEO_START(fp1100)
 	MCFG_SCREEN_UPDATE(fp1100)
-
 	MCFG_PALETTE_LENGTH(2)
 	MCFG_PALETTE_INIT(black_and_white)
-
-	MCFG_VIDEO_START(fp1100)
 	MCFG_GFXDECODE(fp1100)
-
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -379,6 +400,9 @@ ROM_START( fp1100 )
 	ROM_LOAD( "sub1.rom", 0x0000, 0x1000, CRC(8feda489) SHA1(917d5b398b9e7b9a6bfa5e2f88c5b99923c3c2a3))
 	ROM_LOAD( "sub2.rom", 0x1000, 0x1000, CRC(359f007e) SHA1(0188d5a7b859075cb156ee55318611bd004128d7))
 	ROM_LOAD( "sub3.rom", 0x2000, 0xf80, BAD_DUMP CRC(fb2b577a) SHA1(a9ae6b03e06ea2f5db30dfd51ebf5aede01d9672))
+
+	ROM_REGION( 0x0800, "chargen", 0 )
+	ROM_COPY( "sub_ipl", 0x2400, 0x0000, 0x0800 )
 ROM_END
 
 /* Driver */
