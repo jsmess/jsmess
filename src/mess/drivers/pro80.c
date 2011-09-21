fb725874a6e2cf9742caba61291cee55cf752c90
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-09-21 11:22:52 +0000

    PRO80 - added cassette save, and some notes (untested, not sure how to drive this thing).
    git-svn-id: svn://messdev.no-ip.org/mess@12907 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/pro80.c
+++ src/mess/drivers/pro80.c
@@ -9,21 +9,32 @@
         - Use messram for optional ram
         - Fix Step command (don't works due of missing interrupt emulation)
 
+The unit has a PIO, ports 40-43, but it was for expansion only.
+
+The cassette used 2 bits for input, plus a D flipflop and a 74LS221 oneshot.
+
+
 ****************************************************************************/
 
 #define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/z80/z80.h"
+#include "imagedev/cassette.h"
+#include "sound/wave.h"
 #include "pro80.lh"
 
 class pro80_state : public driver_device
 {
 public:
 	pro80_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag)
+		: driver_device(mconfig, type, tag),
+	m_maincpu(*this, "maincpu"),
+	m_cass(*this, CASSETTE_TAG)
 	{ }
 
+	required_device<cpu_device> m_maincpu;
+	required_device<cassette_image_device> m_cass;
 	void machine_reset();
 
 	DECLARE_WRITE8_MEMBER( digit_w );
@@ -52,6 +63,7 @@ WRITE8_MEMBER( pro80_state::segment_w )
 		if (!BIT(m_digit_sel, 3)) output_set_digit_value(3, data);
 		if (!BIT(m_digit_sel, 4)) output_set_digit_value(4, data);
 		if (!BIT(m_digit_sel, 5)) output_set_digit_value(5, data);
+		m_cass->output( BIT(data, 6) ? -1.0 : +1.0);
 
 		m_digit_sel = 0;
 	}
@@ -68,17 +80,18 @@ READ8_MEMBER( pro80_state::kp_r )
 	if (!BIT(m_digit_sel, 4)) data &= input_port_read(machine(), "LINE4");
 	if (!BIT(m_digit_sel, 5)) data &= input_port_read(machine(), "LINE5");
 
+	// cassette-in bits go here - bit 4 = sync bit, bit 5 = data bit
 	return data;
 }
 
-static ADDRESS_MAP_START(pro80_mem, AS_PROGRAM, 8, pro80_state)
+static ADDRESS_MAP_START( pro80_mem, AS_PROGRAM, 8, pro80_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x0000, 0x03ff) AM_ROM
 	AM_RANGE(0x1000, 0x13ff) AM_RAM
 	AM_RANGE(0x1400, 0x17ff) AM_RAM // 2nd RAM is optional
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( pro80_io , AS_IO, 8, pro80_state)
+static ADDRESS_MAP_START( pro80_io, AS_IO, 8, pro80_state )
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	//AM_RANGE(0x40, 0x43) Z80PIO
@@ -127,18 +140,26 @@ void pro80_state::machine_reset()
 }
 
 static MACHINE_CONFIG_START( pro80, pro80_state )
-    /* basic machine hardware */
-    MCFG_CPU_ADD("maincpu",Z80, XTAL_4MHz)
-    MCFG_CPU_PROGRAM_MAP(pro80_mem)
-    MCFG_CPU_IO_MAP(pro80_io)
+	/* basic machine hardware */
+	MCFG_CPU_ADD("maincpu",Z80, XTAL_4MHz)
+	MCFG_CPU_PROGRAM_MAP(pro80_mem)
+	MCFG_CPU_IO_MAP(pro80_io)
 
-    /* video hardware */
-    MCFG_DEFAULT_LAYOUT(layout_pro80)
+	/* video hardware */
+	MCFG_DEFAULT_LAYOUT(layout_pro80)
+
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_MONO("mono")
+	MCFG_SOUND_WAVE_ADD(WAVE_TAG, CASSETTE_TAG)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.25)
+
+	/* Devices */
+	MCFG_CASSETTE_ADD( CASSETTE_TAG, default_cassette_interface )
 MACHINE_CONFIG_END
 
 /* ROM definition */
 ROM_START( pro80 )
-    ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
+	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
 	// This rom dump is taken out of manual for this machine
 	ROM_LOAD( "pro80.bin", 0x0000, 0x0400, CRC(1bf6e0a5) SHA1(eb45816337e08ed8c30b589fc24960dc98b94db2))
 ROM_END
@@ -146,5 +167,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY     FULLNAME       FLAGS */
-COMP( 1981, pro80,  0,       0, 	pro80,		pro80,	 0, 	 "Protec",   "Pro-80",		GAME_NOT_WORKING | GAME_NO_SOUND)
-
+COMP( 1981, pro80,  0,      0,       pro80,     pro80,   0,      "Protec",   "Pro-80", GAME_NOT_WORKING | GAME_NO_SOUND)
