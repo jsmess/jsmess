fd437f27f72ec3b9f8e26f53162a0a51794b5aac
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-10-21 09:39:08 +0000

    New working driver : - Tomy Pyuuta Jr. [Anonymous source]
    -pc88va: Renamed pc88va to pc88va2 and added roms for pc88va (no whatsnew)
    
    git-svn-id: svn://messdev.no-ip.org/mess@13077 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/tutor.c
+++ src/mess/drivers/tutor.c
@@ -749,7 +749,13 @@ ROM_START(pyuuta)
 	ROM_LOAD("tomy29.7", 0x0000, 0x8000, CRC(7553bb6a) SHA1(fa41c45cb6d3daf7435f2a82f77dfa286003255e))      /* system ROM */
 ROM_END
 
-
+ROM_START(pyuutajr)
+	/*CPU memory space*/
+	ROM_REGION(0x14000,"maincpu",0)
+	ROM_LOAD( "ipl.rom", 0x0000, 0x4000, CRC(2ca37e62) SHA1(eebdc5c37d3b532edd5e5ca65eb785269ebd1ac0))      /* system ROM */
+ROM_END  
+  
 /*   YEAR    NAME    PARENT      COMPAT  MACHINE     INPUT   INIT    COMPANY     FULLNAME */
 COMP(1983?,  tutor,  0,          0,      tutor,      tutor,  tutor,  "Tomy",   "Tomy Tutor" , 0)
 COMP(1982,   pyuuta, tutor,      0,      tutor,      tutor,  pyuuta, "Tomy",   "Tomy Pyuuta" , 0)
+COMP(1983,   pyuutajr, tutor,      0,      tutor,      tutor,  pyuuta, "Tomy",   "Tomy Pyuuta Jr." , 0)
