2e701e6f0bb451deb8685c0f2a7be13f691bb378
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2011-11-06 20:35:59 +0000

    mac: Fixed name of Asante MC3NB card, added Apple NuBus Ethernet Card which the Asante is a perfect clone of, fixed 24-bit mirroring so ethernet cards work in all slots [R. Belmont]
    git-svn-id: svn://messdev.no-ip.org/mess@13182 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/nubus_asntmc3b.h
+++ src/mess/machine/nubus_asntmc3b.h
@@ -11,16 +11,15 @@
 //  TYPE DEFINITIONS
 //**************************************************************************
 
-// ======================> nubus_asntm3b_device
+// ======================> nubus_mac8390_device
 
-class nubus_asntm3b_device :
+class nubus_mac8390_device :
 		public device_t,
 		public device_nubus_card_interface
 {
 public:
         // construction/destruction
-        nubus_asntm3b_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
-		nubus_asntm3b_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock);
+		nubus_mac8390_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock);
 
 		// optional information overrides
 		virtual machine_config_constructor device_mconfig_additions() const;
@@ -47,8 +46,21 @@ private:
         UINT8 m_prom[16];
 };
 
+class nubus_asntmc3nb_device : public nubus_mac8390_device
+{
+public:
+    nubus_asntmc3nb_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock); 
+};
+
+class nubus_appleenet_device : public nubus_mac8390_device
+{
+public:
+    nubus_appleenet_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock); 
+    virtual const rom_entry *device_rom_region() const;
+};
 
 // device type definition
-extern const device_type NUBUS_ASNTMC3B;
+extern const device_type NUBUS_ASNTMC3NB;
+extern const device_type NUBUS_APPLEENET;
 
 #endif  /* __NUBUS_ASNTMC3B_H__ */
