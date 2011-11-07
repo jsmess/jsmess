f5b3c789cad2129dd5e86abd1f0aeaea4820b383
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-11-07 12:57:44 +0000

    - mbc200: mapped rest of hardware (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@13187 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mbc200.c
+++ src/mess/drivers/mbc200.c
@@ -47,36 +47,38 @@ public:
 	required_device<device_t> m_floppy1;		
 };
 
-static ADDRESS_MAP_START(mbc200_mem, AS_PROGRAM, 8, mbc200_state)
+static ADDRESS_MAP_START(mbc200_sub_mem, AS_PROGRAM, 8, mbc200_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x0000, 0x2fff ) AM_ROM
 	AM_RANGE( 0x3000, 0xffff ) AM_RAM 
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( mbc200_io , AS_IO, 8, mbc200_state)
+static ADDRESS_MAP_START( mbc200_sub_io , AS_IO, 8, mbc200_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
 	AM_RANGE(0x70, 0x73) AM_DEVREADWRITE("ppi8255_1", i8255_device, read, write)
-	//AM_RANGE(0x, 0x) AM_DEVREADWRITE("ppi8255_2", i8255_device, read, write)
 	AM_RANGE(0xb0, 0xb0) AM_DEVWRITE("crtc", mc6845_device, address_w)
 	AM_RANGE(0xb1, 0xb1) AM_DEVREADWRITE("crtc", mc6845_device, register_r, register_w)	
-	//AM_RANGE(0x0, 0x3) AM_DEVREADWRITE_LEGACY("fdc", wd17xx_r, wd17xx_w)
-	//AM_RANGE(0x, 0x) AM_DEVREADWRITE("i8251_1", i8251_device, data_r, data_w)
-	//AM_RANGE(0x, 0x) AM_DEVREADWRITE("i8251_1", i8251_device, status_r, control_w)
-	//AM_RANGE(0x, 0x) AM_DEVREADWRITE("i8251_2", i8251_device, data_r, data_w)
-	//AM_RANGE(0x, 0x) AM_DEVREADWRITE("i8251_2", i8251_device, status_r, control_w)
-ADDRESS_MAP_END
-/*
-static ADDRESS_MAP_START(mbc200_sub_mem, AS_PROGRAM, 8)
-	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE( 0x0000, 0x1fff ) AM_ROM
-	AM_RANGE( 0x2000, 0xffff ) AM_RAM 
+	//AM_RANGE(0xd0, 0xd3) AM_DEVREADWRITE("ppi8255_2", i8255_device, read, write)
 ADDRESS_MAP_END
 
-static ADDRESS_MAP_START( mbc200_sub_io , AS_IO, 8)
+static ADDRESS_MAP_START(mbc200_mem, AS_PROGRAM, 8, mbc200_state)
 	ADDRESS_MAP_UNMAP_HIGH
+	AM_RANGE( 0x0000, 0x0fff ) AM_ROM
+	AM_RANGE( 0x1000, 0xffff ) AM_RAM 
 ADDRESS_MAP_END
-*/
+
+static ADDRESS_MAP_START( mbc200_io , AS_IO, 8, mbc200_state)
+	ADDRESS_MAP_UNMAP_HIGH
+	ADDRESS_MAP_GLOBAL_MASK(0xff)
+	AM_RANGE(0xe0, 0xe0) AM_DEVREADWRITE("i8251_1", i8251_device, data_r, data_w)
+	AM_RANGE(0xe1, 0xe1) AM_DEVREADWRITE("i8251_1", i8251_device, status_r, control_w)	
+	AM_RANGE(0xe4, 0xe7) AM_DEVREADWRITE_LEGACY("fdc", wd17xx_r, wd17xx_w)
+	AM_RANGE(0xe8, 0xeb) AM_DEVREADWRITE("ppi8255_2", i8255_device, read, write)
+	AM_RANGE(0xec, 0xec) AM_DEVREADWRITE("i8251_2", i8251_device, data_r, data_w)	
+	AM_RANGE(0xed, 0xed) AM_DEVREADWRITE("i8251_2", i8251_device, status_r, control_w)
+ADDRESS_MAP_END
+
 /* Input ports */
 INPUT_PORTS_START( mbc200 )
 INPUT_PORTS_END
@@ -149,12 +151,11 @@ static MACHINE_CONFIG_START( mbc200, mbc200_state )
     /* basic machine hardware */
     MCFG_CPU_ADD("maincpu",Z80, XTAL_8MHz/2) // NEC D780C-1
     MCFG_CPU_PROGRAM_MAP(mbc200_mem)
-    MCFG_CPU_IO_MAP(mbc200_io)	
+    MCFG_CPU_IO_MAP(mbc200_io)
 
-    /*MCFG_CPU_ADD("subcpu",Z80, XTAL_8MHz/2) // NEC D780C-1
+    MCFG_CPU_ADD("subcpu",Z80, XTAL_8MHz/2) // NEC D780C-1
     MCFG_CPU_PROGRAM_MAP(mbc200_sub_mem)
     MCFG_CPU_IO_MAP(mbc200_sub_io)	
-*/
 	
     /* video hardware */
     MCFG_SCREEN_ADD("screen", RASTER)
@@ -182,9 +183,9 @@ MACHINE_CONFIG_END
 /* ROM definition */
 ROM_START( mbc200 )
     ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASEFF )
-	ROM_LOAD( "m5l2764.bin", 0x0000, 0x2000, CRC(377300a2) SHA1(8563172f9e7f84330378a8d179f4138be5fda099))
-	ROM_LOAD( "d2732a.bin",  0x2000, 0x1000, CRC(bf364ce8) SHA1(baa3a20a5b01745a390ef16628dc18f8d682d63b))
+	ROM_LOAD( "d2732a.bin",  0x0000, 0x1000, CRC(bf364ce8) SHA1(baa3a20a5b01745a390ef16628dc18f8d682d63b))
 	ROM_REGION( 0x10000, "subcpu", ROMREGION_ERASEFF )
+	ROM_LOAD( "m5l2764.bin", 0x0000, 0x2000, CRC(377300a2) SHA1(8563172f9e7f84330378a8d179f4138be5fda099))
 ROM_END
 
 /* Driver */
