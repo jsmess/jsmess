8464c81e5c2192445cff4b9a4c876719877e9b9d
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2011-11-13 14:20:14 +0000

    [PET] D9060 is now communicating with SASI controller. (nw) [6522VIA] Implemented CA2 pulse output mode. [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@13237 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/machine/6522via.h
+++ src/emu/machine/6522via.h
@@ -119,6 +119,7 @@ private:
 	static const device_timer_id TIMER_SHIFT = 0;
 	static const device_timer_id TIMER_T1 = 1;
 	static const device_timer_id TIMER_T2 = 2;
+	static const device_timer_id TIMER_CA2 = 3;
 
     attotime cycles_to_time(int c);
     UINT32 time_to_cycles(attotime t);
@@ -179,6 +180,7 @@ private:
     emu_timer *m_t2;
     attotime m_time2;
     UINT8 m_t2_active;
+	emu_timer *m_ca2_timer;
 
     emu_timer *m_shift_timer;
     UINT8 m_shift_counter;
