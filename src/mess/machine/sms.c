3a2476a4190e63cb5e040251a55997c6ca0c175a
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-11-14 09:09:34 +0000

    Removed HSI files not used and removed pce and sms referencing non existing extrainfo (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@13244 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/sms.c
+++ src/mess/machine/sms.c
@@ -5,7 +5,6 @@
 #include "video/smsvdp.h"
 #include "sound/2413intf.h"
 #include "imagedev/cartslot.h"
-#include "hashfile.h"
 
 #define VERBOSE 0
 #define LOG(x) do { if (VERBOSE) logerror x; } while (0)
@@ -1478,7 +1477,6 @@ DEVICE_IMAGE_LOAD( sms_cart )
 	if (image.software_entry() == NULL)
 	{
 		size = image.length();
-		extrainfo = hashfile_extrainfo(image);
 	}
 	else
 		size = image.get_software_region_length("rom");
