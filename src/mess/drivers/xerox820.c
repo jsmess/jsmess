757fb0078c63ba82dea7e651f4594663c5976016
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2011-12-01 08:02:32 +0000

    Added skeleton for : MK-83 [Enrico Lazzerini]
    git-svn-id: svn://messdev.no-ip.org/mess@13429 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/xerox820.c
+++ src/mess/drivers/xerox820.c
@@ -22,6 +22,9 @@
     http://www.classiccmp.org/dunfield/img41867/system.htm
     http://www.microcodeconsulting.com/z80/plus2.htm
 
+	Note:
+	- MK-82 have same roms as original Big Board
+	- MK-83 have 256K of RAM
 */
 
 #define ADDRESS_MAP_MODERN
@@ -942,6 +945,12 @@ ROM_START( bigboard )
 	ROM_LOAD( "bigboard.u73", 0x0000, 0x0800, CRC(10bf0d81) SHA1(7ec73670a4d9d6421a5d6a4c4edc8b7c87923f6c) )
 ROM_END
 
+ROM_START( mk83 )
+	ROM_REGION( 0x1000, "monitor", 0 )
+	ROM_LOAD( "2732mk83.bin", 0x0000, 0x1000, CRC(a845c7e1) SHA1(3ccf629c5cd384953794ac4a1d2b45678bd40e92))
+	ROM_REGION( 0x800, "chargen", 0 )
+	ROM_LOAD( "2716mk83.bin", 0x0000, 0x0800, CRC(10bf0d81) SHA1(7ec73670a4d9d6421a5d6a4c4edc8b7c87923f6c))
+ROM_END
 /* System Drivers */
 
 /*    YEAR  NAME        PARENT      COMPAT  MACHINE     INPUT       INIT    COMPANY                         FULLNAME        FLAGS */
@@ -949,3 +958,4 @@ COMP( 1981, xerox820,	0,			0,		xerox820,	xerox820,	0,		"Xerox",						"Xerox 820"
 COMP( 1983, xerox820ii, xerox820,   0,      xerox820ii, xerox820,   0,      "Xerox",                        "Xerox 820-II", GAME_NOT_WORKING | GAME_NO_SOUND)
 COMP( 1983, xerox168,   xerox820,   0,      xerox168,   xerox820,   0,      "Xerox",                        "Xerox 16/8",   GAME_NOT_WORKING | GAME_NO_SOUND)
 COMP( 1980, bigboard,   0,          0,      xerox820,   xerox820,   0,      "Digital Research Computers",   "Big Board",    GAME_NOT_WORKING | GAME_NO_SOUND)
+COMP( 198?, mk83,   	0,          0,      xerox820,   xerox820,   0,      "<unknown>",   					"MK-83",    GAME_NOT_WORKING | GAME_NO_SOUND)
