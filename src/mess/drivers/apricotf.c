31b473cc3b821c6a844bb14605bb94c6d4c8fac7
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2011-12-19 09:23:43 +0000

    [APRICOTF] Reorganization. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@13625 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/apricotf.c
+++ src/mess/drivers/apricotf.c
@@ -164,88 +164,6 @@ WRITE8_MEMBER( f1_state::system_w )
 //  KEYBOARD
 //**************************************************************************
 
-UINT8 f1_state::read_keyboard()
-{
-	UINT8 data = 0xff;
-	
-	if (!BIT(m_kb_y, 0)) data &= input_port_read(machine(), "Y0");
-	if (!BIT(m_kb_y, 1)) data &= input_port_read(machine(), "Y1");
-	if (!BIT(m_kb_y, 2)) data &= input_port_read(machine(), "Y2");
-	if (!BIT(m_kb_y, 3)) data &= input_port_read(machine(), "Y3");
-	if (!BIT(m_kb_y, 4)) data &= input_port_read(machine(), "Y4");
-	if (!BIT(m_kb_y, 5)) data &= input_port_read(machine(), "Y5");
-	if (!BIT(m_kb_y, 6)) data &= input_port_read(machine(), "Y6");
-	if (!BIT(m_kb_y, 7)) data &= input_port_read(machine(), "Y7");
-	if (!BIT(m_kb_y, 8)) data &= input_port_read(machine(), "Y8");
-	if (!BIT(m_kb_y, 9)) data &= input_port_read(machine(), "Y9");
-	if (!BIT(m_kb_y, 10)) data &= input_port_read(machine(), "YA");
-	if (!BIT(m_kb_y, 11)) data &= input_port_read(machine(), "YB");
-	if (!BIT(m_kb_y, 12)) data &= input_port_read(machine(), "YC");
-	
-	return data;
-}
-
-
-READ8_MEMBER( f1_state::kb_lo_r )
-{
-	return read_keyboard() & 0x0f;
-}
-
-
-READ8_MEMBER( f1_state::kb_hi_r )
-{
-	return read_keyboard() >> 4;
-}
-
-
-READ8_MEMBER( f1_state::kb_p6_r )
-{
-	/*
-	
-		bit		description
-		
-		P60		
-		P61		SHIFT
-		P62		CONTROL
-	
-	*/
-
-	UINT8 modifiers = input_port_read(machine(), "MODIFIERS");
-	
-	return modifiers << 1;
-}
-
-
-WRITE8_MEMBER( f1_state::kb_p3_w )
-{
-	// bit 1 controls the IR LEDs
-}
-
-
-WRITE8_MEMBER( f1_state::kb_y0_w )
-{
-	m_kb_y = (m_kb_y & 0xfff0) | (data & 0x0f);
-}
-
-
-WRITE8_MEMBER( f1_state::kb_y4_w )
-{
-	m_kb_y = (m_kb_y & 0xff0f) | ((data & 0x0f) << 4);
-}
-
-
-WRITE8_MEMBER( f1_state::kb_y8_w )
-{
-	m_kb_y = (m_kb_y & 0xf0ff) | ((data & 0x0f) << 8);
-}
-
-
-WRITE8_MEMBER( f1_state::kb_yc_w )
-{
-	m_kb_y = (m_kb_y & 0xf0ff) | ((data & 0x0f) << 12);
-}
-
-
 
 //**************************************************************************
 //  ADDRESS MAPS
@@ -266,18 +184,6 @@ ADDRESS_MAP_END
 
 
 //-------------------------------------------------
-//  ADDRESS_MAP( act_f1p_mem )
-//-------------------------------------------------
-
-static ADDRESS_MAP_START( act_f1p_mem, AS_PROGRAM, 16, f1p_state )
-	ADDRESS_MAP_UNMAP_HIGH
-	AM_RANGE(0x00000, 0xeffff) AM_RAM// AM_BASE(m_p_videoram)
-	AM_RANGE(0xf0000, 0xf7fff) AM_RAM
-	AM_RANGE(0xf8000, 0xfffff) AM_ROM AM_REGION(I8086_TAG, 0)
-ADDRESS_MAP_END
-
-
-//-------------------------------------------------
 //  ADDRESS_MAP( act_f1_io )
 //-------------------------------------------------
 
@@ -292,22 +198,6 @@ static ADDRESS_MAP_START( act_f1_io, AS_IO, 16, f1_state )
 ADDRESS_MAP_END
 
 
-//-------------------------------------------------
-//  ADDRESS_MAP( kb_io )
-//-------------------------------------------------
-/*
-static ADDRESS_MAP_START( kb_io, AS_IO, 8, f1_state )
-	AM_RANGE(0x00, 0x00) AM_READ(kb_lo_r)
-	AM_RANGE(0x01, 0x01) AM_READ(kb_hi_r)
-	AM_RANGE(0x03, 0x03) AM_WRITE(kb_p3_w)
-	AM_RANGE(0x04, 0x04) AM_WRITE(kb_y0_w)
-	AM_RANGE(0x05, 0x05) AM_WRITE(kb_y4_w)
-	AM_RANGE(0x06, 0x06) AM_READWRITE(kb_p6_r, kb_yc_w)
-	AM_RANGE(0x07, 0x07) AM_WRITE(kb_y8_w)
-ADDRESS_MAP_END
-*/
-
-
 //**************************************************************************
 //  INPUT PORTS
 //**************************************************************************
@@ -317,139 +207,7 @@ ADDRESS_MAP_END
 //-------------------------------------------------
 
 static INPUT_PORTS_START( act )
-	PORT_START("Y0")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y1")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y2")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y3")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y4")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y5")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y6")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y7")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y8")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("Y9")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("YA")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("YB")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	
-	PORT_START("YC")
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD )
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD )
-
-	PORT_START("MODIFIERS")
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_KEYBOARD ) PORT_NAME("CTRL") PORT_CODE(KEYCODE_LCONTROL) PORT_CHAR(UCHAR_MAMEKEY(LCONTROL))
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_KEYBOARD ) PORT_NAME("SHIFT") PORT_CODE(KEYCODE_LSHIFT) PORT_CODE(KEYCODE_RSHIFT) PORT_CHAR(UCHAR_SHIFT_1)
+	// defined in machine/apricotkb.c
 INPUT_PORTS_END
 
 
@@ -457,6 +215,16 @@ INPUT_PORTS_END
 //**************************************************************************
 //  DEVICE CONFIGURATION
 //**************************************************************************
+
+//-------------------------------------------------
+//  APRICOT_KEYBOARD_INTERFACE( kb_intf )
+//-------------------------------------------------
+
+static APRICOT_KEYBOARD_INTERFACE( kb_intf )
+{
+	DEVCB_NULL
+};
+
 
 //-------------------------------------------------
 //  Z80DART_INTERFACE( sio_intf )
@@ -597,40 +365,7 @@ static MACHINE_CONFIG_START( act_f1, f1_state )
 	MCFG_GFXDECODE(act_f1)
 
 	/* Devices */
-	MCFG_Z80DART_ADD(Z80SIO2_TAG, 2500000, sio_intf)
-	MCFG_Z80CTC_ADD(Z80CTC_TAG, 2500000, ctc_intf)
-	MCFG_WD2797_ADD(WD2797_TAG, fdc_intf)
-	MCFG_LEGACY_FLOPPY_2_DRIVES_ADD(act_floppy_interface)
-	MCFG_CENTRONICS_ADD(CENTRONICS_TAG, centronics_intf)
-MACHINE_CONFIG_END
-
-
-//-------------------------------------------------
-//  MACHINE_CONFIG( act_f1p )
-//-------------------------------------------------
-
-static MACHINE_CONFIG_START( act_f1p, f1p_state )
-	/* basic machine hardware */
-	MCFG_CPU_ADD(I8086_TAG, I8086, XTAL_15MHz/3)
-	MCFG_CPU_PROGRAM_MAP(act_f1p_mem)
-	MCFG_CPU_IO_MAP(act_f1_io)
-/*
-	MCFG_CPU_ADD(UPD7507C_TAG, UPD7507, XTAL_32_768kHz)
-	MCFG_CPU_IO_MAP(kb_io)
-*/
-	/* video hardware */
-	MCFG_SCREEN_ADD(SCREEN_TAG, RASTER)
-	MCFG_SCREEN_REFRESH_RATE(50)
-	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-	MCFG_SCREEN_FORMAT(BITMAP_FORMAT_INDEXED16)
-	MCFG_SCREEN_SIZE(640, 256)
-	MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 256-1)
-	MCFG_PALETTE_LENGTH(2)
-	MCFG_PALETTE_INIT(black_and_white)
-	MCFG_GFXDECODE(act_f1)
-	MCFG_DEFAULT_LAYOUT( layout_lcd )
-
-	/* Devices */
+	MCFG_APRICOT_KEYBOARD_ADD(kb_intf)
 	MCFG_Z80DART_ADD(Z80SIO2_TAG, 2500000, sio_intf)
 	MCFG_Z80CTC_ADD(Z80CTC_TAG, 2500000, ctc_intf)
 	MCFG_WD2797_ADD(WD2797_TAG, fdc_intf)
@@ -652,9 +387,6 @@ ROM_START( f1 )
 	ROM_REGION( 0x8000, I8086_TAG, 0 )
 	ROM_LOAD16_BYTE( "lo_f1_1.6.8f",  0x0000, 0x4000, CRC(be018be2) SHA1(80b97f5b2111daf112c69b3f58d1541a4ba69da0) )	// Labelled F1 - LO Vr. 1.6
 	ROM_LOAD16_BYTE( "hi_f1_1.6.10f", 0x0001, 0x4000, CRC(bbba77e2) SHA1(e62bed409eb3198f4848f85fccd171cd0745c7c0) )	// Labelled F1 - HI Vr. 1.6
-
-	ROM_REGION( 0x800, UPD7507C_TAG, 0 )
-	ROM_LOAD( "keyboard controller upd7507c.ic2", 0x000, 0x800, NO_DUMP )
 ROM_END
 
 #define rom_f1e rom_f1
@@ -669,26 +401,6 @@ ROM_START( f10 )
 	ROM_REGION( 0x8000, I8086_TAG, 0 )
 	ROM_LOAD16_BYTE( "lo_f10_3.1.1.8f",  0x0000, 0x4000, CRC(bfd46ada) SHA1(0a36ef379fa9af7af9744b40c167ce6e12093485) )	// Labelled LO-FRange Vr3.1.1
 	ROM_LOAD16_BYTE( "hi_f10_3.1.1.10f", 0x0001, 0x4000, CRC(67ad5b3a) SHA1(a5ececb87476a30167cf2a4eb35c03aeb6766601) )	// Labelled HI-FRange Vr3.1.1
-
-	ROM_REGION( 0x800, UPD7507C_TAG, 0 )
-	ROM_LOAD( "keyboard controller upd7507c.ic2", 0x000, 0x800, NO_DUMP )
-ROM_END
-
-
-//-------------------------------------------------
-//  ROM( fp )
-//-------------------------------------------------
-
-ROM_START( fp )
-	ROM_REGION( 0x8000, I8086_TAG, 0 )
-	ROM_LOAD16_BYTE( "lo_fp_3.1.ic20", 0x0000, 0x4000, CRC(0572add2) SHA1(c7ab0e5ced477802e37f9232b5673f276b8f5623) )	// Labelled 11212721 F97E PORT LO VR 3.1
-	ROM_LOAD16_BYTE( "hi_fp_3.1.ic9",  0x0001, 0x4000, CRC(3903674b) SHA1(8418682dcc0c52416d7d851760fea44a3cf2f914) )	// Labelled 11212721 BD2D PORT HI VR 3.1
-
-	ROM_REGION( 0x1000, "hd63b01v1", 0 )
-	ROM_LOAD( "voice interface hd63b01v01.ic29", 0x0000, 0x1000, NO_DUMP )
-
-	ROM_REGION( 0x800, UPD7507C_TAG, 0 )
-	ROM_LOAD( "keyboard controller upd7507c.ic2", 0x000, 0x800, NO_DUMP )
 ROM_END
 
 
@@ -702,4 +414,3 @@ COMP( 1984, f1,    0,      0,      act_f1,    act,    0,     "ACT",   "Apricot F
 COMP( 1984, f1e,   f1,     0,      act_f1,    act,    0,     "ACT",   "Apricot F1e",           GAME_NOT_WORKING | GAME_NO_SOUND )
 COMP( 1984, f2,    f1,     0,      act_f1,    act,    0,     "ACT",   "Apricot F2",            GAME_NOT_WORKING | GAME_NO_SOUND )
 COMP( 1985, f10,   f1,     0,      act_f1,    act,    0,     "ACT",   "Apricot F10",           GAME_NOT_WORKING | GAME_NO_SOUND )
-COMP( 1984, fp,    0,      0,      act_f1p,   act,    0,     "ACT",   "Apricot Portable / FP", GAME_NOT_WORKING | GAME_NO_SOUND )
--- src/mess/includes/apricotp.h
+++ src/mess/includes/apricotp.h
@@ -1,20 +1,22 @@
 #pragma once
 
-#ifndef __APRICOTF__
-#define __APRICOTF__
+#ifndef __APRICOTP__
+#define __APRICOTP__
 
 #define ADDRESS_MAP_MODERN
 
 #include "emu.h"
 #include "cpu/i86/i86.h"
-#include "cpu/z80/z80daisy.h"
+#include "cpu/m6800/m6800.h"
 #include "formats/basicdsk.h"
 #include "imagedev/flopdrv.h"
+#include "machine/8237dma.h"
 #include "machine/apricotkb.h"
 #include "machine/ctronics.h"
+#include "machine/pic8259.h"
 #include "machine/wd17xx.h"
-#include "machine/z80ctc.h"
 #include "machine/z80dart.h"
+#include "video/mc6845.h"
 #include "rendlay.h"
 
 
@@ -24,10 +26,18 @@
 //**************************************************************************
 
 #define SCREEN_TAG		"screen"
-#define I8086_TAG		"10d"
-#define Z80CTC_TAG		"13d"
-#define Z80SIO2_TAG		"15d"
-#define WD2797_TAG		"5f"
+#define I8086_TAG		"ic7"
+#define I8284_TAG		"ic30"
+#define I8237_TAG		"ic17"
+#define I8259A_TAG		"ic51"
+#define TMS4500_TAG		"ic42"
+#define MC6845_TAG		"ic69"
+#define HD63B01V1_TAG	"ic29"
+#define AD7574_TAG		"ic34"
+#define AD1408_TAG		"ic37"
+#define Z80SIO0_TAG		"ic6"
+#define WD2797_TAG		"ic5"
+#define UPD7507C_TAG	"ic2"
 #define CENTRONICS_TAG	"centronics"
 
 
@@ -36,47 +46,37 @@
 //  TYPE DEFINITIONS
 //**************************************************************************
 
-// ======================> f1_state
+// ======================> fp_state
 
-class f1_state : public driver_device
+class fp_state : public driver_device
 {
 public:
-	f1_state(const machine_config &mconfig, device_type type, const char *tag)
+	fp_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
 		  m_maincpu(*this, I8086_TAG),
-		  m_ctc(*this, Z80CTC_TAG),
-		  m_sio(*this, Z80SIO2_TAG),
+		  m_soundcpu(*this, HD63B01V1_TAG),
+		  m_dmac(*this, I8237_TAG),
+		  m_pic(*this, I8259A_TAG),
+		  m_sio(*this, Z80SIO0_TAG),
 		  m_fdc(*this, WD2797_TAG),
+		  m_crtc(*this, MC6845_TAG),
 		  m_floppy0(*this, FLOPPY_0),
-		  m_centronics(*this, CENTRONICS_TAG),
-		  m_ctc_int(CLEAR_LINE),
-		  m_sio_int(CLEAR_LINE)
+		  m_centronics(*this, CENTRONICS_TAG)
 	{ }
 
 	required_device<cpu_device> m_maincpu;
-	required_device<z80ctc_device> m_ctc;
+	required_device<cpu_device> m_soundcpu;
+	required_device<i8237_device> m_dmac;
+	required_device<device_t> m_pic;
 	required_device<z80dart_device> m_sio;
 	required_device<device_t> m_fdc;
+	required_device<mc6845_device> m_crtc;
 	required_device<device_t> m_floppy0;
 	required_device<device_t> m_centronics;
 
+	virtual void machine_start();
+
 	virtual bool screen_update(screen_device &screen, bitmap_t &bitmap, const rectangle &cliprect);
-	
-	DECLARE_READ16_MEMBER( palette_r );
-	DECLARE_WRITE16_MEMBER( palette_w );
-	DECLARE_WRITE8_MEMBER( system_w );
-	DECLARE_WRITE_LINE_MEMBER( sio_int_w );
-	DECLARE_WRITE_LINE_MEMBER( ctc_int_w );
-	DECLARE_WRITE_LINE_MEMBER( ctc_z1_w );
-	DECLARE_WRITE_LINE_MEMBER( ctc_z2_w );
-	
-	int m_ctc_int;
-	int m_sio_int;
-	
-	UINT16 *m_p_paletteram;
-	UINT16 *m_p_scrollram;
-	int m_40_80;
-	int m_200_256;
 };
 
 
