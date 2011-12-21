dd7ccc33ccfd62cf71d0fb972001fd46ee69f462
angelosa <angelosa@9849079f-6229-0410-a07f-84406ce305ba>
2011-12-21 02:56:30 +0000

    Added new roms to the Alphatronic dump, still not good enought tho
    git-svn-id: svn://messdev.no-ip.org/mess@13646 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/alphatro.c
+++ src/mess/drivers/alphatro.c
@@ -43,17 +43,17 @@ INPUT_PORTS_END
 
 static const gfx_layout charlayout =
 {
-	8,8,
+	8,16,
 	RGN_FRAC(1,1),
 	1,
 	{ RGN_FRAC(0,1) },
 	{ 0, 1, 2, 3, 4, 5, 6, 7 },
-	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8 },
-	8*8
+	{ 0*8, 1*8, 2*8, 3*8, 4*8, 5*8, 6*8, 7*8, 8*8, 9*8, 10*8, 11*8, 12*8, 13*8, 14*8, 15*8 },
+	8*16
 };
 
 static GFXDECODE_START( alphatro )
-	GFXDECODE_ENTRY( "maincpu", 0, charlayout,     0, 1 )
+	GFXDECODE_ENTRY( "gfx1", 0, charlayout,     0, 1 )
 GFXDECODE_END
 
 static MACHINE_START( alphatro )
@@ -126,13 +126,17 @@ in various places of the ROM ... most likely a bad dump.
 
 ROM_START( alphatro )
 	ROM_REGION( 0x10000, "maincpu", ROMREGION_ERASE00 )
-	ROM_LOAD( "rom0.bin",     0x0000, 0x2000, NO_DUMP )
+	ROM_LOAD( "2764.ic-1038"	,     0x0000, 0x2000, BAD_DUMP CRC(e337db3b) SHA1(6010bade6a21975636383179903b58a4ca415e49) ) // ???
     ROM_LOAD( "rom1.bin",     0x2000, 0x2000, BAD_DUMP CRC(1509b15a) SHA1(225c36411de680eb8f4d6b58869460a58e60c0cf) )
 	ROM_LOAD( "rom2.bin",     0x4000, 0x2000, BAD_DUMP CRC(998a865d) SHA1(294fe64e839ae6c4032d5db1f431c35e0d80d367) )
 	ROM_LOAD( "rom3.bin",     0x6000, 0x2000, BAD_DUMP CRC(55cbafef) SHA1(e3376b92f80d5a698cdcb2afaa0f3ef4341dd624) )
 
-	ROM_REGION( 0x800, "gfx1", ROMREGION_ERASE00 )
-	ROM_LOAD( "chr.bin",      0x0000, 0x800, NO_DUMP )
+	ROM_REGION( 0x10000, "user1", ROMREGION_ERASE00 )
+	ROM_LOAD( "613256.ic-1058"	,     0x2000, 0x6000, BAD_DUMP CRC(ceea4cb3) SHA1(b332dea0a2d3bb2978b8422eb0723960388bb467) )
+    ROM_LOAD( "ipl.bin",     0x8000, 0x2000, NO_DUMP  )
+
+	ROM_REGION( 0x1000, "gfx1", ROMREGION_ERASE00 )
+	ROM_LOAD( "2732.ic-1067",      0x0000, 0x1000, BAD_DUMP CRC(61f38814) SHA1(35ba31c58a10d5bd1bdb202717792ca021dbe1a8) ) // should be 1:1
 ROM_END
 
 COMP( 1983, alphatro,   0,       0,    alphatro,   alphatro,  0,  "Triumph-Adler", "Alphatronic PC", GAME_NOT_WORKING | GAME_NO_SOUND)
