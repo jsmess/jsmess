f43dda9e1a9697300479d0341ca135ec56589ea0
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2011-12-29 10:09:17 +0000

    SC1 - added notes, removed all test code.
    git-svn-id: svn://messdev.no-ip.org/mess@13776 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/sc1.c
+++ src/mess/drivers/sc1.c
@@ -4,6 +4,35 @@
 
         12/05/2009 Skeleton driver.
 
+ToDo:
+- Everything I/O related (ROM & RAM are correct)
+- Display
+- Inputs
+- Layout (was copied from slc1; needs to be made suitable for sc1)
+
+Port 80-83 could be a device
+
+This happens at the start:
+'maincpu' (04EF): unmapped i/o memory write to 0081 = 0F & FF
+'maincpu' (04F3): unmapped i/o memory write to 0083 = CF & FF
+'maincpu' (04F7): unmapped i/o memory write to 0083 = BB & FF
+'maincpu' (04FB): unmapped i/o memory write to 0082 = 01 & FF
+'maincpu' (0523): unmapped i/o memory write to 00FC = 02 & FF  **
+'maincpu' (0523): unmapped i/o memory write to 00FC = 04 & FF  **
+
+** These two happen for a while (making a tone from a speaker?)
+
+Then:
+'maincpu' (0523): unmapped i/o memory write to 00FC = 00 & FF
+'maincpu' (0075): unmapped i/o memory write to 0080 = 02 & FF
+'maincpu' (0523): unmapped i/o memory write to 00FC = 20 & FF
+
+Then this happens continuously:
+Port 80 out - 00, 02, FF
+Port FC out - 00, 01, 02, 04, 08, 10, 20, 40, 80 (selecting rows?)
+Port 80 in - upper byte = 20 thru 26
+Port 82 in - upper byte = 0 thru 7
+
 ****************************************************************************/
 #define ADDRESS_MAP_MODERN
 
@@ -24,12 +53,6 @@ public:
 
 	required_device<cpu_device> m_maincpu;
 	required_device<device_t> m_speaker;
-	DECLARE_WRITE8_MEMBER(sc1_fc_w);
-	DECLARE_WRITE8_MEMBER(sc1v2_07_w);
-	DECLARE_WRITE8_MEMBER(sc1v2_io_w);
-	DECLARE_READ8_MEMBER(sc1v2_io_r);
-private:
-	UINT8 m_digit;
 };
 
 /***************************************************************************
@@ -38,37 +61,7 @@ private:
 
 ***************************************************************************/
 
-WRITE8_MEMBER( sc1_state::sc1v2_io_w )
-{
-	bool segonoff = BIT(data, 7);
-	//bool busyled = BIT(data, 4);
-	data &= 15;
 
-	if (data < 8)
-		m_digit = data;
-	else
-	if (data < 12)
-	{
-		speaker_level_w(m_speaker, BIT(data, 1) );
-		return;
-	}
-	else
-	if (offset == 0x2f07)
-		return;
-
-	UINT8 segdata = output_get_digit_value(m_digit);
-	UINT8 segnum  = offset & 7;
-	UINT8 segmask = 1 << segnum;
-
-	if (segonoff)
-		segdata |= segmask;
-	else
-		segdata &= ~segmask;
-
-	output_set_digit_value(m_digit, segdata);
-
-	//output_set_value("busyled", busyled);
-}
 
 
 /***************************************************************************
@@ -77,73 +70,20 @@ WRITE8_MEMBER( sc1_state::sc1v2_io_w )
 
 ***************************************************************************/
 
-READ8_MEMBER( sc1_state::sc1v2_io_r )
-{
-	UINT8 data = 0xff, upper = (offset >> 8) & 7;
-
-	if (upper == 3)
-		data &= input_port_read(machine(), "X0");
-	else
-	if (upper == 4)
-		data &= input_port_read(machine(), "X1");
-	else
-	if (upper == 5)
-		data &= input_port_read(machine(), "X2");
-
-	return data;
-}
-
-WRITE8_MEMBER( sc1_state::sc1_fc_w )
-{
-//  if (BIT(data, 3))
-//      speaker_level_w(m_speaker, BIT(data, 1));
-}
-
-WRITE8_MEMBER( sc1_state::sc1v2_07_w )
-{
-	if (BIT(data, 3))
-		speaker_level_w(m_speaker, BIT(data, 1));
-}
-
 
 static ADDRESS_MAP_START(sc1_mem, AS_PROGRAM, 8, sc1_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x0000, 0x0fff ) AM_ROM
-	AM_RANGE( 0x4000, 0x5fff ) AM_RAM
+	AM_RANGE( 0x4000, 0x43ff ) AM_RAM
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START(sc1_io, AS_IO, 8, sc1_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE( 0xfc, 0xfc ) AM_WRITE(sc1_fc_w)
 ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( sc1 )
-	PORT_START("X0")
-	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("D4 T") PORT_CODE(KEYCODE_4)
-	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C3 L") PORT_CODE(KEYCODE_3)
-	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("B2 S") PORT_CODE(KEYCODE_2)
-	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("A1 B") PORT_CODE(KEYCODE_1)
-	//PORT_BIT(0x1, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("E5 D") PORT_CODE(KEYCODE_5)
-	//PORT_BIT(0x2, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("F6 K") PORT_CODE(KEYCODE_6)
-	//PORT_BIT(0x4, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("G7")   PORT_CODE(KEYCODE_7)
-	//PORT_BIT(0x8, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("H8")   PORT_CODE(KEYCODE_8)
-
-	PORT_START("X1")
-	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("E5 D") PORT_CODE(KEYCODE_5)
-	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("F6 K") PORT_CODE(KEYCODE_6)
-	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("G7")   PORT_CODE(KEYCODE_7)
-	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("H8")   PORT_CODE(KEYCODE_8)
-
-	PORT_START("X2")
-	PORT_BIT(0x0f, IP_ACTIVE_LOW, IPT_UNUSED)
-	PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C")    PORT_CODE(KEYCODE_C)
-	PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("A")    PORT_CODE(KEYCODE_A)
-	PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("St")   PORT_CODE(KEYCODE_S)
-	PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Z")    PORT_CODE(KEYCODE_Z)
 INPUT_PORTS_END
 
 
@@ -177,4 +117,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY                           FULLNAME       FLAGS */
-COMP( 19??, sc1,    0,      0,       sc1,       sc1,     0,  "VEB Mikroelektronik Erfurt", "Schachcomputer SC1", GAME_NOT_WORKING )
+COMP( 1989, sc1,    0,      0,       sc1,       sc1,     0,  "VEB Mikroelektronik Erfurt", "Schachcomputer SC1", GAME_NOT_WORKING | GAME_NO_SOUND )
