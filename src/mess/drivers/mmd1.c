ccbde7016331a944c79ec6750100bb48a4b20847
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-04 13:42:27 +0000

    MMD2: Created layout. Added dipswitches.
    git-svn-id: svn://messdev.no-ip.org/mess@13855 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/mmd1.c
+++ src/mess/drivers/mmd1.c
@@ -154,9 +154,10 @@ public:
 	DECLARE_WRITE8_MEMBER(mmd1_port1_w);
 	DECLARE_WRITE8_MEMBER(mmd1_port2_w);
 	DECLARE_READ8_MEMBER(mmd1_keyboard_r);
+	DECLARE_READ8_MEMBER(mmd2_01_r);
 	DECLARE_READ8_MEMBER(mmd2_03_r);
 	DECLARE_WRITE8_MEMBER(mmd2_04_w);
-	DECLARE_READ8_MEMBER(mmd2_bank_w);
+	DECLARE_READ8_MEMBER(mmd2_bank_r);
 	UINT8 m_return_code;
 };
 
@@ -226,10 +227,10 @@ READ8_MEMBER( mmd1_state::mmd1_keyboard_r )
 		return m_return_code;
 }
 
-READ8_MEMBER( mmd1_state::mmd2_03_r ) { return 1; }
+READ8_MEMBER( mmd1_state::mmd2_03_r ) { return 0; }
 WRITE8_MEMBER( mmd1_state::mmd2_04_w ) { }
 
-READ8_MEMBER( mmd1_state::mmd2_bank_w )
+READ8_MEMBER( mmd1_state::mmd2_bank_r )
 {
 	memory_set_bank(machine(), "bank1", offset);
 	memory_set_bank(machine(), "bank2", offset);
@@ -240,6 +241,14 @@ READ8_MEMBER( mmd1_state::mmd2_bank_w )
 	memory_set_bank(machine(), "bank7", offset);
 	memory_set_bank(machine(), "bank8", offset);
 	return 0xff;
+}
+
+READ8_MEMBER( mmd1_state::mmd2_01_r )
+{
+	// need to add cassin, ttyin bits
+	UINT8 data = 0x87;
+	data |= input_port_read(machine(),"DSW");
+	return data;
 }
 
 
@@ -271,11 +280,11 @@ ADDRESS_MAP_END
 static ADDRESS_MAP_START(mmd2_io, AS_IO, 8, mmd1_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE( 0x00, 0x00 ) AM_WRITE(mmd1_port0_w)
-	AM_RANGE( 0x01, 0x01 ) AM_WRITE(mmd1_port1_w)
+	AM_RANGE( 0x01, 0x01 ) AM_READWRITE(mmd2_01_r,mmd1_port1_w)
 	AM_RANGE( 0x02, 0x02 ) AM_WRITE(mmd1_port2_w)
 	AM_RANGE( 0x03, 0x03 ) AM_READ(mmd2_03_r) // 8279 status/control
-	AM_RANGE( 0x04, 0x04 ) AM_WRITE(mmd2_04_w) // 8279 kbd/display
-	AM_RANGE( 0x05, 0x07 ) AM_READ(mmd2_bank_w)
+	//AM_RANGE( 0x04, 0x04 ) AM_WRITE(mmd2_04_w) // 8279 kbd/display
+	AM_RANGE( 0x05, 0x07 ) AM_READ(mmd2_bank_r)
 ADDRESS_MAP_END
 
 
@@ -303,20 +312,42 @@ static INPUT_PORTS_START( mmd1 )
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_R)
 INPUT_PORTS_END
 
-
 static INPUT_PORTS_START( mmd2 )
-/*
-DIP switches:
-WE 0      - Write enable for addresses $0000..$03FF
-WE 1      - Write enable for addresses $0400..$07FF
-WE 2      - Write enable for addresses $0800..$0BFF
-WE 3      - Write enable for addresses $0C00..$0FFF
-SPARE     - ???
-HEX OCT   - choose display and entry to be in Hexadecimal or Octal
-PUP RESET - ???
-EXEC USER - update binary LED's with data entry? Or not?
-(in either setting, outputs to ports 0,1,2 still show)
+	PORT_START("DSW")
+	PORT_BIT( 0x87, 0x00, IPT_UNUSED )
+	PORT_DIPNAME( 0x20, 0x00, "Sw6") PORT_DIPLOCATION("SW1:1")
+	PORT_DIPSETTING(    0x20, "Hex")
+	PORT_DIPSETTING(    0x00, "Octal")
+	PORT_DIPNAME( 0x10, 0x00, "Sw7") PORT_DIPLOCATION("SW1:2")
+	PORT_DIPSETTING(    0x10, "PUP")
+	PORT_DIPSETTING(    0x00, "Reset")
+	PORT_DIPNAME( 0x08, 0x08, "Sw8") PORT_DIPLOCATION("SW1:3")
+	PORT_DIPSETTING(    0x08, "Exec")
+	PORT_DIPSETTING(    0x00, "User")
 
+	PORT_START("LINE1")
+		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("0") PORT_CODE(KEYCODE_0)
+		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("1") PORT_CODE(KEYCODE_1)
+		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("2") PORT_CODE(KEYCODE_2)
+		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("3") PORT_CODE(KEYCODE_3)
+		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("4") PORT_CODE(KEYCODE_4)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("5") PORT_CODE(KEYCODE_5)
+		PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("6") PORT_CODE(KEYCODE_6)
+		PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("7") PORT_CODE(KEYCODE_7)
+	PORT_START("LINE2")
+		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("S") PORT_CODE(KEYCODE_S)
+		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C") PORT_CODE(KEYCODE_C)
+		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("G") PORT_CODE(KEYCODE_G)
+		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("H") PORT_CODE(KEYCODE_H)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("L") PORT_CODE(KEYCODE_L)
+		PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("A") PORT_CODE(KEYCODE_A)
+		PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("B") PORT_CODE(KEYCODE_B)
+	PORT_START("LINE3")
+		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_R)
+INPUT_PORTS_END
+
+/*
 Keyboard
 0  1  2  3      PREV  STORE  NEXT  STEP
 4  5  6  7      HIGH  LOW  GO  OPTION
@@ -324,7 +355,6 @@ Keyboard
 C  D  E  F      MEM  REGS  AUX  CANCEL
 
 */
-INPUT_PORTS_END
 
 static MACHINE_RESET( mmd1 )
 {
