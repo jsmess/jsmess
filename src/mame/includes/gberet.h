54238a7770de0a33ff4a87f375d89677ad176503
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-23 09:31:11 +0000

    Sync with MAME r15118 (no whatsnew) note: pc_vga and floppy ui support not overwritten
    git-svn-id: svn://messdev.no-ip.org/mess@14135 9849079f-6229-0410-a07f-84406ce305ba

--- src/mame/includes/gberet.h
+++ src/mame/includes/gberet.h
@@ -19,12 +19,13 @@ public:
 	size_t      m_spriteram_size;
 
 	/* video-related */
-	tilemap_t     *m_bg_tilemap;
+	tilemap_t * m_bg_tilemap;
 	UINT8       m_spritebank;
 
 	/* misc */
-	UINT8       m_nmi_enable;
-	UINT8       m_irq_enable;
+	UINT8 *     m_soundlatch;
+	UINT8       m_interrupt_mask;
+	UINT8       m_interrupt_ticks;
 };
 
 
