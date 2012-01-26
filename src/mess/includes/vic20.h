6d64ae44f4e9683d59616d2085c5b2ba32b81345
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-26 13:10:03 +0000

    [VIC20] Cleanup. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@14196 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/includes/vic20.h
+++ src/mess/includes/vic20.h
@@ -1,11 +1,24 @@
+#pragma once
+
 #ifndef __VIC20__
 #define __VIC20__
 
+#define ADDRESS_MAP_MODERN
+
+#include "emu.h"
+#include "includes/cbm.h"
+#include "formats/cbm_snqk.h"
+#include "cpu/m6502/m6502.h"
+#include "imagedev/cartslot.h"
 #include "machine/6522via.h"
 #include "machine/cbmiec.h"
-#include "imagedev/cassette.h"
+#include "machine/cbmipt.h"
+#include "machine/ieee488.h"
+#include "machine/ram.h"
+#include "machine/vic1112.h"
+#include "sound/dac.h"
+#include "sound/mos6560.h"
 
-#define SCREEN_TAG		"screen"
 #define M6502_TAG		"ue10"
 #define M6522_0_TAG		"uab3"
 #define M6522_1_TAG		"uab1"
@@ -13,6 +26,7 @@
 #define M6561_TAG		"ub7"
 #define IEC_TAG			"iec"
 #define TIMER_C1530_TAG	"c1530"
+#define SCREEN_TAG		"screen"
 
 class vic20_state : public driver_device
 {
@@ -22,22 +36,37 @@ public:
 		  m_maincpu(*this, M6502_TAG),
 		  m_via0(*this, M6522_0_TAG),
 		  m_via1(*this, M6522_1_TAG),
+		  m_vic(*this, M6560_TAG),
 		  m_iec(*this, CBM_IEC_TAG),
 		  m_cassette(*this, CASSETTE_TAG),
-		  m_mos6560(*this, M6560_TAG)
+		  m_ram(*this, RAM_TAG)
 	{ }
 
 	required_device<cpu_device> m_maincpu;
 	required_device<via6522_device> m_via0;
 	required_device<via6522_device> m_via1;
+	required_device<device_t> m_vic;
 	required_device<cbm_iec_device> m_iec;
 	required_device<cassette_image_device> m_cassette;
-	required_device<device_t> m_mos6560;
+	required_device<ram_device> m_ram;
+	
+	virtual void machine_start();
 
-	/* keyboard state */
+	DECLARE_READ8_MEMBER( via0_pa_r );
+	DECLARE_WRITE8_MEMBER( via0_pa_w );
+	DECLARE_READ8_MEMBER( via0_pb_r );
+	DECLARE_WRITE8_MEMBER( via0_pb_w );
+	DECLARE_WRITE_LINE_MEMBER( via0_ca2_w );
+	DECLARE_READ8_MEMBER( via1_pa_r );
+	DECLARE_READ8_MEMBER( via1_pb_r );
+	DECLARE_WRITE8_MEMBER( via1_pb_w );
+	DECLARE_WRITE_LINE_MEMBER( via1_ca2_w );
+	DECLARE_WRITE_LINE_MEMBER( via1_cb2_w );
+
+	// keyboard state
 	int m_key_col;
 
-	/* timers */
+	// timers
 	timer_device *m_cassette_timer;
 };
 
