b0d2c4b02a4e1712d384886ac6f4029513828a74
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-27 09:32:31 +0000

    [VIC20] Emulated the VIC-1010 Expansion Module. You can now connect up to 6 cartridges using this expander. (e.g. mess vic20p -exp exp -exp:exp:slot1 ieee488 -exp:exp:slot2 8k -exp:exp:slot3 8k -exp:exp:slot4 8k) [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14231 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/machine/vic1010.h
@@ -0,0 +1,82 @@
+/**********************************************************************
+
+    Commodore VIC-1010 Expansion Module emulation
+
+    Copyright MESS Team.
+    Visit http://mamedev.org for licensing and usage restrictions.
+
+**********************************************************************/
+
+#pragma once
+
+#ifndef __VIC1010__
+#define __VIC1010__
+
+#define ADDRESS_MAP_MODERN
+
+#include "emu.h"
+#include "machine/cbmipt.h"
+#include "machine/vic20exp.h"
+
+
+
+//**************************************************************************
+//  MACROS/CONSTANTS
+//**************************************************************************
+
+#define MAX_SLOTS 6
+
+
+
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
+
+// ======================> vic1010_device
+
+class vic1010_device :  public device_t,
+						public device_vic20_expansion_card_interface
+{
+public:
+    // construction/destruction
+    vic1010_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	
+	// not really public
+	DECLARE_WRITE_LINE_MEMBER( irq_w );
+	DECLARE_WRITE_LINE_MEMBER( nmi_w );
+	DECLARE_WRITE_LINE_MEMBER( res_w );
+
+protected:
+    // device-level overrides
+    virtual void device_start();
+	
+	// optional information overrides
+	virtual machine_config_constructor device_mconfig_additions() const;
+
+	// device_vic20_expansion_card_interface overrides
+	virtual UINT8 vic20_ram1_r(address_space &space, offs_t offset);
+	virtual void vic20_ram1_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_ram2_r(address_space &space, offs_t offset);
+	virtual void vic20_ram2_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_ram3_r(address_space &space, offs_t offset);
+	virtual void vic20_ram3_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_blk1_r(address_space &space, offs_t offset);
+	virtual void vic20_blk1_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_blk2_r(address_space &space, offs_t offset);
+	virtual void vic20_blk2_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_blk3_r(address_space &space, offs_t offset);
+	virtual void vic20_blk3_w(address_space &space, offs_t offset, UINT8 data);
+	virtual UINT8 vic20_blk5_r(address_space &space, offs_t offset);
+	virtual void vic20_blk5_w(address_space &space, offs_t offset, UINT8 data);
+	
+private:
+	vic20_expansion_slot_device *m_expansion_slot[MAX_SLOTS];
+};
+
+
+// device type definition
+extern const device_type VIC1010;
+
+
+
+#endif
