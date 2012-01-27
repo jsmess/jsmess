631679e0aea8f89fb6a0960debef7b2776bc5569
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-27 08:42:50 +0000

    [VIC20] Emulated the VIC-1211A Super Expander with 3K RAM cartridge. [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14228 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/vic1210.h
+++ src/mess/machine/vic1210.h
@@ -1,6 +1,7 @@
 /**********************************************************************
 
     Commodore VIC-1210 3K RAM Expansion Cartridge emulation
+	Commodore VIC-1211A Super Expander with 3K RAM Cartridge emulation
 
     Copyright MESS Team.
     Visit http://mamedev.org for licensing and usage restrictions.
@@ -30,6 +31,7 @@ class vic1210_device :  public device_t,
 {
 public:
     // construction/destruction
+	vic1210_device(const machine_config &mconfig, device_type type, const char *name, const char *tag, device_t *owner, UINT32 clock);
     vic1210_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
 	
 protected:
@@ -44,13 +46,37 @@ protected:
 	virtual UINT8 vic20_ram3_r(address_space &space, offs_t offset);
 	virtual void vic20_ram3_w(address_space &space, offs_t offset, UINT8 data);
 
-private:
 	UINT8 *m_ram;
+};
+
+
+// ======================> vic1211a_device
+
+class vic1211a_device :  public vic1210_device
+{
+public:
+    // construction/destruction
+    vic1211a_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+protected:
+    // device-level overrides
+    virtual void device_start();
+	virtual void device_config_complete() { m_shortname = "vic1211a"; }
+
+	// optional information overrides
+	virtual const rom_entry *device_rom_region() const;
+	
+	// device_vic20_expansion_card_interface overrides
+	virtual UINT8 vic20_blk5_r(address_space &space, offs_t offset);
+
+private:
+	UINT8 *m_rom;
 };
 
 
 // device type definition
 extern const device_type VIC1210;
+extern const device_type VIC1211A;
 
 
 
