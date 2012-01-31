f82ffd2da5c96aca582e9d1dfcd3c6d7c26f2aa1
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-01-31 21:21:12 +0000

    Fixed build. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@14296 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/video/isa_ega.h
+++ src/mess/video/isa_ega.h
@@ -25,6 +25,7 @@ public:
 		// optional information overrides
 		virtual machine_config_constructor device_mconfig_additions() const;
 		virtual const rom_entry *device_rom_region() const;
+		virtual ioport_constructor device_input_ports() const;
 
 		DECLARE_READ8_MEMBER(pc_ega8_3b0_r);
 		DECLARE_WRITE8_MEMBER(pc_ega8_3b0_w);
