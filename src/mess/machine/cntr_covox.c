ed80343aa0f9824845c793db71247da79e76ca8a
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-02 14:06:47 +0000

    Added Covox Stereo-On-1 (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@14318 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/cntr_covox.c
+++ src/mess/machine/cntr_covox.c
@@ -56,3 +56,57 @@ void centronics_covox_device::write(UINT8 data)
 {
 	dac_w(m_dac, 0, data);
 }
+
+//**************************************************************************
+//  COVOX STEREO DEVICE
+//**************************************************************************
+
+// device type definition
+const device_type CENTRONICS_COVOX_STEREO = &device_creator<centronics_covox_stereo_device>;
+
+static MACHINE_CONFIG_FRAGMENT( covox_stereo )
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_STEREO("lspeaker", "rspeaker")
+
+	MCFG_SOUND_ADD("dac_left", DAC, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "lspeaker", 1.0)
+
+	MCFG_SOUND_ADD("dac_right", DAC, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "rspeaker", 1.0)
+MACHINE_CONFIG_END
+
+
+/***************************************************************************
+    IMPLEMENTATION
+***************************************************************************/
+//-------------------------------------------------
+//  centronics_covox_stereo_device - constructor
+//-------------------------------------------------
+
+centronics_covox_stereo_device::centronics_covox_stereo_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
+    : device_t(mconfig, CENTRONICS_COVOX_STEREO, "Covox (Stereo-in-1)", tag, owner, clock),
+	  device_centronics_peripheral_interface( mconfig, *this )
+{
+}
+//-------------------------------------------------
+//  machine_config_additions - device-specific
+//  machine configurations
+//-------------------------------------------------
+
+machine_config_constructor centronics_covox_stereo_device::device_mconfig_additions() const
+{
+	return MACHINE_CONFIG_NAME( covox_stereo );
+}
+
+
+void centronics_covox_stereo_device::device_start()
+{
+	m_dac_left  = subdevice("dac_left");
+	m_dac_right = subdevice("dac_right");
+}
+
+void centronics_covox_stereo_device::write(UINT8 data)
+{
+	if (m_strobe)  dac_w(m_dac_left, 0, data);
+	if (m_auto_fd) dac_w(m_dac_right, 0, data);
+}
