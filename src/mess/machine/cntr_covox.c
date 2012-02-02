3db36e50705850828697fe94c9f28fc3c8a6958f
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-02 12:41:20 +0000

    Added Covox support as slot device (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@14316 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/machine/cntr_covox.c
@@ -0,0 +1,59 @@
+/***************************************************************************
+
+    Covox Speech Thing
+
+***************************************************************************/
+
+#include "emu.h"
+#include "sound/dac.h"
+#include "cntr_covox.h"
+
+//**************************************************************************
+//  COVOX DEVICE
+//**************************************************************************
+
+// device type definition
+const device_type CENTRONICS_COVOX = &device_creator<centronics_covox_device>;
+
+static MACHINE_CONFIG_FRAGMENT( covox )
+	/* sound hardware */
+	MCFG_SPEAKER_STANDARD_MONO("mono")
+
+	MCFG_SOUND_ADD("dac", DAC, 0)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 1.0)
+MACHINE_CONFIG_END
+
+
+/***************************************************************************
+    IMPLEMENTATION
+***************************************************************************/
+//-------------------------------------------------
+//  centronics_covox_device - constructor
+//-------------------------------------------------
+
+centronics_covox_device::centronics_covox_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
+    : device_t(mconfig, CENTRONICS_COVOX, "Covox Speech Thing", tag, owner, clock),
+	  device_centronics_peripheral_interface( mconfig, *this )
+{
+}
+//-------------------------------------------------
+//  machine_config_additions - device-specific
+//  machine configurations
+//-------------------------------------------------
+
+machine_config_constructor centronics_covox_device::device_mconfig_additions() const
+{
+	return MACHINE_CONFIG_NAME( covox );
+}
+
+
+void centronics_covox_device::device_start()
+{
+	m_dac = subdevice("dac");
+}
+
+void centronics_covox_device::write(UINT8 data)
+{
+	m_data = data;
+	dac_w(m_dac, 0, data);
+}
