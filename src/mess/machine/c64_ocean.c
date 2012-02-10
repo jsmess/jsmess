96de5fe88519586d8e0175ca659a3542ac4c17d3
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-10 19:28:09 +0000

    [C64] Added support for the following cartridge types: COMAL 80, Dinamic, Fun Play, GeoRAM, Magic Desk, Mikro Assembler, Ocean (partial), Rex, System 3, Westermann, Zaxxon. [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14415 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mess/machine/c64_ocean.c
@@ -0,0 +1,96 @@
+/**********************************************************************
+
+    Ocean Software cartridge emulation
+
+    Copyright MESS Team.
+    Visit http://mamedev.org for licensing and usage restrictions.
+
+**********************************************************************/
+
+#include "c64_ocean.h"
+
+
+
+//**************************************************************************
+//  DEVICE DEFINITIONS
+//**************************************************************************
+
+const device_type C64_OCEAN = &device_creator<c64_ocean_cartridge_device>;
+
+
+
+//**************************************************************************
+//  LIVE DEVICE
+//**************************************************************************
+
+//-------------------------------------------------
+//  c64_ocean_cartridge_device - constructor
+//-------------------------------------------------
+
+c64_ocean_cartridge_device::c64_ocean_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock) :
+	device_t(mconfig, C64_OCEAN, "C64 Ocean cartridge", tag, owner, clock),
+	device_c64_expansion_card_interface(mconfig, *this)
+{
+}
+
+
+//-------------------------------------------------
+//  device_start - device-specific startup
+//-------------------------------------------------
+
+void c64_ocean_cartridge_device::device_start()
+{
+	// state saving
+	save_item(NAME(m_bank));
+}
+
+
+//-------------------------------------------------
+//  device_reset - device-specific reset
+//-------------------------------------------------
+
+void c64_ocean_cartridge_device::device_reset()
+{
+	m_bank = 0;
+}
+
+
+//-------------------------------------------------
+//  c64_cd_r - cartridge data read
+//-------------------------------------------------
+
+UINT8 c64_ocean_cartridge_device::c64_cd_r(address_space &space, offs_t offset, int roml, int romh, int io1, int io2)
+{
+	UINT8 data = 0;
+	
+	if (!roml)
+	{
+		offs_t addr = (m_bank << 13) | (offset & 0x1fff);
+		data = m_roml[addr & m_roml_mask];
+	}
+	else if (!romh)
+	{
+		offs_t addr = (m_bank << 13) | (offset & 0x1fff);
+		data = m_romh[addr & m_romh_mask];
+	}
+	else if (!io1)
+	{
+		return m_bank;
+	}
+
+	return data;
+}
+
+
+//-------------------------------------------------
+//  c64_cd_w - cartridge data write
+//-------------------------------------------------
+
+void c64_ocean_cartridge_device::c64_cd_w(address_space &space, offs_t offset, UINT8 data, int roml, int romh, int io1, int io2)
+{
+	if (!io1)
+	{
+	logerror("bank %02x\n", data);
+		m_bank = data & 0x3f;
+	}
+}
