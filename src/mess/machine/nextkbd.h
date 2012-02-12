868e66f657352b900659a5b949c22182386eaf1c
galibert <galibert@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-12 14:33:32 +0000

    next: Handle command-~ [O. Galibert]
    git-svn-id: svn://messdev.no-ip.org/mess@14434 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/nextkbd.h
+++ src/mess/machine/nextkbd.h
@@ -3,14 +3,14 @@
 
 #include "emu.h"
 
-#define MCFG_NEXTKBD_ADD(_tag, _int_change_cb)	\
-	MCFG_DEVICE_ADD(_tag, NEXTKBD, 0)			\
-	downcast<nextkbd_device *>(device)->set_int_change_cb(_int_change_cb);
+#define MCFG_NEXTKBD_ADD(_tag, _int_change_cb, _int_power_cb, _int_nmi_cb)					\
+	MCFG_DEVICE_ADD(_tag, NEXTKBD, 0)														\
+	downcast<nextkbd_device *>(device)->setup(_int_change_cb, _int_power_cb, _int_nmi_cb);
 
 class nextkbd_device : public device_t {
 public:
 	nextkbd_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
-	void set_int_change_cb(line_cb_t int_change_cb);
+	void setup(line_cb_t int_change_cb, line_cb_t _int_power_cb, line_cb_t _int_nmi_cb);
 
 	DECLARE_ADDRESS_MAP(amap, 32);
 	ioport_constructor device_input_ports() const;
@@ -37,8 +37,9 @@ private:
 	enum { FIFO_SIZE = 32 };
 	enum { KEYUP = 0x0080, KEYVALID = 0x8000 };
 
-	line_cb_t int_change_cb;
+	line_cb_t int_change_cb, int_power_cb, int_nmi_cb;
 	emu_timer *kbd_timer;
+	bool nmi_active;
 
 	UINT32 control, control2, data, fifo_ir, fifo_iw, fifo_size;
 	UINT32 fifo[FIFO_SIZE];
