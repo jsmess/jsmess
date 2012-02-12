93888c29a8a66984f8efb3e16b8576a035fa5fe3
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-12 07:37:18 +0000

    Split pc mouse in two separate device by Carl (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@14432 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/ser_mouse.h
+++ src/mess/machine/ser_mouse.h
@@ -15,37 +15,54 @@ class serial_mouse_device :
 		public device_rs232_port_interface
 {
 public:
-	serial_mouse_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	serial_mouse_device(const machine_config &mconfig, device_type type, const char* name, const char *tag, device_t *owner, UINT32 clock);
 
 	virtual ioport_constructor device_input_ports() const;
-
-	virtual void dtr_w(UINT8 state) { m_dtr = state; check_state(); m_old_dtr = state; }
-	virtual void rts_w(UINT8 state) { m_rts = state; check_state(); m_old_rts = state; }
 
 protected:
 	virtual void device_start();
 	virtual void device_reset();
 	virtual void device_timer(emu_timer &timer, device_timer_id id, int param, void *ptr);
+	virtual void mouse_trans(int dx, int dy, int nb, int mbc) = 0;
+	void set_mouse_enable(bool state);
+	void queue_data(UINT8 data) {m_queue[m_head] = data; ++m_head %= 256;}
+	UINT8 unqueue_data() {UINT8 ret = m_queue[m_tail]; ++m_tail %= 256; return ret;}
 private:
-	typedef enum
-	{
-		TYPE_MICROSOFT_MOUSE,
-		TYPE_MOUSE_SYSTEMS
-	} PC_MOUSE_PROTOCOL;
-
-	void queue_data(int data);
-	void check_state();
-
-	PC_MOUSE_PROTOCOL m_protocol;
-	UINT8 m_old_dtr, m_old_rts;
-
 	UINT8 m_queue[256];
 	UINT8 m_head, m_tail, m_mb;
 
 	emu_timer *m_timer;
 	rs232_port_device *m_owner;
+	bool m_enabled;
 };
 
-extern const device_type SERIAL_MOUSE;
+class microsoft_mouse_device : public serial_mouse_device
+{
+public:
+	microsoft_mouse_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	virtual void dtr_w(UINT8 state) { m_dtr = state; check_state(); }
+	virtual void rts_w(UINT8 state) { m_rts = state; check_state(); m_old_rts = state; }
+protected:
+	virtual void mouse_trans(int dx, int dy, int nb, int mbc);
+	virtual void device_reset() {m_old_rts = 0; serial_mouse_device::device_reset();} 
+private:
+	void check_state();
+	UINT8 m_old_rts;
+};
+extern const device_type MSFT_SERIAL_MOUSE;
+
+class mouse_systems_mouse_device : public serial_mouse_device
+{
+public:
+	mouse_systems_mouse_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	virtual void dtr_w(UINT8 state) { m_dtr = state; check_state(); }
+	virtual void rts_w(UINT8 state) { m_rts = state; check_state(); }
+protected:
+	virtual void mouse_trans(int dx, int dy, int nb, int mbc);
+private:
+	void check_state() { set_mouse_enable((m_dtr && m_rts)?true:false); }
+};
+
+extern const device_type MSYSTEM_SERIAL_MOUSE;
 
 #endif /* SER_MOUSE_H_ */
