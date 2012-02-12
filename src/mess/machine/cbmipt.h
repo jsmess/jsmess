c964878763baa028d9a09760950592e0d07b488f
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-12 22:14:44 +0000

    [C64] Added support for the Commodore IEEE-488 cartridge. You can now connect any IEEE-488 disk drive to C64 and even another cartridge using the passthru. (e.g. mess c64p -exp ieee488 -exp:ieee488:ieee9 sfd1001 -cart2 congo)  [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14437 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/cbmipt.h
+++ src/mess/machine/cbmipt.h
@@ -9,6 +9,7 @@
 #include "machine/c64_epyx_fast_load.h"
 #include "machine/c64_fun_play.h"
 #include "machine/c64_georam.h"
+#include "machine/c64_ieee488.h"
 #include "machine/c64_magic_desk.h"
 #include "machine/c64_mikro_assembler.h"
 #include "machine/c64_multiscreen.h"
