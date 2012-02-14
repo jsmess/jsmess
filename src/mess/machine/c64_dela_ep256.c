b77c7b171ef751dacf99fd9814f110442c7ce8b8
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-14 17:54:47 +0000

    [C64] Added support for the following cartridge types: Dela EP256, Dela EP64, Rex EP256, Ross, Structured Basic. [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14458 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/c64_dela_ep256.c
+++ src/mess/machine/c64_dela_ep256.c
@@ -1,13 +1,13 @@
 /**********************************************************************
 
-    Dela 7x8K EPROM cartridge emulation
+    Dela 256KB EPROM cartridge emulation
 
     Copyright MESS Team.
     Visit http://mamedev.org for licensing and usage restrictions.
 
 **********************************************************************/
 
-#include "c64_dela_ep7x8.h"
+#include "c64_dela_ep256.h"
 
 
 
@@ -15,22 +15,23 @@
 //  DEVICE DEFINITIONS
 //**************************************************************************
 
-const device_type C64_DELA_EP7X8 = &device_creator<c64_dela_ep7x8_cartridge_device>;
+const device_type C64_DELA_EP256 = &device_creator<c64_dela_ep256_cartridge_device>;
 
 
 //-------------------------------------------------
-//  ROM( c64_dela_ep7x8 )
+//  ROM( c64_dela_ep256 )
 //-------------------------------------------------
 
-ROM_START( c64_dela_ep7x8 )
-	ROM_REGION( 0x10000, "rom", 0 )
-	ROM_CART_LOAD( "rom1", 0x2000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom2", 0x4000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom3", 0x6000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom4", 0x8000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom5", 0xa000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom6", 0xc000, 0x2000, ROM_MIRROR )
-	ROM_CART_LOAD( "rom7", 0xe000, 0x2000, ROM_MIRROR )
+ROM_START( c64_dela_ep256 )
+	ROM_REGION( 0x40000, "rom", 0 )
+	ROM_CART_LOAD( "rom1", 0x00000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom2", 0x08000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom3", 0x10000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom4", 0x18000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom5", 0x20000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom6", 0x28000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom7", 0x30000, 0x08000, ROM_MIRROR )
+	ROM_CART_LOAD( "rom8", 0x38000, 0x08000, ROM_MIRROR )
 ROM_END
 
 
@@ -38,17 +39,17 @@ ROM_END
 //  rom_region - device-specific ROM region
 //-------------------------------------------------
 
-const rom_entry *c64_dela_ep7x8_cartridge_device::device_rom_region() const
+const rom_entry *c64_dela_ep256_cartridge_device::device_rom_region() const
 {
-	return ROM_NAME( c64_dela_ep7x8 );
+	return ROM_NAME( c64_dela_ep256 );
 }
 
 
 //-------------------------------------------------
-//  MACHINE_CONFIG_FRAGMENT( c64_dela_ep7x8 )
+//  MACHINE_CONFIG_FRAGMENT( c64_dela_ep256 )
 //-------------------------------------------------
 
-static MACHINE_CONFIG_FRAGMENT( c64_dela_ep7x8 )
+static MACHINE_CONFIG_FRAGMENT( c64_dela_ep256 )
 	MCFG_CARTSLOT_ADD("rom1")
 	MCFG_CARTSLOT_EXTENSION_LIST("rom,bin")
 	MCFG_CARTSLOT_ADD("rom2")
@@ -63,6 +64,8 @@ static MACHINE_CONFIG_FRAGMENT( c64_dela_ep7x8 )
 	MCFG_CARTSLOT_EXTENSION_LIST("rom,bin")
 	MCFG_CARTSLOT_ADD("rom7")
 	MCFG_CARTSLOT_EXTENSION_LIST("rom,bin")
+	MCFG_CARTSLOT_ADD("rom8")
+	MCFG_CARTSLOT_EXTENSION_LIST("rom,bin")
 MACHINE_CONFIG_END
 
 
@@ -71,10 +74,11 @@ MACHINE_CONFIG_END
 //  machine configurations
 //-------------------------------------------------
 
-machine_config_constructor c64_dela_ep7x8_cartridge_device::device_mconfig_additions() const
+machine_config_constructor c64_dela_ep256_cartridge_device::device_mconfig_additions() const
 {
-	return MACHINE_CONFIG_NAME( c64_dela_ep7x8 );
+	return MACHINE_CONFIG_NAME( c64_dela_ep256 );
 }
+
 
 
 //**************************************************************************
@@ -82,11 +86,11 @@ machine_config_constructor c64_dela_ep7x8_cartridge_device::device_mconfig_addit
 //**************************************************************************
 
 //-------------------------------------------------
-//  c64_dela_ep7x8_cartridge_device - constructor
+//  c64_dela_ep256_cartridge_device - constructor
 //-------------------------------------------------
 
-c64_dela_ep7x8_cartridge_device::c64_dela_ep7x8_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock) :
-	device_t(mconfig, C64_DELA_EP7X8, "C64 Dela 7x8KB EPROM cartridge", tag, owner, clock),
+c64_dela_ep256_cartridge_device::c64_dela_ep256_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock) :
+	device_t(mconfig, C64_DELA_EP256, "C64 Rex 256KB EPROM cartridge", tag, owner, clock),
 	device_c64_expansion_card_interface(mconfig, *this)
 {
 }
@@ -96,12 +100,13 @@ c64_dela_ep7x8_cartridge_device::c64_dela_ep7x8_cartridge_device(const machine_c
 //  device_start - device-specific startup
 //-------------------------------------------------
 
-void c64_dela_ep7x8_cartridge_device::device_start()
+void c64_dela_ep256_cartridge_device::device_start()
 {
 	m_rom = subregion("rom")->base();
 	
 	// state saving
 	save_item(NAME(m_bank));
+	save_item(NAME(m_reset));
 }
 
 
@@ -109,9 +114,10 @@ void c64_dela_ep7x8_cartridge_device::device_start()
 //  device_reset - device-specific reset
 //-------------------------------------------------
 
-void c64_dela_ep7x8_cartridge_device::device_reset()
+void c64_dela_ep256_cartridge_device::device_reset()
 {
-	m_bank = 0xfe;
+	m_reset = 1;
+	m_bank = 0;
 	m_exrom = 0;
 }
 
@@ -120,22 +126,21 @@ void c64_dela_ep7x8_cartridge_device::device_reset()
 //  c64_cd_r - cartridge data read
 //-------------------------------------------------
 
-UINT8 c64_dela_ep7x8_cartridge_device::c64_cd_r(address_space &space, offs_t offset, int roml, int romh, int io1, int io2)
+UINT8 c64_dela_ep256_cartridge_device::c64_cd_r(address_space &space, offs_t offset, int roml, int romh, int io1, int io2)
 {
 	UINT8 data = 0;
 	
 	if (!roml)
 	{
-		offs_t addr = offset & 0x1fff;
-		
-		if (!BIT(m_bank, 0)) data |= m_roml[addr];
-		if (!BIT(m_bank, 1)) data |= m_rom[0x2000 + addr];
-		if (!BIT(m_bank, 2)) data |= m_rom[0x4000 + addr];
-		if (!BIT(m_bank, 3)) data |= m_rom[0x6000 + addr];
-		if (!BIT(m_bank, 4)) data |= m_rom[0x8000 + addr];
-		if (!BIT(m_bank, 5)) data |= m_rom[0xa000 + addr];
-		if (!BIT(m_bank, 6)) data |= m_rom[0xc000 + addr];
-		if (!BIT(m_bank, 7)) data |= m_rom[0xe000 + addr];
+		if (m_reset)
+		{
+			data = m_roml[offset & 0x1fff];
+		}
+		else
+		{
+			offs_t addr = (m_bank << 13) | (offset & 0x1fff);
+			data = m_rom[addr];
+		}
 	}
 
 	return data;
@@ -146,12 +151,29 @@ UINT8 c64_dela_ep7x8_cartridge_device::c64_cd_r(address_space &space, offs_t off
 //  c64_cd_w - cartridge data write
 //-------------------------------------------------
 
-void c64_dela_ep7x8_cartridge_device::c64_cd_w(address_space &space, offs_t offset, UINT8 data, int roml, int romh, int io1, int io2)
+void c64_dela_ep256_cartridge_device::c64_cd_w(address_space &space, offs_t offset, UINT8 data, int roml, int romh, int io1, int io2)
 {
-	if (!io1)
+	if (!io2 && ((offset & 0xf0) == 0xa0))
 	{
-		m_bank = data;
+		/*
 		
-		m_exrom = (data == 0xff);
+			bit		description
+			
+			0		socket selection bit 0
+			1		socket selection bit 1
+			2		socket selection bit 2
+			3
+			4		bank selection bit 0
+			5		bank selection bit 1
+			6		
+			7		EXROM
+		
+		*/
+		
+		m_reset = 0;
+		
+		m_bank = ((data & 0x07) << 2) | ((data >> 4) & 0x03);
+		
+		m_exrom = BIT(data, 7);
 	}
 }
