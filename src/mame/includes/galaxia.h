c82f88f57a1edb11febf8380e55479652d57a0a3
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-03-27 07:52:30 +0000

    Sync with MAME r15793  (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@14814 9849079f-6229-0410-a07f-84406ce305ba

---
+++ src/mame/includes/galaxia.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+
+    Zaccaria Galaxia HW
+
+****************************************************************************/
+
+#include "includes/cvs.h"
+
+class galaxia_state : public cvs_state
+{
+public:
+	galaxia_state(const machine_config &mconfig, device_type type, const char *tag)
+		: cvs_state(mconfig, type, tag) { }
+
+	tilemap_t *m_bg_tilemap;
+	bitmap_ind16 m_temp_bitmap;
+};
+
+
+/*----------- defined in video/galaxia.c -----------*/
+
+PALETTE_INIT( galaxia );
+PALETTE_INIT( astrowar );
+VIDEO_START( galaxia );
+VIDEO_START( astrowar );
+
+SCREEN_UPDATE_IND16( galaxia );
+SCREEN_UPDATE_IND16( astrowar );
