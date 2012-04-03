2f84931f900c6e2f06ee965376aa6b7e5b5e0509
Justin Kerk <dopefishjustin@gmail.com>
2012-04-02 18:40:27 -0700

    Don't show startup screens for now since they are not async-friendly

--- src/emu/ui.c
+++ src/emu/ui.c
@@ -281,6 +281,11 @@ int ui_display_startup_screens(running_machine &machine, int first_time, int sho
 	if (!first_time || (str > 0 && str < 60*5) || &machine.system() == &GAME_NAME(___empty) || (machine.debug_flags & DEBUG_FLAG_ENABLED) != 0)
 		show_gameinfo = show_warnings = show_disclaimer = FALSE;
 
+	#ifdef SDLMAME_EMSCRIPTEN
+	/* don't show startup screens for now since they are not async-friendly */
+	show_gameinfo = show_warnings = show_disclaimer = FALSE;
+	#endif
+
 	/* initialize the on-screen display system */
 	slider_list = slider_current = slider_init(machine);
 
