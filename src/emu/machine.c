f6a2d71209e68b70ff07de4bcd5434010aaf5ad5
Justin Kerk <dopefishjustin@gmail.com>
2012-04-04 20:57:29 -0700

    Bits needed to compile a working demo

--- src/emu/machine.c
+++ src/emu/machine.c
@@ -116,6 +116,10 @@
 
 #include <time.h>
 
+#ifdef SDLMAME_EMSCRIPTEN
+void jsmess_set_main_loop(device_scheduler &sched);
+#endif
+
 
 
 //**************************************************************************
@@ -393,6 +397,10 @@ int running_machine::run(bool firstrun)
 		{
 			g_profiler.start(PROFILER_EXTRA);
 
+			#ifdef SDLMAME_EMSCRIPTEN
+			//break out to our async javascript loop and halt
+			jsmess_set_main_loop(m_scheduler);
+			#endif
 			// execute CPUs if not paused
 			if (!m_paused)
 				m_scheduler.timeslice();
