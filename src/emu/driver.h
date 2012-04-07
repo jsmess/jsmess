11d842be7b139faa0d3405c8f8b518c0550a384f
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-04-07 07:48:30 +0000

    Sync with MAME r15892 (no whatsnew)
    git-svn-id: svn://messdev.no-ip.org/mess@14863 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/driver.h
+++ src/emu/driver.h
@@ -2,7 +2,7 @@
 
     driver.h
 
-    Driver enumeration helpers.
+    Core driver device base class.
 
 ****************************************************************************
 
@@ -47,295 +47,196 @@
 #define __DRIVER_H__
 
 
-/***************************************************************************
-    CONSTANTS
-***************************************************************************/
-
-/* maxima */
-#define MAX_DRIVER_NAME_CHARS	8
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
 
 
+// ======================> driver_device
 
-/* ----- flags for game drivers ----- */
-
-#define ORIENTATION_MASK        		0x00000007
-#define GAME_NOT_WORKING				0x00000008
-#define GAME_UNEMULATED_PROTECTION		0x00000010	/* game's protection not fully emulated */
-#define GAME_WRONG_COLORS				0x00000020	/* colors are totally wrong */
-#define GAME_IMPERFECT_COLORS			0x00000040	/* colors are not 100% accurate, but close */
-#define GAME_IMPERFECT_GRAPHICS			0x00000080	/* graphics are wrong/incomplete */
-#define GAME_NO_COCKTAIL				0x00000100	/* screen flip support is missing */
-#define GAME_NO_SOUND					0x00000200	/* sound is missing */
-#define GAME_IMPERFECT_SOUND			0x00000400	/* sound is known to be wrong */
-#define GAME_SUPPORTS_SAVE				0x00000800	/* game supports save states */
-#define GAME_IS_BIOS_ROOT				0x00001000	/* this driver entry is a BIOS root */
-#define GAME_NO_STANDALONE				0x00002000	/* this driver cannot stand alone */
-#define GAME_REQUIRES_ARTWORK			0x00004000	/* the driver requires external artwork for key elements of the game */
-#define GAME_UNOFFICIAL     			0x00008000	/* unofficial hardware change */
-#define GAME_NO_SOUND_HW				0x00010000	/* sound hardware not available */
-#define GAME_MECHANICAL					0x00020000	/* contains mechanical parts (pinball, redemption games,...) */
-#define GAME_IS_SKELETON				0x00000208	/* mask for skelly games */
-#define GAME_IS_SKELETON_MECHANICAL		0x00024208	/* mask for skelly mechanical games */
-#define GAME_TYPE_ARCADE				0x00040000	/* arcade machine (coin operated machines) */
-#define GAME_TYPE_CONSOLE				0x00080000	/* console system */
-#define GAME_TYPE_COMPUTER				0x00100000	/* any kind of computer including home computers, minis, calcs,... */
-#define GAME_TYPE_OTHER					0x00200000	/* any other emulated system that doesn't fit above (ex. clock, satelite receiver,...) */
-
-/* ----- flags to return from video_update ----- */
-#define UPDATE_HAS_NOT_CHANGED			0x0001	/* the video has not changed */
-
-
-
-/***************************************************************************
-    TYPE DEFINITIONS
-***************************************************************************/
-
-typedef void   (*driver_init_func)(running_machine &machine);
-
-
-struct game_driver
+// base class for machine driver-specific devices
+class driver_device : public device_t
 {
-	const char *		source_file;				/* set this to __FILE__ */
-	const char *		parent;						/* if this is a clone, the name of the parent */
-	const char *		name;						/* short (8-character) name of the game */
-	const char *		description;				/* full name of the game */
-	const char *		year;						/* year the game was released */
-	const char *		manufacturer;				/* manufacturer of the game */
-	machine_config_constructor machine_config;		/* machine driver tokens */
-	ioport_constructor	ipt;					/* pointer to array of input port tokens */
-	void				(*driver_init)(running_machine &machine); /* DRIVER_INIT callback */
-	const rom_entry *	rom;						/* pointer to list of ROMs for the game */
-	const char *		compatible_with;
-	UINT32				flags;						/* orientation and other flags; see defines below */
-	const char *		default_layout;				/* default internally defined layout */
-};
-
-
-// driver_list is a purely static class that wraps the global driver list
-class driver_list
-{
-	DISABLE_COPYING(driver_list);
-
-protected:
-	// construction/destruction
-	driver_list();
-
-public:
-	// getters
-	static int total() { return s_driver_count; }
-
-	// any item by index
-	static const game_driver &driver(int index) { assert(index >= 0 && index < s_driver_count); return *s_drivers_sorted[index]; }
-	static int clone(int index) { return find(driver(index).parent); }
-	static int non_bios_clone(int index) { int result = find(driver(index).parent); return (result != -1 && (driver(result).flags & GAME_IS_BIOS_ROOT) == 0) ? result : -1; }
-	static int compatible_with(int index) { return find(driver(index).compatible_with); }
-
-	// any item by driver
-	static int clone(const game_driver &driver) { int index = find(driver); assert(index != -1); return clone(index); }
-	static int non_bios_clone(const game_driver &driver) { int index = find(driver); assert(index != -1); return non_bios_clone(index); }
-	static int compatible_with(const game_driver &driver) { int index = find(driver); assert(index != -1); return compatible_with(index); }
-
-	// general helpers
-	static int find(const char *name);
-	static int find(const game_driver &driver) { return find(driver.name); }
-
-	// static helpers
-	static bool matches(const char *wildstring, const char *string);
-
-protected:
-	// internal helpers
-	static int driver_sort_callback(const void *elem1, const void *elem2);
-	static int penalty_compare(const char *source, const char *target);
-
-	// internal state
-	static int							s_driver_count;
-	static const game_driver * const	s_drivers_sorted[];
-};
-
-
-// driver_enumerator enables efficient iteration through the driver list
-class driver_enumerator : public driver_list
-{
-	DISABLE_COPYING(driver_enumerator);
-
 public:
 	// construction/destruction
-	driver_enumerator(emu_options &options);
-	driver_enumerator(emu_options &options, const char *filter);
-	driver_enumerator(emu_options &options, const game_driver &filter);
-	~driver_enumerator();
+	driver_device(const machine_config &mconfig, device_type type, const char *tag);
+	virtual ~driver_device();
 
-	// getters
-	int count() const { return m_filtered_count; }
-	int current() const { return m_current; }
-	emu_options &options() const { return m_options; }
-
-	// current item
-	const game_driver &driver() const { return driver_list::driver(m_current); }
-	machine_config &config() const { return config(m_current, m_options); }
-	int clone() { return driver_list::clone(m_current); }
-	int non_bios_clone() { return driver_list::non_bios_clone(m_current); }
-	int compatible_with() { return driver_list::compatible_with(m_current); }
-	void include() { include(m_current); }
-	void exclude() { exclude(m_current); }
-
-	// any item by index
-	bool included(int index) const { assert(index >= 0 && index < s_driver_count); return m_included[index]; }
-	bool excluded(int index) const { assert(index >= 0 && index < s_driver_count); return !m_included[index]; }
-	machine_config &config(int index) const { return config(index,m_options); }
-	machine_config &config(int index, emu_options &options) const;
-	void include(int index) { assert(index >= 0 && index < s_driver_count); if (!m_included[index]) { m_included[index] = true; m_filtered_count++; }  }
-	void exclude(int index) { assert(index >= 0 && index < s_driver_count); if (m_included[index]) { m_included[index] = false; m_filtered_count--; } }
-	using driver_list::driver;
-	using driver_list::clone;
-	using driver_list::non_bios_clone;
-	using driver_list::compatible_with;
-
-	// filtering/iterating
-	int filter(const char *string = NULL);
-	int filter(const game_driver &driver);
-	void include_all();
-	void exclude_all() { memset(m_included, 0, sizeof(m_included[0]) * s_driver_count); m_filtered_count = 0; }
-	void reset() { m_current = -1; }
-	bool next();
-	bool next_excluded();
-
-	// general helpers
-	void set_current(int index) { assert(index >= -1 && index <= s_driver_count); m_current = index; }
-	void find_approximate_matches(const char *string, int count, int *results);
-
-private:
-	// entry in the config cache
-	struct config_entry
+	// indexes into our generic callbacks
+	enum callback_type
 	{
-		friend class simple_list<config_entry>;
-
-	public:
-		// construction/destruction
-		config_entry(machine_config &config, int index);
-		~config_entry();
-
-		// getters
-		config_entry *next() const { return m_next; }
-		int index() const { return m_index; }
-		machine_config *config() const { return m_config; }
-
-	private:
-		// internal state
-		config_entry *		m_next;
-		machine_config *	m_config;
-		int					m_index;
+		CB_MACHINE_START,
+		CB_MACHINE_RESET,
+		CB_SOUND_START,
+		CB_SOUND_RESET,
+		CB_VIDEO_START,
+		CB_VIDEO_RESET,
+		CB_COUNT
 	};
 
-	static const int CONFIG_CACHE_COUNT = 100;
+	// inline configuration helpers
+	static void static_set_game(device_t &device, const game_driver &game);
+	static void static_set_callback(device_t &device, callback_type type, legacy_callback_func callback);
+	static void static_set_palette_init(device_t &device, palette_init_func callback);
+
+	// generic helpers
+
+	// watchdog read/write handlers
+	DECLARE_WRITE8_MEMBER( watchdog_reset_w );
+	DECLARE_READ8_MEMBER( watchdog_reset_r );
+	DECLARE_WRITE16_MEMBER( watchdog_reset16_w );
+	DECLARE_READ16_MEMBER( watchdog_reset16_r );
+	DECLARE_WRITE32_MEMBER( watchdog_reset32_w );
+	DECLARE_READ32_MEMBER( watchdog_reset32_r );
+
+	// sound latch readers
+	DECLARE_READ8_MEMBER( soundlatch_r );
+	DECLARE_READ8_MEMBER( soundlatch2_r );
+	DECLARE_READ8_MEMBER( soundlatch3_r );
+	DECLARE_READ8_MEMBER( soundlatch4_r );
+	DECLARE_READ16_MEMBER( soundlatch_word_r );
+	DECLARE_READ16_MEMBER( soundlatch2_word_r );
+	DECLARE_READ16_MEMBER( soundlatch3_word_r );
+	DECLARE_READ16_MEMBER( soundlatch4_word_r );
+
+	// sound latch writers
+	DECLARE_WRITE8_MEMBER( soundlatch_w );
+	DECLARE_WRITE8_MEMBER( soundlatch2_w );
+	DECLARE_WRITE8_MEMBER( soundlatch3_w );
+	DECLARE_WRITE8_MEMBER( soundlatch4_w );
+	DECLARE_WRITE16_MEMBER( soundlatch_word_w );
+	DECLARE_WRITE16_MEMBER( soundlatch2_word_w );
+	DECLARE_WRITE16_MEMBER( soundlatch3_word_w );
+	DECLARE_WRITE16_MEMBER( soundlatch4_word_w );
+
+	// sound latch clearers
+	DECLARE_WRITE8_MEMBER( soundlatch_clear_w );
+	DECLARE_WRITE8_MEMBER( soundlatch2_clear_w );
+	DECLARE_WRITE8_MEMBER( soundlatch3_clear_w );
+	DECLARE_WRITE8_MEMBER( soundlatch4_clear_w );
+	
+	// general palette helpers
+	void set_color_332(pen_t color, int rshift, int gshift, int bshift, UINT32 data);
+	void set_color_444(pen_t color, int rshift, int gshift, int bshift, UINT32 data);
+	void set_color_555(pen_t color, int rshift, int gshift, int bshift, UINT32 data);
+	void set_color_565(pen_t color, int rshift, int gshift, int bshift, UINT32 data);
+	void set_color_888(pen_t color, int rshift, int gshift, int bshift, UINT32 data);
+	void set_color_4444(pen_t color, int ishift, int rshift, int gshift, int bshift, UINT16 data);
+
+	// 3-3-2 RGB palette write handlers
+	DECLARE_WRITE8_MEMBER( paletteram_BBGGGRRR_w );
+	DECLARE_WRITE8_MEMBER( paletteram_RRRGGGBB_w );
+	DECLARE_WRITE8_MEMBER( paletteram_BBGGRRII_w );
+	DECLARE_WRITE8_MEMBER( paletteram_IIBBGGRR_w );
+
+	// 4-4-4 RGB palette write handlers
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBGGGGRRRR_le_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBGGGGRRRR_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBGGGGRRRR_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBGGGGRRRR_split2_w );	// uses paletteram2
+	DECLARE_WRITE16_MEMBER( paletteram16_xxxxBBBBGGGGRRRR_word_w );
+
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBRRRRGGGG_le_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBRRRRGGGG_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBRRRRGGGG_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxBBBBRRRRGGGG_split2_w );	// uses paletteram2
+	DECLARE_WRITE16_MEMBER( paletteram16_xxxxBBBBRRRRGGGG_word_w );
+
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRBBBBGGGG_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRBBBBGGGG_split2_w );	// uses paletteram2
+
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRGGGGBBBB_le_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRGGGGBBBB_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRGGGGBBBB_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xxxxRRRRGGGGBBBB_split2_w );	// uses paletteram2
+	DECLARE_WRITE16_MEMBER( paletteram16_xxxxRRRRGGGGBBBB_word_w );
+
+	DECLARE_WRITE8_MEMBER( paletteram_RRRRGGGGBBBBxxxx_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_RRRRGGGGBBBBxxxx_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_RRRRGGGGBBBBxxxx_split2_w );	// uses paletteram2
+	DECLARE_WRITE16_MEMBER( paletteram16_RRRRGGGGBBBBxxxx_word_w );
+
+	// 4-4-4-4 IRGB palette write handlers
+	DECLARE_WRITE16_MEMBER( paletteram16_IIIIRRRRGGGGBBBB_word_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_RRRRGGGGBBBBIIII_word_w );
+
+	// 5-5-5 RGB palette write handlers
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBGGGGGRRRRR_le_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBGGGGGRRRRR_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBGGGGGRRRRR_split1_w );	// uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBGGGGGRRRRR_split2_w );	// uses paletteram2
+	DECLARE_WRITE16_MEMBER( paletteram16_xBBBBBGGGGGRRRRR_word_w );
+
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBRRRRRGGGGG_split1_w );  // uses paletteram
+	DECLARE_WRITE8_MEMBER( paletteram_xBBBBBRRRRRGGGGG_split2_w );  // uses paletteram2
+
+	DECLARE_WRITE8_MEMBER( paletteram_xRRRRRGGGGGBBBBB_le_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xRRRRRGGGGGBBBBB_be_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xRRRRRGGGGGBBBBB_split1_w );
+	DECLARE_WRITE8_MEMBER( paletteram_xRRRRRGGGGGBBBBB_split2_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_xRRRRRGGGGGBBBBB_word_w );
+
+	DECLARE_WRITE16_MEMBER( paletteram16_xGGGGGRRRRRBBBBB_word_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_xGGGGGBBBBBRRRRR_word_w );
+
+	DECLARE_WRITE16_MEMBER( paletteram16_RRRRRGGGGGBBBBBx_word_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_GGGGGRRRRRBBBBBx_word_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_RRRRGGGGBBBBRGBx_word_w );
+
+	// 8-8-8 RGB palette write handlers
+	DECLARE_WRITE16_MEMBER( paletteram16_xrgb_word_be_w );
+	DECLARE_WRITE16_MEMBER( paletteram16_xbgr_word_be_w );
+
+protected:
+	// helpers called at startup
+	virtual void driver_start();
+	virtual void machine_start();
+	virtual void sound_start();
+	virtual void video_start();
+
+	// helpers called at reset
+	virtual void driver_reset();
+	virtual void machine_reset();
+	virtual void sound_reset();
+	virtual void video_reset();
+
+	// device-level overrides
+	virtual const rom_entry *device_rom_region() const;
+	virtual ioport_constructor device_input_ports() const;
+	virtual void device_start();
+	virtual void device_reset_after_children();
+
+	// internal helpers
+	inline UINT16 paletteram16_le(offs_t offset) const { return m_generic_paletteram_8[offset & ~1] | (m_generic_paletteram_8[offset |  1] << 8); }
+	inline UINT16 paletteram16_be(offs_t offset) const { return m_generic_paletteram_8[offset |  1] | (m_generic_paletteram_8[offset & ~1] << 8); }
+	inline UINT16 paletteram16_split(offs_t offset) const { return m_generic_paletteram_8[offset] | (m_generic_paletteram2_8[offset] << 8); }
+	inline UINT32 paletteram32_be(offs_t offset) const { return m_generic_paletteram_16[offset | 1] | (m_generic_paletteram_16[offset & ~1] << 16); }
 
 	// internal state
-	int					m_current;
-	int					m_filtered_count;
-	emu_options &		m_options;
-	UINT8 *				m_included;
-	machine_config **	m_config;
-	mutable simple_list<config_entry> m_config_cache;
+	const game_driver *		m_system;					// pointer to the game driver
+
+	legacy_callback_func	m_callbacks[CB_COUNT];		// generic legacy callbacks
+	palette_init_func		m_palette_init;				// one-time palette init callback
+
+public:
+	// generic pointers
+	optional_shared_ptr<UINT8> m_generic_paletteram_8;
+	optional_shared_ptr<UINT8> m_generic_paletteram2_8;
+	optional_shared_ptr<UINT16> m_generic_paletteram_16;
+	optional_shared_ptr<UINT16> m_generic_paletteram2_16;
+	optional_shared_ptr<UINT32> m_generic_paletteram_32;
+	optional_shared_ptr<UINT32> m_generic_paletteram2_32;
 };
 
 
-
-/***************************************************************************
-    MACROS FOR BUILDING GAME DRIVERS
-***************************************************************************/
-
-
-#define DRIVER_INIT_NAME(name)		driver_init_##name
-#define DRIVER_INIT(name)			void DRIVER_INIT_NAME(name)(running_machine &machine)
-#define DRIVER_INIT_CALL(name)		DRIVER_INIT_NAME(name)(machine)
-
-#define driver_init_0				NULL
+// this template function creates a stub which constructs a device
+template<class _DriverClass>
+device_t *driver_device_creator(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock)
+{
+	assert(owner == NULL);
+	assert(clock == 0);
+	return global_alloc_clear(_DriverClass(mconfig, &driver_device_creator<_DriverClass>, tag));
+}
 
 
-#define GAME_NAME(name) driver_##name
-#define GAME_EXTERN(name) extern const game_driver GAME_NAME(name)
-
-#define GAME(YEAR,NAME,PARENT,MACHINE,INPUT,INIT,MONITOR,COMPANY,FULLNAME,FLAGS)	\
-	GAMEL(YEAR,NAME,PARENT,MACHINE,INPUT,INIT,MONITOR,COMPANY,FULLNAME,FLAGS,((const char *)0))
-
-#define GAMEL(YEAR,NAME,PARENT,MACHINE,INPUT,INIT,MONITOR,COMPANY,FULLNAME,FLAGS,LAYOUT)	\
-extern const game_driver GAME_NAME(NAME) =	\
-{											\
-	__FILE__,								\
-	#PARENT,								\
-	#NAME,									\
-	FULLNAME,								\
-	#YEAR,									\
-	COMPANY,								\
-	MACHINE_CONFIG_NAME(MACHINE),			\
-	INPUT_PORTS_NAME(INPUT),				\
-	DRIVER_INIT_NAME(INIT),					\
-	ROM_NAME(NAME),							\
-	NULL,									\
-	(MONITOR)|(FLAGS)|GAME_TYPE_ARCADE,		\
-	&LAYOUT[0]								\
-};
-
-#define CONS(YEAR,NAME,PARENT,COMPAT,MACHINE,INPUT,INIT,COMPANY,FULLNAME,FLAGS)	\
-extern const game_driver GAME_NAME(NAME) =	\
-{											\
-	__FILE__,								\
-	#PARENT,								\
-	#NAME,									\
-	FULLNAME,								\
-	#YEAR,									\
-	COMPANY,								\
-	MACHINE_CONFIG_NAME(MACHINE),			\
-	INPUT_PORTS_NAME(INPUT),				\
-	DRIVER_INIT_NAME(INIT),					\
-	ROM_NAME(NAME),							\
-	#COMPAT,								\
-	ROT0|(FLAGS)|GAME_TYPE_CONSOLE,			\
-	NULL									\
-};
-
-#define COMP(YEAR,NAME,PARENT,COMPAT,MACHINE,INPUT,INIT,COMPANY,FULLNAME,FLAGS)	\
-extern const game_driver GAME_NAME(NAME) =	\
-{											\
-	__FILE__,								\
-	#PARENT,								\
-	#NAME,									\
-	FULLNAME,								\
-	#YEAR,									\
-	COMPANY,								\
-	MACHINE_CONFIG_NAME(MACHINE),			\
-	INPUT_PORTS_NAME(INPUT),				\
-	DRIVER_INIT_NAME(INIT),					\
-	ROM_NAME(NAME),							\
-	#COMPAT,								\
-	ROT0|(FLAGS)|GAME_TYPE_COMPUTER,		\
-	NULL									\
-};
-
-#define SYST(YEAR,NAME,PARENT,COMPAT,MACHINE,INPUT,INIT,COMPANY,FULLNAME,FLAGS)	\
-extern const game_driver GAME_NAME(NAME) =	\
-{											\
-	__FILE__,								\
-	#PARENT,								\
-	#NAME,									\
-	FULLNAME,								\
-	#YEAR,									\
-	COMPANY,								\
-	MACHINE_CONFIG_NAME(MACHINE),			\
-	INPUT_PORTS_NAME(INPUT),				\
-	DRIVER_INIT_NAME(INIT),					\
-	ROM_NAME(NAME),							\
-	#COMPAT,								\
-	ROT0|(FLAGS)|GAME_TYPE_OTHER,		\
-	NULL									\
-};
-
-/***************************************************************************
-    GLOBAL VARIABLES
-***************************************************************************/
-
-GAME_EXTERN(___empty);
-
-
-
-#endif
+#endif	/* __DRIVER_H__ */
--- src/emu/emu.h
+++ src/emu/emu.h
@@ -112,7 +112,7 @@ typedef device_t * (*machine_config_constructor)(machine_config &config, device_
 
 // machine and driver configuration
 #include "mconfig.h"
-#include "driver.h"
+#include "gamedrv.h"
 
 // image-related
 #include "softlist.h"
@@ -123,6 +123,7 @@ typedef device_t * (*machine_config_constructor)(machine_config &config, device_
 
 // the running machine
 #include "machine.h"
+#include "driver.h"
 #include "mame.h"
 
 // video-related
