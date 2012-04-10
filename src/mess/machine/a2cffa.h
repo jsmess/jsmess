7a666402543446e50bd83f5a36bee25af04f1328
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2012-04-10 00:35:27 +0000

    Apple II: CFFA2000 now has savable settings EEPROM, plus default settings patched to enable two drives [R. Belmont]
    git-svn-id: svn://messdev.no-ip.org/mess@14889 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/a2cffa.h
+++ src/mess/machine/a2cffa.h
@@ -38,25 +38,41 @@ protected:
     virtual void write_c0nx(address_space &space, UINT8 offset, UINT8 data);
     virtual UINT8 read_cnxx(address_space &space, UINT8 offset);
     virtual UINT8 read_c800(address_space &space, UINT16 offset);
+    virtual void write_c800(address_space &space, UINT16 offset, UINT8 data);
 
     required_device<device_t> m_ide;
 
-private:
     UINT8 *m_rom;
+    UINT8 m_eeprom[0x1000];
+
+private:
     UINT16 m_lastdata;
+    bool m_writeprotect;
 };
 
-class a2bus_cffa2_device : public a2bus_cffa2000_device
+class a2bus_cffa2_device : public a2bus_cffa2000_device, public device_nvram_interface
 {
 public:
     a2bus_cffa2_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+
+protected:
+    // device_config_nvram_interface overrides
+    virtual void nvram_default();
+    virtual void nvram_read(emu_file &file);
+    virtual void nvram_write(emu_file &file);
 };
 
-class a2bus_cffa2_6502_device : public a2bus_cffa2000_device
+class a2bus_cffa2_6502_device : public a2bus_cffa2000_device, public device_nvram_interface
 {
 public:
     a2bus_cffa2_6502_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
     virtual const rom_entry *device_rom_region() const;
+
+protected:
+    // device_config_nvram_interface overrides
+    virtual void nvram_default();
+    virtual void nvram_read(emu_file &file);
+    virtual void nvram_write(emu_file &file);
 };
 
 // device type definition
