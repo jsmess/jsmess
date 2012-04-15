2874c1c7895d1834fd39bb8b1c66e0ad3ce8ee30
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-04-15 16:29:24 +0000

    Cleanup. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@14937 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/c64_bn1541.h
+++ src/mess/machine/c64_bn1541.h
@@ -53,8 +53,8 @@ public:
 
 protected:
 	// device-level overrides
-	virtual void device_start();
 	virtual void device_config_complete() { m_shortname = "c64_bn1541"; }
+	virtual void device_start();
 
 	// device_c64_user_port_interface overrides
 	virtual UINT8 c64_pb_r(address_space &space, offs_t offset);
