02d872ee0a2c57046f5716dff671c0a7cc59550a
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-04-17 14:05:33 +0000

    [C64] Added support for the 4040+Fast Hack'em cartridge. [Curt Coder]
    git-svn-id: svn://messdev.no-ip.org/mess@14954 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/c64_sw8k.h
+++ src/mess/machine/c64_sw8k.h
@@ -1,6 +1,6 @@
 /**********************************************************************
 
-    PS-64 speech cartridge emulation
+    C64 switchable 8K cartridge emulation
 
     Copyright MESS Team.
     Visit http://mamedev.org for licensing and usage restrictions.
@@ -9,13 +9,12 @@
 
 #pragma once
 
-#ifndef __PS64__
-#define __PS64__
+#ifndef __C64_SW8K__
+#define __C64_SW8K__
 
 
 #include "emu.h"
 #include "machine/c64exp.h"
-#include "sound/votrax.h"
 
 
 
@@ -23,32 +22,34 @@
 //  TYPE DEFINITIONS
 //**************************************************************************
 
-// ======================> c64_ps64_cartridge_device
+// ======================> c64_switchable_8k_cartridge_device
 
-class c64_ps64_cartridge_device : public device_t,
-					    		  public device_c64_expansion_card_interface
+class c64_switchable_8k_cartridge_device : public device_t,
+					    		  		   public device_c64_expansion_card_interface
 {
 public:
 	// construction/destruction
-	c64_ps64_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	c64_switchable_8k_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
 
 	// optional information overrides
-	virtual machine_config_constructor device_mconfig_additions() const;
+	virtual ioport_constructor device_input_ports() const;
 
 protected:
 	// device-level overrides
-    virtual void device_config_complete() { m_shortname = "c64_ps64"; }
+    virtual void device_config_complete() { m_shortname = "c64_sw8k"; }
 	virtual void device_start();
 	virtual void device_reset();
 
 	// device_c64_expansion_card_interface overrides
 	virtual UINT8 c64_cd_r(address_space &space, offs_t offset, int roml, int romh, int io1, int io2);
-	virtual void c64_cd_w(address_space &space, offs_t offset, UINT8 data, int roml, int romh, int io1, int io2);
+
+private:
+	int m_bank;
 };
 
 
 // device type definition
-extern const device_type C64_PS64;
+extern const device_type C64_SW8K;
 
 
 #endif
