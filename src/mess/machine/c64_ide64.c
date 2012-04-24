dca522bc16a1ee34151b86e3a919a01137300dc0
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-04-24 15:36:52 +0000

    [C64] IDE64 WIP, now booting. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@15026 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/c64_ide64.c
+++ src/mess/machine/c64_ide64.c
@@ -11,8 +11,8 @@
 
     TODO:
 
-    - flash write
     - FT245
+    - CompactFlash
 
 */
 
@@ -27,6 +27,7 @@
 #define AT29C010A_TAG		"u3"
 #define DS1302_TAG			"u4"
 #define FT245R_TAG			"u21"
+#define IDE_TAG				"ide"
 
 
 
@@ -44,10 +45,8 @@ const device_type C64_IDE64 = &device_creator<c64_ide64_cartridge_device>;
 static MACHINE_CONFIG_FRAGMENT( c64_ide64 )
 	MCFG_ATMEL_29C010_ADD(AT29C010A_TAG)
 	MCFG_DS1302_ADD(DS1302_TAG)
-	// TODO FT245R
-	// TODO CompactFlash
 
-	MCFG_IDE_CONTROLLER_ADD("ide", NULL, ide_image_devices, "hdd", "hdd")
+	MCFG_IDE_CONTROLLER_ADD(IDE_TAG, NULL, ide_image_devices, "hdd", "hdd")
 MACHINE_CONFIG_END
 
 
@@ -98,7 +97,7 @@ c64_ide64_cartridge_device::c64_ide64_cartridge_device(const machine_config &mco
 	device_c64_expansion_card_interface(mconfig, *this),
 	m_flash_rom(*this, AT29C010A_TAG),
 	m_rtc(*this, DS1302_TAG),
-	m_ide(*this, "ide")
+	m_ide(*this, IDE_TAG)
 {
 }
 
@@ -127,12 +126,13 @@ void c64_ide64_cartridge_device::device_start()
 void c64_ide64_cartridge_device::device_reset()
 {
 	m_bank = 0;
-	m_game = 1;
-	m_exrom = 1;
+
 	m_enable = 1;
 	m_rtc_ce = 0;
 
 	m_wp = input_port_read(*this, "JP1");
+	m_game = !m_wp;
+	m_exrom = !m_wp;
 }
 
 
@@ -153,7 +153,7 @@ UINT8 c64_ide64_cartridge_device::c64_cd_r(address_space &space, offs_t offset, 
 		{
 			ram_oe = 0;
 		}
-		else if (offset >= 0xa000 && offset < 0xc000)
+		else if (offset >= 0x8000 && offset < 0xc000)
 		{
 			rom_oe = 0;
 		}
@@ -172,57 +172,58 @@ UINT8 c64_ide64_cartridge_device::c64_cd_r(address_space &space, offs_t offset, 
 		{
 			rom_oe = 0;
 		}
-		else if (!io1)
+	}
+
+	if (!io1)
+	{
+		// 0x20-0x2f    IDE
+		// 0x30-0x37    I/O
+		// 0x5d-0x5e    FT245
+		// 0x5f-0x5f    DS1302
+		// 0x60-0xff    ROM
+
+		UINT8 io1_offset = offset & 0xff;
+
+		if (io1_offset >= 0x20 && io1_offset < 0x30)
 		{
-			// 0x20-0x2f    IDE
-			// 0x30-0x37    I/O
-			// 0x5d-0x5e    FT245
-			// 0x5f-0x5f    DS1302
-			// 0x60-0xff    ROM
+			m_ide_data = ide_bus_r(m_ide, BIT(offset, 3), offset & 0x07);
 
-			UINT8 io1_offset = offset & 0xff;
+			data = m_ide_data & 0xff;
+		}
+		else if (io1_offset == 0x31)
+		{
+			data = m_ide_data >> 8;
+		}
+		else if (io1_offset == 0x32)
+		{
+			/*
 
-			if (io1_offset >= 0x20 && io1_offset < 0x30)
-			{
-				m_ide_data = ide_bus_r(m_ide, BIT(offset, 3), offset & 0x07);
+                bit     description
 
-				data = m_ide_data & 0xff;
-			}
-			else if (io1_offset == 0x31)
-			{
-				data = m_ide_data >> 8;
-			}
-			else if (io1_offset == 0x32)
-			{
-				/*
+                0       EXROM
+                1       GAME
+                2       A14
+                3       A15
+                4       A16
+                5       v4.x
+                6
+                7
 
-                    bit     description
+            */
 
-                    0       GAME
-                    1       EXROM
-                    2       A14
-                    3       A15
-                    4       A16
-                    5       v4.x
-                    6
-                    7
+			data = 0x20 | (m_bank << 2) | (m_game << 1) | m_exrom;
+		}
+		else if ((io1_offset == 0x5f) && m_rtc_ce)
+		{
+			m_rtc->ds1302_clk_w(0, 0);
 
-                */
+			m_rtc->ds1302_dat_w(0, BIT(data, 0));
 
-				data = 0x20 | (m_bank << 2) | (m_exrom << 1) | m_game;
-			}
-			else if ((io1_offset == 0x5f) && m_rtc_ce)
-			{
-				m_rtc->ds1302_clk_w(0, 0);
-
-				m_rtc->ds1302_dat_w(0, BIT(data, 0));
-
-				m_rtc->ds1302_clk_w(0, 1);
-			}
-			else if (io1_offset >= 0x60)
-			{
-				rom_oe = 0;
-			}
+			m_rtc->ds1302_clk_w(0, 1);
+		}
+		else if (io1_offset >= 0x60)
+		{
+			rom_oe = 0;
 		}
 	}
 
@@ -266,66 +267,65 @@ void c64_ide64_cartridge_device::c64_cd_w(address_space &space, offs_t offset, U
 			offs_t addr = (m_bank << 14) | (offset & 0x3fff);
 			m_flash_rom->write(addr, data);
 		}
-		else if (!io1)
+
+	}
+
+	if (!io1)
+	{
+		// 0x20-0x2f    IDE
+		// 0x30-0x37    I/O
+		// 0x5d-0x5e    FT245
+		// 0x5f-0x5f    DS1302
+		// 0x60-0xff    ROM
+
+		UINT8 io1_offset = offset & 0xff;
+
+		if (io1_offset >= 0x20 && io1_offset < 0x30)
 		{
-			// 0x20-0x2f    IDE
-			// 0x30-0x37    I/O
-			// 0x5d-0x5e    FT245
-			// 0x5f-0x5f    DS1302
-			// 0x60-0xff    ROM
+			m_ide_data = (m_ide_data & 0xff00) | data;
 
-			UINT8 io1_offset = offset & 0xff;
+			ide_bus_w(m_ide, BIT(offset, 3), offset & 0x07, m_ide_data);
+		}
+		else if (io1_offset == 0x31)
+		{
+			m_ide_data = (data << 8) | (m_ide_data & 0xff);
+		}
+		else if ((io1_offset == 0x5f) && m_rtc_ce)
+		{
+			m_rtc->ds1302_clk_w(0, 0);
 
-			if (io1_offset >= 0x20 && io1_offset < 0x30)
-			{
-				m_ide_data = (m_ide_data & 0xff00) | data;
+			data = m_rtc->ds1302_read(0);
 
-				ide_bus_w(m_ide, BIT(offset, 3), offset & 0x07, m_ide_data);
-			}
-			else if (io1_offset == 0x31)
-			{
-				m_ide_data = (data << 8) | (m_ide_data & 0xff);
-			}
-			else if ((io1_offset == 0x5f) && m_rtc_ce)
-			{
-				m_rtc->ds1302_clk_w(0, 0);
+			m_rtc->ds1302_clk_w(0, 1);
+		}
+		else if (io1_offset >= 0x60 && io1_offset < 0x68)
+		{
+			m_bank = offset & 0x07;
+		}
+		else if (io1_offset == 0xfb)
+		{
+			/*
 
-				data = m_rtc->ds1302_read(0);
+                bit     description
 
-				m_rtc->ds1302_clk_w(0, 1);
-			}
-			else if (io1_offset >= 0x60 && io1_offset < 0x68)
-			{
-				m_bank = offset & 0x07;
-			}
-			else if (io1_offset >= 0xfb)
-			{
-				/*
+                0       disable cartridge
+                1       RTC CE
+                2
+                3
+                4
+                5
+                6
+                7
 
-                    bit     description
+            */
 
-                    0       disable cartridge
-                    1       RTC CE
-                    2
-                    3
-                    4
-                    5
-                    6
-                    7
-
-                */
-
-				m_enable = !BIT(data, 0);
-				m_rtc_ce = BIT(data, 1);
-
-				m_game = BIT(offset, 0);
-				m_exrom = BIT(offset, 1);
-			}
-			else if (io1_offset >= 0xfc)
-			{
-				m_game = BIT(offset, 0);
-				m_exrom = BIT(offset, 1);
-			}
+			m_enable = !BIT(data, 0);
+			m_rtc_ce = BIT(data, 1);
+		}
+		else if (io1_offset >= 0xfc)
+		{
+			m_game = BIT(offset, 0);
+			m_exrom = BIT(offset, 1);
 		}
 	}
 }
