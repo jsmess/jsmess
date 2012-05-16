775f3ca3e7794034d1a8c18dcbe68611a8ca28b6
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2012-05-16 12:08:25 +0000

    PIPBUG - connected to serial terminal, marked as WORKING [Robbbert]
    git-svn-id: svn://messdev.no-ip.org/mess@15197 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/pipbug.c
+++ src/mess/drivers/pipbug.c
@@ -3,11 +3,23 @@
         PIPBUG
 
         08/04/2010 Skeleton driver.
+        16/05/2012 Connected to serial terminal.. working
+
+        All input must be in uppercase.
+
+        Commands:
+        A — See and alter memory
+        B — Set breakpoint (2 permitted)
+        C — Clear breakpoint
+        D — Dump memory to paper tape
+        G — Go to address, run
+        L — Load memory from paper tape
+        S — See and alter registers
 
         PIPBUG isn't a computer; it is a the name of the bios used
         in a number of small 2650-based computers from 1976 to 1978.
         Examples include Baby 2650, Eurocard 2650, etc., plus Signetics
-        own PC1001 and PC1500 systems. PIPBUG was written by Signetics.
+        own PC1001, PC1500, and KT9500 systems. PIPBUG was written by Signetics.
 
         The sole means of communication is via a serial terminal.
         PIPBUG uses the SENSE and FLAG pins as serial lines, thus
@@ -16,30 +28,40 @@
         The Baby 2650 (featured in Electronics Australia magazine in
         March 1977) has 256 bytes of RAM.
 
-        TODO
-        - Connect to a terminal when MESS has the capability of a
-             generic serial terminal.
-        - After that, remove all the current video code (only there
-             currently to prevent a crash).
 
 ****************************************************************************/
 
 #include "emu.h"
 #include "cpu/s2650/s2650.h"
+#include "machine/terminal.h"
 
 
 class pipbug_state : public driver_device
 {
 public:
 	pipbug_state(const machine_config &mconfig, device_type type, const char *tag)
-		: driver_device(mconfig, type, tag) { }
+		: driver_device(mconfig, type, tag),
+	m_terminal(*this, TERMINAL_TAG) { }
 
 	DECLARE_WRITE8_MEMBER(pipbug_ctrl_w);
+	DECLARE_READ8_MEMBER(pipbug_serial_r);
+	DECLARE_WRITE8_MEMBER(pipbug_serial_w);
+	required_device<serial_terminal_device> m_terminal;
 };
 
 WRITE8_MEMBER( pipbug_state::pipbug_ctrl_w )
 {
 // 0x80 is written here - not connected in the baby 2650
+}
+
+READ8_MEMBER( pipbug_state::pipbug_serial_r )
+{
+	return m_terminal->tx_r();
+}
+
+WRITE8_MEMBER( pipbug_state::pipbug_serial_w )
+{
+	m_terminal->rx_w(data);
 }
 
 static ADDRESS_MAP_START(pipbug_mem, AS_PROGRAM, 8, pipbug_state)
@@ -51,26 +73,17 @@ ADDRESS_MAP_END
 static ADDRESS_MAP_START(pipbug_io, AS_IO, 8, pipbug_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(S2650_CTRL_PORT, S2650_CTRL_PORT) AM_WRITE(pipbug_ctrl_w)
-	//AM_RANGE(S2650_SENSE_PORT, S2650_FO_PORT) AM_READWRITE(pipbug_serial_in,pipbug_serial_out)
+	AM_RANGE(S2650_SENSE_PORT, S2650_FO_PORT) AM_READWRITE(pipbug_serial_r,pipbug_serial_w)
 ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( pipbug )
 INPUT_PORTS_END
 
-
-static MACHINE_RESET(pipbug)
+static const serial_terminal_interface terminal_intf =
 {
-}
-
-static VIDEO_START( pipbug )
-{
-}
-
-static SCREEN_UPDATE_IND16( pipbug )
-{
-	return 0;
-}
+	DEVCB_NULL
+};
 
 static MACHINE_CONFIG_START( pipbug, pipbug_state )
 	/* basic machine hardware */
@@ -78,20 +91,8 @@ static MACHINE_CONFIG_START( pipbug, pipbug_state )
 	MCFG_CPU_PROGRAM_MAP(pipbug_mem)
 	MCFG_CPU_IO_MAP(pipbug_io)
 
-	MCFG_MACHINE_RESET(pipbug)
-
 	/* video hardware */
-	MCFG_SCREEN_ADD("screen", RASTER)
-	MCFG_SCREEN_REFRESH_RATE(50)
-	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
-	MCFG_SCREEN_SIZE(640, 480)
-	MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
-	MCFG_SCREEN_UPDATE_STATIC(pipbug)
-
-	MCFG_PALETTE_LENGTH(2)
-	MCFG_PALETTE_INIT(black_and_white)
-
-	MCFG_VIDEO_START(pipbug)
+	MCFG_SERIAL_TERMINAL_ADD(TERMINAL_TAG, terminal_intf, 110)
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -103,5 +104,5 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY   FULLNAME       FLAGS */
-COMP( 1979, pipbug,  0,       0,     pipbug,    pipbug,    0,  "Signetics", "PIPBUG", GAME_NOT_WORKING | GAME_NO_SOUND_HW )
+COMP( 1979, pipbug,  0,       0,     pipbug,    pipbug,    0,  "Signetics", "PIPBUG", GAME_NO_SOUND_HW )
 
