76dacce89a64efa650b2c6538cbf29271a7fcb76
rbelmont <rbelmont@9849079f-6229-0410-a07f-84406ce305ba>
2012-05-21 02:41:50 +0000

    Accept common .dc42 extension for DiskCopy images (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@15253 9849079f-6229-0410-a07f-84406ce305ba

--- src/lib/formats/ap_dsk35.c
+++ src/lib/formats/ap_dsk35.c
@@ -1173,7 +1173,7 @@ LEGACY_FLOPPY_OPTIONS_START( apple35_mac )
 		TRACKS([80])
 		SECTOR_LENGTH([512])
 		FIRST_SECTOR_ID([0]))
-	LEGACY_FLOPPY_OPTION( apple35_dc, "dc,dsk,img,image",	"Apple DiskCopy disk image",	apple35_diskcopy_identify,	apple35_diskcopy_construct, NULL,
+	LEGACY_FLOPPY_OPTION( apple35_dc, "dc,dc42,dsk,img,image",	"Apple DiskCopy disk image",	apple35_diskcopy_identify,	apple35_diskcopy_construct, NULL,
 		HEADS([1]-2)
 		TRACKS([80])
 		SECTOR_LENGTH([512])
