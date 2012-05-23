69b760410e54ccb0eeb67dd5a89d808e4efe35c0
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2012-05-23 13:30:58 +0000

    CD2650: added cassette and quickload. Can't test cassette, no idea of syntax to save a file.
    git-svn-id: svn://messdev.no-ip.org/mess@15288 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/cd2650.c
+++ src/mess/drivers/cd2650.c
@@ -18,7 +18,7 @@
         - Lots, probably. The computer is a complete mystery. No pictures,
                 manuals or schematics exist.
         - Using the terminal keyboard, as it is unknown if it has its own.
-        - Cassette interface.
+        - Cassette interface to be tested - no idea what the command syntax is for saving.
         - Need the proper chargen rom.
 
 ****************************************************************************/
@@ -26,6 +26,9 @@
 #include "emu.h"
 #include "cpu/s2650/s2650.h"
 #include "machine/keyboard.h"
+#include "imagedev/snapquik.h"
+#include "imagedev/cassette.h"
+#include "sound/wave.h"
 
 #define MACHINE_RESET_MEMBER(name) void name::machine_reset()
 #define VIDEO_START_MEMBER(name) void name::video_start()
@@ -36,21 +39,34 @@ class cd2650_state : public driver_device
 public:
 	cd2650_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
-	m_maincpu(*this, "maincpu")
-	,
-		m_p_videoram(*this, "p_videoram"){ }
+	m_maincpu(*this, "maincpu"),
+	m_p_videoram(*this, "p_videoram"),
+	m_cass(*this, CASSETTE_TAG) { }
 
-	required_device<cpu_device> m_maincpu;
-	DECLARE_READ8_MEMBER( cd2650_keyin_r );
-	DECLARE_WRITE8_MEMBER( kbd_put );
+	DECLARE_READ8_MEMBER(cd2650_keyin_r);
+	DECLARE_WRITE8_MEMBER(kbd_put);
+	DECLARE_READ8_MEMBER(cass_r);
+	DECLARE_WRITE8_MEMBER(cass_w);
 	const UINT8 *m_p_chargen;
-	required_shared_ptr<const UINT8> m_p_videoram;
 	UINT8 m_term_data;
 	virtual void machine_reset();
 	virtual void video_start();
 	UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	required_device<cpu_device> m_maincpu;
+	required_shared_ptr<const UINT8> m_p_videoram;
+	required_device<cassette_image_device> m_cass;
 };
 
+
+WRITE8_MEMBER( cd2650_state::cass_w )
+{
+	m_cass->output(BIT(data, 0) ? -1.0 : +1.0);
+}
+
+READ8_MEMBER( cd2650_state::cass_r )
+{
+	return (m_cass->input() > 0.03) ? 1 : 0;
+}
 
 READ8_MEMBER( cd2650_state::cd2650_keyin_r )
 {
@@ -66,13 +82,13 @@ static ADDRESS_MAP_START(cd2650_mem, AS_PROGRAM, 8, cd2650_state)
 	AM_RANGE( 0x0000, 0x03ff) AM_ROM
 	AM_RANGE( 0x0400, 0x0fff) AM_RAM
 	AM_RANGE( 0x1000, 0x17ff) AM_RAM AM_SHARE("p_videoram")
-	AM_RANGE( 0x1800, 0x7fff) AM_RAM // expansion ram? the system doesn't use it
+	AM_RANGE( 0x1800, 0x7fff) AM_RAM // expansion ram needed by quickloads
 ADDRESS_MAP_END
 
 static ADDRESS_MAP_START( cd2650_io, AS_IO, 8, cd2650_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(S2650_DATA_PORT,S2650_DATA_PORT) AM_READ(cd2650_keyin_r)
-	//AM_RANGE(S2650_SENSE_PORT, S2650_FO_PORT) AM_READWRITE(cd2650_cass_in,cd2650_cass_out)
+	AM_RANGE(S2650_SENSE_PORT, S2650_FO_PORT) AM_READWRITE(cass_r, cass_w)
 ADDRESS_MAP_END
 
 /* Input ports */
@@ -151,6 +167,90 @@ static ASCII_KEYBOARD_INTERFACE( keyboard_intf )
 	DEVCB_DRIVER_MEMBER(cd2650_state, kbd_put)
 };
 
+QUICKLOAD_LOAD( cd2650 )
+{
+	address_space *space = image.device().machine().device("maincpu")->memory().space(AS_PROGRAM);
+	int i;
+	int quick_addr = 0x440;
+	int exec_addr;
+	int quick_length;
+	UINT8 *quick_data;
+	int read_;
+
+	quick_length = image.length();
+	quick_data = (UINT8*)malloc(quick_length);
+	if (!quick_data)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "Cannot open file");
+		image.message(" Cannot open file");
+		return IMAGE_INIT_FAIL;
+	}
+
+	read_ = image.fread( quick_data, quick_length);
+	if (read_ != quick_length)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "Cannot read the file");
+		image.message(" Cannot read the file");
+		return IMAGE_INIT_FAIL;
+	}
+
+	if (quick_data[0] != 0x40)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "Invalid header");
+		image.message(" Invalid header");
+		return IMAGE_INIT_FAIL;
+	}
+
+	exec_addr = quick_data[1] * 256 + quick_data[2];
+
+	if (exec_addr >= quick_length)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "Exec address beyond end of file");
+		image.message(" Exec address beyond end of file");
+		return IMAGE_INIT_FAIL;
+	}
+
+	if (quick_length < 0x0444)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "File too short");
+		image.message(" File too short");
+		return IMAGE_INIT_FAIL;
+	}
+
+	if (quick_length > 0x8000)
+	{
+		image.seterror(IMAGE_ERROR_INVALIDIMAGE, "File too long");
+		image.message(" File too long");
+		return IMAGE_INIT_FAIL;
+	}
+
+	read_ = 0x1000;
+	if (quick_length < 0x1000)
+		read_ = quick_length;
+
+	for (i = quick_addr; i < read_; i++)
+		space->write_byte(i, quick_data[i]);
+
+	read_ = 0x1780;
+	if (quick_length < 0x1780)
+		read_ = quick_length;
+
+	if (quick_length > 0x157f)
+		for (i = 0x1580; i < read_; i++)
+			space->write_byte(i, quick_data[i]);
+
+	if (quick_length > 0x17ff)
+		for (i = 0x1800; i < quick_length; i++)
+			space->write_byte(i, quick_data[i]);
+
+	/* display a message about the loaded quickload */
+	image.message(" Quickload: size=%04X : exec=%04X",quick_length,exec_addr);
+
+	// Start the quickload
+	cpu_set_reg(image.device().machine().device("maincpu"), STATE_GENPC, exec_addr);
+	return IMAGE_INIT_PASS;
+}
+
 static MACHINE_CONFIG_START( cd2650, cd2650_state )
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu",S2650, XTAL_1MHz)
@@ -168,6 +268,15 @@ static MACHINE_CONFIG_START( cd2650, cd2650_state )
 	MCFG_PALETTE_INIT(black_and_white)
 
 	MCFG_ASCII_KEYBOARD_ADD(KEYBOARD_TAG, keyboard_intf)
+
+	/* quickload */
+	MCFG_QUICKLOAD_ADD("quickload", cd2650, "pgm", 1)
+
+	/* cassette */
+	MCFG_CASSETTE_ADD( CASSETTE_TAG, default_cassette_interface )
+	MCFG_SPEAKER_STANDARD_MONO("mono")
+	MCFG_SOUND_WAVE_ADD(WAVE_TAG, CASSETTE_TAG)
+	MCFG_SOUND_ROUTE(ALL_OUTPUTS, "mono", 0.25)
 MACHINE_CONFIG_END
 
 /* ROM definition */
@@ -183,4 +292,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT     COMPANY        FULLNAME       FLAGS */
-COMP( 1977, cd2650, 0,      0,       cd2650,    cd2650,  0,   "Central Data",   "CD 2650", GAME_NOT_WORKING | GAME_NO_SOUND )
+COMP( 1977, cd2650, 0,      0,       cd2650,    cd2650,  0,   "Central Data",   "CD 2650", 0 )
