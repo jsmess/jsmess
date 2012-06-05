771d88df7e42418f3714ae7abdb2a4e91af34564
micko <micko@9849079f-6229-0410-a07f-84406ce305ba>
2012-06-05 19:18:15 +0000

    fix for null values (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@15410 9849079f-6229-0410-a07f-84406ce305ba

--- src/emu/uimain.c
+++ src/emu/uimain.c
@@ -456,7 +456,7 @@ void ui_menu_slot_devices::populate()
 		const char *title = get_slot_device(slot);
 		// do no display fixed slots
 		if (slot->fixed()) title = slot->get_default_card();
-		
+		if (title==NULL) title = "";
 		item_append(slot->device().tag()+1, strcmp(title,"")==0 ? "------" : title, slot->fixed() ? 0 : (MENU_FLAG_LEFT_ARROW | MENU_FLAG_RIGHT_ARROW), (void *)slot);
 	}
 	item_append(MENU_SEPARATOR_ITEM, NULL, 0, NULL);
@@ -1177,7 +1177,7 @@ void ui_menu_settings_dip_switches::custom_render(void *selectedref, float top, 
 	ioport_field *field = (ioport_field *)selectedref;
 	dip_descriptor *dip;
 
-	if (field!=NULL && field->first_diplocation() == NULL)
+	if (field==NULL || field->first_diplocation() == NULL)
 		return;
 
 	/* add borders */
