32e37efbc0c59650dcfde4421f8246b50de51eee
sronco <sronco@9849079f-6229-0410-a07f-84406ce305ba>
2012-07-12 21:13:20 +0000

    geniusiq: documented a bit the memory map and added preliminary video. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@15582 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/geniusiq.c
+++ src/mess/drivers/geniusiq.c
@@ -27,6 +27,7 @@ Not very much is known about this computer released in 1997.
 
 #include "emu.h"
 #include "cpu/m68000/m68000.h"
+#include "machine/intelfsh.h"
 
 #define MACHINE_RESET_MEMBER(name) void name::machine_reset()
 
@@ -35,25 +36,78 @@ class geniusiq_state : public driver_device
 public:
 	geniusiq_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
-	m_maincpu(*this, "maincpu")
+		m_maincpu(*this, "maincpu"),
+		m_flash(*this, "flash"),
+		m_vram(*this, "vram")
 	{ }
 
 	required_device<cpu_device> m_maincpu;
+	required_device<intelfsh8_device> m_flash;
+	required_shared_ptr<UINT16>	m_vram;
 	virtual void machine_reset();
+	virtual UINT32 screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect);
+	DECLARE_READ8_MEMBER(flash_r);
+	DECLARE_WRITE8_MEMBER(flash_w);
+
+	DECLARE_READ16_MEMBER(unk_r) { return machine().rand(); }
 };
+
+
+UINT32 geniusiq_state::screen_update(screen_device &screen, bitmap_ind16 &bitmap, const rectangle &cliprect)
+{
+	for (int y=0; y<256; y++)
+		for (int x=0; x<256; x+=2)
+		{
+			UINT16 data = m_vram[(y*256 + x)>>1];
+
+			bitmap.pix16(y, x) = (data>>8) & 0xff;
+			bitmap.pix16(y, x + 1) = data & 0xff;
+		}
+
+	return 0;
+}
+
+READ8_MEMBER(geniusiq_state::flash_r)
+{
+	return m_flash->read(offset);
+}
+
+WRITE8_MEMBER(geniusiq_state::flash_w)
+{
+	m_flash->write(offset, data);
+}
 
 static ADDRESS_MAP_START(geniusiq_mem, AS_PROGRAM, 16, geniusiq_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	AM_RANGE(0x000000, 0x1FFFFF) AM_ROM
 	AM_RANGE(0x200000, 0x23FFFF) AM_RAM
+	AM_RANGE(0x300000, 0x30FFFF) AM_RAM		AM_SHARE("vram")
+	AM_RANGE(0x310000, 0x312FFF) AM_RAM
+	AM_RANGE(0x400000, 0x41ffff) AM_READWRITE8(flash_r, flash_w, 0x00ff)
+	//AM_RANGE(0x600300, 0x600301)                      // read during IRQ 4 (mouse ??)
+	//AM_RANGE(0x600500, 0x60050f)                      // read during IRQ 5 (keyboard ??)
+	AM_RANGE(0x601008, 0x601009) AM_READ(unk_r)			// unknown, read at start and expect that bit 2 changes several times before continue
 	// 0x600000 : some memory mapped hardware
 	// Somewhere : internal flash
 	// Somewhere : cartridge port
 ADDRESS_MAP_END
 
 
+static INPUT_CHANGED( trigger_irq )
+{
+	cputag_set_input_line(field.machine(), "maincpu", (int)(FPTR)param, newval ? HOLD_LINE : CLEAR_LINE);
+}
+
 /* Input ports */
 static INPUT_PORTS_START( geniusiq )
+	PORT_START( "DEBUG" )	// for debug purposes, to be removed in the end
+	PORT_BIT( 0x01, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_1 ) PORT_NAME( "IRQ 1" ) PORT_CHANGED( trigger_irq, 1 )
+	PORT_BIT( 0x02, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_2 ) PORT_NAME( "IRQ 2" ) PORT_CHANGED( trigger_irq, 2 )
+	PORT_BIT( 0x04, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_3 ) PORT_NAME( "IRQ 3" ) PORT_CHANGED( trigger_irq, 3 )
+	PORT_BIT( 0x08, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_4 ) PORT_NAME( "IRQ 4" ) PORT_CHANGED( trigger_irq, 4 )
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_5 ) PORT_NAME( "IRQ 5" ) PORT_CHANGED( trigger_irq, 5 )
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_6 ) PORT_NAME( "IRQ 6" ) PORT_CHANGED( trigger_irq, 6 )
+	PORT_BIT( 0x40, IP_ACTIVE_HIGH, IPT_KEYBOARD ) PORT_CODE( KEYCODE_7 ) PORT_NAME( "IRQ 7" ) PORT_CHANGED( trigger_irq, 7 )
 INPUT_PORTS_END
 
 
@@ -65,6 +119,18 @@ static MACHINE_CONFIG_START( geniusiq, geniusiq_state )
 	/* basic machine hardware */
 	MCFG_CPU_ADD("maincpu", M68000, 16000000) // The main crystal is at 32MHz, not sure whats the CPU freq
 	MCFG_CPU_PROGRAM_MAP(geniusiq_mem)
+
+	/* video hardware */
+	MCFG_SCREEN_ADD("screen", RASTER)
+	MCFG_SCREEN_REFRESH_RATE(50)
+	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
+	MCFG_SCREEN_SIZE(256, 256)
+	MCFG_SCREEN_VISIBLE_AREA(0, 256-1, 0, 256-1)
+	MCFG_SCREEN_UPDATE_DRIVER( geniusiq_state, screen_update )
+	MCFG_PALETTE_LENGTH(256)
+
+	/* internal flash */
+	MCFG_AMD_29F040_ADD("flash")	// should be AM29f010
 MACHINE_CONFIG_END
 
 /* ROM definition */
