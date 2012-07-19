e9e467ca326c3002acf9344ea47da8c6f4791f2b
jgevarya <jgevarya@9849079f-6229-0410-a07f-84406ce305ba>
2012-07-19 11:05:37 +0000

    vk100: miscellaneous progress, slight cleanup to keyboard, stubbed and implemented a bunch of vector generator addresses [Lord Nightmare]
    git-svn-id: svn://messdev.no-ip.org/mess@15627 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/vk100.c
+++ src/mess/drivers/vk100.c
@@ -7,7 +7,10 @@
 
         Todo: attach hd46505sp CRTC
               hook up vram
+              - vram space is now present but not hooked to anything
               emulate vector generator hardware
+              - stubbing registers:
+                done: X, Y, Err, Pattern, WOPS
 
  Tony DiCenzo, now the director of standards and architecture at Oracle, was on the team that developed the VK100
  see http://startup.nmnaturalhistory.org/visitorstories/view.php?ii=79
@@ -117,10 +120,82 @@ public:
 	required_device<device_t> m_speaker;
 	//required_device<device_t> m_uart;
 
-	//UINT8 m_statusLED;
+	UINT16 m_vgX;
+	UINT16 m_vgY;
+	UINT8 m_vgErr;
+	UINT8 m_vgPat;
+	UINT8 m_vgWOPS;
+	
+	DECLARE_WRITE8_MEMBER(vgLD_X);
+	DECLARE_WRITE8_MEMBER(vgLD_Y);
+	DECLARE_WRITE8_MEMBER(vgErr);
+	
+	DECLARE_WRITE8_MEMBER(vgPat);
+	
+	DECLARE_WRITE8_MEMBER(vgWOPS);
+	
+	DECLARE_WRITE8_MEMBER(vgEX_VEC);
+	
 	DECLARE_WRITE8_MEMBER(KBDW);
 	DECLARE_READ8_MEMBER(vk100_keyboard_column_r);
 };
+
+/*
+void drawVector(UINT16 px, UINT16 py, UINT16 x, UINT16 y, int pattern)
+{
+	UINT8 *gfx = machine.root_device().memregion("vram")->base();
+}*/
+
+/* ports 0x40 and 0x41: load low and high bytes of vector gen X register */
+WRITE8_MEMBER(vk100_state::vgLD_X)
+{
+	m_vgX &= 0xFF << ((1-offset)*8);
+	m_vgX |= ((UINT16)data) << (offset*8);
+	logerror("VG: X Reg 0x%02X loaded with %04X, new X value is %04X\n", 0x40+offset, ((UINT16)data) << (offset*8), m_vgX);
+}
+
+/* ports 0x42 and 0x43: load low and high bytes of vector gen Y register */
+WRITE8_MEMBER(vk100_state::vgLD_Y)
+{
+	m_vgY &= 0xFF << ((1-offset)*8);
+	m_vgY |= ((UINT16)data) << (offset*8);
+	logerror("VG: Y Reg 0x%02X loaded with %04X, new Y value is %04X\n", 0x42+offset, ((UINT16)data) << (offset*8), m_vgY);
+}
+
+/* port 0x44: "ERR" load bresenham line algorithm 'error' count */
+WRITE8_MEMBER(vk100_state::vgErr)
+{
+	m_vgErr = data;
+	logerror("VG: Err Reg 0x44 loaded with %02X\n", m_vgErr);
+}
+
+/* port 0x46: "Pat" load vg Pattern register */
+WRITE8_MEMBER(vk100_state::vgPat)
+{
+	m_vgPat = data;
+	logerror("VG: Pattern Reg 0x46 loaded with %02X\n", m_vgPat);
+}
+
+/* port 0x63: "WOPS" vector 'pixel' write options
+ * --Attributes to change --   Enable --  Functions --
+ * Blink  Green  Red    Blue   Attrib F1     F0     FN
+ *                             Change
+ * d7     d6     d5     d4     d3     d2     d1     d0
+ */
+WRITE8_MEMBER(vk100_state::vgWOPS)
+{
+	static const char *const functions[] = { "Overlay", "Replace", "Complement", "Erase" };
+	m_vgWOPS = data;
+	logerror("VG: WOPS loaded with %02X: KGRB %d%d%d%d, AttrChange %d, Function %s, Negate %d\n", data, (m_vgWOPS>>7)&1, (m_vgWOPS>>6)&1, (m_vgWOPS>>5)&1, (m_vgWOPS>>4)&1, (m_vgWOPS>>3)&1, functions[(m_vgWOPS>>1)&3], m_vgWOPS&1);
+}
+
+/* port 0x66: "EX VEC" execute a vector (start the state machine) */
+WRITE8_MEMBER(vk100_state::vgEX_VEC)
+{
+	logerror("VG Execute Vector 0x66 written: stub!\n");
+	// TODO: short term: do some calculations here and print the expected starting ram address etc
+	// TODO: long term: fire a timer and actually draw the vector to ram with correct timing
+}
 
 /* port 0x68: "KBDW" d7 is beeper, d6 is keyclick, d5-d0 are keyboard LEDS */
 WRITE8_MEMBER(vk100_state::KBDW)
@@ -135,9 +210,6 @@ WRITE8_MEMBER(vk100_state::KBDW)
 	if (BIT(data, 6)) logerror("kb keyclick bit 6 set: not emulated yet (multivibrator)!\n");
 	beep_set_state( m_speaker, BIT(data, 7));
 	logerror("LED state: %02X: %s %s %s %s %s %s\n", data&0xFF, (data&0x20)?"------- LOCAL ":"ON LINE ----- ", (data&0x10)?"--------- ":"NO SCROLL ", (data&0x8)?"----- ":"BASIC ", (data&0x4)?"--------- ":"HARD-COPY ", (data&0x2)?"-- ":"L1 ", (data&0x1)?"-- ":"L2 ");
-    //popmessage("LED write: %02X\n", data&0xFF);
-    //popmessage("LED state: %02X: %s %s %s %s %s %s\n", data&0xFF, (data&0x20)?"------- LOCAL ":"ON LINE ----- ", (data&0x10)?"--------- ":"NO SCROLL ", (data&0x8)?"----- ":"BASIC ", (data&0x4)?"--------- ":"HARD-COPY ", (data&0x2)?"-- ":"L1 ", (data&0x1)?"-- ":"L2 ");
-    //popmessage("LED status: %x %x %x %x %x %x %x %x\n", (data&0x80), (data&0x40), (data&0x20), data&0x10, data&0x8, data&0x4, data&0x2, data&0x1);
 }
 
 READ8_MEMBER(vk100_state::vk100_keyboard_column_r)
@@ -145,8 +217,7 @@ READ8_MEMBER(vk100_state::vk100_keyboard_column_r)
 	UINT8 code;
 	char kbdcol[8];
 	sprintf(kbdcol,"COL%X", (offset&0xF));
-	code = ioport(kbdcol)->read()&0x3F;
-	code |= ioport("CAPSSHIFT")->read();
+	code = ioport(kbdcol)->read() | ioport("CAPSSHIFT")->read();
 	logerror("Keyboard column %X read, returning %02X\n", offset&0xF, code);
 	return code;
 }
@@ -164,21 +235,19 @@ static ADDRESS_MAP_START(vk100_io, AS_IO, 8, vk100_state)
 	//AM_RANGE(0x00, 0x00) AM_WRITE(crtc_addr_w)
 	//AM_RANGE(0x01, 0x01) AM_READWRITE(crtc_data_r, crtc_data_w)
 	// Comments are from page 118 (5-14) of http://web.archive.org/web/20091015205827/http://www.computer.museum.uq.edu.au/pdf/EK-VK100-TM-001%20VK100%20Technical%20Manual.pdf
-	//AM_RANGE (0x40, 0x40) AM_WRITE(X_low)  //LD X LO \_ 12 bits
-	//AM_RANGE (0x41, 0x41) AM_WRITE(X_high) //LD X HI /
-	//AM_RANGE (0x42, 0x42) AM_WRITE(Y_low)  //LD Y LO \_ 12 bits
-	//AM_RANGE (0x43, 0x43) AM_WRITE(Y_high) //LD Y HI /
-	//AM_RANGE (0x44, 0x44) AM_WRITE(err)    //LD ERR
+	AM_RANGE (0x40, 0x41) AM_WRITE(vgLD_X)  //LD X LO + HI 12 bits
+	AM_RANGE (0x42, 0x43) AM_WRITE(vgLD_Y)  //LD Y LO + HI 12 bits
+	AM_RANGE (0x44, 0x44) AM_WRITE(vgErr)    //LD ERR ('error' in bresenham algorithm)
 	//AM_RANGE (0x45, 0x45) AM_WRITE(sops)   //LD SOPS (screen options)
-	//AM_RANGE (0x46, 0x46) AM_WRITE(pat)    //LD PAT (pattern register)
+	AM_RANGE (0x46, 0x46) AM_WRITE(vgPat)    //LD PAT (pattern register)
 	//AM_RANGE (0x47, 0x47) AM_READWRITE(pmul)   //LD PMUL (pattern multiplier) (should this be write only?)
 	//AM_RANGE (0x60, 0x60) AM_WRITE(du)     //LD DU
 	//AM_RANGE (0x61, 0x61) AM_WRITE(dvm)    //LD DVM
 	//AM_RANGE (0x62, 0x62) AM_WRITE(dir)    //LD DIR (direction)
-	//AM_RANGE (0x63, 0x63) AM_WRITE(wops)   //LD WOPS (write options)
+	AM_RANGE (0x63, 0x63) AM_WRITE(vgWOPS)   //LD WOPS (write options)
 	//AM_RANGE (0x64, 0x64) AM_WRITE(mov)    //EX MOV
 	//AM_RANGE (0x65, 0x65) AM_WRITE(dot)    //EX DOT
-	//AM_RANGE (0x66, 0x66) AM_WRITE(vec)    //EX VEC
+	AM_RANGE (0x66, 0x66) AM_WRITE(vgEX_VEC)    //EX VEC
 	//AM_RANGE (0x67, 0x67) AM_WRITE(er)     //EX ER
 	AM_RANGE (0x68, 0x68) AM_WRITE(KBDW)   //KBDW
 	//AM_RANGE (0x6C, 0x6C) AM_WRITE(baud)   //LD BAUD (baud rate clock divider setting for i8251 tx and rx clocks)
@@ -200,14 +269,14 @@ ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( vk100 )
-	PORT_START("CAPSSHIFT")
+	PORT_START("CAPSSHIFT") // CAPS LOCK and SHIFT appear as the high 2 bits on all rows
 		PORT_BIT(0x3f, IP_ACTIVE_HIGH, IPT_UNUSED)
 		PORT_BIT(0x40, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Caps lock") PORT_CODE(KEYCODE_CAPSLOCK)
 		PORT_BIT(0x80, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Shift") PORT_CODE(KEYCODE_LSHIFT) PORT_CODE(KEYCODE_RSHIFT)
 	PORT_START("COL0")
 		PORT_BIT(0x1f, IP_ACTIVE_LOW, IPT_UNUSED)
-		PORT_BIT(0x20, IP_ACTIVE_HIGH, IPT_UNUSED)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_HIGH, IPT_UNUSED) // row 0 bit 6 is always low, checked by keyboard test
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED) // all rows have these bits left low to save a mask op later
 	PORT_START("COL1")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Set Up") PORT_CODE(KEYCODE_F5)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Esc") PORT_CODE(KEYCODE_ESC)
@@ -215,7 +284,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_UNUSED)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num Enter") PORT_CODE(KEYCODE_ENTER_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("PF1") PORT_CODE(KEYCODE_F1)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL2")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("1") PORT_CODE(KEYCODE_1)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Q") PORT_CODE(KEYCODE_Q)
@@ -223,7 +292,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("No scroll") PORT_CODE(KEYCODE_LALT)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 1") PORT_CODE(KEYCODE_1_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("PF2") PORT_CODE(KEYCODE_F2)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL3")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("2") PORT_CODE(KEYCODE_2)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("W") PORT_CODE(KEYCODE_W)
@@ -231,7 +300,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Z") PORT_CODE(KEYCODE_Z)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 2") PORT_CODE(KEYCODE_2_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("PF3") PORT_CODE(KEYCODE_F3)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL4")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("3") PORT_CODE(KEYCODE_3)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("E") PORT_CODE(KEYCODE_E)
@@ -239,7 +308,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("X") PORT_CODE(KEYCODE_X)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 3") PORT_CODE(KEYCODE_3_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("PF4") PORT_CODE(KEYCODE_F4)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL5")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("4") PORT_CODE(KEYCODE_4)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("R") PORT_CODE(KEYCODE_R)
@@ -247,7 +316,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("C") PORT_CODE(KEYCODE_C)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 4") PORT_CODE(KEYCODE_4_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Up") PORT_CODE(KEYCODE_UP)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL6")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("5") PORT_CODE(KEYCODE_5)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("T") PORT_CODE(KEYCODE_T)
@@ -255,7 +324,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("V") PORT_CODE(KEYCODE_V)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 5") PORT_CODE(KEYCODE_5_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Down") PORT_CODE(KEYCODE_DOWN)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL7")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("6") PORT_CODE(KEYCODE_6)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("V") PORT_CODE(KEYCODE_Y)
@@ -263,7 +332,7 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("B") PORT_CODE(KEYCODE_B)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 6") PORT_CODE(KEYCODE_6_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Left") PORT_CODE(KEYCODE_LEFT)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL8")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("7") PORT_CODE(KEYCODE_7)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("U") PORT_CODE(KEYCODE_U)
@@ -271,56 +340,62 @@ static INPUT_PORTS_START( vk100 )
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Space") PORT_CODE(KEYCODE_SPACE)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 7") PORT_CODE(KEYCODE_7_PAD)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Right") PORT_CODE(KEYCODE_RIGHT)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COL9")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("8") PORT_CODE(KEYCODE_8)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("I") PORT_CODE(KEYCODE_I)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("K") PORT_CODE(KEYCODE_K)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("N") PORT_CODE(KEYCODE_N)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 8") PORT_CODE(KEYCODE_8_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLA")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("9") PORT_CODE(KEYCODE_9)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("O") PORT_CODE(KEYCODE_O)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("L") PORT_CODE(KEYCODE_L)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("M") PORT_CODE(KEYCODE_M)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 9") PORT_CODE(KEYCODE_9_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLB")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("0") PORT_CODE(KEYCODE_0)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("P") PORT_CODE(KEYCODE_P)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME(";") PORT_CODE(KEYCODE_COLON)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME(",") PORT_CODE(KEYCODE_COMMA)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num 0") PORT_CODE(KEYCODE_0_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLC")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("-") PORT_CODE(KEYCODE_MINUS)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("[") PORT_CODE(KEYCODE_OPENBRACE)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("'") PORT_CODE(KEYCODE_QUOTE)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME(".") PORT_CODE(KEYCODE_STOP)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num -") PORT_CODE(KEYCODE_MINUS_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLD")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("=") PORT_CODE(KEYCODE_EQUALS)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("]") PORT_CODE(KEYCODE_CLOSEBRACE)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_UNUSED)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("/") PORT_CODE(KEYCODE_SLASH)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num ,") PORT_CODE(KEYCODE_PLUS_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLE")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("~") PORT_CODE(KEYCODE_TILDE)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Delete") PORT_CODE(KEYCODE_DEL)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("\\") PORT_CODE(KEYCODE_BACKSLASH)
 		PORT_BIT(0x08, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Line feed") PORT_CODE(KEYCODE_RALT)
 		PORT_BIT(0x10, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Num .") PORT_CODE(KEYCODE_DEL_PAD)
-		PORT_BIT(0xe0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 	PORT_START("COLF")
 		PORT_BIT(0x01, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Backspace") PORT_CODE(KEYCODE_BACKSPACE)
 		PORT_BIT(0x02, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Break") PORT_CODE(KEYCODE_F6)
 		PORT_BIT(0x04, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Return") PORT_CODE(KEYCODE_ENTER)
 		PORT_BIT(0x18, IP_ACTIVE_LOW, IPT_UNUSED)
 		PORT_BIT(0x20, IP_ACTIVE_LOW, IPT_KEYBOARD) PORT_NAME("Ctrl") PORT_CODE(KEYCODE_LCONTROL) PORT_CODE(KEYCODE_RCONTROL)
-		PORT_BIT(0xc0, IP_ACTIVE_LOW, IPT_UNUSED)
+		PORT_BIT(0xc0, IP_ACTIVE_HIGH, IPT_UNUSED)
 INPUT_PORTS_END
 
 
@@ -335,6 +410,18 @@ static MACHINE_RESET( vk100 )
 	output_set_value("hardcopy_led", 1);
 	output_set_value("l1_led", 1);
 	output_set_value("l2_led", 1);
+	state->m_vgX = 0;
+	state->m_vgY = 0;
+	state->m_vgErr = 0;
+	state->m_vgPat = 0;
+	state->m_vgWOPS = 0;
+}
+
+static PALETTE_INIT( vk100 )
+{
+	int i;
+	for (i = 0; i < 8; i++)
+		palette_set_color_rgb( machine, i, (i&4)?0xFF:0x00, (i&2)?0xFF:0x00, (i&1)?0xFF:0x00);
 }
 
 static VIDEO_START( vk100 )
@@ -363,14 +450,17 @@ static MACHINE_CONFIG_START( vk100, vk100_state )
 
 	/* video hardware */
 	MCFG_SCREEN_ADD("screen", RASTER)
+	//MCFG_SCREEN_RAW_PARAMS(XTAL_45_6192Mhz/3, 882, 0, 720, 370, 0, 350 )
+	//MCFG_SCREEN_UPDATE_DEVICE( MDA_MC6845_NAME, mc6845_device, screen_update )
+	//MCFG_MC6845_ADD( "crtc", H46505, XTAL_45_6192Mhz/3/16, mc6845_mda_intf)
 	MCFG_SCREEN_REFRESH_RATE(50)
 	MCFG_SCREEN_VBLANK_TIME(ATTOSECONDS_IN_USEC(2500)) /* not accurate */
 	MCFG_SCREEN_SIZE(640, 480)
 	MCFG_SCREEN_VISIBLE_AREA(0, 640-1, 0, 480-1)
 	MCFG_VIDEO_START(vk100)
 	MCFG_SCREEN_UPDATE_STATIC(vk100)
-	MCFG_PALETTE_LENGTH(2)
-	MCFG_PALETTE_INIT(black_and_white)
+	MCFG_PALETTE_LENGTH(8)
+	MCFG_PALETTE_INIT(vk100)
 
 
 	MCFG_DEFAULT_LAYOUT( layout_vk100 )
@@ -391,6 +481,18 @@ ROM_START( vk100 )
 	ROM_LOAD( "23-018e4-00.rom3.ic53", 0x4000, 0x2000, CRC(b3e7903b) SHA1(8ad6ed25cd9b04a9968aa09ab69ba526d35ca550))
 	ROM_LOAD( "23-190e2-00.rom4.ic54", 0x6000, 0x1000, CRC(ad596fa5) SHA1(b30a24155640d32c1b47a3a16ea33cd8df2624f6))
 
+	ROM_REGION( 0x8000, "vram", ROMREGION_ERASEFF ) // 32k of vram
+
+	ROM_REGION( 0x400, "pattern", ROMREGION_ERASEFF )
+    /* This is the "PATTERN ROM", (1k*4, 82s137)
+     * it contains a table of 4 output bits based on input of: (from figure 5-18):
+     * 4 input bits bank-selected from RAM (a0, a1, a2, a3)
+     * 2 select bits to choose one of the 4 bits, sourced from the X reg LSBs (a4, a5)
+     * 3 pattern function bits from WOPS,
+     * and one enable bit from the pattern register shifter
+     */
+	ROM_LOAD( "wb8201_656f1.m1-7643-5.pr4.ic17", 0x0000, 0x0400, CRC(e8ecf59f) SHA1(49e9d109dad3d203d45471a3f4ca4985d556161f)) // label verified from nigwil's board
+	
 	ROM_REGION( 0x10000, "proms", ROMREGION_ERASEFF )
 	// this is either the "SYNC ROM" or the "VECTOR ROM" which handles timing related stuff. (256*4, 82s129)
 	ROM_LOAD( "wb8151_573a2.6301.pr3.ic44", 0x0000, 0x0100, CRC(75885a9f) SHA1(c721dad6a69c291dd86dad102ed3a8ddd620ecc4)) // label verified from nigwil's and andy's board
@@ -398,12 +500,10 @@ ROM_START( vk100 )
 	ROM_LOAD( "wb8146_058b1.6309.pr1.ic99", 0x0100, 0x0100, CRC(71b01864) SHA1(e552f5b0bc3f443299282b1da7e9dbfec60e12bf))  // label verified from nigwil's and andy's board
 	// this is definitely the "TRANSLATOR ROM" described in figure 5-17 on page 5-27 (256*8, 82s135)
 	ROM_LOAD( "wb---0_060b1.6309.pr2.ic77", 0x0200, 0x0100, CRC(198317fc) SHA1(00e97104952b3fbe03a4f18d800d608b837d10ae)) // label verified from nigwil's board
-	// this is definitely the "PATTERN ROM", (1k*4, 82s137)
-	ROM_LOAD( "wb8201_656f1.m1-7643-5.pr4.ic17", 0x0300, 0x0400, CRC(e8ecf59f) SHA1(49e9d109dad3d203d45471a3f4ca4985d556161f)) // label verified from nigwil's board
 	// the following == mb6309 (256x8, 82s135)
-	ROM_LOAD( "wb8141_059b1.tbp18s22n.pr5.ic108", 0x0700, 0x0100, NO_DUMP)  // label verified from andy's board
+	ROM_LOAD( "wb8141_059b1.tbp18s22n.pr5.ic108", 0x0300, 0x0100, NO_DUMP)  // label verified from andy's board
 	// the following = mb6331 (32x8, 82s123)
-	ROM_LOAD( "wb8214_297a1.74s288.pr6.ic89", 0x0800, 0x0100, NO_DUMP) // label verified from nigwil's and andy's board
+	ROM_LOAD( "wb8214_297a1.74s288.pr6.ic89", 0x0400, 0x0100, NO_DUMP) // label verified from nigwil's and andy's board
 ROM_END
 
 /* Driver */
