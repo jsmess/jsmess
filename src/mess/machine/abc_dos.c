ee42ea2d373eafd211644f71fd9bbb08f72d2659
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-08-06 16:06:40 +0000

    [ABC80] Added ROM locations for DOS card. (nw)
    git-svn-id: svn://messdev.no-ip.org/mess@15781 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/machine/abc_dos.c
+++ src/mess/machine/abc_dos.c
@@ -71,20 +71,18 @@ const device_type ABC_DOS = &device_creator<abc_dos_device>;
 //-------------------------------------------------
 
 ROM_START( abc_dos )
-	ROM_REGION( 0x1000, "dos", 0 )
+	ROM_REGION( 0x2000, "dos", ROMREGION_ERASEFF )
 	ROM_DEFAULT_BIOS("ufd20")
 	ROM_SYSTEM_BIOS( 0, "abcdos", "ABC-DOS" ) // Scandia Metric FD2
-	ROMX_LOAD( "abcdos",   0x0000, 0x1000, CRC(2cb2192f) SHA1(a6b3a9587714f8db807c05bee6c71c0684363744), ROM_BIOS(1) )
+	ROMX_LOAD( "abcdos.3d",   0x0000, 0x1000, CRC(2cb2192f) SHA1(a6b3a9587714f8db807c05bee6c71c0684363744), ROM_BIOS(1) )
 	ROM_SYSTEM_BIOS( 1, "dosdd80", "ABC-DOS DD" ) // ABC 830
-	ROMX_LOAD( "dosdd80",  0x0000, 0x1000, CRC(36db4c15) SHA1(ae462633f3a9c142bb029beb14749a84681377fa), ROM_BIOS(2) )
+	ROMX_LOAD( "dosdd80.3d",  0x0000, 0x1000, CRC(36db4c15) SHA1(ae462633f3a9c142bb029beb14749a84681377fa), ROM_BIOS(2) )
 	ROM_SYSTEM_BIOS( 2, "ufd20", "UFD-DOS v.20" ) // ABC 830
-	ROMX_LOAD( "ufddos20", 0x0000, 0x1000, CRC(69b09c0b) SHA1(403997a06cf6495b8fa13dc74eff6a64ef7aa53e), ROM_BIOS(3) )
-
-	ROM_REGION( 0x800, "iec", ROMREGION_ERASEFF )
-	ROM_LOAD( "iec", 0x000, 0x400, NO_DUMP )
-
-	ROM_REGION( 0x800, "printer", ROMREGION_ERASEFF )
-	ROM_LOAD( "printer", 0x000, 0x400, NO_DUMP )
+	ROMX_LOAD( "ufddos20.3d", 0x0000, 0x1000, CRC(69b09c0b) SHA1(403997a06cf6495b8fa13dc74eff6a64ef7aa53e), ROM_BIOS(3) )
+	ROM_LOAD( "printer.4c",   0x1000, 0x400, NO_DUMP )
+	//ROM_LOAD( "spare.4a",   0x1400, 0x400, NO_DUMP )
+	ROM_LOAD( "9704.3c",      0x1800, 0x400, NO_DUMP )
+	//ROM_LOAD( "spare.3a",   0x1c00, 0x400, NO_DUMP )
 ROM_END
 
 
@@ -136,17 +134,9 @@ UINT8 abc_dos_device::abcbus_xmemfl(offs_t offset)
 {
 	UINT8 data = 0xff;
 
-	if (offset >= 0x6000 && offset < 0x7000)
+	if (offset >= 0x6000 && offset < 0x8000)
 	{
-		data = memregion("dos")->base()[offset & 0xfff];
-	}
-	else if (offset >= 0x7000 && offset < 0x7800)
-	{
-		data = memregion("iec")->base()[offset & 0x7ff];
-	}
-	else if (offset >= 0x7800 && offset < 0x8000)
-	{
-		data = memregion("printer")->base()[offset & 0x7ff];
+		data = memregion("dos")->base()[offset & 0x1fff];
 	}
 
 	return data;
