c8769cb653f315bbca08d383eaf40e21489053bf
Robbbert <Robbbert@9849079f-6229-0410-a07f-84406ce305ba>
2012-08-16 11:34:42 +0000

    [SACSTATE] Added comments. Fixed input ports. [Robbbert]
    git-svn-id: svn://messdev.no-ip.org/mess@15925 9849079f-6229-0410-a07f-84406ce305ba

--- src/mess/drivers/sacstate.c
+++ src/mess/drivers/sacstate.c
@@ -4,8 +4,31 @@
 
         23/02/2009 Skeleton driver.
 
-How to use this is unknown, most commands either give an error (! symbol),
-or cause the cpu to halt.
+http://www.digibarn.com/stories/bill-pentz-story/index.html
+
+The year is listed as '1972/73'.
+
+All input to be in lowercase.
+The weird characters that show on screen are various escape sequences.
+These are different depending on the terminal type chosen. The codes
+need to be understood and emulated before this system makes sense.
+
+Known Monitor commands: (from the disassembly)
+!	write buffer to AD000
+*	set RAM036 flag
++	increment AD000 by 1
++n	increment AD000 by n
+-	decrement AD000 by 1
+-n	decrement AD000 by n
+:	clear RAM036 flag
+=	display AD000
+=nnn	set AD000 to nnn
+@	fill buffer with 026
+
+unknown commands: / & d e l r s t u z \ ^ | ~
+
+Other input will either result in '!' message, or halt.
+
 
 ****************************************************************************/
 
@@ -13,7 +36,6 @@ or cause the cpu to halt.
 #include "cpu/i8008/i8008.h"
 #include "machine/terminal.h"
 
-#define MACHINE_RESET_MEMBER(name) void name::machine_reset()
 
 class sacstate_state : public driver_device
 {
@@ -21,40 +43,49 @@ public:
 	sacstate_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag),
 	m_maincpu(*this, "maincpu"),
-	m_terminal(*this, TERMINAL_TAG),
-	m_val(0)
+	m_terminal(*this, TERMINAL_TAG)
 	{ }
 
 	required_device<cpu_device> m_maincpu;
 	required_device<generic_terminal_device> m_terminal;
-	DECLARE_READ8_MEMBER(status_r);
-	DECLARE_READ8_MEMBER(tty_r);
-	DECLARE_READ8_MEMBER(unknown_r);
+	DECLARE_READ8_MEMBER(port00_r);
+	DECLARE_READ8_MEMBER(port01_r);
+	DECLARE_READ8_MEMBER(port04_r);
+	DECLARE_WRITE8_MEMBER(port08_w);
 	DECLARE_WRITE8_MEMBER(kbd_put);
 	UINT8 m_term_data;
 	UINT8 m_val;
 	virtual void machine_reset();
 };
 
-READ8_MEMBER( sacstate_state::status_r )
+READ8_MEMBER( sacstate_state::port01_r )
 {
-	UINT8 old_val = m_val;
-	if (m_term_data) old_val |= 0x04; // data in
-	m_val ^= 0x40;
-	return old_val;
+	UINT8 ret = m_val;
+	if (m_term_data)
+		ret |= 0x04; // data in
+	return ret;
 }
 
-READ8_MEMBER( sacstate_state::tty_r )
+READ8_MEMBER( sacstate_state::port00_r )
 {
 	UINT8 ret = m_term_data;
 	m_term_data = 0;
 	return ret;
 }
 
-READ8_MEMBER( sacstate_state::unknown_r )
+READ8_MEMBER( sacstate_state::port04_r )
 {
 	logerror("unknown_r\n");
 	return 0;
+}
+
+WRITE8_MEMBER( sacstate_state::port08_w )
+{
+	if (data == 0x40)
+		m_val = 0x40;
+	else
+	if (data == 0x04)
+		m_val = 0;
 }
 
 static ADDRESS_MAP_START(sacstate_mem, AS_PROGRAM, 8, sacstate_state)
@@ -66,14 +97,20 @@ ADDRESS_MAP_END
 static ADDRESS_MAP_START(sacstate_io, AS_IO, 8, sacstate_state)
 	ADDRESS_MAP_UNMAP_HIGH
 	ADDRESS_MAP_GLOBAL_MASK(0xff)
-	AM_RANGE(0x00,0x00) AM_READ(tty_r)
-	AM_RANGE(0x01,0x01) AM_READ(status_r)
-	AM_RANGE(0x04,0x04) AM_READ(unknown_r)
+	AM_RANGE(0x00,0x00) AM_READ(port00_r)
+	AM_RANGE(0x01,0x01) AM_READ(port01_r)
+	AM_RANGE(0x04,0x04) AM_READ(port04_r)
+	AM_RANGE(0x08,0x08) AM_WRITE(port08_w)
 	AM_RANGE(0x16,0x16) AM_DEVWRITE(TERMINAL_TAG, generic_terminal_device, write)
+	AM_RANGE(0x17,0x1f) AM_WRITENOP
 ADDRESS_MAP_END
 
 /* Input ports */
 static INPUT_PORTS_START( sacstate )
+	PORT_START("CONFIG")
+	PORT_CONFNAME( 0x08, 0x08, "Terminal Type") // no idea of actual terminal types
+	PORT_CONFSETTING(    0x00, "A")
+	PORT_CONFSETTING(    0x08, "B")
 INPUT_PORTS_END
 
 WRITE8_MEMBER( sacstate_state::kbd_put )
@@ -86,10 +123,10 @@ static GENERIC_TERMINAL_INTERFACE( terminal_intf )
 	DEVCB_DRIVER_MEMBER(sacstate_state, kbd_put)
 };
 
-MACHINE_RESET_MEMBER( sacstate_state )
+void sacstate_state::machine_reset()
 {
 	m_term_data = 0;
-	m_val = 8; // must be 8 or 0
+	m_val = ioport("CONFIG")->read();
 }
 
 static MACHINE_CONFIG_START( sacstate, sacstate_state )
@@ -117,5 +154,5 @@ ROM_END
 
 /* Driver */
 
-/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT    INIT    COMPANY      FULLNAME       FLAGS */
-COMP( ????, sacstate,  0,       0,   sacstate,  sacstate, driver_device,  0,  "SacState", "SacState 8008", GAME_NOT_WORKING | GAME_NO_SOUND)
+/*    YEAR  NAME    PARENT  COMPAT   MACHINE    INPUT     CLASS          INIT    COMPANY      FULLNAME       FLAGS */
+COMP( 1973, sacstate,  0,       0,   sacstate,  sacstate, driver_device,  0,  "SacState", "SacState 8008", GAME_NO_SOUND_HW)
