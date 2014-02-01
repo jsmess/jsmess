32ad9aa3ccf8b47d20987148526797ae3cba1fad
DopefishJustin <dopefishjustin@gmail.com>
2014-02-01 22:16:37 +0000

    Fixed FPS toggle; running_machine and ui_manager objects can now be accessed from js

--- templates/default/post.js
+++ templates/default/post.js
@@ -4,5 +4,6 @@ function _SDL_GL_LoadLibrary() {}
 
 // MESS-JavaScript function mappings
 var JSMESS = JSMESS || {};
-JSMESS.ui_set_show_fps = Module.cwrap('_Z15ui_set_show_fpsb', '', ['number']);
-JSMESS.ui_get_show_fps = Module.cwrap('_Z15ui_get_show_fpsv', 'number');
+JSMESS.get_ui = Module.cwrap('_Z9js_get_uiv', 'number');
+JSMESS.ui_set_show_fps = Module.cwrap('_ZN10ui_manager12set_show_fpsEb', '', ['number', 'number']);
+JSMESS.ui_get_show_fps = Module.cwrap('_ZNK10ui_manager8show_fpsEv', 'number', ['number']);
