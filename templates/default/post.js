3eb8ebd95f57b99153c7327f3475baa434770328
Vitorio <email_about_jsmess@vitor.io>
2014-05-05 16:39:28 -0700

    Emscripten-based muting

--- templates/default/post.js
+++ templates/default/post.js
@@ -10,3 +10,4 @@ JSMESS.get_sound = Module.cwrap('_Z12js_get_soundv', 'number');
 JSMESS.ui_set_show_fps = Module.cwrap('_ZN10ui_manager12set_show_fpsEb', '', ['number', 'number']);
 JSMESS.ui_get_show_fps = Module.cwrap('_ZNK10ui_manager8show_fpsEv', 'number', ['number']);
 JSMESS.sound_manager_mute = Module.cwrap('_ZN13sound_manager4muteEbh', '', ['number', 'number', 'number']);
+JSMESS.sdl_pauseaudio = Module.cwrap('SDL_PauseAudio', '', ['number']);
