f400ddff163fbd1f22f82924d93b45a5baa43453
James Baicoianu <james_github@baicoianu.com>
2015-05-20 02:12:27 -0700

    Added -x option to specify alternate exe, renamed threads to processes for clarity

--- utils/mamebench/mamebench-buildqueue.sh
+++ utils/mamebench/mamebench-buildqueue.sh
@@ -5,9 +5,10 @@ LOGFILE=$2
 BENCHTIME=30
 THREADS=4
 PATTERN="*"
+EXECUTABLE=mame
 
 shift 2
-while getopts "t:p:" opt; do
+while getopts "t:p:x:" opt; do
 	case "$opt" in
 	t)
 		BENCHTIME=$OPTARG
@@ -15,10 +16,13 @@ while getopts "t:p:" opt; do
 	p)
 		PATTERN=$OPTARG
 		;;
+	x)
+		EXECUTABLE=$OPTARG
+		;;
 	esac
 done
 
 for I in "$ROMDIR"/$PATTERN.zip; do 
 	GAME=$(basename "${I/\.zip/}")
-	echo "./mamebench-game.sh \"${ROMDIR}\" \"${LOGFILE}\" $GAME -t $BENCHTIME"
+	echo "./mamebench-game.sh \"${ROMDIR}\" \"${LOGFILE}\" $GAME -t $BENCHTIME -x \"${EXECUTABLE}\""
 done
